de27ab13773cf34ebbe529d0a6561a68e2587777
Raul Glogovetan <raul@glogovetan.com>
2022-08-01 02:14:30 +0300

    Add: local `amplify` config and `User` model

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      walletAddress
+      username
+      displayName
+      avatarHash
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      walletAddress
+      username
+      displayName
+      avatarHash
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      walletAddress
+      username
+      displayName
+      avatarHash
+      createdAt
+      updatedAt
+    }
+  }
+`;
