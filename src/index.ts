e21ebd7927bc17dd2ac37ccc8ecd7fcfe973e201
Raul Glogovetan <raul@glogovetan.com>
2022-07-31 15:00:04 +0300

    Remove: userflow service

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactModal from 'react-modal';
-import userflow from 'userflow.js';
+
+import Entry from '~root/Entry';
 
 const rootNode = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -9,13 +10,8 @@ const root = createRoot(rootNode!);
 
 if (rootNode) {
   ReactModal.setAppElement(rootNode);
-  root.render(createElement('div'));
+  root.render(createElement(Entry));
 }
 
 // @ts-ignore
 if (module.hot) module.hot.accept();
-
-// Initiate Userflow
-if (process.env.USERFLOW_TOKEN) {
-  userflow.init(process.env.USERFLOW_TOKEN);
-}
