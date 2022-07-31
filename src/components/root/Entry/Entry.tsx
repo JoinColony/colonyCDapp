75c846d3268c362250be686cd32336b60568cd1d
Raul Glogovetan <raul@glogovetan.com>
2022-07-31 15:00:43 +0300

    Add: root `Entry` component

---
+++ src/components/root/Entry/Entry.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Provider as ReduxProvider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { StoreContext } from 'redux-react-hook';
+// import { IntlProvider } from 'react-intl';
+// import { ApolloProvider } from '@apollo/client';
+
+// import layout from '~styles/layout.css';
+
+const Entry = () => (
+  <div>CDapp Entry Point</div>
+);
+
+export default Entry;
