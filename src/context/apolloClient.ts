ddcbbc61e6b91d8041a991a05563b569778132b7
Raul Glogovetan <raul@glogovetan.com>
2022-10-04 20:57:55 +0300

    Cleanup: unused packages and dependencies

--- src/context/apolloClient.ts
+++ src/context/apolloClient.ts
@@ -1,43 +1,14 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloLink,
-} from '@apollo/client';
-import { createAuthLink } from 'aws-appsync-auth-link';
-import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
-
-const {
-  AWS_APPSYNC_GRAPHQL_URL = '',
-  AWS_APPSYNC_REGION = '',
-  AWS_APPSYNC_AUTH_TYPE = '',
-  AWS_APPSYNC_API_KEY = '',
-} = process.env;
-
-const auth = {
-  type: AWS_APPSYNC_AUTH_TYPE as string,
-  apiKey: AWS_APPSYNC_API_KEY,
-};
-
-const connectionConfig = {
-  url: AWS_APPSYNC_GRAPHQL_URL,
-  region: AWS_APPSYNC_REGION,
-  auth,
-};
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: AWS_APPSYNC_GRAPHQL_URL,
+  uri: 'http://localhost:20002/graphql',
+  headers: {
+    'x-api-key': 'da2-fakeApiId123456',
+  },
 });
 
-const link = ApolloLink.from([
-  // @ts-ignore
-  createAuthLink(connectionConfig),
-  // @ts-ignore
-  createSubscriptionHandshakeLink(connectionConfig, httpLink),
-]);
-
 export default new ApolloClient({
-  link,
+  link: httpLink,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
