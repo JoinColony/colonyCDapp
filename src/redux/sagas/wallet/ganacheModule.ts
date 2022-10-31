b328cb2493c8ea34b258708457176d13412a938e
Raul Glogovetan <raul@glogovetan.com>
2022-11-01 00:34:40 +0200

    Fix: `ganacheModule` more generic method handler

--- src/redux/sagas/wallet/ganacheModule.ts
+++ src/redux/sagas/wallet/ganacheModule.ts
@@ -7,6 +7,10 @@ import { RpcMethods } from '~types';
 
 import walletIcon from '~images/icons/wallet.svg';
 
+type CustomJsonRpcProvider = providers.JsonRpcProvider & {
+  request: (args) => void;
+};
+
 const ganacheWalletModule = (privateKey, optionalAccountIndex = 1) => {
   const initWallet = () => {
     return {
@@ -14,27 +18,16 @@ const ganacheWalletModule = (privateKey, optionalAccountIndex = 1) => {
       getIcon: async () => walletIcon.content.replace('symbol', 'svg'),
       getInterface: async ({ EventEmitter, chains }) => {
         const [currentChain] = chains;
-        /*
-         * @TODO This needs to be set properly
-         */
         const ganacheProvider = new providers.JsonRpcProvider(
           currentChain.rpcUrl,
         );
+        (ganacheProvider as CustomJsonRpcProvider).request = ({
+          method,
+          params,
+        }) => ganacheProvider.send(method, params);
         const ganacheWallet = new Wallet(privateKey, ganacheProvider);
         const currentWalletAddress = utils.getAddress(ganacheWallet.address);
         const provider = createEIP1193Provider(ganacheProvider, {
-          /*
-           * @NOTE EIP1193Provider apparetly doesn't know about the eth_blockNumber
-           * RPC call, but we need it in order for eth_sendTransaction to work
-           * properly
-           */
-          // @ts-ignore
-          [RpcMethods.BlockNumber]: async () => {
-            const { number } = await ganacheProvider.getBlock('latest');
-            return number;
-          },
-          [RpcMethods.EstimateGas]: async ({ params: [transaction] }) =>
-            ganacheProvider.estimateGas(transaction),
           [RpcMethods.Accounts]: async () => {
             return [currentWalletAddress];
           },
@@ -44,9 +37,6 @@ const ganacheWalletModule = (privateKey, optionalAccountIndex = 1) => {
           [RpcMethods.RequestAccounts]: async () => {
             return [currentWalletAddress];
           },
-          [RpcMethods.ChainId]: async () => {
-            return (currentChain && currentChain.id) || '';
-          },
           [RpcMethods.GetBalance]: async () => {
             const balance = await ganacheProvider.getBalance(
               currentWalletAddress,
@@ -54,20 +44,6 @@ const ganacheWalletModule = (privateKey, optionalAccountIndex = 1) => {
             );
             return balance.toString();
           },
-          [RpcMethods.SendTransaction]: async ({
-            params: [{ gas, ...transaction }],
-          }) => {
-            const { hash } = await ganacheWallet.sendTransaction({
-              gasLimit: gas,
-              ...transaction,
-            });
-            return hash;
-          },
-          [RpcMethods.GetTransactionByHash]: async ({
-            params: [transactionHash],
-          }) => ganacheProvider.getTransaction(transactionHash),
-          [RpcMethods.Sign]: async ({ params: [, message] }) =>
-            ganacheWallet.signMessage(message),
           [RpcMethods.PersonalSign]: async ({ params: [message] }) =>
             ganacheWallet.signMessage(message),
           [RpcMethods.SignTypedData]: async ({ params: [, typedData] }) =>
@@ -86,10 +62,6 @@ const ganacheWalletModule = (privateKey, optionalAccountIndex = 1) => {
               currentWalletAddress,
               JSON.parse(typedData),
             ]),
-          [RpcMethods.GasPrice]: async () => {
-            const gasPrice = await ganacheProvider.getGasPrice();
-            return gasPrice.toHexString();
-          },
         });
         const eventEmitter = new EventEmitter();
         provider.on = eventEmitter.on.bind(eventEmitter);
