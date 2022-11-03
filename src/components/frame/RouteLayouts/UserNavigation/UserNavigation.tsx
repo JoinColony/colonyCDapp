16b527f304be6c5a8d6bb0bf8911b2a167c2d235
Raul Glogovetan <raul@glogovetan.com>
2022-11-03 16:59:54 +0200

    Remove: `UserNavigation` network info, no longer relevant

--- src/components/frame/RouteLayouts/UserNavigation/UserNavigation.tsx
+++ src/components/frame/RouteLayouts/UserNavigation/UserNavigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react'; // useEffect // useMemo,
-import { defineMessages, useIntl } from 'react-intl'; // useIntl
+// import { defineMessages } from 'react-intl';
 // import { useParams } from 'react-router-dom';
 
 // import Icon from '~shared/Icon';
@@ -12,8 +12,6 @@ import AvatarDropdown from '~frame/AvatarDropdown';
 // import {
 //   useUserBalanceWithLockQuery,
 // } from '~data/index';
-import { SUPPORTED_NETWORKS, DEFAULT_NETWORK_INFO } from '~constants';
-import { useAppContext } from '~hooks';
 
 // import { groupedTransactionsAndMessages } from '~redux/selectors';
 
@@ -23,49 +21,19 @@ import styles from './UserNavigation.css';
 
 const displayName = 'frame.RouteLayouts.UserNavigation';
 
-const MSG = defineMessages({
-  // userReputationTooltip: {
-  //   id: `${displayName}.userReputationTooltip`,
-  //   defaultMessage: 'This is your share of the reputation in this colony',
-  // },
-  networkNotSupportedName: {
-    id: `${displayName}.networkNotSupportedName`,
-    defaultMessage: 'Network not supported by CDapp',
-  },
-  networkNotSupportedShort: {
-    id: `${displayName}.networkNotSupportedName`,
-    defaultMessage: 'Not Supported',
-  },
-});
+// const MSG = defineMessages({
+// userReputationTooltip: {
+//   id: `${displayName}.userReputationTooltip`,
+//   defaultMessage: 'This is your share of the reputation in this colony',
+// },
+// });
 
 const UserNavigation = () => {
-  const { wallet } = useAppContext();
-  const { formatMessage } = useIntl();
-
-  const [{ id: networkId = `0x${DEFAULT_NETWORK_INFO.chainId.toString(16)}` }] =
-    wallet?.chains || [{}];
-  /*
-   * convert from hex to number, remove 0x hex prefix
-   */
-  const humanReadableId = parseInt(networkId.slice(2), 16);
-
   // const userLock = userData?.user.userLock;
   // const nativeToken = userLock?.nativeToken;
 
   return (
     <div className={styles.main}>
-      {wallet && (
-        <div
-          className={`${styles.elementWrapper} ${styles.networkInfo}`}
-          title={
-            SUPPORTED_NETWORKS[humanReadableId]?.name ||
-            formatMessage(MSG.networkNotSupportedName)
-          }
-        >
-          {SUPPORTED_NETWORKS[humanReadableId]?.shortName ||
-            formatMessage(MSG.networkNotSupportedShort)}
-        </div>
-      )}
       {/* {userCanNavigate && colonyData?.colonyAddress && (
         <Tooltip
           content={formatMessage(MSG.userReputationTooltip)}
