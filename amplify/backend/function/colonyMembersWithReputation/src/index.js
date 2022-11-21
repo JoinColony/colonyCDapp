39d799f12877162a16cbf2e0e6164f134da5ac7e
danbr <danatcern@hotmail.com>
2022-11-21 16:17:25 +0700

    feat:add reputation related functions

--- amplify/backend/function/colonyMembersWithReputation/src/index.js
+++ amplify/backend/function/colonyMembersWithReputation/src/index.js
@@ -26,7 +26,7 @@ const GRAPHQL_URI = 'http://localhost:20002/graphql';
 
 const ROOT_DOMAIN_ID = 1; // this used to be exported from @colony/colony-js but isn't anymore
 const RPC_URL = 'http://network-contracts.docker:8545'; // this needs to be extended to all supported networks
-const REPUTATION_ENDPOINT = 'http://localhost:3002/reputation';
+const REPUTATION_ENDPOINT = 'http://localhost:3001/reputation';
 
 exports.handler = async (event) => {
   const input = event?.arguments?.input;
@@ -41,14 +41,15 @@ exports.handler = async (event) => {
   });
   const colonyClient = await networkClient.getColonyClient(colonyAddress);
   const { skillId } = await colonyClient.getDomain(domainId ?? ROOT_DOMAIN_ID);
-  // const { addresses } = await colonyClient.getMembersReputation(skillId);
+  const { addresses } = await colonyClient.getMembersReputation(skillId);
+  console.log(`🚀 ~ exports.handler= ~ addresses`, addresses);
 
   // test user addresses (its unknown if they have reputation in the colony)
-  const addresses = [
-    '0x9dF24e73f40b2a911Eb254A8825103723E13209C',
-    '0x27fF0C145E191C22C75cD123C679C3e1F58a4469',
-    '0xb77D57F4959eAfA0339424b83FcFaf9c15407461',
-  ];
+  // const addresses = [
+  //   '0x9dF24e73f40b2a911Eb254A8825103723E13209C',
+  //   '0x27fF0C145E191C22C75cD123C679C3e1F58a4469',
+  //   '0xb77D57F4959eAfA0339424b83FcFaf9c15407461',
+  // ];
 
   // get user for each address
   const members = await Promise.all(
