6050aeae1c6eab7656f883b24391a09598cd665d
danbr <danatcern@hotmail.com>
2022-11-27 17:01:44 +0700

    fix:remove unnecessary funcs

--- amplify/backend/function/getMembersForColony/src/utils.js
+++ amplify/backend/function/getMembersForColony/src/utils.js
@@ -1,7 +1,5 @@
 const { default: fetch, Request } = require('node-fetch');
 
-const ROOT_DOMAIN_ID = 1; // this used to be exported from @colony/colony-js but isn't anymore
-
 const graphqlRequest = async (queryOrMutation, variables, url, authKey) => {
   const options = {
     method: 'POST',
@@ -33,29 +31,6 @@ const graphqlRequest = async (queryOrMutation, variables, url, authKey) => {
   }
 };
 
-const getUserReputation = async ({
-  colonyClient,
-  walletAddress,
-  domainId,
-  rootHash,
-}) => {
-  const { skillId } = await colonyClient.getDomain(domainId ?? ROOT_DOMAIN_ID);
-
-  let reputationAmount;
-  try {
-    reputationAmount = await colonyClient.getReputationWithoutProofs(
-      skillId,
-      walletAddress,
-      rootHash,
-    );
-  } catch (error) {
-    return null;
-  }
-
-  return reputationAmount.toString();
-};
-
 module.exports = {
   graphqlRequest,
-  getUserReputation,
 };
