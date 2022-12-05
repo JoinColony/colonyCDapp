7ac56c5986f7cbf6e3a310f47b408c1981a530ad
Julianahlk <julianayasnog@gmail.com>
2022-12-05 09:34:03 -0500

    Refactor: extracted ColonySection, renamed CreateColonySection

--- src/components/frame/HamburgerDropdown/HamburgerDropdownPopover.tsx
+++ src/components/frame/HamburgerDropdown/HamburgerDropdownPopover.tsx
@@ -1,45 +1,26 @@
 import React from 'react';
-import { defineMessages } from 'react-intl';
+// import { defineMessages } from 'react-intl';
 
-import DropdownMenu, {
-  DropdownMenuItem,
-  DropdownMenuSection,
-} from '~shared/DropdownMenu';
+import DropdownMenu from '~shared/DropdownMenu';
 import {
   ColonySection,
+  CreateColonySection,
   HelperSection,
   MetaSection,
   UserSection,
 } from '~shared/PopoverSection';
-import NavLink from '~shared/NavLink';
 import { useAppContext, useColonyContext } from '~hooks';
 
 import styles from './HamburgerDropdownPopover.css';
 
 const displayName = 'frame.HamburgerDropdown.HamburgerDropdownPopover';
 
-const MSG = defineMessages({
-  actions: {
-    id: `${displayName}.actions`,
-    defaultMessage: 'Actions',
-  },
-  funds: {
-    id: `${displayName}.funds`,
-    defaultMessage: 'Funds',
-  },
-  members: {
-    id: `${displayName}.members`,
-    defaultMessage: 'Members',
-  },
-  extensions: {
-    id: `${displayName}.extensions`,
-    defaultMessage: 'Extensions',
-  },
-  buyTokens: {
-    id: `${displayName}.buyTokens`,
-    defaultMessage: 'Buy Tokens',
-  },
-});
+// const MSG = defineMessages({
+//   buyTokens: {
+//     id: `${displayName}.buyTokens`,
+//     defaultMessage: 'Buy Tokens',
+//   },
+// });
 
 interface Props {
   closePopover: () => void;
@@ -48,32 +29,13 @@ interface Props {
 const HamburgerDropdownPopover = ({ closePopover }: Props) => {
   const { colony } = useColonyContext();
   const { user, wallet } = useAppContext();
-  const colonyHomePath = `/colony/${colony?.name}`;
 
   return (
     <div className={styles.menu}>
       <DropdownMenu onClick={closePopover}>
-        {user?.name && colony?.name && (
-          <DropdownMenuSection>
-            <DropdownMenuItem>
-              <NavLink to={colonyHomePath} text={MSG.actions} />
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <NavLink to={`${colonyHomePath}/funds`} text={MSG.funds} />
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <NavLink to={`${colonyHomePath}/members`} text={MSG.members} />
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <NavLink
-                to={`${colonyHomePath}/extensions`}
-                text={MSG.extensions}
-              />
-            </DropdownMenuItem>
-          </DropdownMenuSection>
-        )}
+        {user?.name && colony?.name && <ColonySection />}
         <UserSection />
-        <ColonySection />
+        <CreateColonySection />
         <HelperSection />
         {wallet?.address && <MetaSection />}
       </DropdownMenu>
--- src/components/shared/PopoverSection/index.ts
+++ src/components/shared/PopoverSection/index.ts
@@ -1,4 +1,5 @@
 export { default as ColonySection } from './ColonySection';
+export { default as CreateColonySection } from './CreateColonySection';
 export { default as HelperSection } from './HelperSection';
 export { default as UserSection } from './UserSection';
 export { default as MetaSection } from './MetaSection';
