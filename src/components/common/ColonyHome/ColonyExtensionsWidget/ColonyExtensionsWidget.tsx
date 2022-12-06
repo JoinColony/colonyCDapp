c40b68cae3cdc66a3d81ff02cadf6c0babc2863d
Julianahlk <julianayasnog@gmail.com>
2022-12-06 18:23:54 -0500

    Refactor: port extensions aside widget

--- src/components/common/ColonyHome/ColonyExtensionsWidget/ColonyExtensionsWidget.tsx
+++ src/components/common/ColonyHome/ColonyExtensionsWidget/ColonyExtensionsWidget.tsx
@@ -1,79 +1,105 @@
-import React from 'react';
-// import { defineMessages, FormattedMessage } from 'react-intl';
+import React, { useMemo } from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { getExtensionHash } from '@colony/colony-js';
 
-// import Heading from '~core/Heading';
-// import { useColonyExtensionsQuery } from '~data/index';
-// import { MiniSpinnerLoader } from '~core/Preloaders';
-// import extensionData from '~data/staticData/extensionData';
-// import NavLink from '~core/NavLink';
-// import ExtensionStatus from '~dashboard/Extensions/ExtensionStatus';
-// import { useColonyContext } from '~hooks';
+import { supportedExtensionsConfig } from '~constants/extensions';
+import { InstalledExtensionData } from '~types';
+import Heading from '~shared/Heading';
+import { MiniSpinnerLoader } from '~shared/Preloaders';
+import NavLink from '~shared/NavLink';
+import ExtensionStatusBadge from '~shared/ExtensionStatusBadge';
+import { useColonyContext } from '~hooks';
+import { useGetColonyExtensionsQuery } from '~gql';
+import { notNull } from '~utils/arrays';
 
-// import styles from './ColonyExtensions.css';
+import styles from './ColonyExtensionsWidget.css';
 
 const displayName = 'dashboard.ColonyHome.ColonyExtensionsWidget';
 
-// const MSG = defineMessages({
-//   title: {
-//     id: `${displayName}.title`,
-//     defaultMessage: 'Enabled extensions',
-//   },
-//   loadingData: {
-//     id: `${displayName}.loadingData`,
-//     defaultMessage: 'Loading extensions data ...',
-//   },
-// });
+const MSG = defineMessages({
+  title: {
+    id: `${displayName}.title`,
+    defaultMessage: 'Enabled extensions',
+  },
+  loadingData: {
+    id: `${displayName}.loadingData`,
+    defaultMessage: 'Loading extensions data ...',
+  },
+});
 
 const ColonyExtensions = () => {
-  // const { colony } = useColonyContext();
+  const { colony } = useColonyContext();
 
-  // const { data, loading } = useColonyExtensionsQuery({
-  //   variables: { address: colony?.colonyAddress },
-  // });
+  const { data, loading } = useGetColonyExtensionsQuery({
+    variables: {
+      colonyAddress: colony?.colonyAddress ?? '',
+    },
+    skip: !colony,
+  });
+  const colonyExtensions = data?.getColony?.extensions?.items.filter(notNull);
 
-  // if (loading) {
-  //   return (
-  //     <MiniSpinnerLoader
-  //       className={styles.main}
-  //       title={MSG.title}
-  //       loadingText={MSG.loadingData}
-  //     />
-  //   );
-  // }
+  const installedExtensionsData = useMemo<InstalledExtensionData[]>(() => {
+    if (!colonyExtensions) {
+      return [];
+    }
 
-  // return data?.processedColony ? (
-  //   <div className={styles.main}>
-  //     <Heading appearance={{ size: 'normal', weight: 'bold' }}>
-  //       <FormattedMessage {...MSG.title} />
-  //     </Heading>
-  //     <ul>
-  //       {data.processedColony.installedExtensions
-  //         .filter(
-  //           (extension) =>
-  //             extension.details?.initialized &&
-  //             !extension.details?.missingPermissions.length,
-  //         )
-  //         .map((extension) => {
-  //           const { address, extensionId } = extension;
-  //           return (
-  //             <li key={address} className={styles.extension}>
-  //               <NavLink
-  //                 className={styles.invisibleLink}
-  //                 to={`/colony/${colony?.name}/extensions/${extensionId}`}
-  //                 text={extensionData[extensionId].name}
-  //               />
-  //               <ExtensionStatus
-  //                 installedExtension={extension}
-  //                 deprecatedOnly
-  //               />
-  //             </li>
-  //           );
-  //         })}
-  //     </ul>
-  //   </div>
-  // ) : null;
+    return colonyExtensions
+      .map<InstalledExtensionData | null>((extension) => {
+        const extensionConfig = supportedExtensionsConfig.find(
+          (e) => getExtensionHash(e.extensionId) === extension?.hash,
+        );
 
-  return <div>Colony Extensions Widget</div>;
+        if (!extensionConfig) {
+          // Unsupported extension
+          return null;
+        }
+
+        return {
+          ...extensionConfig,
+          ...extension,
+        };
+      })
+      .filter(notNull);
+  }, [colonyExtensions]);
+
+  if (loading) {
+    return (
+      <MiniSpinnerLoader
+        className={styles.main}
+        title={MSG.title}
+        loadingText={MSG.loadingData}
+      />
+    );
+  }
+
+  return installedExtensionsData.length ? (
+    <div className={styles.main}>
+      <Heading appearance={{ size: 'normal', weight: 'bold' }}>
+        <FormattedMessage {...MSG.title} />
+      </Heading>
+      <ul>
+        {installedExtensionsData
+          // .filter(
+          //   (extension) =>
+          //     extension.details?.initialized &&
+          //     !extension.details?.missingPermissions.length,
+          // )
+          .map((extension) => {
+            const { address, extensionId } = extension;
+            return (
+              <li key={address} className={styles.extension}>
+                <NavLink
+                  className={styles.invisibleLink}
+                  to={`/colony/${colony?.name}/extensions/${extensionId}`}
+                  text={extension.name}
+                />
+                <ExtensionStatusBadge extension={extension} deprecatedOnly />
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  ) : null;
 };
 
 ColonyExtensions.displayName = displayName;
