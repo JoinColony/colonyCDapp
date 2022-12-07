baa6a68e37ffdc0fd3ba860de86812e1c9a5088f
Jakub Zajac <jakub@colony.io>
2022-12-07 15:22:01 +0000

    Port over `extensionUninstall` saga

--- src/redux/sagas/extensions/index.ts
+++ src/redux/sagas/extensions/index.ts
@@ -3,13 +3,13 @@ import { call, all } from 'redux-saga/effects';
 import extensionInstallSaga from './extensionInstall';
 // import colonyExtensionEnableSaga from './colonyExtensionEnable';
 // import colonyExtensionDeprecateSaga from './colonyExtensionDeprecate';
-// import colonyExtensionUninstallSaga from './colonyExtensionUninstall';
+import extensionUninstallSaga from './extensionUninstall';
 // import colonyExtensionUpgradeSaga from './colonyExtensionUpgrade';
 
 export default function* extensionsSagas() {
   yield all([
     // call(colonyExtensionUpgradeSaga),
-    // call(colonyExtensionUninstallSaga),
+    call(extensionUninstallSaga),
     call(extensionInstallSaga),
     // call(colonyExtensionEnableSaga),
     // call(colonyExtensionDeprecateSaga),
