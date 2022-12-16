88c790970e6b94ea9fc2d8b0be19e234ca499d8a
Jakub Zajac <jakub@colony.io>
2022-12-16 20:57:18 +0000

    Make extensions without init params enabled by default

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -1,9 +1,42 @@
-import { AnyExtensionData, InstalledExtensionData } from '~types';
+import {
+  AnyExtensionData,
+  ExtensionConfig,
+  InstalledExtensionData,
+  ColonyExtension,
+  InstallableExtensionData,
+} from '~types';
 
 /**
- * Type guard to distinguish installed extension data from extension config
+ * Type guard to distinguish installed extension data from installable extension data
  */
 export const isInstalledExtensionData = (
   extension: AnyExtensionData,
 ): extension is InstalledExtensionData =>
   (extension as InstalledExtensionData).address !== undefined;
+
+export const mapToInstallableExtensionData = (
+  extensionConfig: ExtensionConfig,
+  version: number,
+): InstallableExtensionData => {
+  return {
+    ...extensionConfig,
+    availableVersion: version,
+  };
+};
+
+export const mapToInstalledExtensionData = (
+  extensionConfig: ExtensionConfig,
+  colonyExtension: ColonyExtension,
+  version: number,
+): InstalledExtensionData => {
+  // extension is also considered initialized if it has no initialization params
+  const isInitialized =
+    colonyExtension?.isInitialized || !extensionConfig.initializationParams;
+
+  return {
+    ...extensionConfig,
+    ...colonyExtension,
+    availableVersion: version,
+    isInitialized,
+  };
+};
