68921dff43c402455c050dbb3dfba13aacacf809
Jakub Zajac <jakub@colony.io>
2022-12-24 12:27:54 +0000

    Port `useEnabledExtensions` hook

--- src/hooks/useEnabledExtensions.tsx
+++ src/hooks/useEnabledExtensions.tsx
@@ -1,56 +1,25 @@
-// import { Extension } from '@colony/colony-js';
-// import { useColonyExtensionsQuery } from '~data/index';
-// import { Address } from '~types/index';
+import { Extension } from '@colony/colony-js';
 
-// interface Props {
-//   colonyAddress?: Address;
-// }
+import useExtensionsData from './useExtensionsData';
 
-/*
- * @TODO This needs to be addressed and either refactored or removed
- */
+const useEnabledExtensions = () => {
+  const { installedExtensionsData, loading } = useExtensionsData();
 
-// export const useEnabledExtensions = ({ colonyAddress }: Props) => {
-//   const { data: colonyExtensionsData, loading } = useColonyExtensionsQuery({
-//     variables: { address: colonyAddress || '' },
-//   });
-//   const installedExtensions =
-//     colonyExtensionsData?.processedColony?.installedExtensions || [];
+  const oneTxPaymentData = installedExtensionsData.find(
+    (extensionData) => extensionData.extensionId === Extension.OneTxPayment,
+  );
+  const isOneTxPaymentEnabled = !!oneTxPaymentData?.isEnabled;
 
-//   const installedVotingExtension = installedExtensions.find(
-//     ({ extensionId }) => extensionId === Extension.VotingReputation,
-//   );
-//   const installedOneTxPaymentExtension = installedExtensions.find(
-//     ({ extensionId }) => extensionId === Extension.OneTxPayment,
-//   );
+  const votingReputationData = installedExtensionsData.find(
+    (extensionData) => extensionData.extensionId === Extension.VotingReputation,
+  );
+  const isVotingReputationEnabled = !!votingReputationData?.isEnabled;
 
-//   const isVotingExtensionEnabled = !!(
-//     installedVotingExtension &&
-//     installedVotingExtension.details?.initialized &&
-//     !installedVotingExtension.details?.deprecated
-//   );
-//   const isOneTxPaymentExtensionEnabled = !!(
-//     installedOneTxPaymentExtension &&
-//     installedOneTxPaymentExtension.details?.initialized &&
-//     !installedOneTxPaymentExtension.details?.deprecated
-//   );
-
-//   const installedExtensionsAddresses = installedExtensions.map((extension) =>
-//     extension.address?.toLowerCase(),
-//   );
-
-//   return {
-//     isVotingExtensionEnabled,
-//     votingExtensionVersion: installedVotingExtension
-//       ? installedVotingExtension?.details?.version
-//       : null,
-//     isOneTxPaymentExtensionEnabled,
-//     installedExtensionsAddresses,
-//     isLoadingExtensions: loading,
-//   };
-// };
-//
-
-const useEnabledExtensions = (...args) => args;
+  return {
+    loading,
+    isOneTxPaymentEnabled,
+    isVotingReputationEnabled,
+  };
+};
 
 export default useEnabledExtensions;
