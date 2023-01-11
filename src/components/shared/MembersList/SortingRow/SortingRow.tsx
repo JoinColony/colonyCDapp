9fc67b073b4e474c2398552e0f0032968e701fb3
Julianahlk <julianayasnog@gmail.com>
2023-01-11 18:21:03 -0500

    Refactor: extract SortingButton component

--- src/components/shared/MembersList/SortingRow/SortingRow.tsx
+++ src/components/shared/MembersList/SortingRow/SortingRow.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { defineMessages, FormattedMessage } from 'react-intl';
-import classnames from 'classnames';
-
-import Button from '~shared/Button';
-import Icon from '~shared/Icon';
+import { defineMessages } from 'react-intl';
 
 import { SortingMethod } from '~gql';
+
+import SortingButton from './SortingButton';
 
 import styles from './SortingRow.css';
 
@@ -41,42 +39,23 @@ const SortingRow = ({ handleSortingMethodChange, sortingMethod }: Props) => {
   const isSortingByRep =
     sortingMethod === SortingMethod.ByHighestRep ||
     sortingMethod === SortingMethod.ByLowestRep;
+
   return (
     <div className={styles.container}>
-      <Button
-        className={styles.sortingButton}
-        onClick={() => handleSortingMethodChange(nextSortingByRoleMethod)}
-      >
-        <FormattedMessage {...MSG.permissions} />
-        <Icon
-          className={classnames(styles.sortingIcon, {
-            [styles.toggledIcon]: !isSortingByRep,
-          })}
-          name={
-            sortingMethod === SortingMethod.ByMorePermissions
-              ? 'caret-up'
-              : 'caret-down'
-          }
-          title={MSG.permissions}
-        />
-      </Button>
-      <Button
-        className={styles.sortingButton}
-        onClick={() => handleSortingMethodChange(nextSortingByRepMethod)}
-      >
-        <FormattedMessage {...MSG.reputation} />
-        <Icon
-          className={classnames(styles.sortingIcon, {
-            [styles.toggledIcon]: isSortingByRep,
-          })}
-          name={
-            sortingMethod === SortingMethod.ByHighestRep
-              ? 'caret-up'
-              : 'caret-down'
-          }
-          title={MSG.reputation}
-        />
-      </Button>
+      <SortingButton
+        isSortingByRep={isSortingByRep}
+        nextSortingMethod={nextSortingByRoleMethod}
+        handleSortingMethodChange={handleSortingMethodChange}
+        message={MSG.permissions}
+        caretUp={sortingMethod === SortingMethod.ByMorePermissions}
+      />
+      <SortingButton
+        isSortingByRep={isSortingByRep}
+        nextSortingMethod={nextSortingByRepMethod}
+        handleSortingMethodChange={handleSortingMethodChange}
+        message={MSG.reputation}
+        caretUp={sortingMethod === SortingMethod.ByHighestRep}
+      />
     </div>
   );
 };
