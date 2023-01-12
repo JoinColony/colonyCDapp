58f9d23421b67506a53039f029a02b5bc54f876c
Julianahlk <julianayasnog@gmail.com>
2023-01-11 22:03:21 -0500

    Refactor: update to use in-memory sorting

---
+++ src/hooks/useColonyMembersSorting.ts
@@ -0,0 +1,59 @@
+import { useState, useMemo } from 'react';
+import Decimal from 'decimal.js';
+
+import { Watcher, Contributor } from '~types';
+
+import { SortingMethod } from '~gql';
+
+const useColonyMembersSorting = (
+  members: Watcher[] | Contributor[],
+  isContributorsSection: boolean,
+) => {
+  const [sortingMethod, setSortingMethod] = useState<SortingMethod>(
+    SortingMethod.ByHighestRep,
+  );
+
+  const sortedUsers = useMemo(() => {
+    if (!isContributorsSection) {
+      return members;
+    }
+
+    return [...(members as Contributor[])].sort((user1, user2) => {
+      if (
+        sortingMethod === SortingMethod.ByHighestRep &&
+        user1.reputationAmount &&
+        user2.reputationAmount
+      ) {
+        return new Decimal(user2.reputationAmount)
+          .sub(user1.reputationAmount)
+          .toNumber();
+      }
+      if (
+        sortingMethod === SortingMethod.ByLowestRep &&
+        user1.reputationAmount &&
+        user2.reputationAmount
+      ) {
+        return new Decimal(user1.reputationAmount)
+          .sub(user2.reputationAmount)
+          .toNumber();
+      }
+
+      // if (sortingMethod === SORTING_METHODS.BY_MORE_PERMISSIONS) {
+      //   return user2.roles.length - user1.roles.length;
+      // }
+      // if (sortingMethod === SORTING_METHODS.BY_LESS_PERMISSIONS) {
+      //   return user1.roles.length - user2.roles.length;
+      // }
+
+      return 0;
+    });
+  }, [sortingMethod, members, isContributorsSection]);
+
+  return {
+    sortedMembers: sortedUsers,
+    handleSortingMethodChange: setSortingMethod,
+    sortingMethod,
+  };
+};
+
+export default useColonyMembersSorting;
