cc8ce6cce1e5a2e262068197e4ccf7357d287066
Julianahlk <julianayasnog@gmail.com>
2023-01-18 21:54:51 -0500

    Refactor: updates for PR review comments

--- src/utils/lodash.ts
+++ src/utils/lodash.ts
@@ -2,3 +2,4 @@ export { default as once } from 'lodash/once';
 export { default as now } from 'lodash/now';
 export { default as isNil } from 'lodash/isNil';
 export { default as toFinite } from 'lodash/toFinite';
+export { default as sortBy } from 'lodash/sortBy';
