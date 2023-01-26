81ca5597288beb472f67f9287d3aaa3cfd35be16
ArmandoGraterol <armandogrtrl@gmail.com>
2023-01-26 11:43:57 -0300

    Updated display names of Dialogs

--- src/components/shared/PermissionRequiredInfo/PermissionRequiredInfo.css.d.ts
+++ src/components/shared/PermissionRequiredInfo/PermissionRequiredInfo.css.d.ts
@@ -1,9 +1,20 @@
-export const title: string;
-export const requiredPermissionSection: string;
-export const section: string;
-export const sectionLabel: string;
-export const labelIcon: string;
-export const permissionList: string;
-export const listItem: string;
-export const tooltipText: string;
-export const tooltipContent: string;
+declare namespace PermissionRequiredInfoCssNamespace {
+  export interface IPermissionRequiredInfoCss {
+    labelIcon: string;
+    listItem: string;
+    permissionList: string;
+    requiredPermissionSection: string;
+    section: string;
+    sectionLabel: string;
+    title: string;
+    tooltipContent: string;
+    tooltipText: string;
+  }
+}
+
+declare const PermissionRequiredInfoCssModule: PermissionRequiredInfoCssNamespace.IPermissionRequiredInfoCss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: PermissionRequiredInfoCssNamespace.IPermissionRequiredInfoCss;
+};
+
+export = PermissionRequiredInfoCssModule;
