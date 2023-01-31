c002eee115506b399e569413f9cace28de7b275b
William Martin <williamajmartin@gmail.com>
2023-01-31 12:22:17 +0100

    Add: Actions List Item (basic)

--- src/components/shared/ActionsList/ActionsList.tsx
+++ src/components/shared/ActionsList/ActionsList.tsx
@@ -1,6 +1,6 @@
 import React, { ComponentType } from 'react';
 
-import { ActionsListItem } from '~common/ColonyActions';
+import ActionsListItem from './ActionsListItem';
 
 const displayName = 'ActionsList';
 
--- src/components/shared/ActionsList/ActionsListItem.tsx
+++ src/components/shared/ActionsList/ActionsListItem.tsx
@@ -1,62 +1,40 @@
-import React, { useMemo, useCallback, useEffect } from 'react';
-import { BigNumber, BigNumberish } from 'ethers';
-import { AddressZero } from '@ethersproject/constants';
+import React from 'react';
 import {
   FormattedDateParts,
   FormattedMessage,
   defineMessages,
-  useIntl,
 } from 'react-intl';
-import { ColonyRoles } from '@colony/colony-js';
 import Decimal from 'decimal.js';
 
-import HookedUserAvatar from '~users/HookedUserAvatar';
 import Numeral from '~shared/Numeral';
 import Icon from '~shared/Icon';
 import FriendlyName from '~shared/FriendlyName';
 import Tag, { Appearance as TagAppearance } from '~shared/Tag';
-import CountDownTimer from '~dashboard/ActionsPage/CountDownTimer';
+import UserAvatar from '~shared/UserAvatar';
 
-import { getMainClasses, removeValueUnits } from '~utils/css';
+import { getMainClasses } from '~utils/css';
 import {
   getFormattedTokenValue,
   getTokenDecimalsWithFallback,
 } from '~utils/tokens';
-import {
-  useUser,
-  Colony,
-  useColonyHistoricRolesQuery,
-  useTokenInfoLazyQuery,
-  useNetworkContracts,
-} from '~data/index';
-import { createAddress } from '~utils/web3';
-import { FormattedAction, ColonyActions, ColonyMotions } from '~types';
-import { useDataFetcher } from '~utils/hooks';
-import { parseDomainMetadata } from '~utils/colonyActions';
-import { useFormatRolesTitle } from '~utils/hooks/useFormatRolesTitle';
-import { useEnabledExtensions } from '~utils/hooks/useEnabledExtensions';
-import {
-  getUpdatedDecodedMotionRoles,
-  MotionState,
-  MOTION_TAG_MAP,
-} from '~utils/colonyMotions';
-import { ipfsDataFetcher } from '../../../core/fetchers';
 
-import { ClickHandlerProps } from './ActionsList';
+import { FormattedAction } from '~types';
+import { useFormatRolesTitle, useAppContext, useColonyContext } from '~hooks';
+import { MotionState, MOTION_TAG_MAP } from '~utils/colonyMotions';
+import { findDomain } from '~utils/domains';
+import { formatText } from '~utils/intl';
 
-import styles, { popoverWidth, popoverDistance } from './ActionsListItem.css';
+import styles from './ActionsListItem.css';
 
 const displayName = 'ActionsList.ActionsListItem';
 
-const UserAvatar = HookedUserAvatar();
-
 const MSG = defineMessages({
   domain: {
-    id: 'ActionsList.ActionsListItem.domain',
+    id: `${displayName}.domain`,
     defaultMessage: 'Team {domainId}',
   },
   titleCommentCount: {
-    id: 'ActionsList.ActionsListItem.titleCommentCount',
+    id: `${displayName}.titleCommentCount`,
     defaultMessage: `{formattedCommentCount} {commentCount, plural,
       one {comment}
       other {comments}
@@ -75,151 +53,107 @@ export enum ItemStatus {
 
 interface Props {
   item: FormattedAction;
-  colony: Colony;
-  handleOnClick?: (handlerProps: ClickHandlerProps) => void;
+  handleOnClick?: () => void;
 }
 
 const ActionsListItem = ({
   item: {
-    id,
     actionType,
     initiator,
     recipient,
     amount,
-    symbol: colonyTokenSymbol,
-    decimals: colonyTokenDecimals,
+    tokenSymbol,
+    decimals,
     fromDomain: fromDomainId,
     toDomain: toDomainId,
-    transactionHash,
     createdAt,
     commentCount = 0,
-    metadata,
     roles,
     newVersion,
     status = ItemStatus.Defused,
     motionState,
-    motionId,
-    blockNumber,
-    totalNayStake,
-    requiredStake,
-    transactionTokenAddress,
     reputationChange,
   },
-  colony,
   handleOnClick,
 }: Props) => {
-  const { formatMessage, formatNumber } = useIntl();
-  const { data: metadataJSON } = useDataFetcher(
-    ipfsDataFetcher,
-    [metadata as string],
-    [metadata],
-  );
+  const { user } = useAppContext();
+  const { colony } = useColonyContext();
+  // const { isVotingExtensionEnabled } = useEnabledExtensions({
+  //   colonyAddress: colony.colonyAddress,
+  // });
 
-  const { isVotingExtensionEnabled } = useEnabledExtensions({
-    colonyAddress: colony.colonyAddress,
-  });
+  // const { data: historicColonyRoles } = useColonyHistoricRolesQuery({
+  //   variables: {
+  //     colonyAddress: colony.colonyAddress,
+  //     blockNumber,
+  //   },
+  // });
 
-  const { data: historicColonyRoles } = useColonyHistoricRolesQuery({
-    variables: {
-      colonyAddress: colony.colonyAddress,
-      blockNumber,
-    },
-  });
+  // const [fetchTokenInfo, { data: tokenData }] = useTokenInfoLazyQuery();
 
-  const [fetchTokenInfo, { data: tokenData }] = useTokenInfoLazyQuery();
+  // useEffect(() => {
+  //   if (transactionTokenAddress) {
+  //     fetchTokenInfo({ variables: { address: transactionTokenAddress } });
+  //   }
+  // }, [fetchTokenInfo, transactionTokenAddress]);
 
-  useEffect(() => {
-    if (transactionTokenAddress) {
-      fetchTokenInfo({ variables: { address: transactionTokenAddress } });
-    }
-  }, [fetchTokenInfo, transactionTokenAddress]);
+  // const isColonyAddress = recipientAddress === colony?.colonyAddress;
+  // const fallbackRecipientProfile = recipient;
 
-  const initiatorUserProfile = useUser(createAddress(initiator || AddressZero));
-  const recipientAddress = createAddress(recipient);
-  const isColonyAddress = recipientAddress === colony.colonyAddress;
-  const fallbackRecipientProfile = useUser(
-    isColonyAddress ? '' : recipientAddress,
-  );
+  const fromDomain = findDomain(fromDomainId, colony);
+  const toDomain = findDomain(toDomainId, colony);
 
-  const fromDomain = colony.domains.find(
-    ({ ethDomainId }) => ethDomainId === parseInt(fromDomainId, 10),
-  );
-  const toDomain = colony.domains.find(
-    ({ ethDomainId }) => ethDomainId === parseInt(toDomainId, 10),
-  );
-
-  const updatedRoles = getUpdatedDecodedMotionRoles(
-    fallbackRecipientProfile,
-    parseInt(fromDomainId, 10),
-    historicColonyRoles?.historicColonyRoles as unknown as ColonyRoles,
-    roles || [],
-  );
+  // const updatedRoles = getUpdatedDecodedMotionRoles(
+  //   fallbackRecipientProfile,
+  //   parseInt(fromDomainId, 10),
+  //   historicColonyRoles?.historicColonyRoles as unknown as ColonyRoles,
+  //   roles || [],
+  // );
   const { roleMessageDescriptorId, roleTitle } = useFormatRolesTitle(
-    actionType === ColonyMotions.SetUserRolesMotion ? updatedRoles : roles,
+    roles,
+    // actionType === ColonyMotions.SetUserRolesMotion ? updatedRoles : roles,
     actionType,
   );
 
-  const popoverPlacement = useMemo(() => {
-    const offsetSkid = (-1 * removeValueUnits(popoverWidth)) / 2;
-    return [offsetSkid, removeValueUnits(popoverDistance)];
-  }, []);
+  // const totalNayStakeValue = BigNumber.from(totalNayStake || 0);
+  // const isFullyNayStaked = totalNayStakeValue.gte(
+  //   BigNumber.from(requiredStake || 0),
+  // );
 
-  const handleSyntheticEvent = useCallback(
-    () => handleOnClick && handleOnClick({ id, transactionHash }),
-    [handleOnClick, id, transactionHash],
-  );
-
-  const totalNayStakeValue = BigNumber.from(totalNayStake || 0);
-  const isFullyNayStaked = totalNayStakeValue.gte(
-    BigNumber.from(requiredStake || 0),
-  );
-
-  let domainName;
-  if (
-    metadataJSON &&
-    (actionType === ColonyActions.EditDomain ||
-      actionType === ColonyActions.CreateDomain ||
-      actionType === ColonyMotions.CreateDomainMotion)
-  ) {
-    const domainObject = parseDomainMetadata(metadataJSON);
-    domainName = domainObject.domainName;
-  }
   const motionStyles =
     MOTION_TAG_MAP[
-      motionState ||
-        (isVotingExtensionEnabled &&
-          !actionType?.endsWith('Motion') &&
-          MotionState.Forced) ||
+      motionState /* isVotingExtensionEnabled && */ ||
+        (!actionType?.endsWith('Motion') && MotionState.Forced) ||
         MotionState.Invalid
     ];
 
-  const decimals =
-    tokenData?.tokenInfo?.decimals || Number(colonyTokenDecimals);
-  const symbol = tokenData?.tokenInfo?.symbol || colonyTokenSymbol;
+  // const decimals =
+  //   tokenData?.tokenInfo?.decimals || Number(colonyTokenDecimals);
+  // const symbol = tokenData?.tokenInfo?.symbol || colonyTokenSymbol;
   const formattedReputationChange = getFormattedTokenValue(
     new Decimal(reputationChange || '0').abs().toString(),
     decimals,
   );
 
-  const isMotionFinished =
-    motionState === MotionState.Passed ||
-    motionState === MotionState.Failed ||
-    motionState === MotionState.FailedNoFinalizable;
+  // const isMotionFinished =
+  //   motionState === MotionState.Passed ||
+  //   motionState === MotionState.Failed ||
+  //   motionState === MotionState.FailedNoFinalizable;
 
   const stopPropagation = (event) => event.stopPropagation();
 
-  const { feeInverse: networkFeeInverse } = useNetworkContracts();
-  const feePercentage = networkFeeInverse
-    ? BigNumber.from(100).div(networkFeeInverse)
-    : undefined;
+  // const { feeInverse: networkFeeInverse } = useNetworkContracts();
+  // const feePercentage = networkFeeInverse
+  //   ? BigNumber.from(100).div(networkFeeInverse)
+  //   : undefined;
 
-  // In case it is a Payment Motion or Action, calculate the payment the recipient gets, after network fees
-  const paymentReceivedFn = feePercentage
-    ? (paymentAmount: BigNumberish) =>
-        BigNumber.from(paymentAmount)
-          .mul(BigNumber.from(100).sub(feePercentage))
-          .div(100)
-    : (x: any) => x;
+  // // In case it is a Payment Motion or Action, calculate the payment the recipient gets, after network fees
+  // const paymentReceivedFn = feePercentage
+  //   ? (paymentAmount: BigNumberish) =>
+  //       BigNumber.from(paymentAmount)
+  //         .mul(BigNumber.from(100).sub(feePercentage))
+  //         .div(100)
+  //   : (x: any) => x;
 
   return (
     <li data-test="actionItem">
@@ -243,8 +177,8 @@ const ActionsListItem = ({
           noPointer: !handleOnClick,
           [ItemStatus[status]]: !!status,
         })}
-        onClick={handleSyntheticEvent}
-        onKeyPress={handleSyntheticEvent}
+        onClick={handleOnClick}
+        onKeyPress={handleOnClick}
       >
         <div
           /*
@@ -260,20 +194,18 @@ const ActionsListItem = ({
         >
           {initiator && (
             <UserAvatar
-              colony={colony}
               size="s"
-              address={initiator}
-              user={initiatorUserProfile}
+              user={initiator}
               notSet={false}
               showInfo
               popperOptions={{
                 showArrow: false,
-                placement: 'bottom',
+                placement: 'bottom-start',
                 modifiers: [
                   {
                     name: 'offset',
                     options: {
-                      offset: popoverPlacement,
+                      offset: [-20, 10],
                     },
                   },
                 ],
@@ -290,10 +222,7 @@ const ActionsListItem = ({
                   actionType,
                   initiator: (
                     <span className={styles.titleDecoration}>
-                      <FriendlyName
-                        user={initiatorUserProfile}
-                        autoShrinkAddress
-                      />
+                      <FriendlyName user={user} autoShrinkAddress />
                     </span>
                   ),
                   /*
@@ -301,27 +230,24 @@ const ActionsListItem = ({
                    */
                   recipient: (
                     <span className={styles.titleDecoration}>
-                      <FriendlyName
-                        user={fallbackRecipientProfile}
-                        autoShrinkAddress
-                        colony={colony}
-                      />
+                      <FriendlyName user={recipient} autoShrinkAddress />
                     </span>
                   ),
                   amount: (
                     <Numeral
                       value={
-                        actionType === ColonyActions.Payment ||
-                        actionType === ColonyMotions.PaymentMotion
-                          ? paymentReceivedFn(amount)
-                          : amount
+                        amount
+                        // actionType === ColonyActions.Payment ||
+                        // actionType === ColonyMotions.PaymentMotion
+                        //   ? paymentReceivedFn(amount)
+                        //   : amount
                       }
                       decimals={getTokenDecimalsWithFallback(decimals)}
                     />
                   ),
-                  tokenSymbol: symbol,
+                  tokenSymbol,
                   decimals: getTokenDecimalsWithFallback(decimals),
-                  fromDomain: domainName || fromDomain?.name || '',
+                  fromDomain: fromDomain?.name || '',
                   toDomain: toDomain?.name || '',
                   roles: roleTitle,
                   newVersion: newVersion || '0',
@@ -332,7 +258,7 @@ const ActionsListItem = ({
                 }}
               />
             </span>
-            {(motionState || isVotingExtensionEnabled) && (
+            {motionState /* || isVotingExtensionEnabled */ && (
               <div className={styles.motionTagWrapper}>
                 <Tag
                   text={motionStyles.name}
@@ -356,8 +282,8 @@ const ActionsListItem = ({
             </FormattedDateParts>
             {fromDomain && (
               <span className={styles.domain}>
-                {domainName || fromDomain.name ? (
-                  domainName || fromDomain.name
+                {fromDomain.name ? (
+                  fromDomain.name
                 ) : (
                   <FormattedMessage
                     {...MSG.domain}
@@ -372,32 +298,31 @@ const ActionsListItem = ({
                   appearance={{ size: 'extraTiny' }}
                   className={styles.commentCountIcon}
                   name="comment"
-                  title={formatMessage(MSG.titleCommentCount, {
+                  title={formatText(MSG.titleCommentCount, {
                     commentCount,
-                    formattedCommentCount: formatNumber(commentCount),
+                    formattedCommentCount: commentCount.toString(),
                   })}
                 />
                 <Numeral
                   value={commentCount}
-                  title={formatMessage(MSG.titleCommentCount, {
+                  title={formatText(MSG.titleCommentCount, {
                     commentCount,
-                    formattedCommentCount: formatNumber(commentCount),
+                    formattedCommentCount: commentCount.toString(),
                   })}
                 />
               </span>
             )}
           </div>
         </div>
-        {motionId && !isMotionFinished && (
+        {/* {motionId && !isMotionFinished && (
           <div className={styles.countdownTimerContainer}>
             <CountDownTimer
-              colony={colony}
               state={motionState as MotionState}
               motionId={Number(motionId)}
               isFullyNayStaked={isFullyNayStaked}
             />
           </div>
-        )}
+        )} */}
       </div>
     </li>
   );
