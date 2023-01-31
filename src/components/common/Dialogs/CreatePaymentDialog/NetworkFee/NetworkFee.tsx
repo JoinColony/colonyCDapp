fd1ef9b43cab31edc8719ec5ff680f5d4bee51c5
ArmandoGraterol <armandogrtrl@gmail.com>
2023-01-31 12:54:33 -0300

    Added NetworkFee component

---
+++ src/components/common/Dialogs/CreatePaymentDialog/NetworkFee/NetworkFee.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  defineMessages,
+  FormattedMessage,
+  MessageDescriptor,
+} from 'react-intl';
+import { useFormContext } from 'react-hook-form';
+import Decimal from 'decimal.js';
+
+import { calculateFee, getTokenDecimalsWithFallback } from '~utils/tokens';
+import { notNull } from '~utils/arrays';
+import Numeral from '~shared/Numeral';
+
+import { useColonyContext } from '~hooks';
+
+import styles from './NetworkFee.css';
+
+const displayName =
+  'common.CreatePaymentDialog.CreatePaymentDialogForm.NetworkFee';
+
+const MSG = defineMessages({
+  amount: {
+    id: `${displayName}.amount`,
+    defaultMessage: 'Amount',
+  },
+  fee: {
+    id: `${displayName}.fee`,
+    defaultMessage: 'Network fee: {fee} {symbol}',
+  },
+  token: {
+    id: `${displayName}.address`,
+    defaultMessage: 'Token',
+  },
+});
+
+interface Props {
+  customAmountError: string | MessageDescriptor | undefined;
+  networkFeeInverse: string | undefined;
+}
+
+const NetworkFee = ({ networkFeeInverse, customAmountError }: Props) => {
+  const { colony } = useColonyContext();
+  const { getValues } = useFormContext();
+  const values = getValues();
+  const colonyTokens =
+    colony?.tokens?.items
+      .filter(notNull)
+      .map((colonyToken) => colonyToken.token) || [];
+  const selectedToken = colonyTokens.find(
+    (token) => token?.tokenAddress === values.tokenAddress,
+  );
+
+  if (
+    !networkFeeInverse &&
+    customAmountError &&
+    new Decimal(values.amount).isZero()
+  ) {
+    return null;
+  }
+
+  return (
+    <div className={styles.networkFee}>
+      <FormattedMessage
+        {...MSG.fee}
+        values={{
+          fee: (
+            <Numeral
+              // appearance={{
+              //   size: 'small',
+              //   theme: 'grey',
+              // }}
+              value={
+                calculateFee(
+                  values.amount,
+                  networkFeeInverse!,
+                  getTokenDecimalsWithFallback(selectedToken?.decimals),
+                ).feesInWei
+              }
+              decimals={getTokenDecimalsWithFallback(
+                selectedToken && selectedToken.decimals,
+              )}
+            />
+          ),
+          symbol: (selectedToken && selectedToken.symbol) || '???',
+        }}
+      />
+    </div>
+  );
+};
+
+export default NetworkFee;
