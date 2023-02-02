beacd184c8ae88986c895c09f76a775d1c79c83e
ArmandoGraterol <armandogrtrl@gmail.com>
2023-02-02 20:00:11 -0300

    Updated UnlockTokenDialog components

--- src/components/common/Dialogs/UnlockTokenDialog/UnlockTokenForm.css.d.ts
+++ src/components/common/Dialogs/UnlockTokenDialog/UnlockTokenForm.css.d.ts
@@ -1,13 +1,9 @@
 declare namespace UnlockTokenFormCssNamespace {
   export interface IUnlockTokenFormCss {
-    headingContainer: string;
     learnMoreLink: string;
-    modalHeading: string;
-    motionVoteDomain: string;
     noPermissionMessage: string;
     note: string;
     unlocked: string;
-    wideButton: string;
     wrapper: string;
   }
 }
