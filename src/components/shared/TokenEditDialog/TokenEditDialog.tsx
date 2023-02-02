3bc61a9133845ae4670421a3e946909b607a641f
ArmandoGraterol <armandogrtrl@gmail.com>
2023-02-02 19:55:17 -0300

    Updated TokenEditDialog component

--- src/components/shared/TokenEditDialog/TokenEditDialog.tsx
+++ src/components/shared/TokenEditDialog/TokenEditDialog.tsx
@@ -1,23 +1,23 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { ColonyRole } from '@colony/colony-js';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import { AddressZero } from '@ethersproject/constants';
-import { FormState } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 
-import Button from '~shared/Button';
-import { ActionDialogProps, DialogSection } from '~shared/Dialog';
+import {
+  ActionDialogProps,
+  DialogHeading,
+  DialogSection,
+  DialogControls,
+} from '~shared/Dialog';
 import { Annotations } from '~shared/Fields';
-import { Heading3, Heading4 } from '~shared/Heading';
+import { Heading4 } from '~shared/Heading';
 import Paragraph from '~shared/Paragraph';
 import PermissionRequiredInfo from '~shared/PermissionRequiredInfo';
 import PermissionsLabel from '~shared/PermissionsLabel';
 import { TokenSelector } from '~common/CreateColonyWizard';
-import {
-  TokenManagementDialogFormValues,
-  FormattedListToken,
-} from '~common/Dialogs';
+import { TokenManagementDialogFormValues } from '~common/Dialogs';
 // import NotEnoughReputation from '~dashboard/NotEnoughReputation';
-// import MotionDomainSelect from '~dashboard/MotionDomainSelect';
 
 import {
   useTransformer,
@@ -29,6 +29,7 @@ import { hasRoot } from '~utils/checks';
 import { isEqual } from '~utils/lodash';
 
 import TokenItem from './TokenItem';
+import getTokenList from './getTokenList';
 
 import styles from './TokenEditDialog.css';
 
@@ -64,28 +65,25 @@ const MSG = defineMessages({
 
 interface Props extends ActionDialogProps {
   close: (val: any) => void;
-  values: TokenManagementDialogFormValues;
-  // Token list from json file. Not supported on local env
-  tokensList?: FormattedListToken[];
 }
 
 const TokenEditDialog = ({
   close,
-  tokensList = [],
   colony,
   colony: { tokens, nativeToken },
   back,
-  isSubmitting,
-  isValid,
-  values,
 }: // isVotingExtensionEnabled,
-Props & FormState<TokenManagementDialogFormValues>) => {
+Props) => {
   const { user, wallet } = useAppContext();
-
-  const colonyTokens = useMemo(() => tokens?.items || [], [tokens]);
-  const colonyTokenAddresses = useMemo(
-    () => colonyTokens.map((colonyToken) => colonyToken?.token.tokenAddress),
-    [colonyTokens],
+  const {
+    getValues,
+    formState: { isValid, isSubmitting },
+  } = useFormContext();
+  const values = getValues();
+  const tokenList = getTokenList();
+  const colonyTokens = tokens?.items || [];
+  const colonyTokenAddresses = colonyTokens.map(
+    (colonyToken) => colonyToken?.token.tokenAddress,
   );
 
   const hasTokensListChanged = ({
@@ -115,47 +113,20 @@ Props & FormState<TokenManagementDialogFormValues>) => {
 
   const inputDisabled = !userHasPermission || onlyForceAction || isSubmitting;
 
-  const allTokens = useMemo(() => {
-    return [...colonyTokens, ...(canEditTokens ? tokensList : [])]
-      .map((token) => token?.token)
-      .filter(
-        (firstToken, index, mergedTokens) =>
-          mergedTokens.findIndex(
-            (secondToken) =>
-              secondToken?.tokenAddress === firstToken?.tokenAddress,
-          ) === index,
-      );
-  }, [colonyTokens, tokensList, canEditTokens]);
+  const allTokens = [...colonyTokens, ...(canEditTokens ? tokenList : [])]
+    .map((token) => token?.token)
+    .filter(
+      (firstToken, index, mergedTokens) =>
+        mergedTokens.findIndex(
+          (secondToken) =>
+            secondToken?.tokenAddress === firstToken?.tokenAddress,
+        ) === index,
+    );
 
   return (
     <>
       <DialogSection appearance={{ theme: 'sidePadding' }}>
-        <div className={styles.modalHeading}>
-          {/*
-           * @NOTE Always disabled since you can only create this motion in root
-           */}
-          {/* {isVotingExtensionEnabled && (
-            <div className={styles.motionVoteDomain}>
-              <MotionDomainSelect
-                colony={colony}
-                disabled
-              />
-            </div>
-          )} */}
-          <div className={styles.headingContainer}>
-            <Heading3
-              appearance={{ margin: 'none', theme: 'dark' }}
-              text={MSG.title}
-            />
-            {/* {canEditTokens && isVotingExtensionEnabled (
-              <Toggle
-                label={{ id: 'label.force' }}
-                name="forceAction"
-                disabled={isSubmitting}
-              />
-            )} */}
-          </div>
-        </div>
+        <DialogHeading title={MSG.title} />
       </DialogSection>
       {!userHasPermission && (
         <DialogSection appearance={{ theme: 'sidePadding' }}>
@@ -225,21 +196,13 @@ Props & FormState<TokenManagementDialogFormValues>) => {
         <NotEnoughReputation appearance={{ marginTop: 'negative' }} />
       )} */}
       <DialogSection appearance={{ align: 'right', theme: 'footer' }}>
-        <Button
-          appearance={{ theme: 'secondary', size: 'large' }}
-          text={{
+        <DialogControls
+          onSecondaryButtonClick={back === undefined ? close : back}
+          secondaryButtonText={{
             id: back === undefined ? 'button.cancel' : 'button.back',
           }}
-          onClick={back === undefined ? close : back}
-        />
-        <Button
-          appearance={{ theme: 'primary', size: 'large' }}
-          text={{ id: 'button.confirm' }}
-          loading={isSubmitting}
           disabled={!isValid || inputDisabled || !hasTokensListChanged(values)}
-          type="submit"
-          style={{ width: styles.wideButton }}
-          data-test="confirm"
+          dataTest="confirm"
         />
       </DialogSection>
     </>
