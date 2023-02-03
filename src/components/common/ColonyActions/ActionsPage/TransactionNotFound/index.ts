043cdde6fc7b9e6797972ec5859d933e0170b2b6
William Martin <williamajmartin@gmail.com>
2023-02-03 16:46:19 +0100

    Move: tx meta and tx status

--- src/components/common/ColonyActions/ActionsPage/TransactionNotFound/index.ts
+++ src/components/common/ColonyActions/ActionsPage/TransactionNotFound/index.ts
@@ -1,4 +1,2 @@
 export { default } from './TransactionNotFound';
 export { default as TransactionHash } from './TransactionHash';
-export { default as TransactionMeta } from './TransactionMeta';
-export { default as TransactionStatus, Status } from './TransactionStatus';
