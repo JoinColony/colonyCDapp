d8a1a651025f9618eb60be2add012e4bf9a7aca9
William Martin <williamajmartin@gmail.com>
2023-02-03 16:46:19 +0100

    Move: tx meta and tx status

--- src/components/common/ColonyActions/ActionsPage/index.ts
+++ src/components/common/ColonyActions/ActionsPage/index.ts
@@ -1,5 +1,7 @@
-export { default as TransactionNotFound } from './TransactionNotFound/TransactionNotFound';
+export { default as TransactionNotFound } from './TransactionNotFound';
 export { default as ActionsPageLayout } from './Layout';
 export { default as DefaultAction } from './DefaultAction';
+export { default as TransactionMeta } from './TransactionMeta';
+export { default as TransactionStatus } from './TransactionStatus';
 
 export * from './staticMaps';
--- src/components/common/ColonyActions/ActionsPage/staticMaps.ts
+++ src/components/common/ColonyActions/ActionsPage/staticMaps.ts
@@ -1,4 +1,13 @@
-import { Status } from './TransactionNotFound/TransactionStatus';
+import { ColonyRole } from '@colony/colony-js';
+
+import { ColonyAndExtensionsEvents } from '~types';
+
+export enum Status {
+  Failed = 'failed',
+  Pending = 'pending',
+  Succeeded = 'succeeded',
+  SystemMessage = 'systemMessage',
+}
 
 /*
  * Transaction statuses
@@ -7,4 +16,35 @@ export const STATUS_MAP: { [k: number]: Status } = {
   0: Status.Failed,
   1: Status.Succeeded,
   2: Status.Pending,
+};
+
+type EventRolesMap = Partial<{
+  [key in ColonyAndExtensionsEvents]: ColonyRole[];
+}>;
+
+export const EVENT_ROLES_MAP: EventRolesMap = {
+  [ColonyAndExtensionsEvents.OneTxPaymentMade]: [
+    ColonyRole.Administration,
+    ColonyRole.Funding,
+  ],
+  [ColonyAndExtensionsEvents.ColonyFundsMovedBetweenFundingPots]: [
+    ColonyRole.Funding,
+  ],
+  [ColonyAndExtensionsEvents.TokenUnlocked]: [ColonyRole.Root],
+  [ColonyAndExtensionsEvents.TokensMinted]: [ColonyRole.Root],
+  [ColonyAndExtensionsEvents.DomainAdded]: [ColonyRole.Architecture],
+  [ColonyAndExtensionsEvents.DecisionCreated]: [ColonyRole.Root],
+  [ColonyAndExtensionsEvents.ColonyUpgraded]: [ColonyRole.Root],
+  [ColonyAndExtensionsEvents.ColonyMetadata]: [ColonyRole.Root],
+  [ColonyAndExtensionsEvents.DomainMetadata]: [ColonyRole.Architecture],
+  [ColonyAndExtensionsEvents.ColonyRoleSet]: [ColonyRole.Architecture],
+  [ColonyAndExtensionsEvents.RecoveryModeEntered]: [ColonyRole.Recovery],
+  [ColonyAndExtensionsEvents.RecoveryStorageSlotSet]: [ColonyRole.Recovery],
+  [ColonyAndExtensionsEvents.RecoveryModeExitApproved]: [ColonyRole.Recovery],
+  [ColonyAndExtensionsEvents.RecoveryModeExited]: [ColonyRole.Recovery],
+  [ColonyAndExtensionsEvents.ArbitraryReputationUpdate]: [
+    ColonyRole.Root,
+    ColonyRole.Arbitration,
+  ],
+  [ColonyAndExtensionsEvents.Generic]: [],
 };
