150109ce7126479db40c0d137404edb9d84de9c3
Jakub Zajac <jakub@colony.io>
2023-02-27 23:39:23 +0000

    Bump ingestor image hash

--- src/components/common/ColonyActions/ColonyActions.tsx
+++ src/components/common/ColonyActions/ColonyActions.tsx
@@ -234,6 +234,7 @@ const ColonyActions = (/* { ethDomainId }: Props */) => {
             domain: colony.domains?.items.find((d) => !d?.isRoot),
             domainName: 'New Name',
             domainColor: DomainColor.Magenta,
+            description: 'New Description',
           }),
           withMeta({ navigate }),
         )}
