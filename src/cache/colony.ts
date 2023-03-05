e51897ca6c01dec5d49716e966af8a93e93ac010
Raul Glogovetan <raul@glogovetan.com>
2023-03-06 00:39:16 +0200

    Add: local `colony` cache `balance` data updates

---
+++ src/cache/colony.ts
@@ -0,0 +1,34 @@
+import { DEFAULT_NETWORK_TOKEN, ADDRESS_ZERO } from '~constants';
+import { TokenType } from '~gql';
+
+const colony = {
+  Colony: {
+    fields: {
+      /*
+       * @NOTE Add the local's chain native token to the colony's tokens list
+       */
+      tokens: {
+        read: (baseTokens) => ({
+          ...baseTokens,
+          items: [
+            ...baseTokens.items,
+            {
+              __typename: 'ColonyTokens',
+              token: {
+                __typename: 'Token',
+                ...DEFAULT_NETWORK_TOKEN,
+                id: ADDRESS_ZERO,
+                avatar: null,
+                thumbnail: null,
+                tokenAdress: ADDRESS_ZERO,
+                type: TokenType.Colony,
+              },
+            },
+          ],
+        }),
+      },
+    },
+  },
+};
+
+export default colony;
