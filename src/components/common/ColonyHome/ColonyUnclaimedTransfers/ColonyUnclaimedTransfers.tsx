75769e2de4a8daadcf0152da7de07b2488b03f32
Raul Glogovetan <raul@glogovetan.com>
2023-03-06 15:25:57 +0200

    Add: `ColonyUnclaimedTransfers` show ETH transfers

--- src/components/common/ColonyHome/ColonyUnclaimedTransfers/ColonyUnclaimedTransfers.tsx
+++ src/components/common/ColonyHome/ColonyUnclaimedTransfers/ColonyUnclaimedTransfers.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import { ActionButton } from '~shared/Button';
