a7ac8088204cd4bf3938f358aaedc3719aad8a75
ArmandoGraterol <armandogrtrl@gmail.com>
2023-03-08 15:27:56 -0300

    Updated text of warning on ManageWhitelistActiveToggle

--- src/components/common/Dialogs/ManageWhitelistDialog/ManageWhitelistActiveToggle.tsx
+++ src/components/common/Dialogs/ManageWhitelistDialog/ManageWhitelistActiveToggle.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import { Toggle } from '~shared/Fields';
+import { HookFormToggle as Toggle } from '~shared/Fields';
 import { Heading4 } from '~shared/Heading';
 
 import styles from './ManageWhitelistDialogForm.css';
@@ -22,14 +22,14 @@ const MSG = defineMessages({
   },
   tooltipText: {
     id: `${displayName}.tooltipText`,
-    defaultMessage: `Address book is active by default once at least one address is added to the list. You can turn this feature “Off” to deactivate. Use with caution.`,
+    defaultMessage: `Address book is automatically activated when at least one address is added to the list. You can turn this feature “Off” to deactivate. Use with caution.`,
   },
   warningText: {
     id: `${displayName}.warningText`,
     defaultMessage: `<span>Warning.</span>
-      You are deactivating your colony’s address book.
-      This is a list of addresses your colony has flagged as “safe”.
-      Now, instead, all addresses watching your colony will
+      You are deactivating your colony’s address book
+      which is a list of addresses your colony has flagged as “safe”.
+      Once this is deactivated, all addresses watching your colony will
       appear in recipient selection menus. Be vigilant against spoof accounts.`,
   },
 });
