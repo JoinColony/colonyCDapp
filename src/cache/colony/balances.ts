2361d4a43f9691a624f983e8e75f0ce5ecaba90e
Raul Glogovetan <raul@glogovetan.com>
2023-03-13 01:28:19 +0200

    Update: `balances` apollo cache to fix chain native token

--- src/cache/colony/balances.ts
+++ src/cache/colony/balances.ts
@@ -1,4 +1,25 @@
 import { BigNumber } from 'ethers';
+import {
+  DEFAULT_NETWORK_TOKEN,
+  ADDRESS_ZERO,
+  COLONY_TOTAL_BALANCE_DOMAIN_ID,
+} from '~constants';
+
+/*
+ * If token has Address Zero as it's address return local data representing
+ * the local chain token, otherwise just return the token
+ */
+const getTokenDataWithNative = (token) => {
+  if (token.id !== ADDRESS_ZERO) {
+    return token;
+  }
+  return {
+    ...token, // needed to add the correct data types
+    ...DEFAULT_NETWORK_TOKEN,
+  };
+};
+
+const BALANCE_TYPE_NAME = 'ColonyBalance';
 
 const balancesFieldCache = {
   /*
@@ -8,40 +29,74 @@ const balancesFieldCache = {
    * including root
    */
   balances: {
-    read: (domainBalanceRefs, { readField }) => {
+    read: (domainBalanceRefs, { readField, cache }) => {
       const colonyAddress = readField('id');
       const { chainId: colonyChainId } = readField('meta');
       const { items: colonyTokens = [] } = readField('tokens');
-      const totalTokenBalances = domainBalanceRefs.items
-        .map((balanceRef) => {
-          const token = readField('token', balanceRef);
-          const balance = readField('balance', balanceRef);
-          return { token, balance };
-        })
-        .reduce((totals, balance) => {
-          const {
-            token: { id: currentToken },
-            balance: currentBalance,
-          } = balance;
-          if (totals[currentToken]) {
-            return {
-              ...totals,
-              [currentToken]: totals[currentToken].add(currentBalance),
-            };
-          }
+      /*
+       * We're recreating the balances array in order to change the native chain
+       * token data to include the correct one from the one currently deployed
+       *
+       * @NOTE This is actually a wrong way to go about it, and once Multisig
+       * is upon us, we should be using the chain the colony is deployed to
+       * in order to dynamically contruct this (since multiple colonies
+       * can be deployed to multiple chains)
+       *
+       * @WARNING we have to get the full domain, and basically recreate the
+       * whole balance object (including the typename) since, if the alternative
+       * is true, and we pass the reference along, it will actually prevent
+       * us from returning the modified data, and will overwrite our returned
+       * results with the ones already available in the cache.
+       */
+      const allDomainBalances = domainBalanceRefs.items.map((balanceRef) => {
+        const token = readField('token', balanceRef);
+        const balance = readField('balance', balanceRef);
+        const id = readField('id', balanceRef);
+        const domainRef = readField('domain', balanceRef);
+        const domainId = readField('id', domainRef);
+        /*
+         * We're accessing a Apollo internal API to avoid having to call `readField`
+         * for all fields on the domain object.
+         *
+         * @NOTE I choose to go this route as I'm fairly sure we're going to change
+         * the model for domains soon enought, which would have made places like these
+         * needed to be updated after that, which just screams the we're going
+         * to forget about it. This way all model changes are basically transparent
+         * to this code
+         */
+        const domain = cache.data.data[`Domain:${domainId}`];
+        return {
+          __typename: BALANCE_TYPE_NAME,
+          id,
+          domain,
+          token: getTokenDataWithNative(token),
+          balance,
+        };
+      });
+      const totalTokenBalances = allDomainBalances.reduce((totals, balance) => {
+        const {
+          token: { id: currentToken },
+          balance: currentBalance,
+        } = balance;
+        if (totals[currentToken]) {
           return {
             ...totals,
-            [currentToken]: BigNumber.from(currentBalance),
+            [currentToken]: totals[currentToken].add(currentBalance),
           };
-        }, {});
+        }
+        return {
+          ...totals,
+          [currentToken]: BigNumber.from(currentBalance),
+        };
+      }, {});
       return {
         ...domainBalanceRefs,
         items: [
-          ...domainBalanceRefs.items,
+          ...allDomainBalances,
           ...colonyTokens.map(({ token }) => ({
-            __typename: 'ColonyBalance',
+            __typename: BALANCE_TYPE_NAME,
             domain: null,
-            id: `${colonyChainId}_${colonyAddress}_0_${token.id}_balance`,
+            id: `${colonyChainId}_${colonyAddress}_${COLONY_TOTAL_BALANCE_DOMAIN_ID}_${token.id}_balance`,
             token,
             balance: totalTokenBalances[token.id].toString(),
           })),
