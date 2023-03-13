76469af58c07c652ae8f74e6696ed3bad6bccf78
Raul Glogovetan <raul@glogovetan.com>
2023-03-13 22:50:06 +0200

    Refactor: apollo `balances` cache to extract logic outside

--- src/cache/colony/balances.ts
+++ src/cache/colony/balances.ts
@@ -5,6 +5,8 @@ import {
   COLONY_TOTAL_BALANCE_DOMAIN_ID,
 } from '~constants';
 
+const BALANCE_TYPE_NAME = 'ColonyBalance';
+
 /*
  * If token has Address Zero as it's address return local data representing
  * the local chain token, otherwise just return the token
@@ -19,7 +21,68 @@ const getTokenDataWithNative = (token) => {
   };
 };
 
-const BALANCE_TYPE_NAME = 'ColonyBalance';
+/*
+ * We're recreating the balances array in order to change the native chain
+ * token data to include the correct one from the one currently deployed
+ *
+ * @NOTE This is actually a wrong way to go about it, and once Multisig
+ * is upon us, we should be using the chain the colony is deployed to
+ * in order to dynamically contruct this (since multiple colonies
+ * can be deployed to multiple chains)
+ *
+ * @WARNING we have to get the full domain, and basically recreate the
+ * whole balance object (including the typename) since, if the alternative
+ * is true, and we pass the reference along, it will actually prevent
+ * us from returning the modified data, and will overwrite our returned
+ * results with the ones already available in the cache.
+ */
+const getAllDomainBalancesFromRefs = (
+  domainBalanceRefs: { _ref: string }[],
+  { readField, cache },
+) =>
+  domainBalanceRefs.map((balanceRef) => {
+    const token = readField('token', balanceRef);
+    const balance = readField('balance', balanceRef);
+    const id = readField('id', balanceRef);
+    const domainRef = readField('domain', balanceRef);
+    const domainId = readField('id', domainRef);
+    /*
+     * We're accessing a Apollo internal API to avoid having to call `readField`
+     * for all fields on the domain object.
+     *
+     * @NOTE I choose to go this route as I'm fairly sure we're going to change
+     * the model for domains soon enought, which would have made places like these
+     * needed to be updated after that, which just screams the we're going
+     * to forget about it. This way all model changes are basically transparent
+     * to this code
+     */
+    const domain = cache.data.data[`Domain:${domainId}`];
+    return {
+      __typename: BALANCE_TYPE_NAME,
+      id,
+      domain,
+      token: getTokenDataWithNative(token),
+      balance,
+    };
+  });
+
+const getTotalTokenBalances = (allDomainBalances) =>
+  allDomainBalances.reduce((totals, balance) => {
+    const {
+      token: { id: currentToken },
+      balance: currentBalance,
+    } = balance;
+    if (totals[currentToken]) {
+      return {
+        ...totals,
+        [currentToken]: totals[currentToken].add(currentBalance),
+      };
+    }
+    return {
+      ...totals,
+      [currentToken]: BigNumber.from(currentBalance),
+    };
+  }, {});
 
 const balancesFieldCache = {
   /*
@@ -33,62 +96,12 @@ const balancesFieldCache = {
       const colonyAddress = readField('id');
       const { chainId: colonyChainId } = readField('meta');
       const { items: colonyTokens = [] } = readField('tokens');
-      /*
-       * We're recreating the balances array in order to change the native chain
-       * token data to include the correct one from the one currently deployed
-       *
-       * @NOTE This is actually a wrong way to go about it, and once Multisig
-       * is upon us, we should be using the chain the colony is deployed to
-       * in order to dynamically contruct this (since multiple colonies
-       * can be deployed to multiple chains)
-       *
-       * @WARNING we have to get the full domain, and basically recreate the
-       * whole balance object (including the typename) since, if the alternative
-       * is true, and we pass the reference along, it will actually prevent
-       * us from returning the modified data, and will overwrite our returned
-       * results with the ones already available in the cache.
-       */
-      const allDomainBalances = domainBalanceRefs.items.map((balanceRef) => {
-        const token = readField('token', balanceRef);
-        const balance = readField('balance', balanceRef);
-        const id = readField('id', balanceRef);
-        const domainRef = readField('domain', balanceRef);
-        const domainId = readField('id', domainRef);
-        /*
-         * We're accessing a Apollo internal API to avoid having to call `readField`
-         * for all fields on the domain object.
-         *
-         * @NOTE I choose to go this route as I'm fairly sure we're going to change
-         * the model for domains soon enought, which would have made places like these
-         * needed to be updated after that, which just screams the we're going
-         * to forget about it. This way all model changes are basically transparent
-         * to this code
-         */
-        const domain = cache.data.data[`Domain:${domainId}`];
-        return {
-          __typename: BALANCE_TYPE_NAME,
-          id,
-          domain,
-          token: getTokenDataWithNative(token),
-          balance,
-        };
-      });
-      const totalTokenBalances = allDomainBalances.reduce((totals, balance) => {
-        const {
-          token: { id: currentToken },
-          balance: currentBalance,
-        } = balance;
-        if (totals[currentToken]) {
-          return {
-            ...totals,
-            [currentToken]: totals[currentToken].add(currentBalance),
-          };
-        }
-        return {
-          ...totals,
-          [currentToken]: BigNumber.from(currentBalance),
-        };
-      }, {});
+
+      const allDomainBalances = getAllDomainBalancesFromRefs(
+        domainBalanceRefs.items,
+        { readField, cache },
+      );
+      const totalTokenBalances = getTotalTokenBalances(allDomainBalances);
       return {
         ...domainBalanceRefs,
         items: [
