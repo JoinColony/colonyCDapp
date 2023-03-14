90cb3d936cce5c87d9cecb2bd91c2e26ccbf8aad
Jakub Zajac <jakub@colony.io>
2023-03-14 14:27:57 +0000

    Fix: fetchColonyBalances when query fails

--- amplify/backend/function/fetchColonyBalances/src/index.js
+++ amplify/backend/function/fetchColonyBalances/src/index.js
@@ -26,81 +26,79 @@ const provider = new providers.JsonRpcProvider(RPC_URL);
 exports.handler = async ({ source: { id: colonyAddress } }) => {
   const balances = [];
 
-  const {
-    data: { getColony: colony },
-  } = await graphqlRequest(
+  const response = await graphqlRequest(
     getColony,
     { address: colonyAddress },
     GRAPHQL_URI,
     API_KEY,
   );
+  const { getColony: colony } = response?.data || {};
 
-  if (colony) {
-    const networkClient = await getColonyNetworkClient(
-      Network.Custom,
-      provider,
-      { networkAddress },
-    );
+  if (!colony) {
+    return null;
+  }
 
-    const colonyClient = await networkClient.getColonyClient(colonyAddress);
+  const networkClient = await getColonyNetworkClient(Network.Custom, provider, {
+    networkAddress,
+  });
 
-    const { chainId } = colony?.chainMetadata || {};
-    const { items: domains = [] } = colony.domains || {};
-    const { items: tokens = [] } = colony.tokens || {};
+  const colonyClient = await networkClient.getColonyClient(colonyAddress);
 
-    domains.map(async (domain) => {
-      const { nativeId, nativeFundingPotId } = domain;
+  const { chainId } = colony?.chainMetadata || {};
+  const { items: domains = [] } = colony.domains || {};
+  const { items: tokens = [] } = colony.tokens || {};
 
-      // Native chain token. Ie: address 0x0000...0000
+  domains.map(async (domain) => {
+    const { nativeId, nativeFundingPotId } = domain;
+
+    // Native chain token. Ie: address 0x0000...0000
+    balances.push(async () => {
+      const rewardsPotTotal = await colonyClient.getFundingPotBalance(
+        nativeFundingPotId,
+        AddressZero,
+      );
+      /*
+       * We're using this patters so that we could parallelize all calls at once
+       * since this is in essence a multi dimensional array (of async data)
+       */
+      return {
+        id: `${chainId}_${colonyAddress}_${nativeId}_${AddressZero}_balance`,
+        domain,
+        token: {
+          ...tokens[0].token,
+          id: AddressZero,
+          name: '',
+          symbol: '',
+          decimals: 18,
+          type: 'CHAIN_NATIVE',
+        },
+        balance: rewardsPotTotal.toString(),
+      };
+    });
+
+    tokens.map(async ({ token }) => {
+      const { id: tokenAddress } = token;
+
       balances.push(async () => {
         const rewardsPotTotal = await colonyClient.getFundingPotBalance(
           nativeFundingPotId,
-          AddressZero,
+          tokenAddress,
         );
         /*
          * We're using this patters so that we could parallelize all calls at once
          * since this is in essence a multi dimensional array (of async data)
          */
         return {
-          id: `${chainId}_${colonyAddress}_${nativeId}_${AddressZero}_balance`,
+          id: `${chainId}_${colonyAddress}_${nativeId}_${tokenAddress}_balance`,
           domain,
-          token: {
-            ...tokens[0].token,
-            id: AddressZero,
-            name: '',
-            symbol: '',
-            decimals: 18,
-            type: 'CHAIN_NATIVE',
-          },
+          token,
           balance: rewardsPotTotal.toString(),
         };
       });
-
-      tokens.map(async ({ token }) => {
-        const { id: tokenAddress } = token;
-
-        balances.push(async () => {
-          const rewardsPotTotal = await colonyClient.getFundingPotBalance(
-            nativeFundingPotId,
-            tokenAddress,
-          );
-          /*
-           * We're using this patters so that we could parallelize all calls at once
-           * since this is in essence a multi dimensional array (of async data)
-           */
-          return {
-            id: `${chainId}_${colonyAddress}_${nativeId}_${tokenAddress}_balance`,
-            domain,
-            token,
-            balance: rewardsPotTotal.toString(),
-          };
-        });
-      });
     });
+  });
 
-    return {
-      items: await Promise.all(balances.map(async (resolve) => resolve())),
-    };
-  }
-  return null;
+  return {
+    items: await Promise.all(balances.map(async (resolve) => resolve())),
+  };
 };
