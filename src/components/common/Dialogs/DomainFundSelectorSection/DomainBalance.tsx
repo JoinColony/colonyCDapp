bc0a9d7521d6622dc8d93153e0f6d334a51f3f81
Jakub Zajac <jakub@colony.io>
2023-03-15 22:50:01 +0000

    Show domain balance in TransferFundsDialog

--- src/components/common/Dialogs/DomainFundSelectorSection/DomainBalance.tsx
+++ src/components/common/Dialogs/DomainFundSelectorSection/DomainBalance.tsx
@@ -3,7 +3,11 @@ import { defineMessages, FormattedMessage } from 'react-intl';
 import { useFormContext } from 'react-hook-form';
 
 import { Token, Colony } from '~types';
-import { getTokenDecimalsWithFallback, getSelectedToken } from '~utils/tokens';
+import {
+  getTokenDecimalsWithFallback,
+  getSelectedToken,
+  getBalanceForTokenAndDomain,
+} from '~utils/tokens';
 import Numeral from '~shared/Numeral';
 
 import styles from './DomainFundSelector.css';
@@ -19,26 +23,43 @@ const MSG = defineMessages({
 
 interface Props {
   colony: Colony;
+  domainFieldName: string;
 }
 
-const getDomainFundMessageValues = (selectedToken: Token | undefined) => ({
+const getDomainFundMessageValues = (
+  colony: Colony,
+  selectedToken: Token,
+  fromDomain: number,
+) => ({
   amount: (
     <Numeral
-      value={0} // fromDomainTokenBalance || ...
+      value={getBalanceForTokenAndDomain(
+        colony.balances,
+        selectedToken.tokenAddress,
+        fromDomain,
+      )}
       decimals={getTokenDecimalsWithFallback(selectedToken?.decimals)}
     />
   ),
   symbol: selectedToken?.symbol || '???',
 });
 
-const DomainBalance = ({ colony }: Props) => {
+const DomainBalance = ({ colony, domainFieldName }: Props) => {
   const { watch } = useFormContext();
   const tokenAddress = watch('tokenAddress');
+  const domainId = watch(domainFieldName);
   const selectedToken = getSelectedToken(colony, tokenAddress);
+
+  if (!selectedToken) {
+    return null;
+  }
 
   return (
     <div className={styles.domainPotBalance}>
-      <FormattedMessage {...MSG.domainTokenAmount} values={getDomainFundMessageValues(selectedToken)} />
+      <FormattedMessage
+        {...MSG.domainTokenAmount}
+        values={getDomainFundMessageValues(colony, selectedToken, domainId)}
+      />
     </div>
   );
 };
