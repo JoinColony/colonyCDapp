f5173c962ddb7c464ea6f9e4a4e488badf31385c
William Martin <williamajmartin@gmail.com>
2023-03-16 14:39:08 +0100

    Set: Decimal toExpPos

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactModal from 'react-modal';
 import { Logger } from 'ethers/lib.esm/utils';
+import Decimal from 'decimal.js';
 
 import './styles/main.css';
 
 import Entry from './Entry';
 import store from '~redux/createReduxStore';
+
+Decimal.set({ toExpPos: 78 }); // copied from dapp, originally in staking widget.
 
 Logger.setLogLevel(Logger.levels.ERROR);
 
