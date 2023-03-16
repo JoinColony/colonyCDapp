10297d6a8a4fe92b395591c7b74276ca57514cb9
Juliana Halko <julianayasnog@gmail.com>
2023-03-16 16:02:20 -0500

    Refactor: fix merge issue

--- scripts/lambda-functions-dependencies.sh
+++ scripts/lambda-functions-dependencies.sh
@@ -6,6 +6,9 @@ for d in amplify/backend/function/*/ ; do
     [ -L "${d%/}" ] && continue
     fnName=$(echo ${d} | cut -c 26- | rev | cut -c 2- | rev)
     cd "${pwd}/${d}src"
+    # Skip if the folder doesn't contain package.json
+    # Otherwise npm will look for the root package.json and cause an infinite loop
+    !([ -f "package.json" ]) && continue
     echo "Installing dependencies for Lambda Function \"${fnName}\""
     npm i
     echo
