fb4e748a4d925b2df0e32df178759bbccde55f40
William Martin <williamajmartin@gmail.com>
2023-03-20 13:12:59 +0100

    Convert: slider to use hook form

--- src/components/shared/Slider/Slider.css.d.ts
+++ src/components/shared/Slider/Slider.css.d.ts
@@ -1,3 +1,14 @@
-export const main: string;
-export const primary: string;
-export const danger: string;
+declare namespace SliderCssNamespace {
+  export interface ISliderCss {
+    danger: string;
+    main: string;
+    primary: string;
+  }
+}
+
+declare const SliderCssModule: SliderCssNamespace.ISliderCss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: SliderCssNamespace.ISliderCss;
+};
+
+export = SliderCssModule;
