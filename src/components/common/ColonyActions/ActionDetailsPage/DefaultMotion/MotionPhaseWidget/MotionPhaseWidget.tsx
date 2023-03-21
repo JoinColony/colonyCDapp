54eb26703e0b45c1b15bbda952f8158245d909bb
William Martin <williamajmartin@gmail.com>
2023-03-21 11:16:55 +0100

    feat: pass motion id to staking input

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/MotionPhaseWidget.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/MotionPhaseWidget.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { MotionState } from '@colony/colony-js';
 
+import { ColonyAction } from '~types';
 import StakingWidget from './StakingWidget';
 
 const displayName =
   'common.ColonyActions.ActionDetailsPage.DefaultMotion.MotionPhaseWidget';
 
 interface MotionPhaseWidgetProps {
+  actionData: ColonyAction;
   motionState: MotionState;
 }
 
-const MotionPhaseWidget = ({ motionState }: MotionPhaseWidgetProps) => {
+const MotionPhaseWidget = ({
+  actionData,
+  motionState,
+}: MotionPhaseWidgetProps) => {
+  const { motionData } = actionData;
+
+  if (!motionData) {
+    /*
+     * Will not happen. Undefined motion data will result in the invalid transaction view being
+     * rendered by the parent. But, this is cleaner than creating a custom ColonyAction type to reflect
+     * the fact that motion data is defined here.
+     */
+    return null;
+  }
+
   switch (motionState) {
     case MotionState.Staking: {
-      return <StakingWidget />;
+      return <StakingWidget motionData={motionData} />;
     }
 
     /* Extend with other widgets as they get ported. */
