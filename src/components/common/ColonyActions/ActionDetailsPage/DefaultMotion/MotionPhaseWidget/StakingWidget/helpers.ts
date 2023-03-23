f87434b6d1b0182ca3e34623afbefe30926a3259
William Martin <williamajmartin@gmail.com>
2023-03-23 11:12:42 +0100

    feat: wire in RequiredStakeMessage

---
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/helpers.ts
@@ -0,0 +1,20 @@
+import { BigNumber } from 'ethers';
+
+export const getStakeFromSlider = (
+  sliderAmount: number,
+  remainingToStake: string,
+  userMinStake: string,
+) =>
+  BigNumber.from(sliderAmount)
+    .mul(BigNumber.from(remainingToStake).sub(userMinStake))
+    .div(100)
+    .add(userMinStake);
+
+/* BigNumbers round down, therefore if it's been rounded down to zero, display '1' */
+export const getStakePercentage = (
+  stake: BigNumber,
+  remainingToStake: string,
+) =>
+  stake.mul(100).div(remainingToStake).isZero()
+    ? '1'
+    : stake.mul(100).div(remainingToStake).toString();
