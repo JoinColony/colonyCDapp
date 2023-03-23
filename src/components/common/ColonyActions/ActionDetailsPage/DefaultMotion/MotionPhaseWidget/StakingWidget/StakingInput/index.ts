0d4c4413ff3885015749fc00e94c35124cea5e72
William Martin <williamajmartin@gmail.com>
2023-03-23 11:27:27 +0100

    chore: tidy required stake message imports

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/index.ts
@@ -1,4 +1,4 @@
-export { default } from './StakingInput';
+export { default, SLIDER_AMOUNT_KEY } from './StakingInput';
 export { default as StakingControls } from './StakingControls';
 export { default as StakingSliderAnnotation } from './StakingSliderAnnotation';
 export { default as StakingSliderDescription } from './StakingSliderDescription';
