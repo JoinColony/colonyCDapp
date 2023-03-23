1a88c95ffd3b3ede36ef4185ce0622fd509be5d0
William Martin <williamajmartin@gmail.com>
2023-03-23 11:23:36 +0100

    chore: tidy staking input imports

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/index.ts
@@ -1,1 +1,5 @@
 export { default } from './StakingInput';
+export { default as StakingControls } from './StakingControls';
+export { default as StakingSliderAnnotation } from './StakingSliderAnnotation';
+export { default as StakingSliderDescription } from './StakingSliderDescription';
+export { default as StakingWidgetSlider } from './StakingWidgetSlider';
