117abc7a961dee3dda29d2fc40c2ae655c1a46aa
Raul Glogovetan <raul@glogovetan.com>
2023-03-23 18:16:51 +0200

    TEMP: disable local ganache client logic for initial qa

--- src/redux/sagas/utils/getNetworkClient.ts
+++ src/redux/sagas/utils/getNetworkClient.ts
@@ -8,7 +8,7 @@ import { providers } from 'ethers';
 
 import { DEFAULT_NETWORK } from '~constants';
 import { ContextModule, getContext } from '~context';
-import { Network, ColonyJSNetworkMapping } from '~types';
+import { ColonyJSNetworkMapping } from '~types';
 
 /*
  * Return an initialized ColonyNetworkClient instance.
@@ -24,19 +24,19 @@ export default function* getNetworkClient() {
 
   const signer = walletProvider.getSigner();
 
-  let reputationOracleUrl = new URL(`/reputation`, window.location.origin);
+  const reputationOracleUrl = new URL(`/reputation`, window.location.origin);
 
-  if (DEFAULT_NETWORK === Network.Ganache) {
-    reputationOracleUrl = new URL(`/reputation`, 'http://localhost:3001');
-    const {
-      etherRouterAddress: networkAddress,
-      // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require, import/no-dynamic-require
-    } = require('../../../../amplify/mock-data/colonyNetworkArtifacts/etherrouter-address.json');
-    return yield call(getColonyNetworkClient, ColonyJSNetwork.Custom, signer, {
-      networkAddress,
-      reputationOracleEndpoint: reputationOracleUrl.href,
-    });
-  }
+  // if (network === Network.Ganache) {
+  //   reputationOracleUrl = new URL(`/reputation`, 'http://localhost:3001');
+  //   const {
+  //     etherRouterAddress: networkAddress,
+  //     // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require, import/no-dynamic-require
+  //   } = require('../../../../amplify/mock-data/colonyNetworkArtifacts/etherrouter-address.json');
+  //   return yield call(getColonyNetworkClient, ColonyJSNetwork.Custom, signer, {
+  //     networkAddress,
+  //     reputationOracleEndpoint: reputationOracleUrl.href,
+  //   });
+  // }
 
   return yield call(
     getColonyNetworkClient,
