807ea407c548a711a2210456cf53801f2cb3f421
William Martin <williamajmartin@gmail.com>
2023-03-24 15:07:39 +0100

    feat: add useTotalStakeRadios hook

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
@@ -1,3 +1,3 @@
-export { default } from './GroupedTotalStake';
+export { default, StakeSide } from './GroupedTotalStake';
 export { default as GroupedTotalStakeHeading } from './GroupedTotalStakeHeading';
 export { default as TotalStakeRadios } from './TotalStakeRadios';
