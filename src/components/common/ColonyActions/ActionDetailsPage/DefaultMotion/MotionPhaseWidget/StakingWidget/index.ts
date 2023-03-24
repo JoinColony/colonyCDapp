097b1daa7b3f0a19b691698aced798611fff9315
William Martin <williamajmartin@gmail.com>
2023-03-24 15:07:39 +0100

    feat: add useTotalStakeRadios hook

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/index.ts
@@ -1,4 +1,5 @@
 export { default } from './StakingWidget';
 export { StakingSlider, getStakingTransformFn } from './StakingInput';
+export { StakeSide } from './GroupedTotalStake';
 export * from './StakingWidgetProvider';
 export * from './helpers';
