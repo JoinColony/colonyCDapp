c2cf68785a2c0a8a4e262d4cc333c417a0f7b414
William Martin <williamajmartin@gmail.com>
2023-03-24 15:10:38 +0100

    feat: add SubmitButton

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
@@ -1,3 +1,4 @@
 export { default, StakeSide } from './GroupedTotalStake';
 export { default as GroupedTotalStakeHeading } from './GroupedTotalStakeHeading';
 export { default as TotalStakeRadios } from './TotalStakeRadios';
+export { default as SubmitButton } from './SubmitButton';
