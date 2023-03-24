2f52591642c8869533bc33d4d297d9646b611186
William Martin <williamajmartin@gmail.com>
2023-03-24 15:53:26 +0100

    feat: add back button

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/StakingControls.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/StakingControls.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { useStakingWidgetContext } from '../../StakingWidgetProvider';
 
-import ObjectButton from './ObjectButton';
-import StakeButton from './StakeButton';
+import { BackButton, ObjectButton, StakeButton } from '.';
 
 import styles from './StakingControls.css';
 
@@ -9,19 +9,18 @@ const displayName =
   'common.ColonyActions.ActionDetailsPage.DefaultMotion.StakingWidget.StakingWidgetControls';
 
 const StakingControls = () => {
+  const {
+    motionStakes: {
+      raw: { nay: nayStakes },
+    },
+  } = useStakingWidgetContext();
+  const showBackButton = nayStakes !== '0';
+
   return (
     <div className={styles.buttonGroup}>
-      {/*
-      {showBackButton && (
-        <Button
-          appearance={{ theme: 'secondary', size: 'medium' }}
-          text={{ id: 'button.back' }}
-          onClick={() => setIsSummary(true)}
-        />
-      )}
-      */}
+      {showBackButton && <BackButton />}
       <StakeButton />
-      <ObjectButton />
+      {!showBackButton && <ObjectButton />}
       {/*
       {showActivateButton && <ActivateButton />}
       */}
