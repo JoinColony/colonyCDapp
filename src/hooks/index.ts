9b96bb146a0b671e38c042adb474dae38532c81d
William Martin <williamajmartin@gmail.com>
2023-03-24 14:48:31 +0100

    refactor: extract object button logic to hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -53,6 +53,7 @@ export { default as useColonyFundsClaims } from './useColonyFundsClaims';
 export { default as useCurrentSelectedToken } from './useCurrentSelectedToken';
 export { default as useTokenTotalBalance } from './useTokenTotalBalance';
 export { default as useStakingSlider } from './useStakingSlider';
+export { default as useObjectButton } from './useObjectButton';
 /* Used in cases where we need to memoize the transformed output of any data.
  * Transform function has to be pure, obviously
  */
