c0e2b5c2f3c05e4b053f352ba0f94a4802994be6
William Martin <williamajmartin@gmail.com>
2023-03-29 15:51:25 +0200

    feat: add ClaimWidget config

---
+++ src/hooks/useClaimWidgetConfig.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { BigNumber } from 'ethers';
+
+import Numeral from '~shared/Numeral';
+import { intl } from '~utils/intl';
+import { MotionData } from '~types';
+import { ClaimAllMotionRewardsPayload } from '~redux/sagas/motions/claimMotionRewards';
+import Button, { ActionButton } from '~shared/Button';
+import { ActionTypes } from '~redux';
+import { useAppContext, useColonyContext } from '~hooks';
+
+const { formatMessage } = intl({
+  'label.stake': 'Stake',
+  'label.winnings': 'Winnings',
+  'label.total': 'Total',
+  'label.claim': 'Claim your tokens',
+});
+
+const useClaimWidgetConfig = (
+  { motionId, usersStakes, stakerRewards }: MotionData,
+  startPollingAction: (pollInterval: number) => void,
+) => {
+  const { user } = useAppContext();
+  const { colony } = useColonyContext();
+
+  const userAddress = user?.walletAddress;
+  const colonyAddress = colony?.colonyAddress;
+  const nativeTokenDecimals = colony?.nativeToken.decimals;
+  const nativeTokenSymbol = colony?.nativeToken.symbol;
+
+  const userStake = usersStakes.find(({ address }) => address === userAddress);
+  const stakerReward = stakerRewards.find(
+    ({ address }) => address === userAddress,
+  );
+  const claimItem = {
+    label: formatMessage({ id: 'label.claim' }),
+    item: (
+      <Button
+        appearance={{ theme: 'primary', size: 'medium' }}
+        text={{ id: 'button.claim' }}
+        disabled
+      />
+    ),
+  };
+
+  const config = [claimItem];
+
+  // Return basic view if we have insufficient data
+
+  if (!userStake || !stakerReward || !userAddress || !colonyAddress) {
+    return config;
+  }
+
+  const {
+    rewards: { yay: yayReward, nay: nayReward },
+    isClaimed,
+  } = stakerReward;
+
+  const {
+    stakes: {
+      raw: { nay: nayStakes, yay: yayStakes },
+    },
+  } = userStake;
+
+  const totals = BigNumber.from(yayReward).add(nayReward);
+  const userTotalStake = BigNumber.from(nayStakes).add(yayStakes);
+  const userWinnings = totals.sub(userTotalStake);
+
+  // Else, return full widget
+  const buttonTextId = isClaimed ? 'button.claimed' : 'button.claim';
+  const canClaimStakes = !userTotalStake.isZero() && !isClaimed;
+
+  const claimPayload = {
+    userAddress,
+    colonyAddress,
+    motionIds: [BigNumber.from(motionId)],
+  } as ClaimAllMotionRewardsPayload;
+
+  claimItem.item = (
+    <ActionButton
+      actionType={ActionTypes.MOTION_CLAIM}
+      values={claimPayload}
+      appearance={{ theme: 'primary', size: 'medium' }}
+      text={{ id: buttonTextId }}
+      disabled={!canClaimStakes}
+      dataTest="claimStakeButton"
+      onSuccess={() => startPollingAction(1000)}
+    />
+  );
+
+  const stakeItem = {
+    label: formatMessage({ id: 'label.stake' }),
+    item: (
+      <Numeral
+        value={userTotalStake}
+        decimals={nativeTokenDecimals}
+        suffix={nativeTokenSymbol}
+      />
+    ),
+  };
+
+  const winningsItem = {
+    label: formatMessage({ id: 'label.winnings' }),
+    item: (
+      <Numeral
+        value={userWinnings}
+        decimals={nativeTokenDecimals}
+        suffix={nativeTokenSymbol}
+      />
+    ),
+  };
+
+  const totalItem = {
+    label: formatMessage({ id: 'label.total' }),
+    item: (
+      <Numeral
+        value={totals}
+        decimals={nativeTokenDecimals}
+        suffix={nativeTokenSymbol}
+      />
+    ),
+  };
+
+  config.push(stakeItem, winningsItem, totalItem);
+
+  return config;
+};
+
+export default useClaimWidgetConfig;
