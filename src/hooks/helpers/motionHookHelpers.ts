7d9c1ae44987f6176783fd96454a40dc38faae51
William Martin <williamajmartin@gmail.com>
2023-03-29 15:51:54 +0200

    feat: port claim widget ui

--- src/hooks/helpers/motionHookHelpers.ts
+++ src/hooks/helpers/motionHookHelpers.ts
@@ -1,8 +1,8 @@
 import { BigNumber } from 'ethers';
 import { getStakeFromSlider } from '~common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget';
-import { MotionStakes, StakerRewards } from '~gql';
+import { MotionStakes } from '~gql';
 import { Action, ActionTypes } from '~redux';
-import { Address, SetStateFn } from '~types';
+import { SetStateFn } from '~types';
 import { mapPayload } from '~utils/actions';
 
 type StakeMotionPayload = Action<ActionTypes.MOTION_STAKE>['payload'];
@@ -49,14 +49,3 @@ export const getHandleStakeSuccessFn =
     /* On stake success, initiate db polling so ui updates */
     startPolling(1000);
   };
-
-export const isMotionClaimed = (
-  stakerRewards: StakerRewards[],
-  walletAddress: Address,
-) => {
-  const userReward = stakerRewards.find(
-    ({ address }) => address === walletAddress,
-  );
-
-  return !!userReward?.isClaimed;
-};
