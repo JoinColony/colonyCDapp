b5a9ddf9c4bb56117a6daff7735e66d629b9f0f8
William Martin <williamajmartin@gmail.com>
2023-04-03 10:17:40 +0200

    chore: move enum to motions types file

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/GroupedTotalStake/index.ts
@@ -1,4 +1,4 @@
-export { default, StakeSide } from './GroupedTotalStake';
+export { default } from './GroupedTotalStake';
 export { default as GroupedTotalStakeHeading } from './GroupedTotalStakeHeading';
 export { default as TotalStakeRadios } from './TotalStakeRadios';
 export { default as SubmitButton } from './SubmitButton';
