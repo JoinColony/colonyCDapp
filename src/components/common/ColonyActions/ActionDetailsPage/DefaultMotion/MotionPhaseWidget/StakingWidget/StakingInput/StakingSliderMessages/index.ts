c887f8f2d9cbabe79c01780ec7b542ac00b5f7e6
William Martin <williamajmartin@gmail.com>
2023-04-04 14:43:15 +0200

    feat: port minimum stake message

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingSliderMessages/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingSliderMessages/index.ts
@@ -2,3 +2,5 @@ export {
   default as RequiredStakeMessage,
   RequiredStakeMessageProps,
 } from './RequiredStakeMessage';
+export { default as StakingValidationMessage } from './StakingValidationMessage';
+export { default as MinimumStakeMessage } from './MinimumStakeMessage';
