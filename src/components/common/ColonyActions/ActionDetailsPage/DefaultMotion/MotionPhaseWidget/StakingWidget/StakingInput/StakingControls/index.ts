ed8a730e95c5a9ce8334aa31112d73a4031af254
William Martin <williamajmartin@gmail.com>
2023-04-05 11:23:27 +0200

    feat: wire in activate button

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/index.ts
@@ -2,3 +2,4 @@ export { default } from './StakingControls';
 export { default as BackButton } from './BackButton';
 export { default as ObjectButton } from './ObjectButton';
 export { default as StakeButton } from './StakeButton';
+export { default as ActivateButton } from './ActivateButton';
