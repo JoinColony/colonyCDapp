c2620569ca9d1342557ef7debcebe0088cd36ade
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-05 14:36:35 +0200

    ui/15399211-add-tailwing

---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          gray: {
+            25: "#FCFCFD",
+            50: "#F9FAFB",
+            100: "#F2F4F7",
+            200: "#E4E7EC",
+            300: "#D0D5DD",
+            400: "#98A2B3",
+            500: "#667085",
+            600: "#475467",
+            700: "#344054",
+            900: "#101828",
+          },
+          base: {
+            white: "#ffffff",
+            black: "#000000",
+            bg: "#F9FBFC",
+            sprite: "#343434",
+          },
+          blue: {
+            100: "#EFF8FF",
+            200: "#82B1FF",
+            300: "#448AFF",
+            400: "#2962FF",
+          },
+          blueLight: {
+            100: "#E0F2FE",
+            200: "#91D2FA",
+          },
+          pink: {
+            100: "#FCE7F6",
+            200: "#FFBBDC",
+          },
+        },
+        functional: {
+          nagative: {
+            100: "#FEF3F2",
+            200: "#FEE4E2",
+            300: "#F97066",
+            400: "#D92D20",
+          },
+          warning: {
+            100: "#FFFAEB",
+            200: "#FEF0C7",
+            400: "#F79009",
+          },
+          success: {
+            100: "#ECFDF3",
+            200: "#D1FADF",
+            400: "#039855",
+          },
+        },
+        accent: {
+          indigo: {
+            100: "#EEF4FF",
+            200: "#A4BCFD",
+            400: "#444CE7",
+          },
+          purple: {
+            100: "#F4F3FF",
+            200: "#9B8AFB",
+            400: "#6938EF",
+          },
+        },
+      },
+    },
+  },
+};
