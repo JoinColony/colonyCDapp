6ecab3d099b4d1dde9d5e4da923c0798cf79fd5c
William Martin <williamajmartin@gmail.com>
2023-04-05 13:19:29 +0200

    chore: export useStakingControls in index

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/StakingWidget/StakingInput/StakingControls/index.ts
@@ -3,3 +3,4 @@ export { default as BackButton } from './BackButton';
 export { default as ObjectButton } from './ObjectButton';
 export { default as StakeButton } from './StakeButton';
 export { default as ActivateButton } from './ActivateButton';
+export { default as useStakingControls } from './useStakingControls';
