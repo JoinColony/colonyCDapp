3d917ea3ff1cb211754e6283484f38d2dd31085e
William Martin <williamajmartin@gmail.com>
2023-04-06 20:03:14 +0200

    chore: refactor messages in useVoteDetailsConfig

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VotingWidget/useVoteDetailsConfig.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VotingWidget/useVoteDetailsConfig.tsx
@@ -1,89 +1,87 @@
+import { BigNumber } from 'ethers';
 import React from 'react';
-import { defineMessages, MessageDescriptor } from 'react-intl';
+
 import { useAppContext, useColonyContext, useUserReputation } from '~hooks';
 import { DetailItemProps } from '~shared/DetailsWidget';
 import MemberReputation from '~shared/MemberReputation';
-import { MotionState } from '~utils/colonyMotions';
-import { formatText } from '~utils/intl';
-import VoteReward from './VoteReward';
+import { intl } from '~utils/intl';
 
-const MSG = defineMessages({
-  votingMethodLabel: {
-    id: 'dashboard.ActionsPage.VoteWidget.votingMethodLabel',
-    defaultMessage: `Voting method`,
-  },
-  votingMethodValue: {
-    id: 'dashboard.ActionsPage.VoteWidget.votingMethodValue',
-    defaultMessage: `Reputation-weighted`,
-  },
-  votingMethodTooltip: {
-    id: 'dashboard.ActionsPage.VoteWidget.votingMethodTooltip',
-    defaultMessage: `Votes are weighted by reputation in the team in which the vote is happening.`,
-  },
-  reputationTeamLabel: {
-    id: 'dashboard.ActionsPage.VoteWidget.reputationTeamLabel',
-    defaultMessage: `Reputation in team`,
-  },
-  reputationTeamTooltip: {
-    id: 'dashboard.ActionsPage.VoteWidget.reputationTeamTooltip',
-    defaultMessage: `This is the % of the reputation you have in this team.`,
-  },
-  rewardLabel: {
-    id: 'dashboard.ActionsPage.VoteWidget.rewardLabel',
-    defaultMessage: `Reward`,
-  },
-  rewardTooltip: {
-    id: 'dashboard.ActionsPage.VoteWidget.rewardTooltip',
-    defaultMessage: `This is the range of values between which your reward for voting will be, subject to the number of people that participate in the vote.`,
-  },
-  rulesLabel: {
-    id: 'dashboard.ActionsPage.VoteWidget.rulesLabel',
-    defaultMessage: `Rules`,
-  },
-  rulesTooltip: {
-    id: 'dashboard.ActionsPage.VoteWidget.rulesTooltip',
-    defaultMessage: `Votes are secret and must be revealed at the end of the voting period to count.`,
-  },
+import { VoteDetailsProps, VoteButton, VoteReward } from '.';
+
+const { formatMessage } = intl({
+  'label.votingMethod': 'Voting method',
+  'value.votingMethod': 'Reputation-weighted',
+  'tooltip.votingMethod':
+    'Votes are weighted by reputation in the team in which the vote is happening.',
+  'label.reputationTeam': 'Reputation in team',
+  'tooltip.reputationTeam':
+    'This is the % of the reputation you have in this team.',
+  'label.reward': 'Reward',
+  'tooltip.reward': `This is the range of values between which your reward for voting will be, subject to the number of people that participate in the vote.`,
+  'label.rules': 'Rules',
+  'tooltip.rules':
+    'Votes are secret and must be revealed at the end of the voting period to count.',
 });
 
 interface VoteDetailsConfig extends DetailItemProps {
-  label: MessageDescriptor;
+  label: string;
 }
-export const useVoteDetailsConfig = (
-  motionState: MotionState,
-  button: JSX.Element,
-): VoteDetailsConfig[] => {
+
+export const useVoteDetailsConfig = ({
+  //  motionState,
+  motionId,
+  motionDomainId,
+}: // hasUserVoted,
+VoteDetailsProps): VoteDetailsConfig[] => {
   const { user } = useAppContext();
   const { colony } = useColonyContext();
   const { userReputation, totalReputation } = useUserReputation(
     colony?.colonyAddress ?? '',
     user?.walletAddress ?? '',
+    Number(motionDomainId),
   );
-  return [
+  const hasReputationToVote = BigNumber.from(userReputation ?? 0).gt(0);
+
+  const config = [
     {
-      label: MSG.votingMethodLabel,
-      tooltipText: MSG.votingMethodTooltip,
-      item: formatText(MSG.votingMethodValue),
+      label: formatMessage({ id: 'label.votingMethod' }),
+      tooltipText: formatMessage({ id: 'tooltip.votingMethod' }),
+      item: formatMessage({ id: 'value.votingMethod' }),
     },
     {
-      label: MSG.reputationTeamLabel,
-      tooltipText: MSG.reputationTeamTooltip,
-      item: (
-        <MemberReputation
-          userReputation={userReputation}
-          totalReputation={totalReputation}
-        />
-      ),
-    },
-    {
-      label: MSG.rewardLabel,
-      tooltipText: MSG.rewardTooltip,
-      item: <VoteReward motionState={motionState} />,
-    },
-    {
-      label: MSG.rulesLabel,
-      tooltipText: MSG.rulesTooltip,
-      item: button,
+      label: formatMessage({ id: 'label.rules' }),
+      tooltipText: formatMessage({ id: 'tooltip.rules' }),
+      item: <VoteButton />,
     },
   ];
+
+  if (hasReputationToVote) {
+    config.splice(
+      1,
+      0,
+      {
+        label: formatMessage({ id: 'label.reputationTeam' }),
+        tooltipText: formatMessage({ id: 'tooltip.reputationTeam' }),
+        item: (
+          <MemberReputation
+            userReputation={userReputation}
+            totalReputation={totalReputation}
+          />
+        ),
+      },
+      {
+        label: formatMessage({ id: 'label.reward' }),
+        tooltipText: formatMessage({ id: 'tooltip.reward' }),
+        item: (
+          <VoteReward
+            //  motionState={motionState}
+            motionId={motionId}
+            motionDomainId={motionDomainId}
+          />
+        ),
+      },
+    );
+  }
+
+  return config;
 };
