876ddbb910cfe1e8b1434550b8f9dd1231cb636c
William Martin <williamajmartin@gmail.com>
2023-04-06 19:33:45 +0200

    feat: add lambda for voter reward range

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -676,6 +676,14 @@ export type GetUserTokenBalanceReturn = {
   pendingBalance?: Maybe<Scalars['String']>;
 };
 
+export type GetVoterRewardsInput = {
+  colonyAddress: Scalars['String'];
+  motionDomainId: Scalars['String'];
+  motionId: Scalars['String'];
+  rootHash: Scalars['String'];
+  voterAddress: Scalars['String'];
+};
+
 export type MembersForColonyInput = {
   colonyAddress: Scalars['String'];
   domainId?: InputMaybe<Scalars['Int']>;
@@ -1475,6 +1483,7 @@ export type MotionData = {
   stakerRewards: Array<StakerRewards>;
   userMinStake: Scalars['String'];
   usersStakes: Array<UserStakes>;
+  voterRecord: Array<VoterRecord>;
 };
 
 export type MotionDataInput = {
@@ -1490,6 +1499,7 @@ export type MotionDataInput = {
   stakerRewards: Array<StakerRewardsInput>;
   userMinStake: Scalars['String'];
   usersStakes: Array<UserStakesInput>;
+  voterRecord: Array<VoterRecordInput>;
 };
 
 export type MotionStakeValues = {
@@ -1947,6 +1957,7 @@ export type Query = {
   getUserReputation?: Maybe<Scalars['String']>;
   getUserTokenBalance?: Maybe<GetUserTokenBalanceReturn>;
   getUserTokens?: Maybe<UserTokens>;
+  getVoterRewards?: Maybe<VoterRewardsReturn>;
   getWatchedColonies?: Maybe<WatchedColonies>;
   listColonies?: Maybe<ModelColonyConnection>;
   listColonyActions?: Maybe<ModelColonyActionConnection>;
@@ -2164,6 +2175,11 @@ export type QueryGetUserTokenBalanceArgs = {
 
 export type QueryGetUserTokensArgs = {
   id: Scalars['ID'];
+};
+
+
+export type QueryGetVoterRewardsArgs = {
+  input?: InputMaybe<GetVoterRewardsInput>;
 };
 
 
@@ -2836,6 +2852,24 @@ export type UserTokens = {
   updatedAt: Scalars['AWSDateTime'];
   user: User;
   userID: Scalars['ID'];
+};
+
+export type VoterRecord = {
+  __typename?: 'VoterRecord';
+  address: Scalars['String'];
+  voteCount: Scalars['String'];
+};
+
+export type VoterRecordInput = {
+  address: Scalars['String'];
+  voteCount: Scalars['String'];
+};
+
+export type VoterRewardsReturn = {
+  __typename?: 'VoterRewardsReturn';
+  max: Scalars['String'];
+  min: Scalars['String'];
+  reward: Scalars['String'];
 };
 
 export type WatchedColonies = {
