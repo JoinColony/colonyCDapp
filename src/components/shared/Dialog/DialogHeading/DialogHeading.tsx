99d242c4f8a98dddc75725fdba9abd17a152cb29
ArmandoGraterol <armandogrtrl@gmail.com>
2023-04-06 14:36:13 -0300

    Updated DialogHeading with voting reputation components and logic

--- src/components/shared/Dialog/DialogHeading/DialogHeading.tsx
+++ src/components/shared/Dialog/DialogHeading/DialogHeading.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from 'react';
 import { MessageDescriptor } from 'react-intl';
+import { Id } from '@colony/colony-js';
 
+import { useFormContext } from 'react-hook-form';
 import { Heading3 } from '~shared/Heading';
-import { UniversalMessageValues } from '~types';
+import { Colony, UniversalMessageValues } from '~types';
 
-// import { ForceToggle } from '~shared/Fields';
-// import MotionDomainSelect from '~dashboard/MotionDomainSelect';
+import ForceToggle from './ForceToggle';
+import MotionDomainSelect from './MotionDomainSelect';
 
 import styles from './DialogHeading.css';
 
@@ -13,50 +15,56 @@ const displayName = 'DialogHeading';
 
 interface Props {
   title: MessageDescriptor;
+  userHasPermission: boolean;
+  colony: Colony;
+  isVotingExtensionEnabled: boolean;
+  isRootMotion?: boolean;
   titleValues?: UniversalMessageValues;
   children?: ReactNode;
 }
 
-const DialogHeading = ({ title, titleValues, children }: Props) => {
-  // const handleFilterMotionDomains = (optionDomain) => {
-  //     const optionDomainId = parseInt(optionDomain.value, 10);
-  //     if (currentFromDomain === Id.RootDomain) {
-  //       return optionDomainId === Id.RootDomain;
-  //     }
-  //     return (
-  //       optionDomainId === currentFromDomain ||
-  //       optionDomainId === Id.RootDomain
-  //     );
-  //   };
-
-  // const handleMotionDomainChange = (motionDomainId) => setFieldValue('motionDomainId', motionDomainId);
+const DialogHeading = ({
+  colony,
+  title,
+  titleValues,
+  children,
+  userHasPermission,
+  isVotingExtensionEnabled,
+  isRootMotion,
+}: Props) => {
+  const {
+    watch,
+    formState: { isSubmitting },
+  } = useFormContext();
+  const { fromDomainId } = watch();
+  const handleFilterMotionDomains = (optionDomain) => {
+    const optionDomainId = parseInt(optionDomain.value, 10);
+    if (fromDomainId === Id.RootDomain) {
+      return optionDomainId === Id.RootDomain;
+    }
+    return optionDomainId === fromDomainId || optionDomainId === Id.RootDomain;
+  };
 
   return (
     <div className={styles.modalHeading}>
-      {/*
-       * @NOTE We can only create a motion to vote in a subdomain if we
-       * create a payment from that subdomain
-       */}
-      {/* {isVotingExtensionEnabled && (
+      {isVotingExtensionEnabled && (
         <div className={styles.motionVoteDomain}>
           <MotionDomainSelect
             colony={colony}
-            onDomainChange={handleMotionDomainChange}
-            disabled={values.forceAction || isSubmitting}
+            disabled={isRootMotion || isSubmitting}
             filterDomains={handleFilterMotionDomains}
-            initialSelectedDomain={domainId}
           />
         </div>
-      )} */}
+      )}
       <div className={styles.headingContainer}>
         <Heading3
           appearance={{ size: 'medium', margin: 'none', theme: 'dark' }}
           text={title}
           textValues={titleValues}
         />
-        {/* {hasRoles && isVotingExtensionEnabled && (
-          <ForceToggle disabled={!canMakePayment || isSubmitting} />
-        )} */}
+        {userHasPermission && isVotingExtensionEnabled && (
+          <ForceToggle disabled={isSubmitting} />
+        )}
       </div>
       {children}
     </div>
