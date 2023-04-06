718d66ef923767a9fea1e5a83031243374f49626
William Martin <williamajmartin@gmail.com>
2023-04-06 13:07:07 +0200

    feat: port voting widget

---
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VotingWidget/VotingWidget.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { object, number, InferType } from 'yup';
+
+import { ActionHookForm as ActionForm } from '~shared/Fields';
+import { ActionTypes } from '~redux';
+
+import styles from './VotingWidget.css';
+import VotingWidgetHeading from './VotingWidgetHeading';
+import { ColonyActionType } from '~gql';
+import VoteButton from './VoteButton';
+import { MotionData } from '~types';
+import { MotionState } from '~utils/colonyMotions';
+import VoteDetails from './VoteDetails';
+import VotingPanel from './VotingPanel';
+
+const displayName =
+  'common.ColonyActions.ActionDetailsPage.DefaultMotion.VotingWidget';
+
+const MSG = defineMessages({
+  voteHidden: {
+    id: `${displayName}.voteHidden`,
+    defaultMessage: `Your vote is hidden from others.\nYou can change your vote.`,
+  },
+});
+
+const validationSchema = object()
+  .shape({
+    vote: number().required(),
+  })
+  .defined();
+
+type VotingFormValues = InferType<typeof validationSchema>;
+
+interface VotingWidgetProps {
+  actionType: ColonyActionType;
+  motionData: MotionData;
+  motionState: MotionState;
+}
+
+export const VOTE_FORM_KEY = 'vote';
+
+const VotingWidget = ({
+  actionType,
+  // motionData: { motionId },
+  motionState,
+}: VotingWidgetProps) => {
+  const hasUserVoted = false;
+  return (
+    <>
+      {hasUserVoted && (
+        <p className={styles.voteHiddenContainer}>
+          <FormattedMessage {...MSG.voteHidden} />
+        </p>
+      )}
+      <ActionForm<VotingFormValues>
+        defaultValues={{
+          [VOTE_FORM_KEY]: undefined,
+        }}
+        validationSchema={validationSchema}
+        actionType={ActionTypes.MOTION_VOTE}
+        // transform={transform}
+        // onSuccess={handleSuccess}
+      >
+        <div className={styles.main}>
+          <VotingWidgetHeading actionType={actionType} />
+          <VotingPanel />
+          <VoteDetails
+            // motionId={motionId}
+            motionState={motionState}
+            // showReward={hasReputationToVote}
+            button={<VoteButton />}
+          />
+        </div>
+      </ActionForm>
+    </>
+  );
+};
+
+VotingWidget.displayName = displayName;
+
+export default VotingWidget;
