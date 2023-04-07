d76ca7a65580169da7fc13889959b9c5d0084e8c
William Martin <williamajmartin@gmail.com>
2023-04-07 16:43:01 +0200

    feat: use real data for voting widget

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VotingWidget/VotingWidget.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VotingWidget/VotingWidget.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import { object, number, InferType } from 'yup';
+import { BigNumber } from 'ethers';
 
-import { ActionHookForm as ActionForm } from '~shared/Fields';
+import { ActionHookForm as ActionForm, OnSuccess } from '~shared/Fields';
 import { ActionTypes } from '~redux';
 
 import { ColonyActionType } from '~gql';
 import { MotionData } from '~types';
 import { MotionState } from '~utils/colonyMotions';
-import { VoteDetails, VotingPanel, VotingWidgetHeading } from '.';
+import { useAppContext, useColonyContext } from '~hooks';
+import { mapPayload } from '~utils/actions';
+import { MotionVotePayload } from '~redux/sagas/motions/voteMotion';
+
+import { PollingControls } from '../MotionPhaseWidget';
+
+import {
+  VoteDetails,
+  VotingPanel,
+  useVotingWidgetUpdate,
+  VotingWidgetHeading,
+} from '.';
 
 import styles from './VotingWidget.css';
 
@@ -30,7 +42,7 @@ const validationSchema = object()
 
 type VotingFormValues = InferType<typeof validationSchema>;
 
-interface VotingWidgetProps {
+interface VotingWidgetProps extends PollingControls {
   actionType: ColonyActionType;
   motionData: MotionData;
   motionState: MotionState;
@@ -40,12 +52,36 @@ export const VOTE_FORM_KEY = 'vote';
 
 const VotingWidget = ({
   actionType,
-  motionData: { motionId, motionDomainId },
+  motionData: { motionDomainId, motionId, voterRecord },
   motionState,
+  startPollingAction,
+  stopPollingAction,
 }: VotingWidgetProps) => {
-  const hasUserVoted = false;
+  const { colony } = useColonyContext();
+  const { user } = useAppContext();
+  const { hasUserVoted, setHasUserVoted } = useVotingWidgetUpdate(
+    voterRecord,
+    stopPollingAction,
+  );
+
+  const transform = mapPayload(
+    ({ vote }) =>
+      ({
+        colonyAddress: colony?.colonyAddress ?? '',
+        userAddress: user?.walletAddress,
+        vote: Number(vote),
+        motionId: BigNumber.from(motionId),
+      } as MotionVotePayload),
+  );
+
+  const handleSuccess: OnSuccess<VotingFormValues> = (_, { reset }) => {
+    setHasUserVoted(true);
+    reset();
+    startPollingAction(1000);
+  };
+
   return (
-    <>
+    <div>
       {hasUserVoted && (
         <p className={styles.voteHiddenContainer}>
           <FormattedMessage {...MSG.voteHidden} />
@@ -57,21 +93,21 @@ const VotingWidget = ({
         }}
         validationSchema={validationSchema}
         actionType={ActionTypes.MOTION_VOTE}
-        // transform={transform}
-        // onSuccess={handleSuccess}
+        transform={transform}
+        onSuccess={handleSuccess}
       >
         <div className={styles.main}>
           <VotingWidgetHeading actionType={actionType} />
-          <VotingPanel />
+          <VotingPanel motionDomainId={Number(motionDomainId)} />
           <VoteDetails
             motionId={motionId}
-            motionState={motionState}
             motionDomainId={motionDomainId}
-            hasUserVoted={false}
+            motionState={motionState}
+            hasUserVoted={hasUserVoted}
           />
         </div>
       </ActionForm>
-    </>
+    </div>
   );
 };
 
