47bbebf876f85a2de3c22bb16474baa068669d52
William Martin <williamajmartin@gmail.com>
2023-04-10 12:55:24 +0200

    feat: wire in reveal btn

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/RevealWidget/RevealWidget.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/RevealWidget/RevealWidget.tsx
@@ -1,55 +1,74 @@
 import React, { useState } from 'react';
+import { BigNumber } from 'ethers';
 
 import { MotionState, MotionVote } from '~utils/colonyMotions';
 import { MotionData } from '~types';
-import { useAppContext } from '~hooks';
-import { ActionHookForm as ActionForm } from '~shared/Fields';
+import { useAppContext, useColonyContext } from '~hooks';
+import { ActionHookForm as ActionForm, OnSuccess } from '~shared/Fields';
 import { ActionTypes } from '~redux';
+import { mapPayload } from '~utils/actions';
+import { RevealMotionPayload } from '~redux/sagas/motions/revealVoteMotion';
 
 import { VoteDetails } from '../VotingWidget';
 import RevealWidgetHeading from './RevealWidgetHeading';
+import { PollingControls } from '../MotionPhaseWidget';
 
 const displayName =
   'common.ColonyActions.ActionDetailsPage.DefaultMotion.RevealWidget';
 
-interface RevealWidgetProps {
+interface RevealWidgetProps extends PollingControls {
   motionData: MotionData;
   motionState: MotionState;
 }
 
 const RevealWidget = ({
-  motionData: { voterRecord },
+  motionData: { voterRecord, motionId },
   motionData,
   motionState,
+  startPollingAction,
+  stopPollingAction,
 }: RevealWidgetProps) => {
+  const { colony } = useColonyContext();
   const { user } = useAppContext();
-  const [userVoteRevealed, setUserVoteRevealed] = useState(false);
   const currentVotingRecord = voterRecord.find(
     ({ address }) => address === user?.walletAddress,
   );
   const hasUserVoted = !!currentVotingRecord;
-  const handleSuccess = () => {
-    setUserVoteRevealed(true);
+  const vote = currentVotingRecord?.vote ?? null;
+  const [prevVote, setPrevVote] = useState(vote);
+
+  if (vote !== prevVote) {
+    stopPollingAction();
+    setPrevVote(vote);
+  }
+
+  const transform = mapPayload(
+    () =>
+      ({
+        colonyAddress: colony?.colonyAddress,
+        userAddress: user?.walletAddress ?? '',
+        motionId: BigNumber.from(motionId),
+      } as RevealMotionPayload),
+  );
+
+  const handleSuccess: OnSuccess<Record<string, any>> = (_, { reset }) => {
+    reset();
+    startPollingAction(1000);
   };
 
   return (
     <ActionForm
       actionType={ActionTypes.MOTION_REVEAL_VOTE}
       onSuccess={handleSuccess}
+      transform={transform}
     >
-      <>
-        <RevealWidgetHeading
-          hasUserVoted={hasUserVoted}
-          userVoteRevealed={userVoteRevealed}
-          vote={MotionVote.Nay} // todo
-        />
-        <VoteDetails
-          motionData={motionData}
-          motionState={motionState}
-          hasUserVoted={hasUserVoted}
-          userVoteRevealed={userVoteRevealed}
-        />
-      </>
+      <RevealWidgetHeading hasUserVoted={hasUserVoted} vote={vote} />
+      <VoteDetails
+        motionData={motionData}
+        motionState={motionState}
+        hasUserVoted={hasUserVoted}
+        userVoteRevealed={!!vote}
+      />
     </ActionForm>
   );
 };
