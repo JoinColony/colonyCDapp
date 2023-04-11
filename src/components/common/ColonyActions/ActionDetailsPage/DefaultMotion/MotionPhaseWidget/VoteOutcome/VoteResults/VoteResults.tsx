eac39dd5ae2dd3bd18bf9fec2483816dce263983
William Martin <williamajmartin@gmail.com>
2023-04-11 13:48:44 +0200

    port: VoteOutcome and subcomponents

---
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VoteOutcome/VoteResults/VoteResults.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { defineMessages } from 'react-intl';
+
+import { MotionData } from '~gql';
+import { MotionVote } from '~utils/colonyMotions';
+
+import VoteResultsItem from './VoteResultsItem';
+
+import styles from './VoteResults.css';
+
+const displayName =
+  'common.ColonyActions.DefaultMotion.FinalizeMotion.VoteResults';
+
+const MSG = defineMessages({
+  voteYAY: {
+    id: `${displayName}.voteYAY`,
+    defaultMessage: `Yes`,
+  },
+  voteNAY: {
+    id: `${displayName}.voteNAY`,
+    defaultMessage: `No!`,
+  },
+  loadingText: {
+    id: `${displayName}.loadingText`,
+    defaultMessage: 'Loading votes results',
+  },
+});
+
+interface VoteResultsProps {
+  revealedVotes: MotionData['revealedVotes'];
+  voterRecord: MotionData['voterRecord'];
+}
+
+const VoteResults = ({
+  revealedVotes: {
+    percentage: { yay: yayVotePercent, nay: nayVotePercent },
+  },
+  voterRecord,
+}: VoteResultsProps) => {
+  const yayVoters = voterRecord.filter(({ vote }) => vote === MotionVote.Yay);
+  const nayVoters = voterRecord.filter(({ vote }) => vote === MotionVote.Nay);
+
+  return (
+    <div className={styles.main}>
+      <VoteResultsItem
+        value={Number(yayVotePercent)}
+        maxValue={100}
+        title={MSG.voteYAY}
+        voters={yayVoters}
+      />
+      <VoteResultsItem
+        value={Number(nayVotePercent)}
+        maxValue={100}
+        title={MSG.voteNAY}
+        appearance={{ theme: 'disapprove' }}
+        voters={nayVoters}
+      />
+    </div>
+  );
+};
+
+VoteResults.displayName = displayName;
+
+export default VoteResults;
---
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/VoteOutcome/VoteResults/helpers.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+
+import { ContextModule, getContext } from '~context';
+import {
+  GetCurrentUserDocument,
+  GetCurrentUserQuery,
+  GetCurrentUserQueryVariables,
+} from '~gql';
+import { Address, User } from '~types';
+
+const getUsers = (
+  addresses: Address[],
+): Promise<(User | null | undefined)[]> => {
+  const apolloClient = getContext(ContextModule.ApolloClient);
+  return Promise.all(
+    addresses.map(async (address) => {
+      /*
+       * Per rules of hooks, can't call the codegen query hook inside a callback.
+       * Hence doing it directly.
+       */
+      const { data } = await apolloClient.query<
+        GetCurrentUserQuery,
+        GetCurrentUserQueryVariables
+      >({
+        query: GetCurrentUserDocument,
+        variables: {
+          address,
+        },
+      });
+      return data.getUserByAddress?.items[0];
+    }),
+  );
+};
+
+export const useGetUsers = (addresses: Address[]) => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const definedUsers = (await getUsers(addresses)).filter((user) =>
+        Boolean(user),
+      ) as User[];
+      setUsers(definedUsers);
+    };
+    fetchUsers();
+  }, [addresses]);
+
+  return users;
+};
