9f05f40fe78ea119a9cc158a8f2137925f2f7806
William Martin <williamajmartin@gmail.com>
2023-04-11 14:15:32 +0200

    fix: sync btn disabled state with user

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/RevealWidget/useRevealWidgetUpdate.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultMotion/MotionPhaseWidget/RevealWidget/useRevealWidgetUpdate.ts
@@ -12,8 +12,18 @@ export const useRevealWidgetUpdate = (
     ({ address }) => address === user?.walletAddress,
   );
   const hasUserVoted = !!currentVotingRecord;
-  const vote = currentVotingRecord?.vote;
+  const vote = currentVotingRecord?.vote ?? null;
   const [prevVote, setPrevVote] = useState(vote);
+  const [userVoteRevealed, setUserVoteRevealed] = useState(vote !== null);
+
+  /* Keep revealed state in sync with user changes */
+  useEffect(() => {
+    if (!user) {
+      setUserVoteRevealed(false);
+    } else {
+      setUserVoteRevealed(typeof vote === 'number');
+    }
+  }, [user, vote]);
 
   /* Vote has been updated in db, stop polling */
   if (vote !== prevVote) {
@@ -23,5 +33,5 @@ export const useRevealWidgetUpdate = (
 
   useEffect(() => stopPollingAction, [stopPollingAction]);
 
-  return { hasUserVoted, vote };
+  return { hasUserVoted, vote, userVoteRevealed, setUserVoteRevealed };
 };
