6790d9fe1849ba7e31184d3aeb298679db3a2937
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-12 12:26:23 +0200

    editor config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,37 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   env: {
     browser: true,
     commonjs: true,
-    es6: true
+    es6: true,
   },
-  extends: ['eslint-config-airbnb/rules/react', 'eslint-config-airbnb/rules/react-a11y', 'plugin:@typescript-eslint/recommended', '@colony/eslint-config-colony', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint-config-airbnb/rules/react',
+    'eslint-config-airbnb/rules/react-a11y',
+    'plugin:@typescript-eslint/recommended',
+    '@colony/eslint-config-colony',
+    'plugin:storybook/recommended',
+  ],
   plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'jsdoc', 'react-hooks'],
-  overrides: [{
-    files: ['**/__tests__/*.test.js', '**/__tests__/*.test.ts', '**/__tests__/*.test.tsx'],
-    plugins: ['jest'],
-    rules: {
-      'max-len': 'off',
-      'no-underscore-dangle': 'off',
-      // eslint-disable-next-line global-require
-      ...require('./.eslintrc.jest.js')
+  overrides: [
+    {
+      files: ['**/__tests__/*.test.js', '**/__tests__/*.test.ts', '**/__tests__/*.test.tsx'],
+      plugins: ['jest'],
+      rules: {
+        'max-len': 'off',
+        'no-underscore-dangle': 'off',
+        // eslint-disable-next-line global-require
+        ...require('./.eslintrc.jest.js'),
+      },
+      env: {
+        jest: true,
+      },
     },
-    env: {
-      jest: true
-    }
-  }],
+  ],
   rules: {
     // Using the typescript-eslint version for these
     'no-unused-vars': 'off',
@@ -34,12 +42,18 @@ module.exports = {
     'no-dupe-class-members': 'off',
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'warn',
-    camelcase: ['error', {
-      allow: ['^TEMP_']
-    }],
-    'eslint-comments/disable-enable-pair': ['error', {
-      allowWholeFile: true
-    }],
+    camelcase: [
+      'error',
+      {
+        allow: ['^TEMP_'],
+      },
+    ],
+    'eslint-comments/disable-enable-pair': [
+      'error',
+      {
+        allowWholeFile: true,
+      },
+    ],
     // @typescript-eslint
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
@@ -54,16 +68,22 @@ module.exports = {
     'jsx-a11y/label-has-for': 'off',
     // react
     'react/default-props-match-prop-types': 'off',
-    'react/jsx-filename-extension': ['warn', {
-      extensions: ['.test.js', '.tsx']
-    }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.test.js', '.tsx'],
+      },
+    ],
     'react/jsx-one-expression-per-line': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-wrap-multilines': 'off',
     'react/require-default-props': 'off',
-    'react/sort-comp': ['error', {
-      order: ['type-annotations', 'static-methods', 'lifecycle', 'everything-else', 'render']
-    }],
+    'react/sort-comp': [
+      'error',
+      {
+        order: ['type-annotations', 'static-methods', 'lifecycle', 'everything-else', 'render'],
+      },
+    ],
     'react/state-in-constructor': ['error', 'never'],
     'react/static-property-placement': 'off',
     'react-hooks/rules-of-hooks': 'error',
@@ -74,10 +94,13 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
     // Disallow TODO but not @todo; these are expected to be handled by the jsdoc plugin
-    'no-warning-comments': ['error', {
-      terms: ['fixme', 'todo', 'xxx', '@fixme'],
-      location: 'start'
-    }],
+    'no-warning-comments': [
+      'error',
+      {
+        terms: ['fixme', 'todo', 'xxx', '@fixme'],
+        location: 'start',
+      },
+    ],
     'jsdoc/check-indentation': 'off',
     'no-restricted-exports': 'off',
     'no-use-before-define': 'off',
@@ -86,17 +109,23 @@ module.exports = {
     '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/ban-ts-comment': 'off',
     'default-param-last': 'off',
-    'max-len': ['error', {
-      code: 120,
-      // Allow import, export and implements statements. Also long backticks
-      ignorePattern: '^import [^,]+ from |^export | implements | `.{30,}`',
-      ignoreComments: true,
-      tabWidth: 2,
-      ignoreUrls: true
-    }],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        // Allow import, export and implements statements. Also long backticks
+        ignorePattern: '^import [^,]+ from |^export | implements | `.{30,}`',
+        ignoreComments: true,
+        tabWidth: 2,
+        ignoreUrls: true,
+      },
+    ],
     'react/function-component-definition': 'off',
-    'react/jsx-no-useless-fragment': ['error', {
-      allowExpressions: true
-    }]
-  }
+    'react/jsx-no-useless-fragment': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+  },
 };
