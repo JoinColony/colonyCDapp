f6c822d2986d09ec5002ceecb38cf34eb1650a77
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-13 12:20:19 +0200

    component refactor

--- src/components/common/Extensions/SupportingDocuments/SupportingDocuments.tsx
+++ src/components/common/Extensions/SupportingDocuments/SupportingDocuments.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { LAZY_CONSENSUS, PAYMENTS, MOTIONS_AND_DISPUTES } from '~constants';
 // import FileText from '~images/FileText.svg';
-import DocumentHeader from './DocumentHeader';
 import LinkWrapper from './LinkWrapper';
+import { SupportingDocumentsProps } from './types';
 
 const displayName = 'SupportingDocuments';
 
-const SupportingDocuments = () => {
+const SupportingDocuments = ({ isDoubleLinkVisible }: SupportingDocumentsProps) => {
   const { formatMessage } = useIntl();
 
   return (
-    <div className="min-w-[]">
-      <div className="pb-5">
-        <DocumentHeader />
-        {/* <FileText /> */}
-        <LinkWrapper url={LAZY_CONSENSUS} text={formatMessage({ id: 'supporting.documents.link1' })} />
-      </div>
-      <div className="pb-5">
-        <DocumentHeader />
-        <div className="pb-2">
-          {/* <FileText /> */}
-          <LinkWrapper url={PAYMENTS} text={formatMessage({ id: 'supporting.documents.link2' })} />
-        </div>
-        <div>
-          <LinkWrapper url={MOTIONS_AND_DISPUTES} text="How Motions and Disputes work" />
-        </div>
-      </div>
+    <div>
+      <div className="doc-list-title">{formatMessage({ id: 'supporting.documents.title' })}</div>
+      <div className="doc-list-subtitle">{formatMessage({ id: 'supporting.documents.subtitle' })}</div>
+      <div className="min-w-[35.75rem] h-[0.0625rem] bg-gray-200 my-4" />
+      {/* <FileText /> */}
+      <LinkWrapper isDoubleLinkVisible={isDoubleLinkVisible} />
     </div>
   );
 };
