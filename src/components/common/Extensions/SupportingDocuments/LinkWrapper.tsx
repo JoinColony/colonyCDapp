0626d49ae2abf93d563e9a56edf6806836b13dc4
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-13 12:20:19 +0200

    component refactor

--- src/components/common/Extensions/SupportingDocuments/LinkWrapper.tsx
+++ src/components/common/Extensions/SupportingDocuments/LinkWrapper.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import Link from '~shared/Link';
+import { LAZY_CONSENSUS, PAYMENTS, MOTIONS_AND_DISPUTES } from '~constants';
+
+interface LinkWrapperProps {
+  isDoubleLinkVisible: boolean;
+}
 
 const displayName = 'LinkWrapper';
 
-interface ILinkWrapperProps {
-  url: string;
-  text: string;
-}
+const singleLink = [
+  {
+    url: LAZY_CONSENSUS,
+    text: 'supporting.documents.link1',
+  },
+];
 
-const LinkWrapper = ({ url, text }: ILinkWrapperProps) => (
-  <Link to={url} className="font-normal text-sm text-gray-600">
-    {text}
-  </Link>
-);
+const doubleLink = [
+  {
+    url: PAYMENTS,
+    text: 'supporting.documents.link2',
+  },
+  {
+    url: MOTIONS_AND_DISPUTES,
+    text: 'supporting.documents.link3',
+  },
+];
+
+const LinkWrapper = ({ isDoubleLinkVisible }: LinkWrapperProps) => {
+  const { formatMessage } = useIntl();
+  const links = isDoubleLinkVisible ? doubleLink : singleLink;
+
+  return (
+    <>
+      {links.map((item) => (
+        <div className="pb-2">
+          <Link key={item.url} to={item.url} className="font-normal text-sm text-gray-600 hover:text-blue-400">
+            {formatMessage({ id: item.text })}
+          </Link>
+        </div>
+      ))}
+    </>
+  );
+};
 
 LinkWrapper.displayName = displayName;
 
--- src/stories/SupportingDocuments.stories.ts
+++ src/stories/SupportingDocuments.stories.ts
@@ -4,10 +4,19 @@ import SupportingDocuments from '~common/Extensions/SupportingDocuments';
 const meta: Meta<typeof SupportingDocuments> = {
   title: 'Supporting Documents list',
   component: SupportingDocuments,
-  tags: ['autodocs'],
 };
 
 export default meta;
 type Story = StoryObj<typeof SupportingDocuments>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    isDoubleLinkVisible: false,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    isDoubleLinkVisible: true,
+  },
+};
