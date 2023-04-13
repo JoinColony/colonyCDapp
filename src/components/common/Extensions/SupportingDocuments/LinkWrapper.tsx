e9dd7b4441971602fe0c6c9c42a6e75a9b8e222a
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-13 13:04:39 +0200

    move to const file

--- src/components/common/Extensions/SupportingDocuments/LinkWrapper.tsx
+++ src/components/common/Extensions/SupportingDocuments/LinkWrapper.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
 import Link from '~shared/Link';
-import { LAZY_CONSENSUS, PAYMENTS, MOTIONS_AND_DISPUTES } from '~constants';
+import { doubleLink, singleLink } from './const';
 
 interface LinkWrapperProps {
   isDoubleLinkVisible: boolean;
 }
 
 const displayName = 'common.Extensions/SupportingDocuments/LinkWrapper';
-
-const singleLink = [
-  {
-    url: LAZY_CONSENSUS,
-    text: 'supporting.documents.link1',
-  },
-];
-
-const doubleLink = [
-  {
-    url: PAYMENTS,
-    text: 'supporting.documents.link2',
-  },
-  {
-    url: MOTIONS_AND_DISPUTES,
-    text: 'supporting.documents.link3',
-  },
-];
 
 const LinkWrapper = ({ isDoubleLinkVisible }: LinkWrapperProps) => {
   const { formatMessage } = useIntl();
