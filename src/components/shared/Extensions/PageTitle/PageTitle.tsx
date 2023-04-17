4b79641f7835457184f7b187c0314b2d4465b0cc
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-04-17 11:28:54 +0200

    changed styles, added translaitons

--- src/components/shared/Extensions/PageTitle/PageTitle.tsx
+++ src/components/shared/Extensions/PageTitle/PageTitle.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import classNames from 'classnames';
+import { FormattedMessage, defineMessages } from 'react-intl';
 
 import { PageTitleProps } from './types';
 import styles from './PageTitle.module.css';
 
-const displayName = 'PageTitle';
+const displayName = 'Extensions.PageTitle';
+
+const MSG = defineMessages({
+  pay: {
+    id: `${displayName}.pay`,
+    defaultMessage: 'Pay',
+  },
+  decide: {
+    id: `${displayName}.decide`,
+    defaultMessage: 'Decide',
+  },
+  manage: {
+    id: `${displayName}.manage`,
+    defaultMessage: 'Manage',
+  },
+});
 
 const PageTitle = ({ title, subtitle }: PageTitleProps) => (
-  <div className={classNames(styles.wrapper)}>
+  <div className={styles.wrapper}>
     <div className="flex gap-12 items-baseline">
-      <div className="max-w-[872px]">
-        <h1 className="text-2xl text-gray-900 mb-4 font-semibold">{title}</h1>
-        <h2 className="text-xl text-gray-600 leading-normal">{subtitle}</h2>
+      <div className="max-w-[54.5rem]">
+        <h1 className="text-xl lg:text-2xl text-gray-900 mb-4 font-semibold">{title}</h1>
+        {subtitle && <h2 className="text-xl text-gray-600 leading-normal">{subtitle}</h2>}
       </div>
-      <div className="hidden lg:block ">
+      <div className="hidden lg:block">
         {/* it's placeholder for sub navigation */}
-        <ul className="flex gap-8 font-semibold text-md text-gray-700 ">
-          <li>Pay</li>
-          <li>Decide</li>
-          <li>Manage</li>
+        <ul className="flex gap-8 font-semibold text-md text-gray-700">
+          <li>
+            <FormattedMessage {...MSG.pay} />
+          </li>
+          <li>
+            <FormattedMessage {...MSG.decide} />
+          </li>
+          <li>
+            <FormattedMessage {...MSG.manage} />
+          </li>
         </ul>
       </div>
     </div>
