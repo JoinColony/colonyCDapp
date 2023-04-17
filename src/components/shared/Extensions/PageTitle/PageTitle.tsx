4959ca2a8e58fd3c646b06cdb2986243b5d6e007
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-04-17 09:44:53 +0200

    changes in sub navigation placeholder

--- src/components/shared/Extensions/PageTitle/PageTitle.tsx
+++ src/components/shared/Extensions/PageTitle/PageTitle.tsx
@@ -8,12 +8,19 @@ const displayName = 'PageTitle';
 
 const PageTitle = ({ title, subtitle }: PageTitleProps) => (
   <div className={classNames(styles.wrapper)}>
-    <div className="flex gap-12">
+    <div className="flex gap-12 items-baseline">
       <div className="max-w-[872px]">
         <h1 className="text-2xl text-gray-900 mb-4 font-semibold">{title}</h1>
         <h2 className="text-xl text-gray-600 leading-normal">{subtitle}</h2>
       </div>
-      <div className="hidden lg:block">Sub navigation placeholder</div>
+      <div className="hidden lg:block ">
+        {/* it's placeholder for sub navigation */}
+        <ul className="flex gap-8 font-semibold text-md text-gray-700 ">
+          <li>Pay</li>
+          <li>Decide</li>
+          <li>Manage</li>
+        </ul>
+      </div>
     </div>
   </div>
 );
