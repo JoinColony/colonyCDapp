0f944239a81c74cd70ca4dc9c2231a8f14070df9
ArmandoGraterol <armandogrtrl@gmail.com>
2023-04-18 13:01:30 -0300

    Updated ColonyAction query after rebase

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -758,7 +758,6 @@ export type ModelColonyActionConditionInput = {
   fundamentalChainId?: InputMaybe<ModelIntInput>;
   initiatorAddress?: InputMaybe<ModelIdInput>;
   isMotion?: InputMaybe<ModelBooleanInput>;
-  motionDomainId?: InputMaybe<ModelIntInput>;
   newColonyVersion?: InputMaybe<ModelIntInput>;
   not?: InputMaybe<ModelColonyActionConditionInput>;
   or?: InputMaybe<Array<InputMaybe<ModelColonyActionConditionInput>>>;
@@ -788,7 +787,6 @@ export type ModelColonyActionFilterInput = {
   id?: InputMaybe<ModelIdInput>;
   initiatorAddress?: InputMaybe<ModelIdInput>;
   isMotion?: InputMaybe<ModelBooleanInput>;
-  motionDomainId?: InputMaybe<ModelIntInput>;
   newColonyVersion?: InputMaybe<ModelIntInput>;
   not?: InputMaybe<ModelColonyActionFilterInput>;
   or?: InputMaybe<Array<InputMaybe<ModelColonyActionFilterInput>>>;
@@ -1198,7 +1196,6 @@ export type ModelSubscriptionColonyActionFilterInput = {
   id?: InputMaybe<ModelSubscriptionIdInput>;
   initiatorAddress?: InputMaybe<ModelSubscriptionIdInput>;
   isMotion?: InputMaybe<ModelSubscriptionBooleanInput>;
-  motionDomainId?: InputMaybe<ModelSubscriptionIntInput>;
   newColonyVersion?: InputMaybe<ModelSubscriptionIntInput>;
   or?: InputMaybe<Array<InputMaybe<ModelSubscriptionColonyActionFilterInput>>>;
   pendingDomainMetadataId?: InputMaybe<ModelSubscriptionIdInput>;
@@ -3447,7 +3444,8 @@ export const ColonyActionFragmentDoc = gql`
     ${UserFragmentDoc}
 ${TokenFragmentDoc}
 ${DomainFragmentDoc}
-${MotionDataFragmentDoc}`;
+${MotionDataFragmentDoc}
+${DomainMetadataFragmentDoc}`;
 export const ColonyBalanceFragmentDoc = gql`
     fragment ColonyBalance on ColonyBalance {
   id
