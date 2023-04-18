19854457c8022c85a648f84ee74d5fbb6757f3bb
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-18 18:57:29 +0200

    image carousel update

--- src/components/frame/LandingPage/LandingPage.tsx
+++ src/components/frame/LandingPage/LandingPage.tsx
@@ -13,6 +13,7 @@ import { useAppContext, useCanInteractWithNetwork } from '~hooks';
 
 import styles from './LandingPage.css';
 import ImageCarousel from '~common/Extensions/ImageCarousel/ImageCarousel';
+import { images } from '~common/Extensions/ImageCarousel/const';
 
 const displayName = 'frame.LandingPage';
 
