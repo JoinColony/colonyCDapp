6cc8eeb6a019e54da4493892d4101e2082d1809a
William Martin <williamajmartin@gmail.com>
2023-04-18 16:45:52 +0200

    feat: add motion label to from in detail widget

--- src/utils/colonyActions.ts
+++ src/utils/colonyActions.ts
@@ -38,6 +38,7 @@ export const getDetailItemsKeys = (actionType: ColonyActionType) => {
     case actionType.includes(ColonyActionType.Payment): {
       return [
         ActionPageDetails.Type,
+        isMotion(actionType) ? ActionPageDetails.Motion : '',
         ActionPageDetails.FromDomain,
         ActionPageDetails.ToRecipient,
         ActionPageDetails.Amount,
