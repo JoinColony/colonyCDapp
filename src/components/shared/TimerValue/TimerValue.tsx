83791a78d02352b1bb90e9a568df64daed7044a3
Jakub Zajac <jakub@colony.io>
2023-04-20 23:22:25 +0100

    Cleanup components

--- src/components/shared/TimerValue/TimerValue.tsx
+++ src/components/shared/TimerValue/TimerValue.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FormattedMessage, defineMessage } from 'react-intl';
+import { FormattedMessage, defineMessages } from 'react-intl';
 
-const MSG = defineMessage({
+const MSG = defineMessages({
   days: {
     id: 'TimerValue.days',
     defaultMessage: ' {days}d',
