f283ba66d51d2f85582adf2e1a9b5a54f93eb216
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-04-20 14:33:00 +0200

    moved MSG to consts file

--- src/stories/common/PageTitle.stories.tsx
+++ src/stories/common/PageTitle.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import PageTitle from '~shared/Extensions/PageTitle';
 
 const meta: Meta<typeof PageTitle> = {
-  title: 'Shared/Page Title',
+  title: 'Common/Page Title',
   component: PageTitle,
   argTypes: {
     title: {
@@ -23,7 +23,7 @@ const meta: Meta<typeof PageTitle> = {
 export default meta;
 type Story = StoryObj<typeof PageTitle>;
 
-export const PrimarySolid: Story = {
+export const Base: Story = {
   args: {
     title: 'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
     subtitle: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
