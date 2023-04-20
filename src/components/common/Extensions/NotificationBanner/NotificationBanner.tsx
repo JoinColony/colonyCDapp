7d4cb3609de12d84b767e0462d1cf37d70a78d2c
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-04-20 08:24:06 +0200

    ui/5399741: notification-banners

---
+++ src/components/common/Extensions/NotificationBanner/NotificationBanner.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+import React, { FC, PropsWithChildren } from 'react';
+import CopyUrl from './CopyUrl';
+import { NotificationBannerProps } from './NotificationBanner.types';
+import Link from '~shared/Link';
+// import styles from './NotificationBanner.module.css';
+
+const displayName = 'common.Extensions.NotificationBanner';
+
+const NotificationBanner: FC<PropsWithChildren<NotificationBannerProps>> = ({
+  status,
+  title,
+  children,
+  actionText,
+  actionType,
+  isFullSize = true,
+}) => {
+  // @TODO: handle actionType 'call-to-action'
+  return (
+    <div
+      className={clsx('py-3 px-6 border border-solid rounded-lg flex justify-between', {
+        'bg-success-200 border-success-200': status === 'success',
+        'bg-warning-200 border-warning-200': status === 'warning',
+        'bg-negative-200 border-negative-200': status === 'error',
+        'w-full flex-row items-center': isFullSize,
+        'max-w-[380px] flex-col': !isFullSize,
+      })}
+    >
+      <div className={clsx('flex', { 'flex-col': children, 'flex-row': !children })}>
+        <div className="flex items-center">
+          <div>*</div>
+          <div className="text-md font-normal text-gray-900 ml-2">{title}</div>
+        </div>
+        {children && <div className="text-sm font-normal text-gray-600 max-w-[800px]">{children}</div>}
+      </div>
+      <div className={clsx('underline text-xs font-medium', { 'mt-2 ml-4': !isFullSize })}>
+        {actionType === 'copy-url' && <CopyUrl actionText={actionText} />}
+        {actionType === 'redirect' && <Link to="https://external-url.pl">{actionText}</Link>}
+        {actionType === 'call-to-action' && actionText}
+      </div>
+    </div>
+  );
+};
+
+NotificationBanner.displayName = displayName;
+
+export default NotificationBanner;
---
+++ src/components/common/Extensions/NotificationBanner/NotificationBanner.types.ts
@@ -0,0 +1,15 @@
+export interface NotificationBannerProps {
+  status: StatusType;
+  title: string;
+  actionText: string;
+  actionType: ActionType;
+  isFullSize?: boolean;
+}
+
+type ActionType = 'redirect' | 'copy-url' | 'call-to-action';
+
+type StatusType = 'success' | 'warning' | 'error';
+
+export interface CopyUrlProps {
+  actionText: string;
+}
