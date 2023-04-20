b9766324fbeaaa32d480c017957c8f8ce3c182ad
Juliana Halko <julianayasnog@gmail.com>
2023-04-20 14:43:37 -0500

    Refactor: split actions and motions into separate components

--- src/components/common/ColonyActions/ActionDetailsPage/ActionDetailsPageFeed/ActionDetailsPageEvent/index.ts
+++ src/components/common/ColonyActions/ActionDetailsPage/ActionDetailsPageFeed/ActionDetailsPageEvent/index.ts
@@ -1,2 +1,1 @@
-export { default as ActionsPageEvent } from './ActionsPageEvent';
-export { default as MotionsPageEvent } from './MotionsPageEvent';
+export { default as ActionsPageEvent } from './ActionDetailsPageEvent';
