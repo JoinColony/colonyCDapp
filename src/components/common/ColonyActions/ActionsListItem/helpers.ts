fb900ea08f3ead2273004a073d4bbee72ea0063a
ArmandoGraterol <armandogrtrl@gmail.com>
2023-04-20 18:14:45 -0300

    Updated useMotionTag to useMotionStatusDisplay to support countdown timer

--- src/components/common/ColonyActions/ActionsListItem/helpers.ts
+++ src/components/common/ColonyActions/ActionsListItem/helpers.ts
@@ -2,32 +2,13 @@ import { useGetMotionStateQuery } from '~gql';
 import { useColonyContext, useEnabledExtensions } from '~hooks';
 import { motionTags } from '~shared/Tag';
 import { MotionData } from '~types';
-import { getMotionState } from '~utils/colonyMotions';
+import { MotionState, getMotionState } from '~utils/colonyMotions';
 
-export const useMotionTag = (
-  isMotion?: boolean | null,
-  motionData?: MotionData | null,
+const getMotionTag = (
+  networkMotionState: number | null | undefined,
+  isMotion: boolean | null | undefined,
+  motionData: MotionData | null | undefined,
 ) => {
-  const { colony } = useColonyContext();
-  const { isVotingReputationEnabled } = useEnabledExtensions();
-  const { data: motionStateData, loading: loadingMotionState } =
-    useGetMotionStateQuery({
-      skip: !isMotion || !motionData || !colony,
-      variables: {
-        input: {
-          colonyAddress: colony?.colonyAddress ?? '',
-          motionId: Number(motionData?.motionId),
-        },
-      },
-    });
-
-  const networkMotionState = motionStateData?.getMotionState;
-
-  // No tag while loading motion state or voting rep not enabled
-  if (loadingMotionState || !isVotingReputationEnabled) {
-    return () => null;
-  }
-
   // If not motion, then forced action
   if (!isMotion) {
     return motionTags.Forced;
@@ -40,5 +21,49 @@ export const useMotionTag = (
   // If no motion state / data, display as invalid
   return motionTags.Invalid;
 
-  // TODO: handle case where motion is from now-uninstalled voting rep version.
+  // @TODO: handle case where motion is from now-uninstalled voting rep version.
+};
+
+export const useMotionStatusDisplay = (
+  isMotion: boolean | null | undefined,
+  motionData: MotionData | null | undefined,
+) => {
+  const { isVotingReputationEnabled } = useEnabledExtensions();
+  const { colony } = useColonyContext();
+
+  const {
+    data: motionStateData,
+    loading: loadingMotionState,
+    refetch: refetchMotionState,
+  } = useGetMotionStateQuery({
+    skip: !isMotion || !motionData || !colony,
+    variables: {
+      input: {
+        colonyAddress: colony?.colonyAddress ?? '',
+        motionId: Number(motionData?.motionId),
+      },
+    },
+  });
+  const networkMotionState = motionStateData?.getMotionState;
+
+  const showMotionTag = !loadingMotionState && isVotingReputationEnabled;
+  const MotionTag = showMotionTag ? getMotionTag(networkMotionState, isMotion, motionData) : () => null;
+
+  const motionState =
+    networkMotionState && motionData
+      ? getMotionState(networkMotionState, motionData)
+      : MotionState.Invalid;
+
+  const showMotionCountdownTimer =
+    motionState !== MotionState.Passed &&
+    motionState !== MotionState.Failed &&
+    motionState !== MotionState.FailedNotFinalizable &&
+    motionState !== MotionState.Invalid;
+
+  return {
+    motionState,
+    MotionTag,
+    showMotionCountdownTimer,
+    refetchMotionState,
+  };
 };
