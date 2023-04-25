ec8f33a0910163e75a4f573e4401cbdaa1d0a0fb
KatarzynaStefanowiczPagepro <katarzyna.stefanowicz@pagepro.co>
2023-04-25 11:13:15 +0200

    tabs

--- src/components/shared/Extensions/Tabs/consts.tsx
+++ src/components/shared/Extensions/Tabs/consts.tsx
@@ -1,6 +1,12 @@
 import { TabItem } from './types';
 
 export const tabsItems: TabItem[] = [
-  { id: '1', title: 'Overview', content: 'content 1' },
-  { id: '2', title: 'Extension settings', content: 'content 2' },
+  { id: 0, title: 'Overview', content: 'content 1' },
+  { id: 1, title: 'Extension settings', content: 'content 2' },
+  { id: 2, title: 'Tab three', content: 'content 3' },
+  { id: 3, title: 'Tab four', content: 'content 4' },
+  { id: 4, title: 'Tab five', content: 'content 5' },
+  { id: 5, title: 'Tab six', content: 'content 6' },
+  { id: 6, title: 'Tab seven', content: 'content 7' },
+  { id: 7, title: 'Tab eight', content: 'content 8' },
 ];
