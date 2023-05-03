8c6f04dd16746af8a4a7108bd19317823f789e1a
Raul Glogovetan <raul@glogovetan.com>
2023-05-03 17:03:49 +0300

    Fix: pass in env vars to apollo client

--- src/context/apolloClient.ts
+++ src/context/apolloClient.ts
@@ -6,9 +6,9 @@ import cache from '~cache';
  * @TODO This needs to be fetched from a proper location
  */
 const httpLink = new HttpLink({
-  uri: 'http://localhost:20002/graphql',
+  uri: process.env.AWS_APPSYNC_GRAPHQL_URL || 'http://localhost:20002/graphql',
   headers: {
-    'x-api-key': 'da2-fakeApiId123456',
+    'x-api-key': process.env.AWS_APPSYNC_KEY || 'da2-fakeApiId123456',
   },
 });
 
