b78fc953c40bd017ed6d9c70636375cd3fadf00b
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-05-04 15:18:37 +0200

    fix styles

--- src/stories/common/CalamityBanner.stories.tsx
+++ src/stories/common/CalamityBanner.stories.tsx
@@ -20,9 +20,9 @@ export const Base: Story = {
   decorators: [
     (Story) => (
       <Router>
-        <div className="h-full overflow-y-scroll">
+        <body className="overflow-scroll">
           <Story />
-          <div className="h-[3000px]">
+          <div>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
             ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
@@ -75,7 +75,7 @@ export const Base: Story = {
             in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
-        </div>
+        </body>
       </Router>
     ),
   ],
