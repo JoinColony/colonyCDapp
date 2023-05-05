1924d217181c758f1796f40ac0125e58b4eaff2f
Katarzyna Krawczyk <katarzyna.krawczyk@pagepro.pl>
2023-05-05 15:06:28 +0200

    fix colors

--- src/components/shared/Extensions/Toast/Toast.tsx
+++ src/components/shared/Extensions/Toast/Toast.tsx
@@ -8,12 +8,12 @@ import 'react-toastify/dist/ReactToastify.css';
 const displayName = 'Extensions.ToastComponent';
 
 const ToastComponent: FC<ToastComponentProps> = ({ type = 'success', title, description, linkName, url = '' }) => (
-  <div className="flex bg-base-white w-[24.375rem] h-[5.75rem]">
+  <div className="flex bg-base-white relative">
     <div
       className={clsx({
-        '[&>i>svg]:stroke-green-400': type === 'success',
-        '[&>i>svg]:stroke-orange-400': type === 'alert',
-        '[&>i>svg]:stroke-red-400': type === 'warning',
+        '[&>i>svg]:fill-success-400 [&>i>svg]:stroke-success-400': type === 'success',
+        '[&>i>svg]:fill-warning-400 [&>i>svg]:stroke-warning-400': type === 'alert',
+        '[&>i>svg]:fill-negative-400 [&>i>svg]:stroke-negative-400': type === 'warning',
       })}
     >
       <Icon
@@ -22,15 +22,15 @@ const ToastComponent: FC<ToastComponentProps> = ({ type = 'success', title, desc
       />
     </div>
     <div className="flex flex-col ml-[1.140625rem]">
-      {title && <span className="text-md text-gray-900 font-semibold">{title}</span>}
-      {description && <span className="text-md text-gray-600 font-normal">{description}</span>}
+      {title && <span className="text-md text-gray-900 font-semibold pb-1">{title}</span>}
+      {description && <span className="text-md text-gray-600 font-normal pb-1">{description}</span>}
       {linkName && (
         <Link
           to={url}
           className={clsx('text-xs font-medium underline', {
-            'text-green-400': type === 'success',
-            'text-orange-400': type === 'alert',
-            'text-red-400': type === 'warning',
+            'text-success-400': type === 'success',
+            'text-warning-400': type === 'alert',
+            'text-negative-400': type === 'warning',
           })}
         >
           {linkName}
--- src/components/shared/Extensions/Toast/partials/CloseButton.tsx
+++ src/components/shared/Extensions/Toast/partials/CloseButton.tsx
@@ -3,7 +3,7 @@ import { CloseButtonProps } from 'react-toastify';
 import Icon from '~shared/Icon';
 
 const CloseButton = ({ closeToast }: CloseButtonProps) => (
-  <button type="button" className="absolute right-5 top-4 h-4 w-4" onClick={closeToast}>
+  <button type="button" className="absolute right-5 top-4 h-4 w-4 [&>i>svg]:stroke-gray-400" onClick={closeToast}>
     <Icon appearance={{ size: 'tiny' }} name="close" />
   </button>
 );
