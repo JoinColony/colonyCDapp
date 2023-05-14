c3cc458fa49194eabb13bde001bcaec24f742131
Jakub Zajac <jakub@colony.io>
2023-05-14 20:06:08 +0100

    Configure Colony keyFields option in apollo

--- src/cache/colony/index.ts
+++ src/cache/colony/index.ts
@@ -1,8 +1,11 @@
+import { TypePolicies } from '@apollo/client';
+
 import tokensField from './tokens';
 import balancesField from './balances';
 
-const colony = {
+const colony: TypePolicies = {
   Colony: {
+    keyFields: ['colonyAddress'],
     fields: {
       ...tokensField,
       ...balancesField,
