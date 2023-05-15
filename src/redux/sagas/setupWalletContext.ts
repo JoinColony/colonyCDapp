d2831ee31a0e5a5b6f4f32fdd99f1ba7b422135a
William Martin <williamajmartin@gmail.com>
2023-05-15 12:09:51 +0200

    fix: sync wallet ui with metamask

--- src/redux/sagas/setupWalletContext.ts
+++ src/redux/sagas/setupWalletContext.ts
@@ -1,14 +1,50 @@
 import { ContextModule, getContext, setContext } from '~context';
 import { ColonyWallet } from '~types';
-import { getLastWallet } from '~utils/autoLogin';
+import { getLastWallet, setLastWallet } from '~utils/autoLogin';
+import { createAddress } from '~utils/web3';
 
 import { getBasicWallet, getWallet } from './wallet';
+import { disconnectWallet } from './users';
+
+const ONBOARD_METAMASK_WALLET_LABEL = 'MetaMask';
+
+const getMetamaskAddress = () => {
+  // try/catch just in case createAddress errors
+  try {
+    if (window.ethereum) {
+      return createAddress(
+        // @ts-ignore
+        window.ethereum.selectedAddress,
+      );
+    }
+  } catch {
+    // silent
+  }
+  return undefined;
+};
 
 const setupWalletContext = async () => {
   let wallet: ColonyWallet | undefined;
 
   try {
     wallet = getContext(ContextModule.Wallet);
+
+    const selectedMetamaskAddress = getMetamaskAddress();
+    /*
+     * If the wallet we've pulled from context does not have the same address as the selected account
+     * in Metamask, it's because the user just switched their account in metamask.
+     */
+    if (selectedMetamaskAddress && wallet.address !== selectedMetamaskAddress) {
+      disconnectWallet(wallet.label); // disconnect previous wallet
+
+      // replace it in local storage with the wallet the user switched to
+      setLastWallet({
+        type: ONBOARD_METAMASK_WALLET_LABEL,
+        address: selectedMetamaskAddress,
+      });
+
+      wallet = undefined;
+    }
   } catch {
     // wallet not seen in context yet
   }
