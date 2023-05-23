bc0b1592c1a8ae4d6cc55d6d537162d4892c38df
William Martin <williamajmartin@gmail.com>
2023-05-23 13:26:08 +0200

    refactor: colonyAvatarUploader to own component

--- src/components/common/Dialogs/EditColonyDetailsDialog/EditColonyDetailsDialogForm.tsx
+++ src/components/common/Dialogs/EditColonyDetailsDialog/EditColonyDetailsDialogForm.tsx
@@ -3,26 +3,15 @@ import { defineMessages } from 'react-intl';
 import { ColonyRole, Id } from '@colony/colony-js';
 import { useFormContext } from 'react-hook-form';
 
-import AvatarUploader from '~shared/AvatarUploader';
 import {
   ActionDialogProps,
   DialogControls,
   DialogHeading,
   DialogSection,
 } from '~shared/Dialog';
-import {
-  Annotations,
-  HookFormInput as Input,
-  InputStatus,
-} from '~shared/Fields';
-import ColonyAvatar from '~shared/ColonyAvatar';
-import Avatar from '~shared/Avatar';
+import { Annotations, HookFormInput as Input } from '~shared/Fields';
+import { DropzoneErrors } from '~shared/AvatarUploader/helpers';
 import { useActionDialogStatus } from '~hooks';
-import {
-  getOptimisedAvatar,
-  getOptimisedThumbnail,
-} from '~images/optimisation';
-import { FileReaderFile } from '~utils/fileReader/types';
 import { SetStateFn } from '~types';
 
 import {
@@ -31,8 +20,7 @@ import {
   NotEnoughReputation,
   PermissionRequiredInfo,
 } from '../Messages';
-
-import styles from './EditColonyDetailsDialogForm.css';
+import ColonyAvatarUploader from './ColonyAvatarUploader';
 
 const displayName =
   'common.EditColonyDetailsDialog.EditColonyDetailsDialogForm';
@@ -70,15 +58,14 @@ interface EditColonyDetailsDialogFormProps extends ActionDialogProps {
 const EditColonyDetailsDialogForm = ({
   back,
   colony,
-  colony: { colonyAddress, metadata },
+  colony: { metadata },
   enabledExtensionData,
   isForce,
   setIsForce,
 }: EditColonyDetailsDialogFormProps) => {
-  const { setValue, watch } = useFormContext();
+  const { watch } = useFormContext();
   const { colonyAvatarImage, colonyDisplayName, forceAction } = watch();
-  const [showUploadedAvatar, setShowUploadedAvatar] = useState(false);
-  const [avatarFileError, setAvatarFileError] = useState(false);
+  const [avatarFileError, setAvatarFileError] = useState<DropzoneErrors>();
 
   useEffect(() => {
     if (forceAction !== isForce) {
@@ -98,36 +85,6 @@ const EditColonyDetailsDialogForm = ({
     [Id.RootDomain],
     enabledExtensionData,
   );
-
-  const handleFileRead = async (file: FileReaderFile) => {
-    if (!file) {
-      return;
-    }
-
-    try {
-      const optimisedImage = await getOptimisedAvatar(file.file);
-      const optimisedThumbnail = await getOptimisedThumbnail(file.file);
-      setValue('colonyAvatarImage', optimisedImage, { shouldDirty: true });
-      setValue('colonyThumbnail', optimisedThumbnail);
-      setShowUploadedAvatar(true);
-    } catch (e) {
-      setAvatarFileError(true);
-    }
-  };
-
-  const handleFileRemove = async () => {
-    setValue('colonyAvatarImage', null, { shouldDirty: true });
-    setValue('colonyThumbnail', null);
-    setShowUploadedAvatar(true);
-  };
-
-  /*
-   * This helps us hook into the internal file uplaoder error state,
-   * so that we can invalidate the form if the uploaded file format is incorrect
-   */
-  const handleFileReadError = async () => {
-    setAvatarFileError(true);
-  };
 
   const hasEditedColony =
     metadata?.displayName !== colonyDisplayName ||
@@ -152,51 +109,12 @@ const EditColonyDetailsDialogForm = ({
         </DialogSection>
       )}
       <DialogSection>
-        <AvatarUploader
-          avatar={showUploadedAvatar ? colonyAvatarImage : metadata?.avatar}
-          disabled={disabledInput}
-          label={MSG.logo}
-          handleFileAccept={handleFileRead}
-          handleFileRemove={handleFileRemove}
-          avatarPlaceholder={
-            <>
-              {showUploadedAvatar ? (
-                /*
-                 * If we have a currently uploaded avatar, **or** if we just remove it
-                 * show the default colony avatar, without values coming from the
-                 * colony.
-                 *
-                 * Note that in case of the avatar being removed, `avatarURL` will be
-                 * passed as `undefined` so that the blockies show.
-                 * This is intended functionality
-                 */
-                <Avatar
-                  avatar={colonyAvatarImage}
-                  seed={colonyAddress.toLowerCase()}
-                  title={metadata?.displayName || colony.name || colonyAddress}
-                  size="xl"
-                />
-              ) : (
-                /*
-                 * Show the colony hooked avatar. This is only visible until the
-                 * user interacts with avatar input for the first time
-                 */
-                <ColonyAvatar
-                  colony={colony}
-                  colonyAddress={colonyAddress}
-                  size="xl"
-                  preferThumbnail={false}
-                />
-              )}
-            </>
-          }
-          handleFileReject={handleFileReadError}
+        <ColonyAvatarUploader
+          colony={colony}
+          setAvatarFileError={setAvatarFileError}
+          disabledInput={disabledInput}
+          avatarFileError={avatarFileError}
         />
-        {avatarFileError && (
-          <div className={styles.avatarUploadError}>
-            <InputStatus error={MSG.invalidAvatarFormat} />
-          </div>
-        )}
       </DialogSection>
       <DialogSection>
         <Input
@@ -232,7 +150,7 @@ const EditColonyDetailsDialogForm = ({
       )}
       <DialogSection appearance={{ align: 'right', theme: 'footer' }}>
         <DialogControls
-          disabled={disabledSubmit || avatarFileError || !hasEditedColony}
+          disabled={disabledSubmit || !!avatarFileError || !hasEditedColony}
           dataTest="confirmButton"
           onSecondaryButtonClick={back}
           isVotingReputationEnabled={
