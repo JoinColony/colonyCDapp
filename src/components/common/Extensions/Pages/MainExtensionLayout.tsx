eed2cbd4840d9b705327287acc9d19432a121442
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-05-25 12:21:50 +0200

    fix: added missing wallet component to handle install

--- src/components/common/Extensions/Pages/MainExtensionLayout.tsx
+++ src/components/common/Extensions/Pages/MainExtensionLayout.tsx
@@ -1,10 +1,15 @@
 import React, { FC, PropsWithChildren } from 'react';
 import Header from './Header';
+import Wallet from '~frame/RouteLayouts/UserNavigation/Wallet/Wallet';
 
 const MainExtensionLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <Header />
+      {/* @TODO: Remove wallet component when we have a proper wallet */}
+      <div className="hidden">
+        <Wallet />
+      </div>
       <main className="lg:max-w-[74.375rem] px-6 mx-auto">{children}</main>
     </div>
   );
