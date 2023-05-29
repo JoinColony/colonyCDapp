fbbc6bc7743e745678987345e8bda4e793f10d2d
Arren V <33682027+arrenv@users.noreply.github.com>
2023-05-29 14:56:21 +0200

    Updates after rebase

--- src/components/common/Extensions/SubNavigation/partials/LearnMore.tsx
+++ src/components/common/Extensions/SubNavigation/partials/LearnMore.tsx
@@ -1,16 +1,34 @@
 import React, { PropsWithChildren, FC } from 'react';
 
-import ExternalLink from '~shared/ExternalLink/ExternalLink';
+import { FormattedMessage } from 'react-intl';
+import ExternalLink from '~shared/Extensions/ExternalLink';
 
 import { LearnMoreProps } from './types';
+import Icon from '~shared/Icon';
 
-const displayName = 'common.Extensions.SubNavigation.partials.LearnMore';
+const displayName = 'Extensions.LearnMore';
 
-const LearnMore: FC<PropsWithChildren<LearnMoreProps>> = ({ chunks, href }) => {
+const LearnMore: FC<PropsWithChildren<LearnMoreProps>> = ({ message, href }) => {
+  const { id, defaultMessage } = message;
+
   return (
-    <ExternalLink href={href}>
-      <div className="font-semibold ml-1 text-gray-900 underline underline-offset-2 hover:cursor-pointer">{chunks}</div>
-    </ExternalLink>
+    <div className="text-sm flex justify-center items-center font-normal text-gray-900">
+      <Icon name="question" appearance={{ size: 'tiny' }} />
+      <span className="block ml-2">
+        <FormattedMessage
+          id={id}
+          defaultMessage={defaultMessage}
+          values={{
+            // eslint-disable-next-line react/no-unstable-nested-components
+            a: (chunks) => (
+              <ExternalLink className="underline font-semibold ml-px" href={href}>
+                {chunks}
+              </ExternalLink>
+            ),
+          }}
+        />
+      </span>
+    </div>
   );
 };
 
--- src/components/shared/Fields/Input/HookForm/InputComponent.tsx
+++ src/components/shared/Fields/Input/HookForm/InputComponent.tsx
@@ -1,184 +1,107 @@
-import React, { useCallback, useState, InputHTMLAttributes, useMemo, RefObject, useEffect } from 'react';
-import { defineMessages } from 'react-intl';
-import Cleave from 'cleave.js/react';
-import { CleaveOptions } from 'cleave.js/options';
-import { ChangeEvent } from 'cleave.js/react/props';
-import Decimal from 'decimal.js';
-
-import Button from '~shared/Button';
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 import { getMainClasses } from '~utils/css';
-import { isNil } from '~utils/lodash';
 
-import { InputComponentAppearance as Appearance } from '../Input';
-import styles from './InputComponent.css';
+import { HookFormInputProps as InputProps } from './Input';
+import FormattedInput from './FormattedInputComponent';
 
-const MSG = defineMessages({
-  max: {
-    id: `users.Fileds.Input.InputComponent.max`,
-    defaultMessage: 'Max',
-  },
-});
+import styles from '../InputComponent.css';
 
-type CleaveHTMLInputElement = HTMLInputElement & { rawValue: string };
-
-interface MaxButtonParams {
-  setFieldValue: (field, value) => void;
-  maxAmount: string;
-  fieldName: string;
+interface LengthWidgetProps {
+  length: number;
+  maxLength: number;
 }
 
-export interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'form'> {
-  /** Appearance object */
-  appearance?: Appearance;
+const LengthWidget = ({ length, maxLength }: LengthWidgetProps) => (
+  <span className={styles.characterCounter}>
+    {length}/{maxLength}
+  </span>
+);
 
-  /** Options for cleave.js formatting (see [this list](https://github.com/nosir/cleave.js/blob/master/doc/options.md)) */
-  formattingOptions?: CleaveOptions;
-
-  /** Input field name (form variable) */
-  name: string;
-
-  /** Pass a ref to the `<input>` element */
-  innerRef?: RefObject<any> | ((ref: HTMLInputElement | null) => void);
-
-  /** Pass params to a max button - implemented only in Cleave options */
-  maxButtonParams?: MaxButtonParams;
-  /** Testing */
-  dataTest?: string;
+export interface HookFormInputComponentProps extends Omit<InputProps, 'placeholder'> {
+  placeholder?: string;
+  inputValueLength: number;
 }
 
-const InputComponent = ({
-  appearance,
-  formattingOptions,
-  innerRef,
-  onChange,
-  placeholder,
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-  // Cleave TS defs don't expect/allow these
-  contentEditable,
-  draggable,
-  spellCheck,
-  maxLength,
-  value,
-  maxButtonParams,
-  dataTest,
-  /* eslint-enable @typescript-eslint/no-unused-vars */
-  ...props
-}: Props) => {
-  /* problem with cleave types, that's why `any` */
-  const [cleave, setCleave] = useState<any>(null);
+const displayName = 'HookFormInputComponent';
 
-  const length = value ? value.toString().length : 0;
+const getAppearanceObject = (
+  maxLength: HookFormInputComponentProps['maxLength'],
+  maxButtonParams: HookFormInputComponentProps['maxButtonParams'],
+  appearance: HookFormInputComponentProps['appearance'],
+) => {
+  let classes: any = {
+    ...appearance,
+  };
 
-  const handleCleaveChange = useCallback(
-    (evt: ChangeEvent<CleaveHTMLInputElement>): void => {
-      // We are reassigning the value here as cleave just adds a `rawValue` prop
-      if (evt.currentTarget !== undefined) {
-        // eslint-disable-next-line no-param-reassign
-        evt.currentTarget.value = evt.currentTarget.rawValue;
-      } else {
-        // setCleaveValue(evt.currentTarget?.rawValue);
-        // @ts-ignore
-        // eslint-disable-next-line no-param-reassign
-        evt.currentTarget = {
-          // @ts-ignore
-          value: evt.currentTarget?.rawValue as string,
-        };
-      }
-      if (onChange && evt.target.name) onChange(evt);
-    },
-    [onChange],
-  );
-  /*
-   * @NOTE Coerce cleave into handling dynamically changing options
-   * See here for why this isn't yet supported "officially":
-   * https://github.com/nosir/cleave.js/issues/352#issuecomment-447640572
-   */
-  const dynamicCleaveOptionKey = useMemo(() => JSON.stringify(formattingOptions), [formattingOptions]);
-
-  useEffect(() => {
-    if (isNil(value) && cleave) {
-      cleave.setRawValue('');
-    }
-  }, [cleave, value]);
-
-  if (formattingOptions) {
-    if (typeof innerRef === 'object') {
-      console.error('Cleave inner ref must be a function');
-      return null;
-    }
-    return maxButtonParams === undefined ? (
-      <Cleave
-        {...props}
-        value={value || ''}
-        key={dynamicCleaveOptionKey}
-        className={getMainClasses(appearance, styles)}
-        htmlRef={innerRef}
-        options={formattingOptions}
-        onChange={handleCleaveChange}
-        placeholder={placeholder}
-        data-test={dataTest}
-      />
-    ) : (
-      <div className={styles.inputContainer}>
-        <Button
-          className={styles.maxButton}
-          text={MSG.max}
-          onClick={() => {
-            maxButtonParams?.setFieldValue(maxButtonParams?.fieldName, maxButtonParams.maxAmount);
-            const decimalValue = new Decimal(maxButtonParams.maxAmount);
-            if (decimalValue.lt(0.00001) && decimalValue.gt(0)) {
-              cleave?.setRawValue(decimalValue.toSD(5, Decimal.ROUND_DOWN).toNumber());
-            } else {
-              cleave?.setRawValue(new Decimal(maxButtonParams.maxAmount).toDP(5, Decimal.ROUND_DOWN).toNumber());
-            }
-          }}
-          dataTest="inputMaxButton"
-        />
-        <Cleave
-          {...props}
-          key={dynamicCleaveOptionKey}
-          className={getMainClasses(appearance, styles)}
-          htmlRef={innerRef}
-          options={formattingOptions}
-          onChange={handleCleaveChange}
-          placeholder={placeholder}
-          onInit={(cleaveInstance) => setCleave(cleaveInstance)}
-          data-test={dataTest}
-        />
-      </div>
-    );
+  if (maxLength) {
+    classes = {
+      ...classes,
+      paddingRight: 'extra',
+    };
   }
 
-  return !maxLength ? (
-    <input
-      className={getMainClasses(appearance, styles)}
-      onChange={onChange}
-      placeholder={placeholder}
-      ref={innerRef}
-      value={value || ''}
-      data-test={dataTest}
-      {...props}
-    />
-  ) : (
+  if (maxButtonParams) {
+    classes = {
+      ...classes,
+      paddingLeft: 'extra',
+    };
+  }
+  return classes;
+};
+
+const HookFormInputComponent = ({
+  appearance,
+  dataTest,
+  formattingOptions,
+  inputValueLength: length,
+  maxLength,
+  maxButtonParams,
+  name,
+  onChange,
+  onBlur,
+  ...restInputProps
+}: HookFormInputComponentProps) => {
+  const { register } = useFormContext();
+  const { onChange: hookFormOnChange, onBlur: hookFormOnBlur, ref } = register(name);
+
+  const classes = getAppearanceObject(maxLength, maxButtonParams, appearance);
+  const className = getMainClasses(classes, styles);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    hookFormOnChange(e);
+    onChange?.(e);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    hookFormOnBlur(e);
+    onBlur?.(e);
+  };
+
+  const props = {
+    ...restInputProps,
+    onChange: handleChange,
+    onBlur: handleBlur,
+    'data-test': dataTest,
+    maxLength,
+    name,
+    className,
+    tabIndex: 0,
+  };
+
+  return (
     <div className={styles.inputContainer}>
-      <input
-        className={getMainClasses({ paddingRight: 'extra', ...appearance }, styles)}
-        onChange={onChange}
-        placeholder={placeholder}
-        ref={innerRef}
-        maxLength={maxLength}
-        value={value || ''}
-        data-test={dataTest}
-        {...props}
-      />
-      {maxLength && (
-        <span className={styles.characterCounter}>
-          {length}/{maxLength}
-        </span>
+      {formattingOptions ? (
+        <FormattedInput {...props} maxButtonParams={maxButtonParams} options={formattingOptions} />
+      ) : (
+        <input {...props} className={className} ref={ref} />
       )}
+      {maxLength && <LengthWidget maxLength={maxLength} length={length} />}
     </div>
   );
 };
 
-export default InputComponent;
+HookFormInputComponent.displayName = displayName;
+
+export default HookFormInputComponent;
