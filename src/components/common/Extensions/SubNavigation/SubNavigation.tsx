c749a8b1472388f1ec4ad6b4c45a766d2b579d96
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-05-30 10:02:59 +0200

    fix: reverted chages

--- src/components/common/Extensions/SubNavigation/SubNavigation.tsx
+++ src/components/common/Extensions/SubNavigation/SubNavigation.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { PropsWithChildren, useState, FC } from 'react';
 
-import SubNavigationComponent, { SubNavigationMobile } from '~common/Extensions/SubNavigation';
-import { useMobile } from '~hooks';
+import { useDetectClickOutside } from '~hooks';
 
-const SubNavigation = () => {
-  const isMobile = useMobile();
+import SubNavigationItem from './SubNavigationItem';
+import { displayName, subNavigationItems } from './consts';
 
-  return isMobile ? <SubNavigationMobile /> : <SubNavigationComponent />;
+const SubNavigation: FC<PropsWithChildren> = () => {
+  const [openIndex, setOpenIndex] = useState<number>();
+
+  const ref = useDetectClickOutside({ onTriggered: () => setOpenIndex(undefined) });
+
+  return (
+    <ul className="flex gap-8 font-semibold text-lg text-gray-700" ref={ref}>
+      {subNavigationItems.map(({ id, label, content, icon }) => (
+        <SubNavigationItem
+          key={id}
+          id={id}
+          label={label}
+          content={content}
+          icon={icon}
+          isOpen={openIndex === id}
+          setOpen={() => setOpenIndex((prevState) => (prevState === id ? undefined : id))}
+        />
+      ))}
+    </ul>
+  );
 };
+
+SubNavigation.displayName = displayName;
 
 export default SubNavigation;
