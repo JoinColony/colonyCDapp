3a1644c7e88a9e6f35e504375dca8eedcb349c59
krawczykkatarzyna <katarzyna.krawczyk@pagepro.pl>
2023-05-31 10:46:38 +0200

    fix ts error

--- src/components/shared/Extensions/Accordion/partials/ContentTypeText.tsx
+++ src/components/shared/Extensions/Accordion/partials/ContentTypeText.tsx
@@ -5,7 +5,9 @@ const displayName = 'Extensions.Accordion.partials.AccordionDetails';
 
 const ContentTypeText: FC<ContentTypeProps> = ({ title, subTitle }) => (
   <div>
+    {/* @ts-ignore */}
     <div className="font-medium text-gray-900 text-md pb-1">{title}</div>
+    {/* @ts-ignore */}
     <div className="text-sm font-normal text-gray-600">{subTitle}</div>
   </div>
 );
