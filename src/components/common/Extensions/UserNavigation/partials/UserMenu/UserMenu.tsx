8ffe768858067063098fc267212d795deb431079
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-05-31 12:39:52 +0200

    feat: added user submenus

--- src/components/common/Extensions/UserNavigation/partials/UserMenu/UserMenu.tsx
+++ src/components/common/Extensions/UserNavigation/partials/UserMenu/UserMenu.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import clsx from 'clsx';
 import { useIntl } from 'react-intl';
 import { UserMenuProps } from './types';
@@ -11,6 +11,7 @@ import PopoverBase from '~shared/Extensions/PopoverBase';
 import TitledContent from '~common/Extensions/TitledContent/TitledContent';
 import WalletConnectedTopMenu from '../WalletConnectedTopMenu/WalletConnectedTopMenu';
 import Link from '~shared/Extensions/Link';
+import UserSubmenu from '../UserSubmenu/UserSubmenu';
 
 const displayName = 'common.Extensions.UserNavigation.partials.UserMenu';
 
@@ -29,6 +30,7 @@ const UserMenu: FC<UserMenuProps> = ({
   const { formatMessage } = useIntl();
   const { connectWallet } = useAppContext();
   const { name, profile } = user || {};
+  const [activeSubmenu, setActiveSubmenu] = useState<string | null>(null);
 
   const iconSize = isMobile ? 'small' : 'extraTiny';
 
@@ -36,95 +38,135 @@ const UserMenu: FC<UserMenuProps> = ({
     <PopoverBase
       setTooltipRef={setTooltipRef}
       tooltipProps={tooltipProps}
-      classNames={clsx('p-6 rounded-lg flex border-gray-100 justify-start z-[9999] tooltip-container', {
-        'w-full border-none shadow-none': isMobile,
-        'w-[20.125rem]': !isMobile,
-        'h-[32rem] md:h-[23rem]': !isWalletConnected,
-        'md:h-[29rem]': isWalletConnected,
-      })}
-    >
-      {isWalletConnected ? (
-        <WalletConnectedTopMenu
-          userName={profile?.displayName || name}
-          isVerified={isVerified}
-          walletAddress={walletAddress}
-          userReputation={userReputation}
-          totalReputation={totalReputation}
-          nativeToken={nativeToken}
-          avatar={profile?.thumbnail || profile?.avatar || ''}
-          user={user}
-        />
-      ) : (
-        <>
-          <div className={styles.mobileButtons}>
-            <Button mode="tertiaryOutline" isFullRounded>
-              <Icon name="cardholder" appearance={{ size: 'tiny' }} />
-              <p className="text-sm font-inter font-medium ml-1">{formatMessage({ id: 'connectWallet' })}</p>
-            </Button>
-            <Button mode="tertiaryOutline" isFullRounded>
-              <Icon name="list" appearance={{ size: 'extraTiny' }} />
-              <p className="text-sm font-inter font-medium ml-1">{formatMessage({ id: 'help' })}</p>
-            </Button>
-          </div>
-          <div className="w-full pb-6 mb-6 border-b border-b-gray-200 md:pb-5 md:mb-5">
-            <Button mode="quaternaryOutline" isFullSize onClick={connectWallet}>
-              {formatMessage({ id: 'connectWallet' })}
-            </Button>
-          </div>
-        </>
+      classNames={clsx(
+        'p-6 rounded-lg flex border-gray-100 justify-start z-[9999] tooltip-container relative overflow-hidden',
+        {
+          'w-full border-none shadow-none': isMobile,
+          'w-[20.125rem]': !isMobile,
+          'h-[32rem] md:h-[23rem]': !isWalletConnected && !activeSubmenu,
+          'h-[35rem] md:h-[29rem]': isWalletConnected && !activeSubmenu,
+          'h-[16rem]': activeSubmenu,
+        },
       )}
-      <div className="w-full pb-6 mb-6 border-b border-b-gray-200 md:pb-5 md:mb-5">
-        <TitledContent title={{ id: 'userMenu.optionsTitle' }}>
-          <ul className="text-lg font-semibold md:font-normal md:text-md text-gray-900">
-            <li className="mb-4">
-              <Link to="/" className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <Icon name="circles-three-plus" appearance={{ size: iconSize }} />
-                  <p className="ml-2">{formatMessage({ id: 'userMenu.getStartedTitle' })}</p>
-                </div>
-              </Link>
-            </li>
-            <li className="mb-4">
-              <Link to="/" className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <Icon name="lifebuoy" appearance={{ size: iconSize }} />
-                  <p className="ml-2">{formatMessage({ id: 'userMenu.contactAndSupportTitle' })}</p>
-                </div>
-                <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
-              </Link>
-            </li>
-            <li className="mb-4">
-              <Link to="/" className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <Icon name="code" appearance={{ size: iconSize }} />
-                  <p className="ml-2">{formatMessage({ id: 'userMenu.developersTitle' })}</p>
-                </div>
-                <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <Icon name="briefcase" appearance={{ size: iconSize }} />
-                  <p className="ml-2">{formatMessage({ id: 'userMenu.legalAndPrivacyTitle' })}</p>
-                </div>
-                <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
-              </Link>
-            </li>
-          </ul>
-        </TitledContent>
+    >
+      <div
+        className={clsx('absolute inset-0 p-6 transition-transform', {
+          'translate-x-0': activeSubmenu,
+          'translate-x-full': !activeSubmenu,
+        })}
+      >
+        <button
+          type="button"
+          className="flex items-center text-gray-400 text-xs font-medium"
+          onClick={() => setActiveSubmenu(null)}
+        >
+          <Icon name="caret-left" appearance={{ size: 'extraTiny' }} />
+          <p className="ml-2 uppercase">{activeSubmenu && formatMessage({ id: activeSubmenu })}</p>
+        </button>
+        {activeSubmenu && <UserSubmenu submenuId={activeSubmenu} />}
       </div>
-      {isWalletConnected && (
-        <div className="w-full mb-6 md:mb-5">
-          <TitledContent title={{ id: 'userMenu.other' }}>
-            <Link to="/" className="flex items-center text-lg font-semibold md:font-normal md:text-md text-gray-900">
-              <Icon name="plugs" appearance={{ size: iconSize }} />
-              <p className="ml-2">{formatMessage({ id: 'userMenu.disconnectWalletTitle' })}</p>
-            </Link>
+      <div
+        className={clsx('transition-transform', {
+          '-translate-x-0': !activeSubmenu,
+          '-translate-x-[100vw]': activeSubmenu,
+        })}
+      >
+        {isWalletConnected && (
+          <WalletConnectedTopMenu
+            userName={profile?.displayName || name}
+            isVerified={isVerified}
+            walletAddress={walletAddress}
+            userReputation={userReputation}
+            totalReputation={totalReputation}
+            nativeToken={nativeToken}
+            avatar={profile?.thumbnail || profile?.avatar || ''}
+            user={user}
+          />
+        )}
+        {!isWalletConnected && (
+          <>
+            <div className={styles.mobileButtons}>
+              <Button mode="tertiaryOutline" isFullRounded>
+                <Icon name="cardholder" appearance={{ size: 'tiny' }} />
+                <p className="text-sm font-inter font-medium ml-1">{formatMessage({ id: 'connectWallet' })}</p>
+              </Button>
+              <Button mode="tertiaryOutline" isFullRounded>
+                <Icon name="list" appearance={{ size: 'extraTiny' }} />
+                <p className="text-sm font-inter font-medium ml-1">{formatMessage({ id: 'help' })}</p>
+              </Button>
+            </div>
+            <div className="w-full pb-6 mb-6 border-b border-b-gray-200 md:pb-5 md:mb-5">
+              <Button mode="quaternaryOutline" isFullSize onClick={connectWallet}>
+                {formatMessage({ id: 'connectWallet' })}
+              </Button>
+            </div>
+          </>
+        )}
+        <div className="w-full pb-6 mb-6 border-b border-b-gray-200 md:pb-5 md:mb-5">
+          <TitledContent title={{ id: 'userMenu.optionsTitle' }}>
+            <ul className="text-lg font-semibold md:font-normal md:text-md">
+              <li className="mb-4">
+                <Link to="/" className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <Icon name="circles-three-plus" appearance={{ size: iconSize }} />
+                    <p className="ml-2">{formatMessage({ id: 'userMenu.getStartedTitle' })}</p>
+                  </div>
+                </Link>
+              </li>
+              <li className="mb-4">
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full"
+                  onClick={() => setActiveSubmenu('userMenu.contactAndSupportTitle')}
+                >
+                  <div className="flex items-center">
+                    <Icon name="lifebuoy" appearance={{ size: iconSize }} />
+                    <p className="ml-2">{formatMessage({ id: 'userMenu.contactAndSupportTitle' })}</p>
+                  </div>
+                  <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
+                </button>
+              </li>
+              <li className="mb-4">
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full"
+                  onClick={() => setActiveSubmenu('userMenu.developersTitle')}
+                >
+                  <div className="flex items-center">
+                    <Icon name="code" appearance={{ size: iconSize }} />
+                    <p className="ml-2">{formatMessage({ id: 'userMenu.developersTitle' })}</p>
+                  </div>
+                  <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="flex items-center justify-between w-full"
+                  onClick={() => setActiveSubmenu('userMenu.legalAndPrivacyTitle')}
+                >
+                  <div className="flex items-center">
+                    <Icon name="briefcase" appearance={{ size: iconSize }} />
+                    <p className="ml-2">{formatMessage({ id: 'userMenu.legalAndPrivacyTitle' })}</p>
+                  </div>
+                  <Icon name="caret-right" appearance={{ size: 'extraTiny' }} />
+                </button>
+              </li>
+            </ul>
           </TitledContent>
         </div>
-      )}
-      <ThemeSwitcher />
+        {isWalletConnected && (
+          <div className="w-full mb-6 md:mb-5">
+            <TitledContent title={{ id: 'userMenu.other' }}>
+              <Link to="/" className="flex items-center text-lg font-semibold md:font-normal md:text-md text-gray-900">
+                <Icon name="plugs" appearance={{ size: iconSize }} />
+                <p className="ml-2">{formatMessage({ id: 'userMenu.disconnectWalletTitle' })}</p>
+              </Link>
+            </TitledContent>
+          </div>
+        )}
+        <ThemeSwitcher />
+      </div>
     </PopoverBase>
   );
 };
---
+++ src/components/common/Extensions/UserNavigation/partials/UserSubmenu/UserSubmenu.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { UserSubmenuProps } from './types';
+import { userSubmenuItems } from './consts';
+import Icon from '~shared/Icon/Icon';
+
+const UserSubmenu: FC<UserSubmenuProps> = ({ submenuId }) => (
+  <ul className="mt-2">
+    {userSubmenuItems[submenuId].map((item) => (
+      <li key={item.id} className="mb-4 last:mb-0">
+        <Link to={item.url} className="flex items-center">
+          <Icon name={item.icon} appearance={{ size: 'small' }} />{' '}
+          <p className="ml-2 text-lg font-semibold md:font-normal md:text-md">{item.label}</p>
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
+
+export default UserSubmenu;
