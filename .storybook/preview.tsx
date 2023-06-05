13df03c180b2ca18a905d312802b4c615f808199
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-06-05 13:10:30 +0200

    fix: import order

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,10 @@
-import '~utils/yup/customMethods';
+import React from 'react';
 import type { Preview } from '@storybook/react';
+import { MockedProvider } from '@apollo/client/testing';
+
+import '~utils/yup/customMethods';
 import '../src/styles/main.global.css';
 import { reactIntl } from './reactIntl.js';
-import { MockedProvider } from '@apollo/client/testing';
-import React from 'react';
 import { applyTheme } from "../src/components/frame/Extensions/themes/utils";
 
 const preview: Preview = {
