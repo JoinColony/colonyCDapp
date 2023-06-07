e1d6ce6838d1f1d8786af2a5f020707b54588cc7
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:35:01 +0200

    Add Link components

---
+++ src/components/shared/Extensions/NavLink/types.ts
@@ -0,0 +1,11 @@
+import { MessageDescriptor } from 'react-intl';
+import { NavLinkProps as ReactNavLinkProps } from 'react-router-dom';
+import { SimpleMessageValues } from '~types';
+
+export interface NavLinkProps extends Omit<ReactNavLinkProps, 'title'> {
+  to: string | any;
+  text?: MessageDescriptor | string;
+  textValues?: SimpleMessageValues;
+  title?: MessageDescriptor | string;
+  titleValues?: SimpleMessageValues;
+}
