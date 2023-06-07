1a81f92ea3d4c4ad0c0d6ab0ebf8dfb72af9a258
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:35:33 +0200

    Add Card, Popover, and Modal

---
+++ src/components/shared/Extensions/PopoverBase/PopoverBase.tsx
@@ -0,0 +1,32 @@
+import React, { FC, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import { PopoverBaseProps } from './types';
+import Card from '../Card';
+
+const displayName = 'shared.Extensions.PopoverBase';
+
+const PopoverBase: FC<PropsWithChildren<PopoverBaseProps>> = ({
+  tooltipProps,
+  setTooltipRef,
+  classNames,
+  children,
+  cardProps,
+  withTooltipStyles = true,
+}) => {
+  return (
+    <div
+      ref={setTooltipRef}
+      {...tooltipProps({
+        className: clsx(classNames, {
+          'z-[9999] tooltip-container': withTooltipStyles,
+        }),
+      })}
+    >
+      {cardProps ? <Card {...cardProps}>{children}</Card> : children}
+    </div>
+  );
+};
+
+PopoverBase.displayName = displayName;
+
+export default PopoverBase;
