277a9b78dcfbcc77965bb1ca6ccb3e5426acc535
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:32:25 +0200

    Add Extension Content components

---
+++ src/components/common/Extensions/SupportingDocuments/SupportingDocuments.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { useIntl } from 'react-intl';
+import LinkWrapper from './LinkWrapper';
+import { SupportingDocumentsProps } from './types';
+
+const displayName = 'common.Extensions.SupportingDocuments';
+
+const SupportingDocuments: FC<SupportingDocumentsProps> = ({
+  isDoubleLinkVisible = false,
+}) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <div>
+      <div className="uppercase text-xs font-medium text-blue-400">
+        {formatMessage({ id: 'supporting.documents.title' })}
+      </div>
+      <div className="font-semibold text-md text-gray-900">
+        {formatMessage({ id: 'supporting.documents.subtitle' })}
+      </div>
+      <span className="block border-b border-gray-200 my-4" />
+      <LinkWrapper isDoubleLinkVisible={isDoubleLinkVisible} />
+    </div>
+  );
+};
+
+SupportingDocuments.displayName = displayName;
+
+export default SupportingDocuments;
