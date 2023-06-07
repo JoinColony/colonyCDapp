dcf0d1cd0d1455a285c5c7598dce40833a0d8cd1
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:18:58 +0200

    Add admin pages

---
+++ src/components/frame/Extensions/pages/ExtensionDetailsPage/ExtensionDetailsPage.tsx
@@ -0,0 +1,118 @@
+import React, { FC, useState } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useParams } from 'react-router-dom';
+
+import { useColonyContext, useExtensionData } from '~hooks';
+import Icon from '~shared/Icon';
+import ExtensionDetails from './partials/ExtensionDetails';
+import Spinner from '~shared/Extensions/Spinner';
+import ThreeColumns from '~frame/Extensions/ThreeColumns';
+import Navigation from '~common/Extensions/Navigation';
+import SupportingDocuments from '~common/Extensions/SupportingDocuments';
+import ImageCarousel from '~common/Extensions/ImageCarousel';
+import ActionButtons from '../partials/ActionButtons';
+import NotificationBanner from '~common/Extensions/NotificationBanner';
+import { isInstalledExtensionData } from '~utils/extensions';
+
+const HeadingChunks = (chunks: React.ReactNode[]) => (
+  <h4 className="font-semibold text-gray-900 mt-6 mb-4">{chunks}</h4>
+);
+
+const displayName = 'frame.Extensions.pages.ExtensionDetailsPage';
+
+const ExtensionDetailsPage: FC = () => {
+  const { extensionId } = useParams();
+  const { colony } = useColonyContext();
+  const { extensionData } = useExtensionData(extensionId ?? '');
+  const { formatMessage } = useIntl();
+  // @TODO: Change extension missing permissions functionality
+  const [isPermissionEnabled, setIsPermissionEnabled] = useState(false);
+
+  if (!colony || !extensionData) {
+    return null;
+  }
+
+  if (!extensionData) {
+    return (
+      <p>
+        {formatMessage({ id: 'extensionDetailsPage.unsupportedExtension' })}
+      </p>
+    );
+  }
+
+  const showEnableBanner =
+    extensionData.extensionId !== 'VotingReputation' &&
+    !isInstalledExtensionData(extensionData);
+
+  return (
+    <Spinner loadingText={{ id: 'loading.colonyDetailsPage' }}>
+      <ThreeColumns
+        leftAside={<Navigation />}
+        topRow={
+          <>
+            {showEnableBanner && (
+              <div className="mb-6">
+                <NotificationBanner
+                  title={
+                    isPermissionEnabled
+                      ? { id: 'extensionReEnable.notification.updated' }
+                      : { id: 'extensionReEnable.notification.missing' }
+                  }
+                  status={isPermissionEnabled ? 'success' : 'warning'}
+                  actionText={
+                    isPermissionEnabled
+                      ? { id: 'extensionReEnable.notification.enabled' }
+                      : { id: 'extensionReEnable.notification.enable' }
+                  }
+                  actionType="call-to-action"
+                  onClick={() => setIsPermissionEnabled(true)}
+                />
+              </div>
+            )}
+            <div className="flex justify-between flex-col flex-wrap sm:items-center sm:flex-row sm:gap-6">
+              <div className="flex flex-col sm:items-center sm:flex-row sm:gap-2 sm:grow">
+                <div className="flex items-center shrink-0">
+                  <Icon
+                    name={extensionData.icon}
+                    appearance={{ size: 'large' }}
+                  />
+                  <h4 className="ml-2 text-xl font-semibold text-gray-900">
+                    {formatMessage(extensionData.name)}
+                  </h4>
+                </div>
+                {/* @TODO get these values from API (badge and active installs number) */}
+                <div className="flex items-center justify-between gap-4 mt-4 sm:mt-0 sm:grow">
+                  <span>badge</span>
+                  <p className="text-gray-400 text-sm">
+                    17,876 {formatMessage({ id: 'active.installs' })}
+                  </p>
+                </div>
+              </div>
+              <ActionButtons extensionData={extensionData} />
+            </div>
+          </>
+        }
+        withSlider={<ImageCarousel />}
+        rightAside={<ExtensionDetails extensionData={extensionData} />}
+      >
+        <div>
+          <div className="mt:mt-[4.25rem] text-md text-gray-600">
+            <FormattedMessage
+              {...extensionData.descriptionLong}
+              values={{
+                h4: HeadingChunks,
+              }}
+            />
+          </div>
+          <div className="mt-6">
+            <SupportingDocuments />
+          </div>
+        </div>
+      </ThreeColumns>
+    </Spinner>
+  );
+};
+
+ExtensionDetailsPage.displayName = displayName;
+
+export default ExtensionDetailsPage;
---
+++ src/components/frame/Extensions/pages/ExtensionDetailsPage/partials/ExtensionDetails/ExtensionDetails.tsx
@@ -0,0 +1,150 @@
+import React, { FC, useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+
+import { ExtensionDetailsProps } from './types';
+import SpecificSidePanel from '~common/Extensions/SpecificSidePanel/SpecificSidePanel';
+import Button from '~shared/Extensions/Button';
+import { useExtensionDetails } from './hooks';
+import Modal from '~shared/Extensions/Modal';
+import Checkbox from '~common/Extensions/Checkbox';
+
+const displayName =
+  'frame.Extensions.pages.ExtensionDetailsPage.partials.ExtensionDetails';
+
+const ExtensionDetails: FC<ExtensionDetailsProps> = ({ extensionData }) => {
+  const { formatMessage } = useIntl();
+  const {
+    canExtensionBeUninstalled,
+    canExtensionBeDeprecated,
+    handleDeprecate,
+    handleUninstall,
+    handleReEnable,
+  } = useExtensionDetails(extensionData);
+  const [isDeprecateModalOpen, setIsDeprecateModalOpen] = useState(false);
+  const [isUninstallModalOpen, setIsUninstallModalOpen] = useState(false);
+  const [isReEnableModalOpen, setIsReEnableModalOpen] = useState(false);
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
+  useEffect(() => {
+    if (!isUninstallModalOpen) {
+      setIsCheckboxChecked(false);
+    }
+  }, [isUninstallModalOpen]);
+
+  return (
+    <div>
+      <SpecificSidePanel extensionData={extensionData} />
+      {canExtensionBeDeprecated && (
+        <div className="mt-6">
+          <Button
+            mode="primaryOutline"
+            isFullSize
+            onClick={() => setIsDeprecateModalOpen(true)}
+          >
+            {formatMessage({ id: 'extensionDetailsPage.deprecate' })}
+          </Button>
+          <Modal
+            isOpen={isDeprecateModalOpen}
+            icon="question"
+            onClose={() => setIsDeprecateModalOpen(false)}
+            title={formatMessage({ id: 'extensionDetailsPage.deprecate' })}
+            subTitle={formatMessage({
+              id: 'extensionDetailsPage.deprecateDescription',
+            })}
+            onConfirm={handleDeprecate}
+            confirmMessage={formatMessage({
+              id: 'extensionDetailsPage.deprecate',
+            })}
+            closeMessage={formatMessage({ id: 'extensionDetailsPage.cancel' })}
+            buttonMode="primarySolid"
+          >
+            <p className="text-gray-600 text-md mt-6">
+              {formatMessage({ id: 'extensionDetailsPage.confirmation' })}
+            </p>
+          </Modal>
+        </div>
+      )}
+      {canExtensionBeUninstalled && (
+        <div className="mt-6 flex flex-col gap-4">
+          <div>
+            <Button
+              mode="primaryOutline"
+              isFullSize
+              onClick={() => setIsReEnableModalOpen(true)}
+            >
+              {formatMessage({ id: 'extensionReEnable.title' })}
+            </Button>
+            <Modal
+              title={formatMessage({ id: 'extensionReEnable.modal.title' })}
+              subTitle={formatMessage({
+                id: 'extensionReEnable.modal.subTitle',
+              })}
+              isOpen={isReEnableModalOpen}
+              onClose={() => setIsReEnableModalOpen(false)}
+              onConfirm={handleReEnable}
+              confirmMessage={formatMessage({ id: 'button.confirm' })}
+              closeMessage={formatMessage({
+                id: 'extensionDetailsPage.cancel',
+              })}
+            />
+          </div>
+          <div>
+            <Button
+              mode="primaryOutline"
+              isFullSize
+              onClick={() => setIsUninstallModalOpen(true)}
+            >
+              {formatMessage({ id: 'extensionDetailsPage.uninstall' })}
+            </Button>
+            <Modal
+              isOpen={isUninstallModalOpen}
+              icon="trash"
+              isWarning
+              onClose={() => setIsUninstallModalOpen(false)}
+              onConfirm={handleUninstall}
+              title={formatMessage({
+                id: 'extensionDetailsPage.uninstallTitle',
+              })}
+              subTitle={formatMessage({
+                id: 'extensionDetailsPage.uninstallDescription',
+              })}
+              confirmMessage={formatMessage({
+                id: 'extensionDetailsPage.confirmUninstall',
+              })}
+              closeMessage={formatMessage({
+                id: 'extensionDetailsPage.cancelUninstall',
+              })}
+              disabled={!isCheckboxChecked}
+            >
+              <div className="rounded-md border bg-negative-100 border-negative-400 p-4 text-negative-400 mt-6">
+                <ul className="text-sm list-disc pl-4">
+                  <li>
+                    {formatMessage({
+                      id: 'extensionDetailsPage.uninstallBoxOne',
+                    })}
+                  </li>
+                  <li>
+                    {formatMessage({
+                      id: 'extensionDetailsPage.uninstallBoxTwo',
+                    })}
+                  </li>
+                </ul>
+              </div>
+              <Checkbox
+                name="uninstall"
+                id="uninstall"
+                label={{ id: 'extensionDetailsPage.uninstallConfirmation' }}
+                onChange={() => setIsCheckboxChecked((prevState) => !prevState)}
+                classNames="mt-5"
+              />
+            </Modal>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+ExtensionDetails.displayName = displayName;
+
+export default ExtensionDetails;
