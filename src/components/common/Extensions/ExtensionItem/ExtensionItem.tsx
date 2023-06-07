53961ac5f3235f0cdf098de653630fca84d59a0f
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:32:40 +0200

    Add extension item

---
+++ src/components/common/Extensions/ExtensionItem/ExtensionItem.tsx
@@ -0,0 +1,68 @@
+import React, { FC } from 'react';
+import { useIntl } from 'react-intl';
+import { ExtensionItemProps } from './types';
+import ExtensionStatusBadge from '~common/Extensions/ExtensionStatusBadge';
+import Button from '~shared/Extensions/Button';
+import Icon from '~shared/Icon';
+import { useMobile } from '~hooks';
+import Link from '~shared/Extensions/Link';
+import styles from './ExtensionItem.module.css';
+import { useExtensionItem } from './hooks';
+
+const displayName = 'common.Extensions.ExtensionItem';
+
+const ExtensionItem: FC<ExtensionItemProps> = ({
+  title,
+  description,
+  version,
+  icon,
+  extensionId = '',
+}) => {
+  const { formatMessage } = useIntl();
+  const isMobile = useMobile();
+  const {
+    badgeMessage,
+    extensionUrl,
+    handleInstallClick,
+    isExtensionInstalled,
+    status,
+  } = useExtensionItem(extensionId);
+
+  return (
+    <div className="flex items-center flex-shrink-0">
+      <Icon name={icon} appearance={{ size: 'large' }} />
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between ml-4 w-full gap-6 sm:gap-12">
+        <div>
+          <div className="flex items-center">
+            <h5 className="text-md font-medium mr-2">{formatMessage(title)}</h5>
+            <p className="text-xs font-medium text-gray-600 mr-2">v{version}</p>
+            <ExtensionStatusBadge mode={status} text={badgeMessage} />
+          </div>
+          <p className="text-sm text-gray-600 mt-0.5">
+            {formatMessage(description)}
+          </p>
+        </div>
+        {isExtensionInstalled && (
+          <div className="sm:ml-4 flex-shrink-0">
+            <Link to={extensionUrl} className={styles.extensionItemButton}>
+              {formatMessage({ id: 'button.manage' })}
+            </Link>
+          </div>
+        )}
+        {!isExtensionInstalled && (
+          <Button
+            mode="primarySolid"
+            isFullSize={isMobile}
+            onClick={handleInstallClick}
+          >
+            {formatMessage({ id: 'button.install' })}
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+ExtensionItem.displayName = displayName;
+
+export default ExtensionItem;
---
+++ src/components/common/Extensions/ExtensionItem/hooks.ts
@@ -0,0 +1,52 @@
+import { useCallback, useMemo } from 'react';
+import { useAsyncFunction, useColonyContext, useExtensionData } from '~hooks';
+import { ActionTypes } from '~redux';
+import { isInstalledExtensionData } from '~utils/extensions';
+import { useExtensionsBadge } from '~hooks/useExtensionsBadgeStatus';
+
+export const useExtensionItem = (extensionId: string) => {
+  const { colony } = useColonyContext();
+  const { extensionData } = useExtensionData(extensionId);
+
+  const isExtensionInstalled =
+    extensionData && isInstalledExtensionData(extensionData);
+
+  const { status, badgeMessage } = useExtensionsBadge(extensionData);
+
+  const extensionValues = useMemo(() => {
+    return {
+      colonyAddress: colony?.colonyAddress,
+      extensionData,
+    };
+  }, [colony?.colonyAddress, extensionData]);
+
+  const submit = ActionTypes.EXTENSION_INSTALL;
+  const error = ActionTypes.EXTENSION_INSTALL_ERROR;
+  const success = ActionTypes.EXTENSION_INSTALL_SUCCESS;
+
+  const asyncFunction = useAsyncFunction({ submit, error, success });
+
+  const handleInstallClick = useCallback(async () => {
+    try {
+      await asyncFunction(extensionValues);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [asyncFunction, extensionValues]);
+
+  // when extensionId: "VotingReputation" and extenstion is installed and enabled then url should redirect to extenstion setup page
+  const extensionUrl =
+    isExtensionInstalled &&
+    extensionId === 'VotingReputation' &&
+    extensionData.isEnabled
+      ? `/colony/${colony?.name}/extensions/${extensionId}/setup`
+      : `/colony/${colony?.name}/extensions/${extensionId}`;
+
+  return {
+    extensionUrl,
+    handleInstallClick,
+    isExtensionInstalled,
+    status,
+    badgeMessage,
+  };
+};
