70e73c8cf0f6e5167496dc49e3e2eae1afd75a27
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-07 18:33:15 +0200

    Add Button components

---
+++ src/components/shared/Extensions/ConnectingWalletButton/ConnectingWalletButton.tsx
@@ -0,0 +1,37 @@
+import React, { useLayoutEffect } from 'react';
+import { useIntl } from 'react-intl';
+import { useAppContext } from '~hooks';
+import Button from '~shared/Extensions/Button';
+import { getLastWallet } from '~utils/autoLogin';
+
+const displayName = 'Extensions.ConnectingWalletButton';
+
+const ConnectingWalletButton = () => {
+  const { formatMessage } = useIntl();
+  const { wallet, walletConnecting, connectWallet } = useAppContext();
+
+  useLayoutEffect(() => {
+    if (!wallet && connectWallet && getLastWallet()) {
+      connectWallet();
+    }
+  }, [connectWallet, wallet]);
+
+  return (
+    <>
+      {walletConnecting && (
+        <Button
+          isPending={walletConnecting}
+          mode="pending"
+          title="pending"
+          onClick={connectWallet}
+        >
+          {formatMessage({ id: 'pending' })}
+        </Button>
+      )}
+    </>
+  );
+};
+
+ConnectingWalletButton.displayName = displayName;
+
+export default ConnectingWalletButton;
---
+++ src/stories/shared/PopoverButton.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PopoverButton from '~shared/Extensions/PopoverButton';
+
+const meta: Meta<typeof PopoverButton> = {
+  title: 'Shared/Popover Button',
+  component: PopoverButton,
+  argTypes: {
+    type: {
+      name: 'Mode',
+      options: ['deposit', 'withdraw', 'view'],
+      control: {
+        type: 'select',
+      },
+    },
+    isDisabled: {
+      name: 'Disabled',
+      control: {
+        type: 'boolean',
+      },
+    },
+    isFullWidth: {
+      name: 'isFullWidth',
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  args: {
+    type: 'deposit',
+    isDisabled: false,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof PopoverButton>;
+
+export const Primary: Story = {
+  args: {
+    type: 'deposit',
+  },
+};
