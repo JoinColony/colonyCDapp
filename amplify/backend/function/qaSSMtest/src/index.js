80d76cc065f2238116735e3b10f4e5d67fecbf00
William Martin <williamajmartin@gmail.com>
2023-06-15 18:50:12 +0200

    refactor: add getParams to handle all params

--- amplify/backend/function/qaSSMtest/src/index.js
+++ amplify/backend/function/qaSSMtest/src/index.js
@@ -1,10 +1,15 @@
-const { getGraphQLURI } = require('./utils');
+const { getParam } = require('./utils');
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
 exports.handler = async () => {
-  const uri = await getGraphQLURI();
-  return uri;
+  const graphqlUrl = await getParam('graphqlUrl');
+  const reputationEndpoint = await getParam('reputationEndpoint');
+  const chainNetworkContract = await getParam('chainNetworkContract');
+  const chainRpcEndpoint = await getParam('chainRpcEndpoint');
+  const chainNetwork = await getParam('chainNetwork');
+ console.log({graphqlUrl, reputationEndpoint, chainNetworkContract, chainRpcEndpoint, chainNetwork})
+  return null;
 };
