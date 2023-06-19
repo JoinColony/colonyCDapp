d4fa70707d6ab164a8c78a7f8cf0bf7cde613bad
William Martin <williamajmartin@gmail.com>
2023-06-19 12:40:35 +0200

    test: endpoints inside qaSSMTest

--- amplify/backend/function/qaSSMtest/src/index.js
+++ amplify/backend/function/qaSSMtest/src/index.js
@@ -1,24 +1,82 @@
-const { getParam } = require('/opt/nodejs/getParams');
+const { getColonyNetworkClient, Network } = require('@colony/colony-js');
+const { providers } = require('ethers');
+
+const { getColony } = require('./queries');
+const { graphqlRequest } = require('./utils');
+
+let apiKey = 'da2-fakeApiId123456';
+let graphqlURL = 'http://localhost:20002/graphql';
+let rpcURL = 'http://network-contracts.docker:8545'; // this needs to be extended to all supported networks
+let network = Network.Custom;
+let networkAddress;
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+  if (ENV === 'qa') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [networkAddress, apiKey, graphqlURL, rpcURL, network] = await getParams([
+      'chainNetworkContract',
+      'appSyncApi',
+      'graphqlUrl',
+      'chainRpcEndpoint',
+      'chainNetwork',
+    ]);
+  } else {
+    const {
+      etherRouterAddress,
+    } = require('../../../../mock-data/colonyNetworkArtifacts/etherrouter-address.json');
+    networkAddress = etherRouterAddress;
+  }
+};
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-exports.handler = async () => {
-  const graphqlUrl = await getParam('graphqlUrl');
-  const reputationEndpoint = await getParam('reputationEndpoint');
-  const chainNetworkContract = await getParam('chainNetworkContract');
-  const chainRpcEndpoint = await getParam('chainRpcEndpoint');
-  const chainNetwork = await getParam('chainNetwork');
-  const appSyncApi = await getParam('appSyncApi');
+exports.handler = async ({ source: { id: colonyAddress } }) => {
+  try {
+    await setEnvVariables();
+  } catch (e) {
+    console.error(
+      'Unable to set environment variables. Exiting lambda early. Reason:',
+      e,
+    );
+    return {
+      items: [],
+    };
+  }
 
   console.log({
-    graphqlUrl,
-    reputationEndpoint,
-    chainNetworkContract,
-    chainRpcEndpoint,
-    chainNetwork,
-    appSyncApi,
+    apiKey,
+    graphqlURL,
+    rpcURL,
+    network,
+    networkAddress,
   });
-  return null;
+
+  const response = await graphqlRequest(
+    getColony,
+    { address: colonyAddress },
+    graphqlURL,
+    apiKey,
+  );
+
+  const { getColony: colony } = response?.data || {};
+
+  if (!colony) {
+    return { items: [] };
+  }
+
+  const provider = new providers.JsonRpcProvider(rpcURL);
+  const networkClient = await getColonyNetworkClient(network, provider, {
+    networkAddress,
+  });
+
+  const colonyClient = await networkClient.getColonyClient(colonyAddress);
+
+  console.log({ response, colonyClient, networkClient });
+
+  return {
+    items: [],
+  };
 };
