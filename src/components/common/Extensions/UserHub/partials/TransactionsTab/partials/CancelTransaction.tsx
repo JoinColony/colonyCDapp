aca2aade665597588a5152cb02b54db766fddf96
krawczykkatarzyna <katarzyna.krawczyk@pagepro.pl>
2023-06-19 11:31:45 +0200

    ui/15399758 cancel transaction logic

---
+++ src/components/common/Extensions/UserHub/partials/TransactionsTab/partials/CancelTransaction.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import { useIntl } from 'react-intl';
+
+import { CancelTransactionProps } from '../types';
+import Button from '~shared/Extensions/Button/Button';
+
+const displayName =
+  'common.Extensions.UserHub.partials.TransactionsTab.partials.CancelTransaction';
+
+const CancelTransaction: FC<CancelTransactionProps> = ({
+  isShowingCancelConfirmation,
+  handleCancelTransaction,
+  toggleCancelConfirmation,
+}) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <>
+      {isShowingCancelConfirmation ? (
+        <div className="flex gap-2">
+          <Button
+            type="button"
+            mode="textButton"
+            className="text-red-400"
+            onClick={handleCancelTransaction}
+          >
+            {formatMessage({ id: 'button.yes' })}
+          </Button>
+          <span>/</span>
+          <Button
+            type="button"
+            mode="textButton"
+            className="text-blue-400"
+            onClick={toggleCancelConfirmation}
+          >
+            {formatMessage({ id: 'button.no' })}
+          </Button>
+        </div>
+      ) : (
+        <Button
+          type="button"
+          mode="textButton"
+          onClick={toggleCancelConfirmation}
+        >
+          {formatMessage({ id: 'button.cancel' })}
+        </Button>
+      )}
+    </>
+  );
+};
+
+CancelTransaction.displayName = displayName;
+
+export default CancelTransaction;
