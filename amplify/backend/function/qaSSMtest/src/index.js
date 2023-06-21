6266b3fc503dcb6ee6f244650b27b2f5685db078
William Martin <williamajmartin@gmail.com>
2023-06-21 18:31:02 +0200

    test: test lambda with reputationOracle endpoint

--- amplify/backend/function/qaSSMtest/src/index.js
+++ amplify/backend/function/qaSSMtest/src/index.js
@@ -1,20 +1,32 @@
-const { getColonyNetworkClient, Network } = require('@colony/colony-js');
 const { providers } = require('ethers');
-
-const { getColony, getColony2, getTokenByAddress } = require('./queries');
-const { graphqlRequest } = require('./utils');
+const { getColonyNetworkClient } = require('@colony/colony-js');
 
 let apiKey = 'da2-fakeApiId123456';
 let graphqlURL = 'http://localhost:20002/graphql';
-let rpcURL = 'http://network-contracts.docker:8545'; // this needs to be extended to all supported networks
-let network = Network.Custom;
+let reputationOracleEndpoint;
+let rpcURL;
+let network;
 let networkAddress;
 
 const setEnvVariables = async () => {
   const ENV = process.env.ENV;
   if (ENV === 'qa') {
     const { getParams } = require('/opt/nodejs/getParams');
-    [apiKey, graphqlURL] = await getParams(['appsyncApiKey', 'graphqlUrl']);
+    [
+      apiKey,
+      graphqlURL,
+      reputationOracleEndpoint,
+      networkAddress,
+      rpcURL,
+      network,
+    ] = await getParams([
+      'appsyncApiKey',
+      'graphqlUrl',
+      'reputationEndpoint',
+      'networkContractAddress',
+      'chainRpcEndpoint',
+      'chainNetwork',
+    ]);
   }
 };
 
@@ -22,29 +34,26 @@ const setEnvVariables = async () => {
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
-exports.handler = async (event) => {
-  const {
-    id: colonyAddress,
-    name,
-    colonyNativeTokenId,
-  } = event.arguments?.input || {};
-
+exports.handler = async ({ walletAddress, colonyAddress }) => {
   await setEnvVariables();
+  const provider = new providers.JsonRpcProvider(rpcURL);
 
-  const colonyQuery = await graphqlRequest(
-    getColony2,
-    { id: colonyAddress, name },
-    graphqlURL,
-    apiKey,
+  const networkClient = getColonyNetworkClient(network, provider, {
+    networkAddress,
+    reputationOracleEndpoint,
+  });
+  const colonyClient = await networkClient.getColonyClient(colonyAddress);
+  const { skillId } = await colonyClient.getDomain(1);
+  const { reputationAmount } = await colonyClient.getReputationWithoutProofs(
+    skillId,
+    walletAddress,
   );
-
-  const tokenQuery = await graphqlRequest(
-    getTokenByAddress,
-    { id: colonyNativeTokenId },
-    graphqlURL,
+  console.log({
     apiKey,
-  );
-
-  console.log({ colonyQuery, tokenQuery });
+    graphqlURL,
+    reputationOracleEndpoint,
+    reputationAmount,
+    rpcURL,
+  });
   return null;
 };
