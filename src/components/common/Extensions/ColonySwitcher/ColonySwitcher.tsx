98198f0bc2085aa6df62d112561d80a8814b3c9b
KatarzynaStefanowiczPagepro <katarzyna.stefanowicz@pagepro.co>
2023-06-23 09:46:08 +0200

    colony switcher - added todo

--- src/components/common/Extensions/ColonySwitcher/ColonySwitcher.tsx
+++ src/components/common/Extensions/ColonySwitcher/ColonySwitcher.tsx
@@ -114,12 +114,18 @@ const ColonySwitcher = () => {
           )}
         </div>
       )}
+      {/* @TODO button */}
       {isMobile && isOpen && (
         <button
           type="button"
           aria-label={formatMessage({ id: 'ariaLabel.closeDropdown' })}
           onClick={() => setIsOpen(false)}
-          className="text-gray-400 sm:pr-4"
+          className={clsx(
+            'flex items-center transition-all duration-normal hover:text-gray-500',
+            {
+              'w-[3.5rem]': !isMobile,
+            },
+          )}
         >
           <Icon name="close" appearance={{ size: 'extraTiny' }} />
         </button>
