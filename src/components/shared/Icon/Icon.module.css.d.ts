d9ccb5720727922d93d298b23b158981c0ac5ed8
KatarzynaStefanowiczPagepro <katarzyna.stefanowicz@pagepro.co>
2023-06-23 13:55:56 +0200

    removed unused file

--- src/components/shared/Icon/Icon.module.css.d.ts
+++ src/components/shared/Icon/Icon.module.css.d.ts
@@ -1,7 +1,6 @@
 declare namespace IconModuleCssNamespace {
   export interface IIconModuleCss {
     main: string;
-    sizeExtraExtraTiny: string;
     sizeExtraTiny: string;
     sizeHuge: string;
     sizeLarge: string;
