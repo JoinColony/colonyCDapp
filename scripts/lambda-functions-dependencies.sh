0f7e9f86ce7c4b4c12055446905b3b4769a90a76
Arren V <33682027+arrenv@users.noreply.github.com>
2023-06-25 12:38:22 +0200

    Debugging for directory issue

--- scripts/lambda-functions-dependencies.sh
+++ scripts/lambda-functions-dependencies.sh
@@ -3,13 +3,16 @@
 pwd=$(pwd)
 
 for d in amplify/backend/function/*/ ; do
+    echo "Current directory: $d"
     [ -L "${d%/}" ] && continue
     fnName=$(echo ${d} | cut -c 26- | rev | cut -c 2- | rev)
-    cd "${pwd}/${d}src"
-    # Skip if the folder doesn't contain package.json
-    # Otherwise npm will look for the root package.json and cause an infinite loop
-    !([ -f "package.json" ]) && continue
-    echo "Installing dependencies for Lambda Function \"${fnName}\""
-    npm i
-    echo
+    if [ -d "${pwd}/${d}src" ]; then
+        cd "${pwd}/${d}src"
+        !([ -f "package.json" ]) && continue
+        echo "Installing dependencies for Lambda Function \"${fnName}\""
+        npm i
+        echo
+    else
+        echo "Directory ${pwd}/${d}src does not exist"
+    fi
 done
