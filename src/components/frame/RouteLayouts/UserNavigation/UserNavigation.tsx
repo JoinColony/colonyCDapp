c22ae42c96d2482869499b9a80684042726836fc
William Martin <williamajmartin@gmail.com>
2023-06-28 11:23:39 +0200

    update: calls to getUserTokenBalance

--- src/components/frame/RouteLayouts/UserNavigation/UserNavigation.tsx
+++ src/components/frame/RouteLayouts/UserNavigation/UserNavigation.tsx
@@ -58,6 +58,7 @@ const UserNavigation = () => {
       input: {
         walletAddress: wallet?.address ?? '',
         tokenAddress: nativeToken?.tokenAddress ?? '',
+        colonyAddress: colonyAddress ?? '',
       },
     },
     skip: !wallet?.address || !nativeToken?.tokenAddress,
