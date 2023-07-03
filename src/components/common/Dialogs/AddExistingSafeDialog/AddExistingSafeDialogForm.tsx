220bb9e1ef49896898185d16266e9609c80752fa
Armando Graterol <armandogrtrl@gmail.com>
2023-07-03 17:18:51 -0300

    Implemented Add Safe step 1 dialog

---
+++ src/components/common/Dialogs/AddExistingSafeDialog/AddExistingSafeDialogForm.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { defineMessages } from 'react-intl';
+import { useFormContext } from 'react-hook-form';
+
+import { DialogSection } from '~shared/Dialog';
+import Heading from '~shared/Heading';
+// import { ColonySafe } from '~data/index';
+import { Address } from '~types/index';
+
+import { NetworkOption, SafeContract } from './helpers';
+import {
+  CheckSafe,
+  // ConnectSafe,
+  // ConfirmSafe,
+} from './index';
+
+import styles from './AddExistingSafeDialogForm.css';
+
+export type SafeData =
+  | SafeContract
+  | undefined
+  | null
+  | Record<string, never>
+  | 'alreadyExists';
+
+const displayName = 'common.AddExistingSafeDialog.AddExistingSafeDialogForm';
+
+const MSG = defineMessages({
+  title: {
+    id: `${displayName}.title`,
+    defaultMessage: 'Adding a Safe',
+  },
+});
+
+export interface AddExistingSafeProps {
+  back: () => void;
+  networkOptions: NetworkOption[];
+  colonySafes: Array<any>; // ColonySafe[];
+  colonyAddress: Address;
+  loadingState: [boolean, React.Dispatch<React.SetStateAction<boolean>>][];
+  stepIndex: number;
+  setStepIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const AddExistingSafeDialogForm = ({
+  networkOptions,
+  loadingState,
+  stepIndex,
+  setStepIndex,
+  ...props
+}: AddExistingSafeProps) => {
+  const { watch } = useFormContext();
+  const { chainId } = watch();
+  const [selectedChain, setSelectedChain] = useState<NetworkOption>(
+    // chainId's initial value is set from networkOptions, therefore will return
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    networkOptions.find((options) => options.value === chainId)!,
+  );
+
+  const renderStep = () => {
+    switch (stepIndex) {
+      case 1:
+        return (
+          <CheckSafe
+            {...props}
+            networkOptions={networkOptions}
+            setStepIndex={setStepIndex}
+            loadingState={loadingState}
+            selectedChain={selectedChain}
+            setSelectedChain={setSelectedChain}
+          />
+        );
+      // case 2:
+      //   return (
+      //     <ConnectSafe
+      //       {...props}
+      //       values={values}
+      //       setStepIndex={setStepIndex}
+      //       safeAddress={contractAddress}
+      //       loadingState={loadingState[1]}
+      //     />
+      //   );
+      // case 3:
+      //   return (
+      //     <ConfirmSafe {...props} values={values} setStepIndex={setStepIndex} />
+      //   );
+      default:
+        return null;
+    }
+  };
+  return (
+    <div className={styles.main}>
+      <DialogSection appearance={{ theme: 'sidePadding' }}>
+        <div className={styles.headingContainer}>
+          <Heading
+            appearance={{ size: 'medium', margin: 'none', theme: 'dark' }}
+            text={MSG.title}
+          />
+        </div>
+      </DialogSection>
+      {renderStep()}
+    </div>
+  );
+};
+
+AddExistingSafeDialogForm.displayName = displayName;
+
+export default AddExistingSafeDialogForm;
