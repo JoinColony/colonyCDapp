39d6755bf98b88e44e87f397275d0a186285077c
krawczykkatarzyna <katarzyna.krawczyk@pagepro.pl>
2023-07-05 14:31:41 +0200

    fixes after CR

--- src/components/v5/shared/BurgerMenu/BurgerMenu.tsx
+++ src/components/v5/shared/BurgerMenu/BurgerMenu.tsx
@@ -6,12 +6,15 @@ import { BurgerMenuProps } from './types';
 const displayName = 'v5.shared.BurgerMenu.BurgerMenu';
 
 const BurgerMenu: FC<BurgerMenuProps> = ({ isVertical }) => (
-  <div className="text-gray-400 hover:text-blue-400 transition-all duration-norma cursor-pointer">
+  <button
+    type="button"
+    className="text-gray-400 hover:text-blue-400 transition-all duration-normal cursor-pointer"
+  >
     <Icon
       name={isVertical ? 'dots-three-vertical' : 'dots-three'}
       appearance={{ size: 'extraTiny' }}
     />
-  </div>
+  </button>
 );
 
 BurgerMenu.displayName = displayName;
