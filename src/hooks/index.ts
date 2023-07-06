fffd9860e1db94f6e3c634dcb77f55b7dfbf4b8e
Armando Graterol <armandogrtrl@gmail.com>
2023-07-06 17:40:15 -0300

    Added useClipboardCopy hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -60,6 +60,7 @@ export { default as useUserByAddress } from './useUserByAddress';
 export { default as useUserByName } from './useUserByName';
 export { default as useUserByNameOrAddress } from './useUserByNameOrAddress';
 export { default as useGetColonyMembers } from './useGetColonyMembers';
+export { default as useClipboardCopy } from './useClipboardCopy';
 
 /* Used in cases where we need to memoize the transformed output of any data.
  * Transform function has to be pure, obviously
