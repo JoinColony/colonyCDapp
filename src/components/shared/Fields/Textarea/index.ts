da223aacc4bc5df35cb275d578e28c394b42522d
Alicja Kujawa <alicjak94.ak@gmail.com>
2023-07-06 16:58:06 +0300

    Remove TextAutoresize

--- src/components/shared/Fields/Textarea/index.ts
+++ src/components/shared/Fields/Textarea/index.ts
@@ -1,6 +1,5 @@
 export { default } from './Textarea';
 export { Props as TextareaComponentProps } from './Textarea';
-export { default as TextareaAutoresize } from './TextareaAutoresize';
 export {
   default as HookFormTextArea,
   TextareaComponentProps as HookFormTextareaComponentProps,
