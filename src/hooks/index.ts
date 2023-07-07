8160867ea05f9ed18fe234d77294aafbe960e25c
thomassdk <thomassdk@pm.me>
2023-07-07 08:52:13 +0200

    tidy: remove obsolete userSettings hook & context

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -34,7 +34,6 @@ export { default as useSelectedUser } from './useSelectedUser';
 export { default as useTitle } from './useTitle';
 export { default as useTokenInfo, TokenInfoProvider } from './useTokenInfo';
 export { default as useUserAvatarImageFromIPFS } from './useUserAvatarImageFromIPFS';
-export { default as useUserSettings, SlotKey } from './useUserSettings';
 export { default as useCanEditProfile } from './useCanEditProfile';
 export { default as useWindowSize } from './useWindowSize';
 export { default as useAppContext } from './useAppContext';
