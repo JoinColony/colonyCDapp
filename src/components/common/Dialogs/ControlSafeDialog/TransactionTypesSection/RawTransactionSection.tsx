dd3899535c26c336003829d61305f01eb4574dd4
thomassdk <thomassdk@pm.me>
2023-07-12 17:42:56 +0200

    port: basic placeholder transaction type selector

--- src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/RawTransactionSection.tsx
+++ src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/RawTransactionSection.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { DialogSection } from '~shared/Dialog';
+
 const displayName = `dashboard.ControlSafeDialog.RawTransactionSection`;
 
 const RawTransactionSection = () => {
-  return <h2>TODO</h2>;
+  return (
+    <DialogSection>
+      <h2>TODO</h2>
+    </DialogSection>
+  );
 };
 
 RawTransactionSection.displayName = displayName;
