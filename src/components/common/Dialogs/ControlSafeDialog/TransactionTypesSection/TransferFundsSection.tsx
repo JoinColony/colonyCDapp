d8811cd8ec3005f37d17d0e6a5182e768a3c7c71
thomassdk <thomassdk@pm.me>
2023-07-12 13:31:26 +0200

    wip: minimal form working

---
+++ src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/TransferFundsSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const displayName = `dashboard.ControlSafeDialog.TransferFundsSection`;
+
+const TransferFundsSection = () => {
+  return <h2>TODO</h2>;
+};
+
+TransferFundsSection.displayName = displayName;
+
+export default TransferFundsSection;
---
+++ src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/TransferNFTSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const displayName = `dashboard.ControlSafeDialog.TransferNFTSection`;
+
+const TransferNFTSection = () => {
+  return <h2>TODO</h2>;
+};
+
+TransferNFTSection.displayName = displayName;
+
+export default TransferNFTSection;
