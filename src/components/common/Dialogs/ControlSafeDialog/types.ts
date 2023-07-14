4fb2c143648f66410c439ee540a3ff3813a05dc3
thomassdk <thomassdk@pm.me>
2023-07-14 14:31:03 +0200

    refactor: remove duplicate helpers and types

--- src/components/common/Dialogs/ControlSafeDialog/types.ts
+++ src/components/common/Dialogs/ControlSafeDialog/types.ts
@@ -10,66 +10,11 @@ export interface ControlSafeProps {
   // setStepIndex: React.Dispatch<React.SetStateAction<number>>;
 }
 
-/* NOTE: this will be removed when the original is ported to the saga helpers */
-export interface SelectedSafe {
-  id: string; // Making explicit that this is the module address
-  walletAddress: string; // And this is the safe address
-  profile: {
-    displayName: string;
-  };
-}
-
-export type SimpleUserProfile = {
-  avatarHash?: string;
-  displayName?: string;
-  username?: string;
-  walletAddress: string;
-};
-
-export type SimpleUser = {
-  id: string;
-  profile: SimpleUserProfile;
-};
-
-export type FunctionParamType = {
-  name: string;
-  type: string;
-};
-
-export type SafeBalanceToken = Erc20Token | SafeNativeToken;
-
-export type Erc20Token = {
-  name: string;
-  decimals: number;
-  symbol: string;
-  logoUri: string;
-  address: string;
-};
-
-export type SafeNativeToken = {
-  name: string;
-  decimals: number;
-  symbol: string;
-  address: string;
-  networkName: string;
-};
-
-export type SafeTransaction = {
-  transactionType: string;
-  tokenAddress?: string;
-  tokenData?: SafeBalanceToken;
-  amount?: string;
-  rawAmount?: string;
-  recipient?: SimpleUser;
-  data: string;
-  contract?: SimpleUser;
-  abi: string;
-  contractFunction: string;
-  functionParamTypes?: FunctionParamType[];
-};
-
-export interface SafeBalance {
-  balance: number;
-  tokenAddress: string | null;
-  token: SafeBalanceToken | null;
+export interface TransactionSectionProps
+  extends Pick<ControlSafeProps, 'colony'> {
+  disabledInput: boolean;
+  // this is optional only while porting and is non optional when finished
+  transactionFormIndex?: number;
+  handleInputChange: () => void;
+  handleValidation: () => void;
 }
