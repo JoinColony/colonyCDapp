49361888b63f9b0ddfdf42021881e20328df54d9
thomassdk <thomassdk@pm.me>
2023-07-14 15:29:33 +0200

    refactor: extract recipient picker component

--- src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/shared.tsx
+++ src/components/common/Dialogs/ControlSafeDialog/TransactionTypesSection/shared.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage } from 'react-intl';
+import { useGetMembersForColonyQuery } from '~gql';
 
 import { DialogSection } from '~shared/Dialog';
-import { Message } from '~types';
+import { ItemDataType } from '~shared/OmniPicker';
+import { SpinnerLoader } from '~shared/Preloaders';
+import SingleUserPicker, {
+  filterUserSelection,
+} from '~shared/SingleUserPicker';
+import UserAvatar from '~shared/UserAvatar';
+import { Colony, Message, User } from '~types';
 
 import styles from './TransactionTypesSection.css';
+
+const displayName = `common.ControlSafeDialog.shared`;
+
+export const MSG = defineMessages({
+  recipient: {
+    id: `${displayName}.recipient`,
+    defaultMessage: 'Select Recipient',
+  },
+  userPickerPlaceholder: {
+    id: `${displayName}.userPickerPlaceholder`,
+    defaultMessage: 'Select or paste a wallet address',
+  },
+});
 
 interface ErrorMessageProps {
   error: Message;
@@ -21,3 +41,69 @@ export const ErrorMessage = ({ error }: ErrorMessageProps) => (
     </div>
   </DialogSection>
 );
+
+export const AvatarXS = (item: ItemDataType<User>) => (
+  <UserAvatar user={item} size="xs" />
+);
+
+interface LoadingProps {
+  message: Message;
+}
+
+export const Loading = ({ message }: LoadingProps) => (
+  <DialogSection>
+    <div className={styles.spinner}>
+      <SpinnerLoader appearance={{ size: 'medium' }} loadingText={message} />
+    </div>
+  </DialogSection>
+);
+
+interface RecipientPickerProps {
+  colony: Colony;
+  transactionFormIndex: number;
+  disabledInput: boolean;
+}
+
+export const RecipientPicker = ({
+  colony,
+  transactionFormIndex = 0,
+  disabledInput,
+}: RecipientPickerProps) => {
+  const { data: members } = useGetMembersForColonyQuery({
+    skip: !colony?.colonyAddress,
+    variables: {
+      input: {
+        colonyAddress: colony?.colonyAddress ?? '',
+      },
+    },
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const users = [
+    ...(members?.getMembersForColony?.contributors || []),
+    ...(members?.getMembersForColony?.watchers || []),
+  ].map(({ user }) => ({
+    walletAddress: '',
+    name: '',
+    ...user,
+    // Needed to satisfy Omnipicker's key
+    id: user?.walletAddress,
+  }));
+
+  return (
+    <div className={styles.singleUserPickerContainer}>
+      <SingleUserPicker
+        data={users}
+        label={MSG.recipient}
+        name={`transactions.${transactionFormIndex}.recipient`}
+        filter={filterUserSelection}
+        renderAvatar={AvatarXS}
+        disabled={disabledInput}
+        placeholder={MSG.userPickerPlaceholder}
+        dataTest="paymentRecipientPicker"
+        itemDataTest="paymentRecipientItem"
+        valueDataTest="paymentRecipientName"
+      />
+    </div>
+  );
+};
