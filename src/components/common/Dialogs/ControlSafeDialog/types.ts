7caba8e76b212b375e0cfa900a87230c6cada78a
thomassdk <thomassdk@pm.me>
2023-07-14 10:38:56 +0200

    tmp: add temporary types for safe transaction data

--- src/components/common/Dialogs/ControlSafeDialog/types.ts
+++ src/components/common/Dialogs/ControlSafeDialog/types.ts
@@ -18,3 +18,52 @@ export interface SelectedSafe {
     displayName: string;
   };
 }
+
+export type SimpleUserProfile = {
+  avatarHash?: string;
+  displayName?: string;
+  username?: string;
+  walletAddress: string;
+};
+
+export type SimpleUser = {
+  id: string;
+  profile: SimpleUserProfile;
+};
+
+export type FunctionParamType = {
+  name: string;
+  type: string;
+};
+
+export type SafeBalanceToken = Erc20Token | SafeNativeToken;
+
+export type Erc20Token = {
+  name: string;
+  decimals: number;
+  symbol: string;
+  logoUri: string;
+  address: string;
+};
+
+export type SafeNativeToken = {
+  name: string;
+  decimals: number;
+  symbol: string;
+  address: string;
+  networkName: string;
+};
+
+export type SafeTransaction = {
+  transactionType: string;
+  tokenAddress?: string;
+  tokenData?: SafeBalanceToken;
+  amount?: string;
+  rawAmount?: string;
+  recipient?: SimpleUser;
+  data: string;
+  contract?: SimpleUser;
+  abi: string;
+  contractFunction: string;
+  functionParamTypes?: FunctionParamType[];
+};
