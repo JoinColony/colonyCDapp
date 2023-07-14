cc17cc16bacafabe99684d1ee367b68b21ca5057
thomassdk <thomassdk@pm.me>
2023-07-14 15:40:14 +0200

    fix: use new type folder structure

--- src/components/shared/SingleUserPicker/SingleSafePicker.tsx
+++ src/components/shared/SingleUserPicker/SingleSafePicker.tsx
@@ -3,7 +3,7 @@ import React, { ComponentProps, useMemo, ReactNode } from 'react';
 import { SUPPORTED_SAFE_NETWORKS } from '~constants';
 import { Safe } from '~types';
 import Avatar from '~shared/Avatar';
-import { SelectedSafe } from '~common/Dialogs/ControlSafeDialog/types';
+import { SelectedSafe } from '~types/safes';
 
 import { ItemDataType } from '../OmniPicker';
 import SingleUserPicker, {
