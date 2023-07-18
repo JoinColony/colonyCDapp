0ceff72d4c6020ffcc4f74305f14d7ae894c942a
thomassdk <thomassdk@pm.me>
2023-07-18 11:14:56 +0200

    refactor: move single safe picker to control safe

--- src/components/shared/SingleUserPicker/index.ts
+++ src/components/shared/SingleUserPicker/index.ts
@@ -1,6 +1,5 @@
-export { default } from './SingleUserPicker';
+export { default, AvatarRenderFn, OnSelectedFn } from './SingleUserPicker';
 
-export { default as SingleSafePicker } from './SingleSafePicker';
 export { default as ItemDefault } from './ItemDefault';
 
 export * from './utils';
