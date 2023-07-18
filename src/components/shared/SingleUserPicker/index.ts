8342674a28f831ae44903f07ca7dbbae3cbba49b
thomassdk <thomassdk@pm.me>
2023-07-18 16:37:14 +0200

    fix: rebase errors

--- src/components/shared/SingleUserPicker/index.ts
+++ src/components/shared/SingleUserPicker/index.ts
@@ -1,6 +1,5 @@
 export { default, AvatarRenderFn, OnSelectedFn } from './SingleUserPicker';
 
-export { default as SingleSafePicker } from './SingleSafePicker';
 export { default as ItemDefault } from './ItemDefault';
 
 export * from './utils';
