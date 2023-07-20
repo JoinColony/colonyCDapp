4994daa443e8c6fa1f80e6a2127b7e5a908d196f
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-07-20 15:19:43 +0200

    fix: switch register

--- src/components/v5/common/Fields/Switch/types.ts
+++ src/components/v5/common/Fields/Switch/types.ts
@@ -1,6 +1,9 @@
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
 export type SwitchProps = {
   id?: string;
   isDisabled?: boolean;
   isChecked?: boolean;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  register?: UseFormRegister<FieldValues>;
 };
