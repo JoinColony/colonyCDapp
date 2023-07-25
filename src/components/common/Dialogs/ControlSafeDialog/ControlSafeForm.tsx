d932de8071f7e728eb9980b885add7002ae8db67
Armando Graterol <armandogrtrl@gmail.com>
2023-07-25 09:46:08 +0100

    Removed inverting of disabledInputs in ControlSafeForm

--- src/components/common/Dialogs/ControlSafeDialog/ControlSafeForm.tsx
+++ src/components/common/Dialogs/ControlSafeDialog/ControlSafeForm.tsx
@@ -147,11 +147,8 @@ const ControlSafeForm = ({
   const isSupportedColonyVersion = version >= 12;
 
   /* invert this only for testing! */
-  const disabledInputs = !(
-    !userHasPermission ||
-    isSubmitting ||
-    !isSupportedColonyVersion
-  );
+  const disabledInputs =
+    !userHasPermission || isSubmitting || !isSupportedColonyVersion;
 
   const renderTransactionSection = (transactionIndex: number) => {
     const transactionType = watch(
