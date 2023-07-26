17a7a8e7944ce564acd7ad85130b9c1e089c34da
KatarzynaStefanowiczPagepro <katarzyna.stefanowicz@pagepro.co>
2023-07-26 12:32:01 +0200

    pills

--- src/components/frame/Extensions/pages/partials/types.ts
+++ src/components/frame/Extensions/pages/partials/types.ts
@@ -1,9 +1,10 @@
 import { MessageDescriptor } from 'react-intl';
 import { AnyExtensionData } from '~types';
+import { ExtensionStatusBadgeMode } from '~v5/common/Pills/types';
 
 export interface ActionButtonProps {
   extensionData: AnyExtensionData;
-  extensionStatusMode?: string;
+  extensionStatusMode?: ExtensionStatusBadgeMode | string;
   extensionStatusText?: MessageDescriptor | string;
 }
 
