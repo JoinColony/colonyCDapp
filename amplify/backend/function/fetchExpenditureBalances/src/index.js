e55e598562d5b6b587443bc7b19178c497dd2e0e
Jakub Zajac <jakub@colony.io>
2023-08-04 16:17:11 +0100

    Feat: Return balance for each token used in an expenditure

--- amplify/backend/function/fetchExpenditureBalances/src/index.js
+++ amplify/backend/function/fetchExpenditureBalances/src/index.js
@@ -1,13 +1,70 @@
+const { getColonyNetworkClient, Network } = require('@colony/colony-js');
+const {
+  providers,
+  utils: { Logger },
+} = require('ethers');
+
+Logger.setLogLevel(Logger.levels.ERROR);
+
+let rpcURL = 'http://network-contracts.docker:8545'; // this needs to be extended to all supported networks
+let network = Network.Custom;
+let networkAddress;
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+  if (ENV === 'qa') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [rpcURL, networkAddress, network] = await getParams([
+      'chainRpcEndpoint',
+      'networkContractAddress',
+      'chainNetwork',
+    ]);
+  } else {
+    const {
+      etherRouterAddress,
+    } = require('../../../../mock-data/colonyNetworkArtifacts/etherrouter-address.json');
+    networkAddress = etherRouterAddress;
+  }
+};
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+  setEnvVariables();
 
-  return [
-    {
-      tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
-      amount: '1000',
-    },
-  ];
+  const expenditure = event.source;
+
+  const provider = new providers.JsonRpcProvider(rpcURL);
+  const networkClient = getColonyNetworkClient(network, provider, {
+    networkAddress,
+  });
+  const colonyClient = await networkClient.getColonyClient(
+    expenditure.colonyId,
+  );
+
+  // Get all the token addresses that are used in the expenditure
+  const tokenAddresses = expenditure.slots
+    .map((slot) => slot.payouts?.map((payout) => payout.tokenAddress) ?? [])
+    .flat();
+
+  // Remove duplicates
+  const uniqueTokenAddresses = [...new Set(tokenAddresses)];
+
+  // Get the balances for each token address
+  const balances = await Promise.all(
+    uniqueTokenAddresses.map(async (tokenAddress) => {
+      const tokenBalance = await colonyClient.getFundingPotBalance(
+        expenditure.nativeFundingPotId,
+        tokenAddress,
+      );
+
+      return {
+        tokenAddress,
+        amount: tokenBalance.toString(),
+      };
+    }),
+  );
+
+  return balances;
 };
