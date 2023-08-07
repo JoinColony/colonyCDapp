4383f2efb8caed9186e49d4e9a02440c441c2ac7
thomassdk <thomassdk@pm.me>
2023-08-07 14:00:16 +0200

    chore: remove unused type

--- src/components/common/Dialogs/ControlSafeDialog/ControlSafeForm.tsx
+++ src/components/common/Dialogs/ControlSafeDialog/ControlSafeForm.tsx
@@ -11,7 +11,7 @@ import ExternalLink from '~shared/ExternalLink';
 import Button from '~shared/Button';
 import Icon from '~shared/Icon';
 import { filterUserSelection } from '~shared/SingleUserPicker';
-import { SelectedPickerItem, SafeTransaction, SelectedSafe } from '~types';
+import { SelectedPickerItem, SafeTransaction } from '~types';
 import { SAFE_INTEGRATION_LEARN_MORE } from '~constants/externalUrls';
 import { useActionDialogStatus } from '~hooks';
 import { isEmpty, isEqual, omit } from '~utils/lodash';
