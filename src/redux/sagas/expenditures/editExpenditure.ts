d9c49fea80108c66fbeed7f85c094564dea1cc38
Jakub Zajac <jakub@colony.io>
2023-08-10 22:22:31 +0100

    Update token addresses in editExpenditure saga

--- src/redux/sagas/expenditures/editExpenditure.ts
+++ src/redux/sagas/expenditures/editExpenditure.ts
@@ -1,5 +1,6 @@
-import { fork, put, takeEvery } from 'redux-saga/effects';
+import { all, fork, put, takeEvery } from 'redux-saga/effects';
 import { ClientType } from '@colony/colony-js';
+import { BigNumber } from 'ethers';
 
 import { Action, ActionTypes, AllActions } from '~redux';
 import {
@@ -7,6 +8,7 @@ import {
   transactionPending,
   transactionReady,
 } from '~redux/actionCreators';
+import { DEFAULT_TOKEN_DECIMALS } from '~constants';
 
 import { putError, takeFrom } from '../utils';
 import {
@@ -14,6 +16,7 @@ import {
   createTransaction,
   createTransactionChannels,
 } from '../transactions';
+import { groupExpenditureSlotsByTokenAddresses } from '../utils/expenditures';
 
 function* editExpenditure({
   payload: { colonyAddress, expenditure, slots },
@@ -21,33 +24,87 @@ function* editExpenditure({
 }: Action<ActionTypes.EXPENDITURE_EDIT>) {
   const batchKey = 'editExpenditure';
 
-  const { setRecipients }: Record<string, ChannelDefinition> =
-    yield createTransactionChannels(meta.id, ['setRecipients']);
+  // Group slots by token address, this is useful as we need to call setExpenditurePayouts method separately for each token
+  const slotsByTokenAddress = groupExpenditureSlotsByTokenAddresses(slots);
+
+  const {
+    setRecipients,
+    ...setPayoutsChannels
+  }: Record<string, ChannelDefinition> = yield createTransactionChannels(
+    meta.id,
+    [
+      'setRecipients',
+      // setExpenditurePayouts transactions will use token address as channel id
+      ...slotsByTokenAddress.keys(),
+    ],
+  );
 
   try {
     yield fork(createTransaction, setRecipients.id, {
       context: ClientType.ColonyClient,
       methodName: 'setExpenditureRecipients',
       identifier: colonyAddress,
+      params: [
+        expenditure.nativeId,
+        slots.map((_, index) => index + 1),
+        slots.map((slot) => slot.recipientAddress),
+      ],
       group: {
         key: batchKey,
         id: meta.id,
-        index: 1,
+        index: 0,
       },
       ready: false,
     });
 
-    yield put(transactionPending(setRecipients.id));
-    yield put(
-      transactionAddParams(setRecipients.id, [
-        expenditure.nativeId,
-        slots.map((_, index) => index + 1),
-        slots.map((slot) => slot.recipientAddress),
-      ]),
+    yield all(
+      Object.values(setPayoutsChannels).map((channel, index) =>
+        fork(createTransaction, channel.id, {
+          context: ClientType.ColonyClient,
+          methodName: 'setExpenditurePayouts',
+          identifier: colonyAddress,
+          group: {
+            key: batchKey,
+            id: meta.id,
+            index: index + 1,
+          },
+          ready: false,
+        }),
+      ),
     );
-    yield put(transactionReady(setRecipients.id));
 
+    yield put(transactionPending(setRecipients.id));
+    yield put(transactionReady(setRecipients.id));
     yield takeFrom(setRecipients.channel, ActionTypes.TRANSACTION_SUCCEEDED);
+
+    // Call setExpenditurePayouts for each token
+    yield all(
+      [...slotsByTokenAddress.entries()]
+        .map(([tokenAddress, tokenSlots]) => [
+          put(transactionPending(setPayoutsChannels[tokenAddress].id)),
+          put(
+            transactionAddParams(setPayoutsChannels[tokenAddress].id, [
+              expenditure.nativeId,
+              tokenSlots.map((slot) => slot.id),
+              tokenAddress,
+              tokenSlots.map((slot) =>
+                BigNumber.from(slot.amount).mul(
+                  // @TODO: This should get the token decimals of the selected token
+                  BigNumber.from(10).pow(DEFAULT_TOKEN_DECIMALS),
+                ),
+              ),
+            ]),
+          ),
+          put(transactionReady(setPayoutsChannels[tokenAddress].id)),
+          takeFrom(
+            setPayoutsChannels[tokenAddress].channel,
+            ActionTypes.TRANSACTION_SUCCEEDED,
+          ),
+        ])
+        .flat(),
+    );
+
+    // @TODO: Call contract methods to remove the "remaining" expenditure slots
 
     yield put<AllActions>({
       type: ActionTypes.EXPENDITURE_EDIT_SUCCESS,
@@ -58,7 +115,9 @@ function* editExpenditure({
     return yield putError(ActionTypes.EXPENDITURE_EDIT_ERROR, error, meta);
   }
 
-  setRecipients.channel.close();
+  [setRecipients, ...Object.values(setPayoutsChannels)].map((channel) =>
+    channel.channel.close(),
+  );
 
   return null;
 }
