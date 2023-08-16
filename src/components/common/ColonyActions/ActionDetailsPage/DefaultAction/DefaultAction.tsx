2bd00de6e8519f828dbb12d33c216ff6ce6ba1d3
thomassdk <thomassdk@pm.me>
2023-08-16 17:06:16 +0200

    wip: port safe transaction banner

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultAction/DefaultAction.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultAction/DefaultAction.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import DetailsWidget from '~shared/DetailsWidget';
 
 import { useColonyContext, useEnabledExtensions } from '~hooks';
-import { ColonyAction } from '~types';
+import { ColonyAction, ExtendedColonyActionType } from '~types';
 import { Forced as ForcedTag } from '~shared/Tag';
+import { ETHEREUM_NETWORK } from '~constants';
+
+import SafeTransactionBanner from '../SafeTransactionBanner';
 
 import DefaultActionContent from './DefaultActionContent';
 
 import styles from './DefaultAction.css';
+import { getExtendedActionType } from '~utils/colonyActions';
 
 const displayName = 'common.ColonyActions.DefaultAction';
 
@@ -25,8 +29,22 @@ const DefaultAction = ({ actionData }: DefaultActionProps) => {
     return null;
   }
 
+  const hasPendingSafeTransactions = true;
+
+  const extendedActionType = getExtendedActionType(actionData, colony.metadata);
+
   return (
     <div className={styles.main}>
+      {hasPendingSafeTransactions &&
+        extendedActionType === ExtendedColonyActionType.SafeTransaction && (
+          <SafeTransactionBanner
+            chainId={(
+              actionData?.safeTransaction?.safe?.chainId ||
+              ETHEREUM_NETWORK.chainId
+            ).toString()}
+            transactionHash={actionData?.safeTransaction?.id || ''}
+          />
+        )}
       {/* {isMobile && <ColonyHomeInfo showNavigation isMobile />} */}
       {isVotingReputationEnabled && <ForcedTag />}
       <div className={styles.container}>
