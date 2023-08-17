5e386e75790c711ef92da3d8464c772330e0729f
William Martin <williamajmartin@gmail.com>
2023-08-17 17:26:43 +0200

    fix: duplicate members in list

--- src/hooks/members/utils.ts
+++ src/hooks/members/utils.ts
@@ -1,85 +1,5 @@
 import { ColonyRole } from '@colony/colony-js';
-import { ContextModule, getContext } from '~context';
-import {
-  GetColonyExtensionsByColonyAddressDocument,
-  GetColonyExtensionsByColonyAddressQuery,
-  GetColonyExtensionsByColonyAddressQueryVariables,
-} from '~gql';
-import { ColonyContributor } from '~types';
-import { notNull } from '~utils/arrays';
-import { unionBy, intersectionBy } from '~utils/lodash';
-import { ContributorTypeFilter } from '~v5/common/TableFiltering/types';
-
-export const getExtensionInstallations = async (colonyAddress: string) => {
-  const apollo = getContext(ContextModule.ApolloClient);
-
-  const { data } =
-    (await apollo.query<
-      GetColonyExtensionsByColonyAddressQuery,
-      GetColonyExtensionsByColonyAddressQueryVariables
-    >({
-      query: GetColonyExtensionsByColonyAddressDocument,
-      variables: {
-        colonyAddress,
-      },
-    })) ?? {};
-
-  return new Set(
-    data?.getExtensionByColonyAndHash?.items
-      .filter(notNull)
-      .map((colonyExtension) => colonyExtension.id) ?? [],
-  );
-};
-
-export const intersectContributors = ({
-  contributorTypes,
-  contributors: {
-    contributorsWithReputation,
-    permissionedContributors,
-    ...restContributors
-  },
-  permissions,
-}: {
-  contributors: {
-    contributorsWithReputation: ColonyContributor[];
-    permissionedContributors: ColonyContributor[];
-    [k: string]: ColonyContributor[];
-  };
-  permissions: ColonyRole[];
-  contributorTypes: ContributorTypeFilter[];
-}) => {
-  // Performing this union first ensures that if an address has both permission and reputation entries, that
-  // the entry with reputation will be preserved. This enables us to sort contributors by reputation correctly.
-  let allContributors: ColonyContributor[] = unionBy(
-    contributorsWithReputation,
-    permissionedContributors,
-    // @ts-ignore
-    ...Object.values(restContributors),
-    'address',
-  );
-
-  if (!permissions.length && contributorTypes.length) {
-    allContributors = intersectionBy(
-      contributorsWithReputation,
-      allContributors,
-      'address',
-    );
-  } else if (permissions.length && !contributorTypes.length) {
-    allContributors = intersectionBy(
-      allContributors,
-      permissionedContributors,
-      'address',
-    );
-  } else if (permissions.length && contributorTypes.length) {
-    allContributors = intersectionBy(
-      contributorsWithReputation,
-      permissionedContributors,
-      'address',
-    );
-  }
-
-  return allContributors;
-};
+import { uniqBy } from '~utils/lodash';
 
 export const hasSomeRole = (
   roles: Partial<
@@ -101,13 +21,18 @@ export const hasSomeRole = (
 export const updateQuery = (prev, { fetchMoreResult }) => {
   if (!fetchMoreResult.getContributorsByColony) return prev;
 
+  const mergedItems = uniqBy(
+    [
+      ...(prev.getContributorsByColony?.items ?? []),
+      ...(fetchMoreResult.getContributorsByColony.items ?? []),
+    ],
+    'contributorAddress',
+  );
+
   return {
     getContributorsByColony: {
       ...fetchMoreResult.getContributorsByColony,
-      items: [
-        ...(prev.getContributorsByColony?.items ?? []),
-        ...(fetchMoreResult.getContributorsByColony.items ?? []),
-      ],
+      items: mergedItems,
     },
   };
 };
--- src/utils/lodash.ts
+++ src/utils/lodash.ts
@@ -8,3 +8,4 @@ export { default as isEqual } from 'lodash/isEqual';
 export { default as xor } from 'lodash/xor';
 export { default as unionBy } from 'lodash/unionBy';
 export { default as intersectionBy } from 'lodash/intersectionBy';
+export { default as uniqBy } from 'lodash/uniqBy';
