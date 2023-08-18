22b63602cdb0f9ef28ef321de81aa53aac7eabdb
thomassdk <thomassdk@pm.me>
2023-08-18 13:23:51 +0200

    fix: transaction banner not displaying properly

--- src/components/common/ColonyActions/ActionDetailsPage/DefaultAction/DefaultAction.tsx
+++ src/components/common/ColonyActions/ActionDetailsPage/DefaultAction/DefaultAction.tsx
@@ -3,12 +3,9 @@ import React from 'react';
 import DetailsWidget from '~shared/DetailsWidget';
 
 import { useColonyContext, useEnabledExtensions } from '~hooks';
-import { ColonyAction, ExtendedColonyActionType } from '~types';
+import { ColonyAction } from '~types';
 import { Forced as ForcedTag } from '~shared/Tag';
-import { ETHEREUM_NETWORK } from '~constants';
-import { getExtendedActionType } from '~utils/colonyActions';
 
-import SafeTransactionBanner from '../SafeTransactionBanner';
 import DefaultActionContent from './DefaultActionContent';
 
 import styles from './DefaultAction.css';
@@ -28,22 +25,8 @@ const DefaultAction = ({ actionData }: DefaultActionProps) => {
     return null;
   }
 
-  const hasPendingSafeTransactions = true;
-
-  const extendedActionType = getExtendedActionType(actionData, colony.metadata);
-
   return (
     <div className={styles.main}>
-      {hasPendingSafeTransactions &&
-        extendedActionType === ExtendedColonyActionType.SafeTransaction && (
-          <SafeTransactionBanner
-            chainId={(
-              actionData?.safeTransaction?.safe?.chainId ||
-              ETHEREUM_NETWORK.chainId
-            ).toString()}
-            transactionHash={actionData?.safeTransaction?.id || ''}
-          />
-        )}
       {/* {isMobile && <ColonyHomeInfo showNavigation isMobile />} */}
       {isVotingReputationEnabled && <ForcedTag />}
       <div className={styles.container}>
