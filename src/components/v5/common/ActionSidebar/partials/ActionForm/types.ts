69d2d04fa2b3276e44ae5eaf77b2b9cf3f5ef393
krawczykkatarzyna <katarzyna.krawczyk@pagepro.pl>
2023-08-23 19:28:09 +0200

    fix type

--- src/components/v5/common/ActionSidebar/partials/ActionForm/types.ts
+++ src/components/v5/common/ActionSidebar/partials/ActionForm/types.ts
@@ -1,9 +1,13 @@
+import { UseFormReturn } from 'react-hook-form';
 import * as yup from 'yup';
 import { ActionTypes } from '~redux';
 import { ActionTransformFnType } from '~utils/actions';
 
 export type ActionFormProps = {
-  useActionHook: () => void;
+  useActionHook: () => {
+    methods: UseFormReturn<Record<string, any>, any, undefined>;
+    onSubmit: (values) => Promise<any>;
+  };
 };
 
 export type ActionHookOptionsProps = {
