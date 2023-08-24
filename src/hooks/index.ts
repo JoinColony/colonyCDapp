45c0ab924fb4163282cba3f37cae7c2317525313
Jakub Zajac <jakub@colony.io>
2023-08-24 13:03:41 +0100

    Feat: `useCurrentBlockTime` hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -60,6 +60,7 @@ export { default as useUserByAddress } from './useUserByAddress';
 export { default as useUserByName } from './useUserByName';
 export { default as useUserByNameOrAddress } from './useUserByNameOrAddress';
 export { default as useEnoughTokensForStaking } from './useEnoughTokensForStaking';
+export { default as useCurrentBlockTime } from './useCurrentBlockTime';
 
 /* Used in cases where we need to memoize the transformed output of any data.
  * Transform function has to be pure, obviously
