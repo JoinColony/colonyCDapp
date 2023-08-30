f7f1037fe5d111be90da7aa6528a7e3fa94b5707
Jakub Zajac <jakub@colony.io>
2023-08-31 00:19:26 +0100

    Feat: reclaimExpenditureStake saga

--- src/components/common/Expenditures/StakedExpenditure/ReclaimStakeButton/ReclaimStakeButton.tsx
+++ src/components/common/Expenditures/StakedExpenditure/ReclaimStakeButton/ReclaimStakeButton.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import { ExpenditureType } from '~gql';
+import { ExpenditureStatus, ExpenditureType } from '~gql';
 import { ActionTypes } from '~redux';
 import { ActionButton } from '~shared/Button';
-import { Expenditure } from '~types';
+import { Colony, Expenditure } from '~types';
+import { pipe, withMeta } from '~utils/actions';
+
+const allowedStatuses = [
+  ExpenditureStatus.Cancelled,
+  ExpenditureStatus.Finalized,
+];
 
 interface ReclaimStakeButtonProps {
+  colony: Colony;
   expenditure: Expenditure;
 }
 
-const ReclaimStakeButton = ({ expenditure }: ReclaimStakeButtonProps) => {
+const ReclaimStakeButton = ({
+  colony,
+  expenditure,
+}: ReclaimStakeButtonProps) => {
   if (
     expenditure.metadata?.type !== ExpenditureType.Staked ||
-    expenditure.hasReclaimedStake
+    expenditure.hasReclaimedStake ||
+    !allowedStatuses.includes(expenditure.status)
   ) {
     return null;
   }
 
+  const transform = pipe(withMeta({}));
+
   return (
-    <ActionButton actionType={ActionTypes.RECLAIM_EXPENDITURE_STAKE}>
+    <ActionButton
+      actionType={ActionTypes.RECLAIM_EXPENDITURE_STAKE}
+      values={{
+        colonyAddress: colony.colonyAddress,
+        nativeExpenditureId: expenditure.nativeId,
+      }}
+      transform={transform}
+    >
       Reclaim stake
     </ActionButton>
   );
