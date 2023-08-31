c8d65d3a0f0d7aa9b534e3d1faf2e383cc4fc2de
thomassdk <thomassdk@pm.me>
2023-08-31 18:16:14 +0200

    feat: save chain native token to db
    If a native token attempts to be fetched from the db, handle this
    by saving the native token with the id `${ADDRESS_ZERO}_${Network.shortname}`

--- amplify/backend/function/fetchTokenFromChain/src/utils.js
+++ amplify/backend/function/fetchTokenFromChain/src/utils.js
@@ -1,8 +1,10 @@
 const { default: fetch, Request } = require('node-fetch');
+const { constants } = require('ethers');
 
 const TokenType = {
   COLONY: 'COLONY',
   ERC20: 'ERC20',
+  CHAIN_NATIVE: 'CHAIN_NATIVE',
 };
 
 // These are a subsection of the constants in the main project
@@ -11,8 +13,32 @@ const TokenType = {
 const ETHEREUM_NETWORK = {
   shortName: 'Mainnet',
 };
+
 const BINANCE_NETWORK = {
   shortName: 'BNB',
+};
+
+const BASE_NATIVE_TOKEN = {
+  id: constants.AddressZero,
+  type: 'CHAIN_NATIVE',
+};
+
+const ETHER_TOKEN = {
+  name: 'Ether',
+  symbol: 'ETH',
+  decimals: 18,
+  chainMetadata: {
+    chainId: 1,
+  },
+};
+
+const BINANCE_TOKEN = {
+  name: 'Binance',
+  symbol: 'BNB',
+  decimals: 18,
+  chainMetadata: {
+    chainId: 56,
+  },
 };
 
 const graphqlRequest = async (queryOrMutation, variables, url, authKey) => {
@@ -58,9 +84,42 @@ const getTokenType = async (client) => {
   }
 };
 
+const getRpcUrlParamName = (network) => {
+  let chainRpcParam = 'chainRpcEndpoint';
+
+  switch (network) {
+    case BINANCE_NETWORK.shortName:
+      chainRpcParam = 'bnbRpcEndpoint';
+      break;
+    case ETHEREUM_NETWORK.shortName:
+    default:
+    // Use default chainRpcParam ie Ethereum to set `rpcURL`
+  }
+
+  return chainRpcParam;
+};
+
+const getChainNativeToken = (network) => {
+  switch (network) {
+    case BINANCE_NETWORK.shortName:
+      return {
+        ...BASE_NATIVE_TOKEN,
+        ...BINANCE_TOKEN,
+      };
+    case ETHEREUM_NETWORK.shortName:
+    default:
+      return {
+        ...BASE_NATIVE_TOKEN,
+        ...ETHER_TOKEN,
+      };
+  }
+};
+
 module.exports = {
   graphqlRequest,
   getTokenType,
   ETHEREUM_NETWORK,
   BINANCE_NETWORK,
+  getRpcUrlParamName,
+  getChainNativeToken,
 };
