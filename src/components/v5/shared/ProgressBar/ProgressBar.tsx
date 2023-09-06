7bc2c2e796ceeccf8400db2aafd6472ea08ab6e2
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-09-06 14:03:56 +0200

    feat: added tall version and additional message

--- src/components/v5/shared/ProgressBar/ProgressBar.tsx
+++ src/components/v5/shared/ProgressBar/ProgressBar.tsx
@@ -1,17 +1,41 @@
 import React, { FC } from 'react';
 
+import clsx from 'clsx';
+import { useIntl } from 'react-intl';
 import { ProgressBarProps } from './types';
 
-const ProgressBar: FC<ProgressBarProps> = ({ progress }) => (
-  <div className="flex items-center">
-    <div className="relative w-full h-2 rounded bg-gray-200">
-      <span
-        className="bg-blue-400 h-full absolute left-0 top-0 rounded"
-        style={{ width: `${progress}%` }}
-      />
+const ProgressBar: FC<ProgressBarProps> = ({
+  progress,
+  isTall,
+  additionalText,
+}) => {
+  const { formatMessage } = useIntl();
+
+  const additionalMessage =
+    additionalText &&
+    (typeof additionalText === 'string'
+      ? additionalText
+      : formatMessage(additionalText));
+
+  return (
+    <div className="flex items-center">
+      <div
+        className={clsx('relative w-full rounded bg-gray-200', {
+          'h-2.5': isTall,
+          'h-2': !isTall,
+        })}
+      >
+        <span
+          className="bg-blue-400 h-full absolute left-0 top-0 rounded"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+      <span className="text-3 text-gray-600 ml-3">{progress || 0}%</span>
+      {additionalMessage && (
+        <span className="text-3 text-gray-600 ml-1">{additionalMessage}</span>
+      )}
     </div>
-    <span className="text-3 text-gray-600 ml-3">{progress || 0}%</span>
-  </div>
-);
+  );
+};
 
 export default ProgressBar;
--- src/components/v5/shared/ProgressBar/types.ts
+++ src/components/v5/shared/ProgressBar/types.ts
@@ -1,3 +1,7 @@
+import { MessageDescriptor } from 'react-intl';
+
 export interface ProgressBarProps {
   progress?: number;
+  isTall?: boolean;
+  additionalText?: string | MessageDescriptor;
 }
