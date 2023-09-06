1dca7e1b1cd6b1329b1d99319362afd66291359f
William Martin <williamajmartin@gmail.com>
2023-09-06 18:20:38 +0100

    add: fund motion btn

--- src/hooks/useDialogActionPermissions.ts
+++ src/hooks/useDialogActionPermissions.ts
@@ -2,9 +2,8 @@ import { ColonyRole } from '@colony/colony-js';
 import { useFormContext } from 'react-hook-form';
 
 import { useAppContext } from '~hooks';
-import { getUserRolesForDomain } from '~transformers';
 import { Colony } from '~types';
-import { userHasRole } from '~utils/checks';
+import { addressHasRoles } from '~utils/checks';
 
 const useDialogActionPermissions = (
   colony: Colony,
@@ -17,14 +16,11 @@ const useDialogActionPermissions = (
   const { watch } = useFormContext();
   const forceAction = watch('forceAction');
 
-  const hasRoles = requiredRolesDomains.every((domainId) => {
-    const userDomainRoles = getUserRolesForDomain(
-      colony,
-      wallet?.address || '',
-      domainId,
-    );
-
-    return requiredRoles.every((role) => userHasRole(userDomainRoles, role));
+  const hasRoles = addressHasRoles({
+    address: wallet?.address ?? '',
+    colony,
+    requiredRolesDomains,
+    requiredRoles,
   });
 
   const onlyForceAction =
--- src/utils/checks/userHasRoles.ts
+++ src/utils/checks/userHasRoles.ts
@@ -1,7 +1,31 @@
 import { ColonyRole } from '@colony/colony-js';
+import { getUserRolesForDomain } from '~transformers';
+import { Colony } from '~types';
 
 export const userHasRole = (userRoles: ColonyRole[], role: ColonyRole) =>
   userRoles.includes(role);
+
+export const addressHasRoles = ({
+  requiredRolesDomains,
+  colony,
+  address,
+  requiredRoles,
+}: {
+  requiredRolesDomains: number[];
+  colony: Colony;
+  address: string;
+  requiredRoles: ColonyRole[];
+}) => {
+  return requiredRolesDomains.every((domainId) => {
+    const userDomainRoles = getUserRolesForDomain(
+      colony,
+      address || '',
+      domainId,
+    );
+
+    return requiredRoles.every((role) => userHasRole(userDomainRoles, role));
+  });
+};
 
 export const canEnterRecoveryMode = (userRoles: ColonyRole[]) =>
   userHasRole(userRoles, ColonyRole.Recovery);
