8dec8a85522967d1d428e32cfe715c04ff0363e0
krawczykkatarzyna <katarzyna.krawczyk@pagepro.pl>
2023-09-13 16:59:26 +0200

    update types

--- src/components/v5/common/TableWithBurgerMenu/types.ts
+++ src/components/v5/common/TableWithBurgerMenu/types.ts
@@ -1,11 +1,17 @@
+import {
+  FieldValues,
+  UseFieldArrayAppend,
+  UseFieldArrayRemove,
+  UseFormGetValues,
+} from 'react-hook-form';
 import { TableProps } from '../Table/types';
 
 export interface TableWithBurgerMenuProps
   extends Pick<TableProps, 'tableTitle' | 'columns'> {
   actions: {
-    fields: TableProps[];
-    append: () => void;
-    remove: () => void;
-    getValues: () => void;
+    fields: Record<'id', string>[];
+    append: UseFieldArrayAppend<FieldValues>;
+    remove: UseFieldArrayRemove;
+    getValues: UseFormGetValues<FieldValues>;
   };
 }
