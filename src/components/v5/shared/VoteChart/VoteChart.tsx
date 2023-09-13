ade290fca690fa57b6c40cb92172cdb9b0511ac1
Joanna <joanna.dyszkiewicz@pagepro.pl>
2023-09-13 15:40:03 +0200

    Vote Chart component

---
+++ src/components/v5/shared/VoteChart/VoteChart.tsx
@@ -0,0 +1,84 @@
+import React, { FC } from 'react';
+import clsx from 'clsx';
+import { useIntl } from 'react-intl';
+import { VoteChartProps } from './types';
+import VoteChartBar from './partials/VoteChartBar';
+import { VOTE_CHART_BAR_DIRECTION } from './partials/VoteChartBar/types';
+import VoteChartThresholdIndicator from './partials/VoteChartThresholdIndicator';
+
+const VoteChart: FC<VoteChartProps> = ({
+  percentageVotesFor = 0,
+  forLabel,
+  percentageVotesAgainst = 0,
+  againstLabel,
+  threshold,
+  thresholdLabel,
+}) => {
+  const intl = useIntl();
+
+  const forValue =
+    (percentageVotesFor < 0 && 0) ||
+    (percentageVotesFor > 100 && 100) ||
+    percentageVotesFor;
+
+  const againstValue =
+    (percentageVotesAgainst < 0 && 0) ||
+    (percentageVotesAgainst > 100 && 100) ||
+    percentageVotesAgainst;
+
+  return (
+    <div className="w-full">
+      <p className="text-xs font-medium text-center mb-1 text-blue-400">
+        {thresholdLabel ||
+          intl.formatMessage(
+            { id: 'motion.staking.threshold.label' },
+            { threshold: `${threshold}%` },
+          )}
+      </p>
+      <div className="flex pt-2">
+        <div className="flex flex-1 flex-col items-center gap-1">
+          <VoteChartBar
+            value={againstValue}
+            barBackgroundClassName="bg-red-300"
+            direction={VOTE_CHART_BAR_DIRECTION.Left}
+          />
+          <span
+            className={clsx('text-xs text-center transition', {
+              'text-red-300 font-medium': againstValue > 0,
+              'text-gray-500': againstValue === 0,
+            })}
+          >
+            {againstValue}% {againstLabel}
+          </span>
+        </div>
+        <div className="flex flex-1 flex-col items-center gap-1">
+          <div className="relative w-full">
+            <div
+              className="absolute top-0 bottom-0 h-full z-[1]"
+              style={{
+                left: `${threshold}%`,
+              }}
+            >
+              <VoteChartThresholdIndicator />
+            </div>
+            <VoteChartBar
+              value={forValue}
+              barBackgroundClassName="bg-purple-200"
+              direction={VOTE_CHART_BAR_DIRECTION.Right}
+            />
+          </div>
+          <span
+            className={clsx('text-xs text-center transition', {
+              'text-purple-200 font-medium': forValue > 0,
+              'text-gray-500': forValue === 0,
+            })}
+          >
+            {forValue}% {forLabel}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VoteChart;
---
+++ src/components/v5/shared/VoteChart/partials/VoteChartThresholdIndicator/VoteChartThresholdIndicator.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+
+const VoteChartThresholdIndicator: FC = () => (
+  <div
+    className={`
+      pt-[.375rem]
+      flex
+      justify-center
+      h-[calc(100%+0.375rem)]
+      -mt-[.375rem]
+      relative
+      before:border-4
+      before:border-transparent
+      before:border-t-blue-400
+      before:absolute
+      before:left-1/2
+      before:-top-0
+      before:-translate-x-1/2
+    `}
+  >
+    <div
+      className={`
+        h-full
+        w-[.125rem]
+        bg-blue-400
+      `}
+    />
+  </div>
+);
+
+export default VoteChartThresholdIndicator;
