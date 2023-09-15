ecb9310e722887e8947df7890dcc44b57ee59504
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-09-15 09:35:51 +0200

    fix: used react node instead messagedescriptor

--- src/components/v5/shared/ProgressBar/ProgressBar.tsx
+++ src/components/v5/shared/ProgressBar/ProgressBar.tsx
@@ -1,44 +1,33 @@
 import React, { FC } from 'react';
 
 import clsx from 'clsx';
-import { useIntl } from 'react-intl';
 import { ProgressBarProps } from './types';
 
 const ProgressBar: FC<ProgressBarProps> = ({
   progress,
   isTall,
   additionalText,
-}) => {
-  const { formatMessage } = useIntl();
-
-  const additionalMessage =
-    additionalText &&
-    (typeof additionalText === 'string'
-      ? additionalText
-      : formatMessage(additionalText));
-
-  return (
-    <div className="flex items-center">
-      <div
-        className={clsx('relative w-full rounded bg-gray-200', {
-          'h-2.5 rounded-lg': isTall,
-          'h-2 rounded': !isTall,
+}) => (
+  <div className="flex items-center">
+    <div
+      className={clsx('relative w-full rounded bg-gray-200', {
+        'h-2.5 rounded-lg': isTall,
+        'h-2 rounded': !isTall,
+      })}
+    >
+      <span
+        className={clsx('bg-blue-400 h-full absolute left-0 top-0', {
+          'rounded-lg': isTall,
+          rounded: !isTall,
         })}
-      >
-        <span
-          className={clsx('bg-blue-400 h-full absolute left-0 top-0', {
-            'rounded-lg': isTall,
-            rounded: !isTall,
-          })}
-          style={{ width: `${progress}%` }}
-        />
-      </div>
-      <span className="text-3 text-gray-600 ml-3">{progress || 0}%</span>
-      {additionalMessage && (
-        <span className="text-3 text-gray-600 ml-1">{additionalMessage}</span>
-      )}
+        style={{ width: `${progress}%` }}
+      />
     </div>
-  );
-};
+    <span className="text-3 text-gray-600 ml-3">{progress || 0}%</span>
+    {additionalText && (
+      <span className="text-3 text-gray-600 ml-1">{additionalText}</span>
+    )}
+  </div>
+);
 
 export default ProgressBar;
--- src/components/v5/shared/ProgressBar/types.ts
+++ src/components/v5/shared/ProgressBar/types.ts
@@ -1,7 +1,5 @@
-import { MessageDescriptor } from 'react-intl';
-
 export interface ProgressBarProps {
   progress?: number;
   isTall?: boolean;
-  additionalText?: string | MessageDescriptor;
+  additionalText?: React.ReactNode;
 }
