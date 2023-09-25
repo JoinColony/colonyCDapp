0e2b56fbd1fcd4f72e61638801edeba039f3dfb4
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-09-25 10:15:21 +0200

    fix: added consts, used input base

--- src/components/v5/common/ActionSidebar/partials/TokenSelect/partials/TokenStatus/TokenStatus.tsx
+++ src/components/v5/common/ActionSidebar/partials/TokenSelect/partials/TokenStatus/TokenStatus.tsx
@@ -3,6 +3,7 @@ import React, { FC, PropsWithChildren } from 'react';
 import clsx from 'clsx';
 import { TokenStatusProps } from './types';
 import Icon from '~shared/Icon';
+import { ICON_NAME } from './consts';
 
 const displayName =
   'v5.common.ActionsContent.partials.TokenSelect.partials.TokenStatus';
@@ -10,24 +11,17 @@ const displayName =
 const TokenStatus: FC<PropsWithChildren<TokenStatusProps>> = ({
   status,
   children,
-}) => {
-  const iconName = {
-    success: 'check',
-    error: 'warning-circle',
-  };
-
-  return (
-    <div
-      className={clsx('flex items-center gap-2 text-md', {
-        'text-success-400': status === 'success',
-        'text-negative-400': status === 'error',
-      })}
-    >
-      <Icon name={iconName[status]} appearance={{ size: 'extraTiny' }} />
-      {children}
-    </div>
-  );
-};
+}) => (
+  <div
+    className={clsx('flex items-center gap-2 text-md', {
+      'text-success-400': status === 'success',
+      'text-negative-400': status === 'error',
+    })}
+  >
+    <Icon name={ICON_NAME[status]} appearance={{ size: 'extraTiny' }} />
+    {children}
+  </div>
+);
 
 TokenStatus.displayName = displayName;
 
--- src/components/v5/shared/SearchSelect/partials/SearchInput/SearchInput.tsx
+++ src/components/v5/shared/SearchSelect/partials/SearchInput/SearchInput.tsx
@@ -1,27 +1,24 @@
 import React, { FC, InputHTMLAttributes } from 'react';
-import { useIntl } from 'react-intl';
 
 import Icon from '~shared/Icon';
+import { formatText } from '~utils/intl';
+import InputBase from '~v5/common/Fields/InputBase';
 
 const displayName = 'v5.SearchSelect.partials.SearchInput';
 
-const SearchInput: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-  const { formatMessage } = useIntl();
-
-  return (
-    <div className="relative w-full">
-      <span className="absolute top-3 left-3 flex">
-        <Icon name="magnifying-glass" appearance={{ size: 'tiny' }} />
-      </span>
-      <input
-        {...props}
-        className={`py-2.5 pr-3 pl-8 border rounded-lg 
-        border-gray-300 w-full text-3 text-gray-500 placeholder:text-gray-500`}
-        placeholder={formatMessage({ id: 'placeholder.search' })}
-      />
-    </div>
-  );
-};
+const SearchInput: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => (
+  <div className="relative w-full">
+    <span className="absolute top-3.5 left-3 flex">
+      <Icon name="magnifying-glass" appearance={{ size: 'tiny' }} />
+    </span>
+    <InputBase
+      {...props}
+      mode="primary"
+      className="py-2.5 pr-3 pl-8 rounded-lg w-full text-3"
+      placeholder={formatText({ id: 'placeholder.search' })}
+    />
+  </div>
+);
 
 SearchInput.displayName = displayName;
 
