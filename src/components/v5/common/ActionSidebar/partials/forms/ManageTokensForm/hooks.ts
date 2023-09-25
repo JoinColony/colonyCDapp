010f06c7f15fb794f1412cfa1ecbc112ec7a3c57
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-09-25 09:33:20 +0200

    fix: added basic form

---
+++ src/components/v5/common/ActionSidebar/partials/forms/ManageTokensForm/hooks.ts
@@ -0,0 +1,60 @@
+import * as yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+import { useCallback, useMemo } from 'react';
+import { Id } from '@colony/colony-js';
+import { ActionTypes } from '~redux';
+import { mapPayload, pipe, withMeta } from '~utils/actions';
+import { useAppContext, useColonyContext } from '~hooks';
+import { MAX_ANNOTATION_LENGTH } from '~constants';
+import { ActionFormBaseProps } from '../../../types';
+import { useActionFormBaseHook } from '../../../hooks';
+import { getTokenManagementDialogPayload } from '~common/Dialogs/TokenManagementDialog/helpers';
+
+const validationSchema = yup
+  .object()
+  .shape({
+    createdIn: yup.number().defined(),
+    decisionMethod: yup.string().defined(),
+    annotation: yup.string().max(MAX_ANNOTATION_LENGTH).defined(),
+  })
+  .defined();
+
+export const useManageTokens = (
+  getFormOptions: ActionFormBaseProps['getFormOptions'],
+) => {
+  const { colony } = useColonyContext();
+  const { user } = useAppContext();
+  const navigate = useNavigate();
+
+  useActionFormBaseHook({
+    getFormOptions,
+    validationSchema,
+    actionType: ActionTypes.ACTION_UNLOCK_TOKEN,
+    defaultValues: useMemo(
+      () => ({
+        decisionMethod: '',
+        annotation: '',
+        createdIn: Id.RootDomain.toString(),
+      }),
+      [],
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    transform: useCallback(
+      pipe(
+        mapPayload((payload) => {
+          const values = {
+            motionDomainId: payload.createdIn,
+            decisionMethod: payload.decisionMethod,
+            annotation: payload.annotation,
+          };
+          if (colony) {
+            return getTokenManagementDialogPayload(colony, values);
+          }
+          return null;
+        }),
+        withMeta({ navigate }),
+      ),
+      [colony, user, navigate],
+    ),
+  });
+};
