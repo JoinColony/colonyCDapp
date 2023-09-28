56473f2f5526799699a6b4d145a9087ba81bcefe
thomassdk <thomassdk@pm.me>
2023-09-28 11:30:07 +0200

    feat: add discussion helpers
    This includes the following functions:
    1) Subscribe and publish to the pub/sub channel
    2) useDiscussion hook to simplify pub/sub subscription
    3) getIpAddress for use in Spam filtering
    4) sortByDate to organise comments asc or desc
    5) submitVote to create a vote and publish to pub/sub
    6) updateVote to modify a vote and publish to pub/sub
    7) updateComment to recursively update comments on pub/sub messages
    8) addComment to recursively add comments on pub/sub messages
    9) updateComments a wrapper for updateComment and addComment
    10) updateVotes a placeholder function to recursively update votes

---
+++ src/components/shared/Discussion/generated.js
@@ -0,0 +1,56 @@
+﻿import { graphqlOperation, API } from '@aws-amplify/api';
+
+export const config = {
+  aws_appsync_graphqlEndpoint:
+    'https://bpenqiamsferrck5qa5ymusohe.appsync-api.eu-west-2.amazonaws.com/graphql',
+  aws_appsync_region: 'eu-west-2',
+  aws_appsync_authenticationType: 'API_KEY',
+};
+
+export const subscribeDoc = /* GraphQL */ `
+  subscription Subscribe($name: String!) {
+    subscribe(name: $name) {
+      data
+      name
+    }
+  }
+`;
+
+export const publishDoc = /* GraphQL */ `
+  mutation Publish($data: AWSJSON!, $name: String!) {
+    publish(data: $data, name: $name) {
+      data
+      name
+    }
+  }
+`;
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {Object} data the data to publish to the channel
+ */
+export async function publish(name, data) {
+  return await API.graphql(graphqlOperation(publishDoc, { name, data }));
+}
+
+/**
+ * @param  {string} name the name of the channel
+ * @param  {nextCallback} next callback function that will be called with subscription payload data
+ * @param  {function} [error] optional function to handle errors
+ * @returns {Observable} an observable subscription object
+ */
+export function subscribe(name, next, error) {
+  return API.graphql(graphqlOperation(subscribeDoc, { name })).subscribe({
+    next: ({ provider, value }) => {
+      next(value.data.subscribe, provider, value);
+    },
+    error: error || console.log,
+  });
+}
+
+/**
+ * @callback nextCallback
+ * @param {Object} data the subscription response including the `name`, and `data`.
+ * @param {Object} [provider] the provider object
+ * @param {Object} [payload] the entire payload
+ */
---
+++ src/components/shared/Discussion/helpers.ts
@@ -0,0 +1,220 @@
+import { useEffect } from 'react';
+import { Amplify } from '@aws-amplify/core';
+
+import { Comment, Vote } from '~types';
+import { notNull } from '~utils/arrays';
+import { ContextModule, getContext } from '~context';
+import {
+  CreateVoteDocument,
+  CreateVoteInput,
+  CreateVoteMutation,
+  CreateVoteMutationVariables,
+  UpdateVoteDocument,
+  UpdateVoteInput,
+  UpdateVoteMutation,
+  UpdateVoteMutationVariables,
+} from '~gql';
+
+// @ts-ignore
+import { config as AWS_CONFIG, subscribe, publish } from './generated';
+
+Amplify.configure({
+  ...AWS_CONFIG,
+  // eslint-disable-next-line camelcase
+  aws_appsync_apiKey: process.env.PUB_SUB_API_KEY,
+});
+
+export function useDiscussion(
+  id: string,
+  onNewMessage: (comment: Comment) => void,
+) {
+  useEffect(() => {
+    // Subscribe via WebSockets
+    const subscription = subscribe(id, async ({ data }: { data: any }) => {
+      const newMessage = JSON.parse(data);
+
+      if (newMessage) {
+        onNewMessage(newMessage);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [id, onNewMessage]);
+}
+
+export async function getIpAddress() {
+  try {
+    // Use the ipify API to determine the user's public IP address
+    const response = await fetch('https://api.ipify.org?format=json');
+    if (response.ok) {
+      const data = await response.json();
+      return data.ip as string;
+    }
+    console.error('Request failed', response.status, response.statusText);
+    return '';
+  } catch (error) {
+    console.error('An error occurred while fetching the IP address', error);
+    return '';
+  }
+}
+
+export const sortByDate = (
+  comments: Comment[],
+  latestFirst = false,
+): Comment[] => {
+  // Choose the subtraction order based on the flag latestFirst.
+  const newComments = [...comments].sort((a, b) =>
+    latestFirst
+      ? new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      : new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+  );
+
+  // Use map to create a new array of comments with sorted nested comments.
+  return newComments.map((comment) => {
+    // If there are nested comments, sort them recursively.
+    if (comment.comments && comment.comments.items.length > 0) {
+      return {
+        ...comment,
+        comments: {
+          ...comment.comments,
+          items: sortByDate(
+            comment?.comments?.items?.filter(notNull),
+            latestFirst,
+          ),
+        },
+      };
+    }
+    return { ...comment }; // Return a new comment object to avoid modifying the original.
+  });
+};
+
+export const submitVote = async (
+  actionId: string,
+  vote: Required<Pick<CreateVoteInput, 'commentVotesId'>> &
+    Omit<CreateVoteInput, 'commentVotesId'>,
+) => {
+  const apolloClient = getContext(ContextModule.ApolloClient);
+  await apolloClient.mutate<CreateVoteMutation, CreateVoteMutationVariables>({
+    mutation: CreateVoteDocument,
+    variables: {
+      input: {
+        ...vote,
+      },
+    },
+  });
+
+  await publish(
+    actionId,
+    JSON.stringify({ type: 'vote', action: 'create', vote }),
+  );
+};
+
+export const updateVote = async (
+  actionId: string,
+  vote: Required<Pick<UpdateVoteInput, 'amount' | 'id' | 'type'>>,
+) => {
+  const apolloClient = getContext(ContextModule.ApolloClient);
+  await apolloClient.mutate<UpdateVoteMutation, UpdateVoteMutationVariables>({
+    mutation: UpdateVoteDocument,
+    variables: {
+      input: {
+        ...vote,
+      },
+    },
+  });
+
+  await publish(
+    actionId,
+    JSON.stringify({ type: 'vote', action: 'update', vote }),
+  );
+};
+
+const updateComment = (cachedComment: Comment, updatedComment: Comment) => {
+  const targetId = updatedComment.id;
+
+  if (cachedComment?.id === targetId) {
+    return {
+      ...cachedComment,
+      ...updatedComment,
+    };
+  }
+
+  const childComments = cachedComment?.comments?.items?.filter(notNull);
+
+  if (childComments) {
+    return {
+      ...cachedComment,
+      comments: {
+        ...cachedComment.comments,
+        items: childComments.map((item) => updateComment(item, updatedComment)),
+      },
+    };
+  }
+
+  return cachedComment;
+};
+
+function addComment(parentComment: Comment, newComment: Comment) {
+  // Base case: if the current item is the one we're looking for, add the comment.
+  if (parentComment.id === newComment.commentCommentsId) {
+    const newItems = parentComment.comments?.items
+      ? [...parentComment.comments.items, newComment]
+      : [newComment];
+    return {
+      ...parentComment,
+      comments: {
+        ...parentComment.comments,
+        items: newItems,
+      },
+    };
+  }
+
+  const childComment = parentComment?.comments?.items?.filter(notNull);
+
+  // Recursive case: if the item has comments, search each comment.
+  if (childComment) {
+    return {
+      ...parentComment,
+      comments: {
+        ...parentComment.comments,
+        items: childComment.map((comment) => addComment(comment, newComment)),
+      },
+    };
+  }
+
+  // If the item is not the one we're looking for and has no comments, return it unmodified.
+  return parentComment;
+}
+
+// NOTE: At some point this will need to take into account the selected sorting method
+export const updateComments = (
+  cachedComments: Array<Comment>,
+  message: {
+    data: Comment;
+    action: 'create' | 'edit';
+  },
+): Array<Comment> => {
+  const { data, action } = message;
+
+  if (action === 'edit') {
+    return [...cachedComments.map((comment) => updateComment(comment, data))];
+  }
+
+  if (!message.data.commentCommentsId) {
+    return [...cachedComments, data];
+  }
+
+  return [...cachedComments.map((comment) => addComment(comment, data))];
+};
+
+export const updateVotes = (
+  cachedComments: Array<Comment>,
+  message: {
+    data: Vote;
+    action: 'create' | 'edit';
+  },
+): Array<Comment> => {
+  console.warn(`Implement updating votes: ${message}`);
+
+  return cachedComments;
+};
