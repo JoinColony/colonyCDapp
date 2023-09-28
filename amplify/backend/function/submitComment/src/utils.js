b01d0efd3cbab99679e19ca76c73f8666dadf9a2
thomassdk <thomassdk@pm.me>
2023-09-28 11:14:46 +0200

    feat: add submitComment graphql utils

---
+++ amplify/backend/function/submitComment/src/utils.js
@@ -0,0 +1,41 @@
+const { default: fetch, Request } = require('node-fetch');
+
+const graphqlRequest = async (
+  queryOrMutation,
+  variables,
+  apiKey,
+  graphqlURL,
+) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-api-key': apiKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: queryOrMutation,
+      variables,
+    }),
+  };
+
+  const request = new Request(graphqlURL, options);
+
+  let body;
+  let response;
+
+  try {
+    response = await fetch(request);
+    body = await response.json();
+    return body;
+  } catch (error) {
+    /*
+     * Something went wrong... obviously
+     */
+    console.error(error);
+    return null;
+  }
+};
+
+module.exports = {
+  graphqlRequest,
+};
