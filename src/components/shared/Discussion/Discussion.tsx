120e6d4b2a517a9192a49ba483c0a9b3417ba64b
thomassdk <thomassdk@pm.me>
2023-09-28 11:21:58 +0200

    feat: add Discussion react component

---
+++ src/components/shared/Discussion/Discussion.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import { ColonyAction } from '~types';
+import { notNull } from '~utils/arrays';
+
+import CommentInput from './CommentInput';
+import Comments from './Comments';
+import styles from './Discussion.css';
+
+interface Props {
+  actionData: ColonyAction;
+}
+
+const displayName = 'Discussion';
+
+const Discussion = ({ actionData: { discussion, transactionHash } }: Props) => {
+  const savedComments = discussion?.comments?.items?.filter(notNull) || [];
+  const [inReplyToId, setInReplyToId] = useState<string>();
+
+  return (
+    <div className={styles.container}>
+      <Comments
+        actionId={transactionHash}
+        savedComments={savedComments}
+        setInReplyToId={setInReplyToId}
+      />
+      <CommentInput actionId={transactionHash} parentId={inReplyToId} />
+    </div>
+  );
+};
+
+Discussion.displayName = displayName;
+
+export default Discussion;
