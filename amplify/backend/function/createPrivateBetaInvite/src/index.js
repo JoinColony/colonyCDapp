5c32f1b4e9500f19b8f40500334edc978bd76534
thomassdk <thomassdk@pm.me>
2023-10-11 13:10:27 +0200

    feat: add createPrivateBetaInviteCode mutation

--- amplify/backend/function/createPrivateBetaInvite/src/index.js
+++ amplify/backend/function/createPrivateBetaInvite/src/index.js
@@ -1,17 +1,42 @@
+const { graphqlRequest } = require('./utils');
+const { createPrivateBetaInviteCode } = require('./graphql');
 
+let apiKey = 'da2-fakeApiId123456';
+let graphqlURL = 'http://localhost:20002/graphql';
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+  if (ENV === 'qa') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [apiKey, graphqlURL] = await getParams(['appsyncApiKey', 'graphqlUrl']);
+  }
+};
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+
+  try {
+    await setEnvVariables();
+  } catch (e) {
+    throw new Error('Unable to set environment variables. Reason:', e);
+  }
+
+  const creation = await graphqlRequest(
+    createPrivateBetaInviteCode,
+    {},
+    graphqlURL,
+    apiKey,
+  );
+
+  if (creation.errors || !creation.data) {
+    const [error] = creation.errors;
+    throw new Error(error?.message || 'Could not create private beta invite');
+  }
+
+  const inviteCode = creation?.data?.createPrivateBetaInviteCode?.id;
+
+  console.log({ inviteCode });
 };
