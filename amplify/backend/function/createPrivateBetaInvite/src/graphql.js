a3568085f040c051bd05809300b42c414f347a1e
thomassdk <thomassdk@pm.me>
2023-10-11 13:10:27 +0200

    feat: add createPrivateBetaInviteCode mutation

---
+++ amplify/backend/function/createPrivateBetaInvite/src/graphql.js
@@ -0,0 +1,9 @@
+module.exports = {
+  createPrivateBetaInviteCode: /* GraphQL */ `
+    mutation CreatePrivateBetaInviteCode {
+      createPrivateBetaInviteCode(input: {}) {
+        id
+      }
+    }
+  `,
+};
