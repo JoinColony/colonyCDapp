b09698b06e1c77cbfb767fb9fb0a27785cc1f07e
thomassdk <thomassdk@pm.me>
2023-10-16 11:02:43 +0200

    feat: add shared lambda utils

---
+++ amplify/backend/function/getParams.ts
@@ -0,0 +1,64 @@
+const { default: fetch } = require('node-fetch');
+
+interface Resource {
+  Parameter: {
+    Value: string;
+  };
+}
+
+const AWS_SESSION_TOKEN = process.env.AWS_SESSION_TOKEN;
+const ENV = process.env.ENV;
+
+const ParamNames = {
+  graphqlUrl: `%2Famplify%2Fcdapp%2F${ENV}%2Faws_appsync_graphql_url`,
+  reputationEndpoint: `%2Famplify%2Fcdapp%2F${ENV}%2Freputation_endpoint`,
+  networkContractAddress: `%2Famplify%2Fcdapp%2F${ENV}%2Fchain_network_contract`,
+  chainRpcEndpoint: `%2Famplify%2Fcdapp%2F${ENV}%2Fchain_rpc_endpoint`,
+  chainNetwork: `%2Famplify%2Fcdapp%2F${ENV}%2Fchain_network`,
+  appsyncApiKey: `%2Famplify%2Fcdapp%2F${ENV}%2Faws_appsync_api_key`,
+  firebaseAdminConfig: `%2Famplify%2Fcdapp%2F${ENV}%2FfirebaseAdminConfig`,
+  mailJetApiKey: `%2Famplify%2Fcdapp%2F${ENV}%2FmailJetApiKey`,
+  mailJetApiSecret: `%2Famplify%2Fcdapp%2F${ENV}%2FmailJetApiSecret`,
+} as const;
+
+export const getParam = async (paramName: keyof typeof ParamNames) => {
+  if (!(paramName in ParamNames)) {
+    throw Error(`Invalid param name '${paramName}' provided.`);
+  }
+
+  if (!AWS_SESSION_TOKEN) {
+    throw Error(`No AWS Session Token found`);
+  }
+
+  const decrpytParam =
+    paramName === 'appsyncApiKey' ? '&withDecryption=true' : '';
+
+  // Retrieve param from Parameter Store
+  try {
+    const res = await fetch(
+      `http://localhost:2773/systemsmanager/parameters/get?name=${ParamNames[paramName]}${decrpytParam}`,
+      {
+        headers: {
+          'X-Aws-Parameters-Secrets-Token': AWS_SESSION_TOKEN,
+        },
+      },
+    );
+    const resource = (await res.json()) as Resource;
+    const {
+      Parameter: { Value },
+    } = resource;
+    return Value;
+  } catch (e) {
+    console.error(e);
+    return '';
+  }
+};
+
+export const getParams = async (params: Array<keyof typeof ParamNames>) => {
+  if (!Array.isArray(params)) {
+    console.error('Parameter names must be passed as an array of strings.');
+    return [];
+  }
+
+  return Promise.all(params.map((param) => getParam(param)));
+};
---
+++ amplify/backend/function/utils.ts
@@ -0,0 +1,53 @@
+import fetch, { Request } from 'node-fetch';
+import { DocumentNode } from 'graphql';
+
+let devApiKey = 'da2-fakeApiId123456';
+let devGraphqlURL = 'http://localhost:20002/graphql';
+
+export interface QueryOrMutation<TData = Record<string, unknown>> {
+  errors: Array<string>;
+  data: TData;
+}
+
+export const graphqlRequest = async <
+  TData = Record<string, unknown>,
+  TVariables = Record<string, unknown>,
+>(
+  queryOrMutation: string | DocumentNode,
+  variables: TVariables,
+  url: string = devGraphqlURL,
+  apiKey: string = devApiKey,
+): Promise<QueryOrMutation<TData>> => {
+  const query =
+    typeof queryOrMutation === 'string'
+      ? queryOrMutation
+      : queryOrMutation.loc?.source.body;
+
+  if (!query) {
+    throw new Error(
+      "Couldn't extract GraphQL query from the provided DocumentNode.",
+    );
+  }
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'x-api-key': apiKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  };
+
+  const request = new Request(url, options);
+
+  try {
+    const response = await fetch(request);
+    const body = await response.json();
+    return body as QueryOrMutation<TData>;
+  } catch (error) {
+    throw error;
+  }
+};
