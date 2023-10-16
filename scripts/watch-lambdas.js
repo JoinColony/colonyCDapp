13e21f541fd763cf1f68432f5e18271d7373e5d5
thomassdk <thomassdk@pm.me>
2023-10-16 12:24:34 +0200

    feat: match lambda output to docker output

--- scripts/watch-lambdas.js
+++ scripts/watch-lambdas.js
@@ -11,9 +11,9 @@ exec('npm run build-lambdas');
 watcher.on('change', (path) => {
   exec(`./scripts/bundle-lambda.sh ${path}`, (error, stdout) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console.error(`lambdas   | ${error}`);
       return;
     }
-    console.log(stdout);
+    console.log(`lambdas   | ${stdout}`);
   });
 });
