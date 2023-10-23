2bc84ee885236827d664e75cab1828ecf391d542
Joanna <joanna.dyszkiewicz@pagepro.pl>
2023-10-23 09:58:52 +0200

    feat: staking information hook

--- src/components/v5/common/ActionSidebar/partials/Motions/steps/StakingStep/hooks.tsx
+++ src/components/v5/common/ActionSidebar/partials/Motions/steps/StakingStep/hooks.tsx
@@ -73,56 +73,58 @@ export const useStakingInformation = (
     usersStakes.map((user) => user.address),
   );
 
-  const getVotesArray = (isNay?: boolean) => {
-    return (
-      usersStakes?.reduce((result, item) => {
-        if (
-          !item || isNay
-            ? item.stakes?.raw?.nay === '0'
-            : item.stakes?.raw?.yay === '0'
-        ) {
-          return result;
-        }
+  const sortedUsersStakes = usersStakes?.sort((a, b) => {
+    const aStakeNumber = BigNumber.from(a.stakes?.raw?.yay);
+    const bStakeNumber = BigNumber.from(b.stakes?.raw?.yay);
 
-        const userName = users?.length
-          ? users?.find((user) => {
-              const { walletAddress } = user || {};
-              return walletAddress === item.address;
-            })?.profile?.displayName
-          : undefined;
+    if (aStakeNumber.eq(bStakeNumber)) {
+      return 0;
+    }
 
-        if (!userName) {
-          return result;
-        }
+    return aStakeNumber.gt(bStakeNumber) ? -1 : 1;
+  });
 
-        // @todo: add sorting
-        return [
-          ...result,
-          {
-            key: item.address,
-            info: formatText(
-              { id: 'motion.staking.staked' },
-              {
-                value: (
-                  <Numeral
-                    value={item.stakes?.raw?.yay}
-                    decimals={tokenDecimals}
-                    suffix={tokenSymbol}
-                  />
-                ),
-              },
-            ),
-            userProps: {
-              userName,
+  const getVotesArray = (voteFieldName: 'yay' | 'nay') =>
+    sortedUsersStakes?.reduce((result, item) => {
+      const voteValue = item.stakes?.raw?.[voteFieldName];
+      if (!item || voteValue === '0') {
+        return result;
+      }
+
+      const userName = users?.length
+        ? users?.find((user) => user?.walletAddress === item.address)?.profile
+            ?.displayName
+        : undefined;
+
+      if (!userName) {
+        return result;
+      }
+
+      return [
+        ...result,
+        {
+          key: item.address,
+          info: formatText(
+            { id: 'motion.staking.staked' },
+            {
+              value: (
+                <Numeral
+                  value={voteValue}
+                  decimals={tokenDecimals}
+                  suffix={tokenSymbol}
+                />
+              ),
             },
+          ),
+          userProps: {
+            userName,
           },
-        ];
-      }, []) || []
-    );
-  };
+        },
+      ];
+    }, []) || [];
 
-  const votesFor = getVotesArray();
-  const votesAgainst = getVotesArray(true);
+  const votesFor = getVotesArray('yay');
+  const votesAgainst = getVotesArray('nay');
 
   return {
     votesFor,
