4a5cbd274e418e8fb477a72812cfd75d298fc435
Armando Graterol <armandogrtrl@gmail.com>
2023-10-25 11:10:57 -0300

    Added extra buttons to colony header

--- src/components/frame/Extensions/layouts/ColonyHeader.tsx
+++ src/components/frame/Extensions/layouts/ColonyHeader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { defineMessages } from 'react-intl';
 
 import { useMobile } from '~hooks';
 import ColonyNavigation from '~common/Extensions/ColonyNavigation';
 import UserHubButton from '~common/Extensions/UserHubButton';
-import { CompletedButton, PendingButton } from '~v5/shared/Button';
+import Button, { CompletedButton, PendingButton } from '~v5/shared/Button';
 import {
   TransactionGroupStates,
   useUserTransactionContext,
@@ -12,6 +13,17 @@ import {
 import Header from './Header';
 
 const displayName = 'frame.Extensions.ColonyHeader';
+
+const MSG = defineMessages({
+  joinButtonText: {
+    id: `${displayName}.joinButtonText`,
+    defaultMessage: 'Join',
+  },
+  inviteMembers: {
+    id: `${displayName}.inviteMembers`,
+    defaultMessage: 'Invite members',
+  },
+});
 
 const ColonyHeader = () => {
   const isMobile = useMobile();
@@ -35,7 +47,20 @@ const ColonyHeader = () => {
       navBar={navBar}
       userHub={userHub}
       txButtons={txButtons}
-      extra={<div>BUTTON</div>}
+      extra={
+        <>
+          <Button
+            className="ml-4 mr-2"
+            mode="solidBlack"
+            text={MSG.joinButtonText}
+          />
+          <Button
+            text={MSG.inviteMembers}
+            mode="primaryOutline"
+            iconName="paper-plane-tilt"
+          />
+        </>
+      }
     />
   );
 };
