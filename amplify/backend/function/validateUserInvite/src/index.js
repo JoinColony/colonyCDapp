ccf9345a85419d557a1df807f8e341eeb011b836
thomassdk <thomassdk@pm.me>
2023-10-26 10:16:55 +0200

    chore: add necessary lambda functions

--- amplify/backend/function/validateUserInvite/src/index.js
+++ amplify/backend/function/validateUserInvite/src/index.js
@@ -1,17 +1,36 @@
+const { graphqlRequest } = require('./utils');
+const { createPrivateBetaInviteCode } = require('./graphql');
 
+let apiKey = 'da2-fakeApiId123456';
+let graphqlURL = 'http://localhost:20002/graphql';
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+  if (ENV === 'qa') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [apiKey, graphqlURL] = await getParams(['appsyncApiKey', 'graphqlUrl']);
+  }
+};
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+
+  try {
+    await setEnvVariables();
+  } catch (e) {
+    throw new Error('Unable to set environment variables. Reason:', e);
+  }
+
+  return {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
     //  headers: {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+    //  },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
 };
