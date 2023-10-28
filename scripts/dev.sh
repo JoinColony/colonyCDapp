6034ffc785e3215a6c37b1a00a17fd6d2bd78d47
thomassdk <thomassdk@pm.me>
2023-10-28 15:15:58 +0200

    fix: stop codegen running too quickly

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-node codegen &
-
 npm run docker:build
 
-npm run docker:compose up -- --force-recreate -V
+npm run docker:compose up -- --force-recreate -V &
+
+# Give some time for the previous docker containers to be stopped
+# otherwise codegen will run too quickly
+sleep 10
+
+node codegen
