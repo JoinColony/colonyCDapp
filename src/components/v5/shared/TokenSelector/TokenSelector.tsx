cff8de2c67401242f8c4550103e4157ab70295a5
thomassdk <thomassdk@pm.me>
2023-10-28 18:00:34 +0200

    port: input selector to v5

---
+++ src/components/v5/shared/TokenSelector/TokenSelector.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import Input, { InputProps } from '~v5/common/Fields/Input';
+import { createAddress, isAddress } from '~utils/web3';
+import { useGetTokenFromEverywhereQuery } from '~gql';
+
+const displayName = 'shared.TokenSelector';
+
+interface Props extends Omit<InputProps, 'name'> {
+  /** Name of token address input. Defaults to 'tokenAddress' */
+  addressFieldName?: string;
+  /** Name of token field. Defaults to 'token' */
+  tokenFieldName?: string;
+}
+
+const TokenSelector = ({
+  addressFieldName = 'tokenAddress',
+  tokenFieldName = 'token',
+  ...inputProps
+}: Props) => {
+  const {
+    watch,
+    /* formState: { isValid, isDirty, isValidating }, */
+    setValue,
+    clearErrors,
+  } = useFormContext();
+  const tokenAddressField = watch(addressFieldName);
+  const tokenAddress = isAddress(tokenAddressField)
+    ? createAddress(tokenAddressField)
+    : tokenAddressField;
+
+  const {
+    data,
+    /* loading: isFetchingAddress,
+     * error: fetchingTokenError, */
+  } = useGetTokenFromEverywhereQuery({
+    variables: {
+      input: {
+        tokenAddress,
+      },
+    },
+    skip: !isAddress(tokenAddress),
+  });
+
+  const token = data?.getTokenFromEverywhere?.items?.[0] ?? null;
+
+  useEffect(() => {
+    // When token is updated (either found or null), clear errors and set the values in hook-form
+    clearErrors(addressFieldName);
+    setValue(tokenFieldName, token, {
+      shouldValidate: true,
+    });
+  }, [addressFieldName, clearErrors, setValue, token, tokenFieldName]);
+
+  /* const displayLoading =
+   *   isFetchingAddress || (isValidating && isAddress(tokenAddress)); */
+
+  return <Input name={addressFieldName} {...inputProps} />;
+};
+
+TokenSelector.displayName = displayName;
+
+export default TokenSelector;
