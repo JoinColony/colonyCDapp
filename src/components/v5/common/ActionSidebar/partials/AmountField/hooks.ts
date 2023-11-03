0abbae346300e4afe3f2f7020d306565d632502a
Arren V <33682027+arrenv@users.noreply.github.com>
2023-11-03 12:57:06 +0100

    Fix for linting

--- src/components/v5/common/ActionSidebar/partials/AmountField/hooks.ts
+++ src/components/v5/common/ActionSidebar/partials/AmountField/hooks.ts
@@ -32,7 +32,6 @@ export const useAmountField = (
   );
 
   const adjustInputWidth = (value: string) => {
-
     // This regex matches strings that contain only digits and commas, preventing width breaking issues
     const digitsAndCommasRegex = /^[\d,]*$/;
 
