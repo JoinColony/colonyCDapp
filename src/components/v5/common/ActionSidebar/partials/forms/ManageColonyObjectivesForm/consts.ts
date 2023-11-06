ec16a6140206dc0c10c6c34104abd3071e7fe69d
thomassdk <thomassdk@pm.me>
2023-11-06 14:18:34 +0100

    refactor: useIntl for validation messages

--- src/components/v5/common/ActionSidebar/partials/forms/ManageColonyObjectivesForm/consts.ts
+++ src/components/v5/common/ActionSidebar/partials/forms/ManageColonyObjectivesForm/consts.ts
@@ -1,5 +1,25 @@
+import { defineMessages } from 'react-intl';
 import * as yup from 'yup';
+
+import { formatText } from '~utils/intl';
 import { MAX_ANNOTATION_LENGTH, MAX_COLONY_DISPLAY_NAME } from '~constants';
+
+import { displayName } from './ManageColonyObjectivesForm';
+
+const MSG = defineMessages({
+  title: {
+    id: `${displayName}.title`,
+    description: 'Colony objective title is required',
+  },
+  description: {
+    id: `${displayName}.description`,
+    description: 'Colony objective description is required',
+  },
+  progress: {
+    id: `${displayName}.progress`,
+    description: 'Colony objective progress is required',
+  },
+});
 
 export const validationSchema = yup
   .object()
@@ -8,16 +28,16 @@ export const validationSchema = yup
       .string()
       .trim()
       .max(MAX_COLONY_DISPLAY_NAME)
-      .required(() => 'Colony objective title is required'),
+      .required(() => formatText(MSG.title)),
     colonyObjectiveDescription: yup
       .string()
       .trim()
       .max(MAX_ANNOTATION_LENGTH)
-      .required(() => 'Colony objective description is required'),
+      .required(() => formatText(MSG.description)),
     colonyObjectiveProgress: yup
       .number()
       .max(100)
-      .required(() => 'Colony objective progress is required'),
+      .required(() => formatText(MSG.progress)),
     createdIn: yup.string().defined(),
     decisionMethod: yup.string().defined(),
     description: yup.string().max(MAX_ANNOTATION_LENGTH).defined(),
