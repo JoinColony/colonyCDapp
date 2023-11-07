bbac86fbb03fee01262eb136169c1915df429449
Joanna <joanna.dyszkiewicz@pagepro.pl>
2023-11-07 09:27:44 +0100

    fix: ButtonLink title

--- src/components/v5/shared/Button/ButtonLink.tsx
+++ src/components/v5/shared/Button/ButtonLink.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable react/button-has-type */
 import React, { FC, PropsWithChildren } from 'react';
-import { useIntl } from 'react-intl';
 import clsx from 'clsx';
 
 import { ButtonLinkProps } from './types';
@@ -18,8 +16,8 @@ const ButtonLink: FC<PropsWithChildren<ButtonLinkProps>> = ({
   disabled = false,
   loading = false,
   title,
-  text,
   textValues,
+  text,
   className,
   isFullRounded = false,
   isFullSize,
@@ -27,65 +25,58 @@ const ButtonLink: FC<PropsWithChildren<ButtonLinkProps>> = ({
   iconSize = 'tiny',
   isIconRight,
   ...rest
-}) => {
-  const { formatMessage } = useIntl();
-
-  const titleText =
-    typeof title === 'string' ? title : title && formatMessage(title);
-
-  return (
-    <>
-      {loading ? (
-        <SpinnerLoader appearance={{ size: 'medium' }} />
-      ) : (
-        <Link
-          className={clsx(
-            'flex items-center justify-center font-medium transition-all duration-normal',
-            `${isFullRounded ? 'rounded-full' : 'rounded-lg'}`,
-            {
-              'text-md min-h-[2.5rem] px-4 py-2': size === 'default',
-              'text-md min-h-[2.5rem] px-[0.875rem] py-[0.625rem]':
-                size === 'large',
-              '!rounded-[0.1875rem] capitalize text-4 px-2 py-1':
-                size === 'extraSmall',
-              'text-sm min-h-[2.125rem] px-3 py-2': size === 'small',
-              [styles.primarySolid]: mode === 'primarySolid',
-              [styles.primaryOutline]: mode === 'primaryOutline',
-              [styles.primaryOutlineFull]: mode === 'primaryOutlineFull',
-              [styles.secondarySolid]: mode === 'secondarySolid',
-              [styles.secondaryOutline]: mode === 'secondaryOutline',
-              [styles.quinary]: mode === 'quinary',
-              [styles.senary]: mode === 'senary',
-              [styles.quaternary]: mode === 'quaternary',
-              [styles.tertiary]: mode === 'tertiary' || size === 'large',
-              [styles.septenary]: mode === 'septenary',
-              [styles.completed]: mode === 'completed',
-              'pointer-events-none': disabled || loading,
-              'w-full': isFullSize,
-              'border border-gray-300 !text-gray-300 !bg-base-white':
-                disabled && isIconRight,
-            },
-            className,
-          )}
-          aria-busy={loading}
-          title={titleText}
-          {...rest}
+}) => (
+  <>
+    {loading ? (
+      <SpinnerLoader appearance={{ size: 'medium' }} />
+    ) : (
+      <Link
+        className={clsx(
+          'flex items-center justify-center font-medium transition-all duration-normal',
+          `${isFullRounded ? 'rounded-full' : 'rounded-lg'}`,
+          {
+            'text-md min-h-[2.5rem] px-4 py-2': size === 'default',
+            'text-md min-h-[2.5rem] px-[0.875rem] py-[0.625rem]':
+              size === 'large',
+            '!rounded-[0.1875rem] capitalize text-4 px-2 py-1':
+              size === 'extraSmall',
+            'text-sm min-h-[2.125rem] px-3 py-2': size === 'small',
+            [styles.primarySolid]: mode === 'primarySolid',
+            [styles.primaryOutline]: mode === 'primaryOutline',
+            [styles.primaryOutlineFull]: mode === 'primaryOutlineFull',
+            [styles.secondarySolid]: mode === 'secondarySolid',
+            [styles.secondaryOutline]: mode === 'secondaryOutline',
+            [styles.quinary]: mode === 'quinary',
+            [styles.senary]: mode === 'senary',
+            [styles.quaternary]: mode === 'quaternary',
+            [styles.tertiary]: mode === 'tertiary' || size === 'large',
+            [styles.septenary]: mode === 'septenary',
+            [styles.completed]: mode === 'completed',
+            'pointer-events-none': disabled || loading,
+            'w-full': isFullSize,
+            'border border-gray-300 !text-gray-300 !bg-base-white':
+              disabled && isIconRight,
+          },
+          className,
+        )}
+        aria-busy={loading}
+        title={title}
+        {...rest}
+      >
+        <ButtonContent
+          mode={mode}
+          iconName={iconName}
+          iconSize={iconSize}
+          isIconRight={isIconRight}
+          textValues={textValues}
+          text={text}
         >
-          <ButtonContent
-            mode={mode}
-            iconName={iconName}
-            iconSize={iconSize}
-            isIconRight={isIconRight}
-            text={text}
-            textValues={textValues}
-          >
-            {children}
-          </ButtonContent>
-        </Link>
-      )}
-    </>
-  );
-};
+          {children}
+        </ButtonContent>
+      </Link>
+    )}
+  </>
+);
 
 ButtonLink.displayName = displayName;
 
