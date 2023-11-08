1b0fb7038a9d583878326878142fd21266ad06f1
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2023-11-08 11:47:30 +0100

    Changed text color of selected user, Fixed verified user check

--- src/components/v5/common/ActionSidebar/partials/UserSelect/types.ts
+++ src/components/v5/common/ActionSidebar/partials/UserSelect/types.ts
@@ -2,8 +2,6 @@ import { SearchSelectOptionProps } from '~v5/shared/SearchSelect/types';
 
 export interface UserSelectHookProps extends SearchSelectOptionProps {
   isLoading: boolean;
-  isUserVerified?: boolean;
-  isRecipientNotVerified: boolean;
 }
 
 export interface UserSelectProps {
