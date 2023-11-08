cc04e271290413c960a4d386db5ef4a0033934a5
KornelKwiatkowski <kornel.kwiatkowski@pagepro.pl>
2023-11-08 15:38:13 +0100

    fix: change colors of radio buttons

--- src/components/v5/common/ActionSidebar/partials/Motions/consts.ts
+++ src/components/v5/common/ActionSidebar/partials/Motions/consts.ts
@@ -2,15 +2,13 @@ import { MotionVote } from '~utils/colonyMotions';
 import { formatText } from '~utils/intl';
 import { ButtonRadioButtonItem } from '~v5/common/Fields/RadioButtons/ButtonRadioButtons/types';
 
-// @TODO: Fix radio buttons colors
-
 export const supportOption = {
   label: formatText({ id: 'motion.support' }),
   id: 'support',
   value: MotionVote.Yay,
-  colorClassName: 'text-purple-200',
+  colorClassName: 'text-purple-400',
   iconClassName: 'text-purple-400',
-  hoverColorClassName: 'md:hover:text-purple-400',
+  hoverColorClassName: 'md:hover:text-purple-400 md:hover:border-purple-400',
   iconName: 'thumbs-up',
 };
 
@@ -18,9 +16,10 @@ export const opposeOption = {
   label: formatText({ id: 'motion.oppose' }),
   id: 'oppose',
   value: MotionVote.Nay,
-  colorClassName: 'text-negative-300',
+  colorClassName: 'text-negative-400',
   iconClassName: 'text-negative-400',
-  hoverColorClassName: 'md:hover:text-negative-400',
+  hoverColorClassName:
+    'md:hover:text-negative-400 md:hover:border-negative-400',
   iconName: 'thumbs-down',
 };
 
