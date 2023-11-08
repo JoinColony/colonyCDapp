d7b05ed7beebcc54d819623163da213c3bc75c2d
Armando Graterol <armandogrtrl@gmail.com>
2023-11-08 15:20:17 -0300

    Updated story of PageLayout

--- src/stories/frame/PageLayout.stories.tsx
+++ src/stories/frame/PageLayout.stories.tsx
@@ -6,6 +6,142 @@ import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import Logo from '~images/logo-new.svg';
 
 import PageLayout from '~v5/frame/PageLayout';
+import NavigationSidebar from '~v5/frame/NavigationSidebar';
+
+const SideBar = () => (
+  <NavigationSidebar
+    logo={<Logo />}
+    additionalMobileContent={<p>mobile content</p>}
+    mobileBottomContent={<p>mobile bottom content</p>}
+    hamburgerLabel="Menu"
+    colonySwitcherProps={{
+      avatarProps: {
+        chainIconName: 'gnosis',
+      },
+      content: {
+        title: 'Select a Colony',
+        content: <p>colony</p>,
+        bottomActionProps: {
+          text: 'Create new colony',
+          iconName: 'plus',
+        },
+      },
+    }}
+    mainMenuItems={[
+      {
+        key: '1',
+        iconName: 'layout',
+        label: 'Dashboard',
+        secondLevelMenuProps: {
+          title: 'Dashboard',
+          content: <p>content</p>,
+          description:
+            'The Metacolony is the DAO that will build, maintain, and support the Colony ecosystem, and you can be part of it.',
+          bottomActionProps: {
+            text: 'Create new action',
+            iconName: 'plus',
+          },
+        },
+      },
+      {
+        key: '2',
+        iconName: 'user',
+        label: 'Members',
+        secondLevelMenuProps: {
+          title: 'Members',
+          content: <p>content</p>,
+          description: 'View and manage all members of the Colony.',
+        },
+        relatedActionsProps: {
+          title: 'Related actions',
+          items: [
+            {
+              key: '1',
+              label: 'Members 1',
+              href: '/',
+            },
+            {
+              key: '2',
+              label: 'Members 2',
+            },
+          ],
+        },
+      },
+      {
+        key: '3',
+        iconName: 'bank',
+        label: 'Finances',
+        secondLevelMenuProps: {
+          title: 'Finances',
+          content: <p>content</p>,
+          description: 'View and manage all members of the Colony.',
+        },
+        relatedActionsProps: {
+          title: 'Related actions',
+          items: [
+            {
+              key: '1',
+              label: 'Finances 1',
+              href: '/',
+            },
+            {
+              key: '2',
+              label: 'Finances 2',
+            },
+          ],
+        },
+      },
+      {
+        key: '4',
+        iconName: 'handshake',
+        label: 'Agreements',
+        secondLevelMenuProps: {
+          title: 'Agreements',
+          content: <p>content</p>,
+          description: 'View and manage all members of the Colony.',
+        },
+        relatedActionsProps: {
+          title: 'Related actions',
+          items: [
+            {
+              key: '1',
+              label: 'Agreements 1',
+              href: '/',
+            },
+            {
+              key: '2',
+              label: 'Agreements 2',
+            },
+          ],
+        },
+      },
+      {
+        key: '5',
+        iconName: 'gear-six',
+        label: 'Admin',
+        secondLevelMenuProps: {
+          title: 'Admin',
+          content: <p>content</p>,
+          description: 'View and manage all members of the Colony.',
+        },
+        relatedActionsProps: {
+          title: 'Related actions',
+          items: [
+            {
+              key: '1',
+              label: 'Admin 1',
+              href: '/',
+            },
+            {
+              key: '2',
+              label: 'Admin 2',
+            },
+          ],
+        },
+      },
+    ]}
+  />
+);
 
 const pageLayoutMeta: Meta<typeof PageLayout> = {
   title: 'Frame/Page Layout',
@@ -47,138 +183,7 @@ const pageLayoutMeta: Meta<typeof PageLayout> = {
       },
       userNavigation: <p>user navigation</p>,
     },
-    navigationSidebarProps: {
-      logo: <Logo />,
-      additionalMobileContent: <p>mobile content</p>,
-      mobileBottomContent: <p>mobile bottom content</p>,
-      hamburgerLabel: 'Menu',
-      colonySwitcherProps: {
-        avatarProps: {
-          chainIconName: 'gnosis',
-        },
-        content: {
-          title: 'Select a Colony',
-          content: <p>colony</p>,
-          bottomActionProps: {
-            text: 'Create new colony',
-            iconName: 'plus',
-          },
-        },
-      },
-      mainMenuItems: [
-        {
-          key: '1',
-          iconName: 'layout',
-          label: 'Dashboard',
-          secondLevelMenuProps: {
-            title: 'Dashboard',
-            content: <p>content</p>,
-            description:
-              'The Metacolony is the DAO that will build, maintain, and support the Colony ecosystem, and you can be part of it.',
-            bottomActionProps: {
-              text: 'Create new action',
-              iconName: 'plus',
-            },
-          },
-        },
-        {
-          key: '2',
-          iconName: 'user',
-          label: 'Members',
-          secondLevelMenuProps: {
-            title: 'Members',
-            content: <p>content</p>,
-            description: 'View and manage all members of the Colony.',
-          },
-          relatedActionsProps: {
-            title: 'Related actions',
-            items: [
-              {
-                key: '1',
-                label: 'Members 1',
-                href: '/',
-              },
-              {
-                key: '2',
-                label: 'Members 2',
-              },
-            ],
-          },
-        },
-        {
-          key: '3',
-          iconName: 'bank',
-          label: 'Finances',
-          secondLevelMenuProps: {
-            title: 'Finances',
-            content: <p>content</p>,
-            description: 'View and manage all members of the Colony.',
-          },
-          relatedActionsProps: {
-            title: 'Related actions',
-            items: [
-              {
-                key: '1',
-                label: 'Finances 1',
-                href: '/',
-              },
-              {
-                key: '2',
-                label: 'Finances 2',
-              },
-            ],
-          },
-        },
-        {
-          key: '4',
-          iconName: 'handshake',
-          label: 'Agreements',
-          secondLevelMenuProps: {
-            title: 'Agreements',
-            content: <p>content</p>,
-            description: 'View and manage all members of the Colony.',
-          },
-          relatedActionsProps: {
-            title: 'Related actions',
-            items: [
-              {
-                key: '1',
-                label: 'Agreements 1',
-                href: '/',
-              },
-              {
-                key: '2',
-                label: 'Agreements 2',
-              },
-            ],
-          },
-        },
-        {
-          key: '5',
-          iconName: 'gear-six',
-          label: 'Admin',
-          secondLevelMenuProps: {
-            title: 'Admin',
-            content: <p>content</p>,
-            description: 'View and manage all members of the Colony.',
-          },
-          relatedActionsProps: {
-            title: 'Related actions',
-            items: [
-              {
-                key: '1',
-                label: 'Admin 1',
-                href: '/',
-              },
-              {
-                key: '2',
-                label: 'Admin 2',
-              },
-            ],
-          },
-        },
-      ],
-    },
+    sidebar: <SideBar />,
   },
 };
 
