13fd473b881317eebfe8e447ff58d13106820d45
Armando Graterol <armandogrtrl@gmail.com>
2023-11-09 14:00:30 -0300

    Added base content in ColonyCreatedModal

--- src/components/v5/common/Modals/ColonyCreatedModal/ColonyCreatedModal.tsx
+++ src/components/v5/common/Modals/ColonyCreatedModal/ColonyCreatedModal.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
+import Icon from '~shared/Icon';
+import Button from '~v5/shared/Button';
+
 import Modal from '~v5/shared/Modal';
 
 const displayName = 'v5.common.Modals.ColonyCreatedModal';
@@ -9,6 +12,22 @@ const MSG = defineMessages({
   modalTitle: {
     id: `${displayName}.modalTitle`,
     defaultMessage: 'Congratulations!',
+  },
+  modalDescription: {
+    id: `${displayName}.modalDescription`,
+    defaultMessage: `Your Colony has been created and is ready for you to jump right in. Grow your Colony by inviting your friends and collaborators to join.`,
+  },
+  modalButton: {
+    id: `${displayName}.modalButton`,
+    defaultMessage: 'Explore your Colony',
+  },
+  modalSubtitle: {
+    id: `${displayName}.modalSubtitle`,
+    defaultMessage: 'Invite a person to create a Colony',
+  },
+  modalSubtitleDescription: {
+    id: `${displayName}.modalSubtitleDescription`,
+    defaultMessage: `You can invite 1 member to create a Colony and test out the new features during the private beta.`,
   },
 });
 
@@ -20,7 +39,24 @@ interface Props {
 const ColonyCreatedModal = ({ isOpen, onClose }: Props) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <FormattedMessage {...MSG.modalTitle} />
+      <div className="mt-16 flex flex-col items-center pb-8 border-b border-b-gray-200 mb-8">
+        <Icon className="mb-3" name="confetti" appearance={{ size: 'large' }} />
+        <h3 className="heading-3 mb-2">
+          <FormattedMessage {...MSG.modalTitle} />
+        </h3>
+        <p className="text-gray-600 text-sm mb-6 text-center">
+          <FormattedMessage {...MSG.modalDescription} />
+        </p>
+        <Button mode="primarySolid" text={MSG.modalButton} onClick={onClose} />
+      </div>
+      <div className="flex flex-col items-center">
+        <h4 className="heading-5 mb-2">
+          <FormattedMessage {...MSG.modalSubtitle} />
+        </h4>
+        <p className="text-gray-600 text-md mb-6 text-center">
+          <FormattedMessage {...MSG.modalSubtitleDescription} />
+        </p>
+      </div>
     </Modal>
   );
 };
