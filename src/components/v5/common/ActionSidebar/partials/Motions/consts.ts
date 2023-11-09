bcf6c388aed680cf5bc8b4e8bdcd64b6abeb6464
KornelKwiatkowski <kornel.kwiatkowski@pagepro.pl>
2023-11-09 12:55:20 +0100

    fix: remove hover when radio button is checked

--- src/components/v5/common/ActionSidebar/partials/Motions/consts.ts
+++ src/components/v5/common/ActionSidebar/partials/Motions/consts.ts
@@ -7,7 +7,7 @@ export const supportOption = {
   id: 'support',
   value: MotionVote.Yay,
   colorClassName: 'text-purple-200',
-  checkedColorClassName: 'bg-purple-400',
+  checkedColorClassName: 'bg-purple-400 border-purple-400',
   iconClassName: 'text-purple-400',
   hoverColorClassName: 'md:hover:text-purple-400 md:hover:border-purple-400',
   iconName: 'thumbs-up',
@@ -18,7 +18,7 @@ export const opposeOption = {
   id: 'oppose',
   value: MotionVote.Nay,
   colorClassName: 'text-negative-300',
-  checkedColorClassName: 'bg-negative-400',
+  checkedColorClassName: 'bg-negative-400 border-negative-400',
   iconClassName: 'text-negative-400',
   hoverColorClassName:
     'md:hover:text-negative-400 md:hover:border-negative-400',
