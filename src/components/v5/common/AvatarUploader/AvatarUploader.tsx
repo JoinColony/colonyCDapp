4a7304d0e7a53a47d042cf9432463ea75fbd3256
thomassdk <thomassdk@pm.me>
2023-11-15 18:36:36 +0100

    fix: avatar icon and dropzone spacing

--- src/components/v5/common/AvatarUploader/AvatarUploader.tsx
+++ src/components/v5/common/AvatarUploader/AvatarUploader.tsx
@@ -1,11 +1,7 @@
 import React, { FC } from 'react';
 
 import FileUpload from './partials/FileUpload';
-import {
-  useAvatarUploader,
-  useGetUploaderText,
-  UseAvatarUploaderProps,
-} from './hooks';
+import { useAvatarUploader, UseAvatarUploaderProps } from './hooks';
 import { AvatarUploaderProps } from './types';
 import ProgressContent from './partials/ProgressContent';
 import { getPlaceholder } from './utils';
@@ -31,20 +27,13 @@ const AvatarUploader: FC<AvatarUploaderProps & UseAvatarUploaderProps> = ({
     uploadProgress,
     file,
   } = useAvatarUploader({ updateFn });
-  const uploaderText = useGetUploaderText(fileOptions);
 
   return (
     <div className="flex sm:flex-row flex-col gap-4 sm:gap-2">
-      <div className="flex items-center gap-4 sm:items-start sm:w-16 mr-4">
-        <div className="flex-shrink-0">
-          {getPlaceholder(isLoading, avatarPlaceholder)}
-        </div>
-        <div className="sm:hidden text-gray-600 text-sm">{uploaderText}</div>
+      <div className="flex-shrink-0">
+        {getPlaceholder(isLoading, avatarPlaceholder)}
       </div>
       <div className="flex flex-col gap-2 w-full">
-        {/* <div className="hidden sm:block text-gray-600 text-sm">
-          {uploaderText}
-        </div> */}
         <FileUpload
           dropzoneOptions={{
             disabled,
