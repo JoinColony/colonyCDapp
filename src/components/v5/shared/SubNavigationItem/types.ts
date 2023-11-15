b3c55720c7eb82d23e157a2eb35753ab5b175cda
KornelKwiatkowski <kornel.kwiatkowski@pagepro.pl>
2023-11-15 09:06:01 +0100

    fix: change icon size for filter items

--- src/components/v5/shared/SubNavigationItem/types.ts
+++ src/components/v5/shared/SubNavigationItem/types.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MessageDescriptor } from 'react-intl';
+import { IconSize } from '~shared/Icon/Icon';
 
 import { FilterOptionProps } from '~v5/common/Filter/types';
 import { FilterType } from '~v5/common/TableFiltering/types';
