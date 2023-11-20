e552fba29b8e73109f3370107faa7799ca106b54
thomassdk <thomassdk@pm.me>
2023-11-20 14:36:07 +0100

    fix: icon and font styles for pills

--- src/components/v5/common/Fields/Input/partials/InputPills.tsx
+++ src/components/v5/common/Fields/Input/partials/InputPills.tsx
@@ -15,13 +15,16 @@ const InputPills: FC<PillProps> = ({ message, status }) => {
 
   return (
     <div
-      className={clsx(`flex self-start text-3 absolute gap-1`, {
-        'text-negative-400': status === 'error',
-        'text-success-400': status === 'success',
-        'text-warning-400': status === 'warning',
-      })}
+      className={clsx(
+        `flex self-start items-center text-sm font-normal absolute gap-1`,
+        {
+          'text-negative-400': status === 'error',
+          'text-success-400': status === 'success',
+          'text-warning-400': status === 'warning',
+        },
+      )}
     >
-      <Icon name={iconType} appearance={{ size: 'small' }} />
+      <Icon name={iconType} appearance={{ size: 'tiny' }} />
       <span>{formatText(message)}</span>
     </div>
   );
