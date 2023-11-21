a556f054d482f9d61f385a5b77ca3d757ed516e5
Jakub Zajac <jakub@colony.io>
2023-11-21 22:13:50 +0000

    Wire activate tokens modal with motion not enough tokens info

--- src/components/v5/common/ActionSidebar/partials/Motions/steps/StakingStep/partials/NotEnoughTokensInfo/NotEnoughTokensInfo.tsx
+++ src/components/v5/common/ActionSidebar/partials/Motions/steps/StakingStep/partials/NotEnoughTokensInfo/NotEnoughTokensInfo.tsx
@@ -1,28 +1,32 @@
 import React, { FC } from 'react';
+import { useTokensModalContext } from '~context';
 import { formatText } from '~utils/intl';
 
 const displayName =
   'v5.common.ActionSidebar.partials.motions.MotionSimplePayment.steps.StakingStep.partials.NotEnoughTokensInfo';
 
-const NotEnoughTokensInfo: FC = () => (
-  <>
-    <p className="text-sm mb-1">
-      {formatText({
-        id: 'motion.staking.notEnoughTokens',
-      })}
-    </p>
-    <button
-      type="button"
-      className="text-4 underline transition-all md:hover:opacity-80"
-      // @todo: add a toggler to open activate tokens modal
-      onClick={() => {}}
-    >
-      {formatText({
-        id: 'motion.staking.activateTokens',
-      })}
-    </button>
-  </>
-);
+const NotEnoughTokensInfo: FC = () => {
+  const { toggleOnTokensModal } = useTokensModalContext();
+
+  return (
+    <>
+      <p className="text-sm mb-1">
+        {formatText({
+          id: 'motion.staking.notEnoughTokens',
+        })}
+      </p>
+      <button
+        type="button"
+        className="text-4 underline transition-all md:hover:opacity-80"
+        onClick={() => toggleOnTokensModal()}
+      >
+        {formatText({
+          id: 'motion.staking.activateTokens',
+        })}
+      </button>
+    </>
+  );
+};
 
 NotEnoughTokensInfo.displayName = displayName;
 
