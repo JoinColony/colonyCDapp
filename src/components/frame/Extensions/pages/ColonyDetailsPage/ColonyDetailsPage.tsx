7239081242f2a9aceb9bdd50736e8f64bff0af1d
Cezary Dryl <cezary.dryl@pagepro.pl>
2023-11-22 09:59:00 +0100

    fix: added icons and fixed mobile view for external links, objective description width/size

--- src/components/frame/Extensions/pages/ColonyDetailsPage/ColonyDetailsPage.tsx
+++ src/components/frame/Extensions/pages/ColonyDetailsPage/ColonyDetailsPage.tsx
@@ -1,8 +1,18 @@
 import React, { FC } from 'react';
-import clsx from 'clsx';
 import { FormattedMessage } from 'react-intl';
 
-import styles from './ColonyDetailsPage.module.css';
+import {
+  GlobeHemisphereEast,
+  Scroll,
+  YoutubeLogo,
+  DiscordLogo,
+  TwitterLogo,
+  TelegramLogo,
+  GithubLogo,
+  InstagramLogo,
+  FacebookLogo,
+} from 'phosphor-react';
+import { ExternalLinks } from '~gql';
 import { useColonyContext, useMobile } from '~hooks';
 import Avatar from '~v5/shared/Avatar';
 import Button, { TextButton } from '~v5/shared/Button';
@@ -26,22 +36,39 @@ const ColonyDetailsPage: FC = () => {
   const { avatar, thumbnail, description, externalLinks, objective } =
     metadata || {};
 
+  const IconComponent = {
+    [ExternalLinks.Custom]: <GlobeHemisphereEast size={18} />,
+    [ExternalLinks.Whitepaper]: <Scroll size={18} />,
+    [ExternalLinks.Youtube]: <YoutubeLogo size={18} />,
+    [ExternalLinks.Discord]: <DiscordLogo size={18} />,
+    [ExternalLinks.Twitter]: <TwitterLogo size={18} />,
+    [ExternalLinks.Telegram]: <TelegramLogo size={18} />,
+    [ExternalLinks.Github]: <GithubLogo size={18} />,
+    [ExternalLinks.Instagram]: <InstagramLogo size={18} />,
+    [ExternalLinks.Facebook]: <FacebookLogo size={18} />,
+  };
+
   const {
     actionSidebarToggle: [, { toggleOn: toggleActionSidebarOn }],
   } = useActionSidebarContext();
 
   return (
     <div>
-      <div className={clsx('pt-[4.375rem] px-6 pb-6 mt-10', styles.box)}>
+      <div className="pt-[4.375rem] px-6 pb-6 mt-10 bg-gray-25 border border-gray-200 rounded-lg relative">
         <div className="absolute left-6 -top-11 rounded-full border-4 border-gray-100 flex">
           <Avatar size="md" avatar={avatar || thumbnail || ''} />
         </div>
         <h2 className="heading-2 mb-1">{name}</h2>
         <p className="text-md text-gray-600 mb-5">{description}</p>
-        <div className="mb-6 flex gap-x-2">
+        <div className="mb-6 flex flex-col gap-4 sm:flex-row">
           {externalLinks?.length ? (
             externalLinks.map(({ name: linkName, link }) => (
-              <ExternalLink href={link} key={`${linkName}:${link}`}>
+              <ExternalLink
+                href={link}
+                key={`${linkName}:${link}`}
+                className="text-md flex items-center gap-2"
+              >
+                {IconComponent[linkName]}
                 {linkName}
               </ExternalLink>
             ))
@@ -65,17 +92,12 @@ const ColonyDetailsPage: FC = () => {
           }}
         />
       </div>
-      <div
-        className={clsx(
-          'p-6 mt-12 flex flex-col justify-between items-center gap-6 sm:flex-row sm:gap-12',
-          styles.box,
-        )}
-      >
-        <div className="w-full sm:max-w-[32.875rem]">
+      <div className="p-6 mt-12 flex flex-col justify-between items-center gap-6 sm:flex-row sm:gap-12 bg-gray-25 border border-gray-200 rounded-lg relative">
+        <div className="w-full">
           <h3 className="heading-4 mb-4">
-            <FormattedMessage id="colonyDetailsPage.objectiveTitle" />
+            {formatText({ id: 'colonyDetailsPage.objectiveTitle' })}
           </h3>
-          <p className="text-md text-gray-600 mb-6">
+          <p className="text-sm text-gray-600 mb-6">
             <FormattedMessage
               id="colonyDetailsPage.objectiveDescription"
               values={{ br: <br /> }}
@@ -96,7 +118,7 @@ const ColonyDetailsPage: FC = () => {
         </div>
         <div className="w-full sm:max-w-[20.375rem]">
           <h5 className="text-3 mb-2">
-            <FormattedMessage id="colonyDetailsPage.objectiveBoxTitle" />
+            {formatText({ id: 'colonyDetailsPage.objectiveBoxTitle' })}
           </h5>
           <ObjectiveBox objective={objective} />
         </div>
@@ -104,7 +126,8 @@ const ColonyDetailsPage: FC = () => {
           // @TODO: Test functionality to create objective on mobile
           <Button
             mode="primarySolid"
-            text={{ id: 'button.createObjective' }}
+            text={{ id: 'button.manageObjective' }}
+            textValues={{ existing: !!objective?.title }}
             isFullSize={isMobile}
             onClick={() => {
               toggleActionSidebarOn({
