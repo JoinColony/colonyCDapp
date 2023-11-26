3b3ffcd207ec3bdb0bcadd6bb14512d92ca8919c
Raul Glogovetan <raul@glogovetan.com>
2023-11-26 23:43:58 +0200

    Fix: staked, passed motion, outcome step

--- src/components/v5/common/ActionSidebar/partials/Motions/types.ts
+++ src/components/v5/common/ActionSidebar/partials/Motions/types.ts
@@ -1,3 +1,12 @@
+import { MotionState } from '@colony/colony-js';
+
 export interface MotionsProps {
   transactionId: string;
 }
+
+export enum CustomStep {
+  StakedMotionOutcome = 'stakedmotionoutcome',
+  VotedMotionOutcome = 'votedmotionoutcome',
+}
+
+export type Steps = MotionState | CustomStep;
