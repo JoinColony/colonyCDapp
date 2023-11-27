47335428e4069fed5715cf6367c681aa5be7ac4e
Raul Glogovetan <raul@glogovetan.com>
2023-11-27 14:03:47 +0200

    Fix: voting `OutcomeStep` display correct side winnings

--- src/components/v5/common/ActionSidebar/partials/Motions/steps/OutcomeStep/OutcomeStep.tsx
+++ src/components/v5/common/ActionSidebar/partials/Motions/steps/OutcomeStep/OutcomeStep.tsx
@@ -1,19 +1,24 @@
 import React, { FC } from 'react';
 
-import { formatText } from '~utils/intl';
-import { MotionState } from '~utils/colonyMotions';
 import MenuWithSections from '~v5/shared/MenuWithSections';
 import { UserAvatarsItem } from '~v5/shared/UserAvatars/types';
 
+import { formatText } from '~utils/intl';
+import { MotionState, MotionVote } from '~utils/colonyMotions';
+import { useAppContext } from '~hooks';
+
+import { OutcomeStepProps } from './types';
 import { useOutcomeStep } from './hooks';
 import VoteStatuses from './partials/VoteStatuses';
-import { OutcomeStepProps } from './types';
 
 const displayName =
   'v5.common.ActionSidebar.partials.motions.Motion.steps.OutcomeStep';
 
 const OutcomeStep: FC<OutcomeStepProps> = ({ motionData, motionState }) => {
+  const { wallet, user } = useAppContext();
   const { voteStatuses } = useOutcomeStep(motionData);
+
+  const canInteract = !!wallet && !!user;
 
   const voters: UserAvatarsItem[] =
     motionData?.voterRecord.map((voter) => ({
@@ -22,6 +27,25 @@ const OutcomeStep: FC<OutcomeStepProps> = ({ motionData, motionState }) => {
       vote: voter.vote ?? undefined,
     })) || [];
 
+  const currentUserVote = voters.find(
+    ({ address }) => address === wallet?.address,
+  );
+  const currentUserVoted = !!currentUserVote;
+
+  let outcome = false;
+  if (
+    currentUserVote?.vote === MotionVote.Yay &&
+    motionState === MotionState.Passed
+  ) {
+    outcome = true;
+  }
+  if (
+    currentUserVote?.vote === MotionVote.Nay &&
+    motionState === MotionState.Failed
+  ) {
+    outcome = true;
+  }
+
   return (
     <MenuWithSections
       sections={[
@@ -29,14 +53,15 @@ const OutcomeStep: FC<OutcomeStepProps> = ({ motionData, motionState }) => {
           key: '1',
           content: (
             <div className="flex flex-col gap-4">
-              <h3 className="text-center text-1 mb-2">
-                {formatText({
-                  id:
-                    motionState === MotionState.Passed
+              {canInteract && currentUserVoted && (
+                <h3 className="text-center text-1 mb-2">
+                  {formatText({
+                    id: outcome
                       ? 'motion.outcomeStep.win.title'
                       : 'motion.outcomeStep.lost.title',
-                })}
-              </h3>
+                  })}
+                </h3>
+              )}
               <VoteStatuses items={voteStatuses} voters={voters} />
             </div>
           ),
