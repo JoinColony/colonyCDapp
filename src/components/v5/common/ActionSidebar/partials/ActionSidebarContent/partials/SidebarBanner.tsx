3afadf8a78c41d573cabc6a34936f4c06f612658
Armando Graterol <armandogrtrl@gmail.com>
2023-11-29 18:03:50 -0300

    Added missing permission one tx payment extension banner to action form

--- src/components/v5/common/ActionSidebar/partials/ActionSidebarContent/partials/SidebarBanner.tsx
+++ src/components/v5/common/ActionSidebar/partials/ActionSidebarContent/partials/SidebarBanner.tsx
@@ -1,17 +1,49 @@
 import React, { FC } from 'react';
+import { Extension, Id } from '@colony/colony-js';
 
 import { useFormContext, useWatch } from 'react-hook-form';
-import { Action } from '~constants/actions';
-import { useFlatFormErrors } from '~hooks';
+import { ACTION, Action } from '~constants/actions';
+import { useColonyContext, useExtensionData, useFlatFormErrors } from '~hooks';
+import { formatText } from '~utils/intl';
+import NotificationBanner from '~v5/shared/NotificationBanner';
+import { AnyExtensionData, Colony } from '~types';
+import { addressHasRoles } from '~utils/checks';
+
 import {
   ACTION_TYPE_FIELD_NAME,
   useCreateActionTypeNotification,
   useCreateActionTypeNotificationHref,
 } from '../../../consts';
-import { formatText } from '~utils/intl';
-import NotificationBanner from '~v5/shared/NotificationBanner';
+
+const checkOneTxPaymentPermissions = (
+  extensionData: AnyExtensionData,
+  colony: Colony | undefined,
+) => {
+  const {
+    neededColonyPermissions,
+    // address will be undefined if the extension hasn't been installed / initialized yet
+    // @ts-expect-error
+    address,
+    isInitialized,
+    isDeprecated,
+  } = extensionData;
+
+  // If the extension itself doesn't have the correct permissions, show the banner
+  const noPermissions =
+    isInitialized &&
+    !isDeprecated &&
+    !addressHasRoles({
+      requiredRolesDomains: [Id.RootDomain],
+      colony,
+      requiredRoles: neededColonyPermissions,
+      address,
+    });
+
+  return noPermissions;
+};
 
 export const SidebarBanner: FC = () => {
+  const { colony } = useColonyContext();
   const { formState } = useFormContext();
   const hasErrors = !formState.isValid && formState.isSubmitted;
   const selectedAction: Action | undefined = useWatch({
@@ -26,11 +58,18 @@ export const SidebarBanner: FC = () => {
   const actionTypeNofiticationHref =
     useCreateActionTypeNotificationHref(selectedAction);
 
+  const oneTxPaymentExtensionId =
+    selectedAction === ACTION.SIMPLE_PAYMENT ? Extension.OneTxPayment : '';
+  const { extensionData } = useExtensionData(oneTxPaymentExtensionId);
+  const oneTxPaymentPermissionNeeded = extensionData
+    ? checkOneTxPaymentPermissions(extensionData, colony)
+    : false;
+
   if (actionTypeNotificationTitle) {
     return (
       <div className="mt-7">
         <NotificationBanner
-          status="error"
+          status={oneTxPaymentPermissionNeeded ? 'warning' : 'error'}
           icon="warning-circle"
           callToAction={
             <a
