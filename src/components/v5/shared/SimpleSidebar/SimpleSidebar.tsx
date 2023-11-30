5d2611d30ac9b8931e35935c942d4cd5cb6dfb11
Armando Graterol <armandogrtrl@gmail.com>
2023-11-30 16:18:00 -0300

    Improved spacing on disconnected state of landing page navbar

--- src/components/v5/shared/SimpleSidebar/SimpleSidebar.tsx
+++ src/components/v5/shared/SimpleSidebar/SimpleSidebar.tsx
@@ -5,8 +5,10 @@ import ColonyLogo from '~images/logo-new.svg';
 
 const SimpleSidebar: FC = () => {
   return (
-    <nav className="flex flex-col items-center justify-between border text-center border-gray-200 rounded-lg p-4 h-full ">
-      <Icon appearance={{ size: 'extraBig' }} name="colony-icon" />
+    <nav className="flex flex-col items-center justify-between border text-center border-gray-200 rounded-lg px-4 pt-4 pb-6 h-full ">
+      <div className="flex items-center justify-center w-11 h-11">
+        <Icon appearance={{ size: 'extraBig' }} name="colony-icon" />
+      </div>
       <ColonyLogo />
     </nav>
   );
