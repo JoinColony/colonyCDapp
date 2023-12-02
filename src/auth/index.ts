beabaa80623c646ff161ae7e30fa448ca66129b3
Raul Glogovetan <raul@glogovetan.com>
2023-12-02 13:19:34 +0200

    Refactor: auth logic to recover if server goes down

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -4,6 +4,29 @@ import { SiweMessage } from 'siwe';
 import { ContextModule, getContext } from '~context';
 import { isFullWallet } from '~types';
 
+/*
+ * @NOTE This only works on Chrome (and maybe Chromium based browsers),
+ * but definitely not on Firefox
+ *
+ * On Firefox this API is yet to be implemented and last time I checked,
+ * I couldn't find a polyfill to do this
+ *
+ * PS: the `cookie-store` ponyfill, while it works on both Firefox and Chrome,
+ * it doesn't have the same interface as the native Chrome one, and it doesn't
+ * return the expiration date, which is the only thing we need out of the cookie
+ */
+declare global {
+  interface Window {
+    cookieStore: {
+      get: (name: string) => Promise<Record<string, any>>;
+      getAll: () => Promise<Record<string, any>[]>;
+      delete: (name: string) => Promise<void>;
+    };
+  }
+}
+
+const AUTH_COOKIE_NAME = 'auth';
+
 const authProxyRequest = async (urlPartial: string, options?: RequestInit) => {
   const { host, origin } = window.location;
   try {
@@ -24,6 +47,34 @@ const authProxyRequest = async (urlPartial: string, options?: RequestInit) => {
   } catch (error) {
     console.error('Could not send authentication request', error);
     return null;
+  }
+};
+
+const clearAuthCookie = async () => {
+  if (window.cookieStore) {
+    const cookie = await window.cookieStore.get(AUTH_COOKIE_NAME);
+    if (cookie) {
+      await window.cookieStore.delete(AUTH_COOKIE_NAME);
+    }
+  }
+};
+
+export const getAuthExpirationTimeout = async (): Promise<number> => {
+  if (window.cookieStore) {
+    const cookie = await window.cookieStore.get(AUTH_COOKIE_NAME);
+    if (cookie && cookie.expires) {
+      return Math.round(cookie.expires - Date.now());
+    }
+  }
+  return 0;
+};
+
+export const deauthenticateWallet = async () => {
+  try {
+    await authProxyRequest('deauth', { method: 'POST' });
+    await clearAuthCookie();
+  } catch (error) {
+    console.error('Could not deauthenticate user', error);
   }
 };
 
@@ -66,42 +117,28 @@ export const authenticateWallet = async () => {
 
     const signature = await signer.signMessage(message);
 
-    return authProxyRequest('auth', {
+    await clearAuthCookie();
+
+    await authProxyRequest('auth', {
       method: 'POST',
       body: JSON.stringify({ message, signature }),
     });
+
+    /*
+     * We need to start performing a check to see if the user is authenticated
+     * As there's a issue when the server goes down / comes back up, it will
+     * break the auth session
+     *
+     * The UI fix for that is to disconect / authenticate again, so we try to
+     * replicate that in code, automatically
+     */
+    const secondAuthCheck = await authProxyRequest('check');
+
+    if (secondAuthCheck?.message !== 'authenticated') {
+      await deauthenticateWallet();
+      await authenticateWallet();
+    }
+    return null;
   }
   return null;
-};
-
-export const deauthenticateWallet = async () => {
-  try {
-    await authProxyRequest('deauth', { method: 'POST' });
-  } catch (error) {
-    console.error('Could not deauthenticate user', error);
-  }
-};
-
-export const getAuthExpirationTimeout = async (): Promise<number> => {
-  /*
-   * @NOTE This only works on Chrome (and maybe Chromium based browsers),
-   * but definitely not on Firefox
-   *
-   * On Firefox this API is yet to be implemented and last time I checked,
-   * I couldn't find a polyfill to do this
-   *
-   * PS: the `cookie-store` ponyfill, while it works on both Firefox and Chrome,
-   * it doesn't have the same interface as the native Chrome one, and it doesn't
-   * return the expiration date, which is the only thing we need out of the cookie
-   */
-
-  // @ts-ignore
-  if (window.cookieStore) {
-    // @ts-ignore
-    const cookie = await window.cookieStore.get('auth');
-    if (cookie && cookie.expires) {
-      return Math.round(cookie.expires - Date.now());
-    }
-  }
-  return 0;
 };
