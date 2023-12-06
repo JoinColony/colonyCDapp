10e165406b5d745a94b0c436ea30ba04e8ac5430
Bassgeta <kerry.mahne@gmail.com>
2023-12-06 15:18:14 +0100

    lint: long className lint

--- src/components/common/Extensions/UserNavigation/partials/UserSubmenu/UserSubmenu.tsx
+++ src/components/common/Extensions/UserNavigation/partials/UserSubmenu/UserSubmenu.tsx
@@ -27,7 +27,10 @@ const UserSubmenu: FC<UserSubmenuProps> = ({ submenuId }) => {
       {userSubmenuItems[submenuId].map((item) => (
         <li
           key={item.id}
-          className={clsx('mb-2 last:mb-0 sm:mb-0 hover:bg-gray-50 rounded -ml-4 w-[calc(100%+2rem)]', item.className)}
+          className={clsx(
+            'mb-2 last:mb-0 sm:mb-0 hover:bg-gray-50 rounded -ml-4 w-[calc(100%+2rem)]',
+            item.className,
+          )}
         >
           {item.external ? (
             <a
