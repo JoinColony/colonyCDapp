915d0b4e30a7bbd84f4fb90427f4b8f4c3d24151
Jakub Zajac <jakub@colony.io>
2023-12-07 23:29:55 +0000

    Cleanup: Remove unused ColonySwitcher component

--- src/components/common/Extensions/ColonySwitcher/index.ts
+++
@@ -1,4 +0,0 @@
-export { default } from './ColonySwitcher';
-export { default as ColoniesDropdown } from './partials/ColoniesDropdown';
-export { default as ColonyItem } from './partials/ColonyItem';
-export { default as ColonyAvatarWrapper } from './partials/ColonyAvatarWrapper';
