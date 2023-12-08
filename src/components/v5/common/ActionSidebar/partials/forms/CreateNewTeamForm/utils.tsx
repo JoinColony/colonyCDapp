a469d8ab304ef472d0b6c4de442e6e00e1567a40
Jakub Zajac <jakub@colony.io>
2023-12-08 01:32:46 +0000

    Cleanup Create New Team action, wire annotations

--- src/components/v5/common/ActionSidebar/partials/forms/CreateNewTeamForm/utils.tsx
+++ src/components/v5/common/ActionSidebar/partials/forms/CreateNewTeamForm/utils.tsx
@@ -1,8 +1,11 @@
 import { DeepPartial } from 'utility-types';
+
 import { ActionTitleMessageKeys } from '~common/ColonyActions/helpers/getActionTitleValues';
 import { ColonyActionType } from '~gql';
+import { Colony } from '~types';
 import { DecisionMethod } from '~v5/common/ActionSidebar/hooks';
 import { DescriptionMetadataGetter } from '~v5/common/ActionSidebar/types';
+
 import { CreateNewTeamFormValues } from './consts';
 
 export const createNewTeamDescriptionMetadataGetter: DescriptionMetadataGetter<
@@ -27,3 +30,17 @@ export const createNewTeamDescriptionMetadataGetter: DescriptionMetadataGetter<
     },
   );
 };
+
+export const getCreateNewTeamPayload = (
+  colony: Colony,
+  values: CreateNewTeamFormValues,
+) => ({
+  ...values,
+  domainName: values.teamName,
+  isCreateDomain: true,
+  colonyAddress: colony.colonyAddress,
+  colonyName: colony.name,
+  customActionTitle: values.title,
+  motionDomainId: values.createdIn,
+  annotationMessage: values.description,
+});
