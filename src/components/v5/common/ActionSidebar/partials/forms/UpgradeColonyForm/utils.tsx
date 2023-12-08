0f2a76225638ceffacaaf8aab22d0e9c62dcca14
Jakub Zajac <jakub@colony.io>
2023-12-08 02:11:43 +0000

    Cleanup Upgrade Colony action, wire annotations

--- src/components/v5/common/ActionSidebar/partials/forms/UpgradeColonyForm/utils.tsx
+++ src/components/v5/common/ActionSidebar/partials/forms/UpgradeColonyForm/utils.tsx
@@ -1,7 +1,11 @@
 import { DeepPartial } from 'utility-types';
+
 import { ColonyActionType } from '~gql';
+import { RootMotionMethodNames } from '~redux';
+import { Colony } from '~types';
 import { DecisionMethod } from '~v5/common/ActionSidebar/hooks';
 import { DescriptionMetadataGetter } from '~v5/common/ActionSidebar/types';
+
 import { UpgradeColonyFormValues } from './consts';
 
 export const upgradeColonyDescriptionMetadataGetter: DescriptionMetadataGetter<
@@ -15,3 +19,15 @@ export const upgradeColonyDescriptionMetadataGetter: DescriptionMetadataGetter<
     newColonyVersion: (colony?.version ?? 0) + 1,
   });
 };
+
+export const getUpgradeColonyPayload = (
+  colony: Colony,
+  values: UpgradeColonyFormValues,
+) => ({
+  operationName: RootMotionMethodNames.Upgrade,
+  colonyAddress: colony.colonyAddress,
+  colonyName: colony.name,
+  version: colony.version,
+  motionParams: [colony.version + 1],
+  annotationMessage: values.description,
+});
