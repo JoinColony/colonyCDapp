f8a5c38676fb4adae3ca0c4564c9cf1746142e34
William Martin <williamajmartin@gmail.com>
2023-12-21 13:26:22 +0100

    feat: set preferred currency given user location

---
+++ src/utils/currency/location.ts
@@ -0,0 +1,51 @@
+import { SupportedCurrencies } from '~gql';
+import { countryCodeToCurrencyMap, locationApiConfig } from './config';
+import { IpLocationResponse, IpifyResponse } from './types';
+import { buildAPIEndpoint, fetchData } from './utils';
+
+const { ipGeolocatorEndpoint, ipLookupEndpoint } = locationApiConfig;
+
+/**
+ * Fetch the user's country code based on their IP address.
+ * @returns The user's country code, e.g. 'US'
+ */
+const getUserIp = async () => {
+  const url = buildAPIEndpoint(new URL(ipLookupEndpoint), { format: 'json' });
+  const ip = await fetchData<IpifyResponse>(
+    url,
+    (response) => {
+      return response.ip;
+    },
+    'Unable to get user IP address.',
+  );
+
+  return ip;
+};
+
+const getUserCountryCode = async (ip: string) => {
+  const url = buildAPIEndpoint(new URL(ipGeolocatorEndpoint), { ip });
+  return fetchData<IpLocationResponse>(
+    url,
+    // eslint-disable-next-line camelcase
+    ({ country_code2 }) => country_code2,
+    'Failed to fetch user country code. ',
+  );
+};
+
+/**
+ * @returns The user's currency based on their location, e.g. 'USD'
+ */
+export const getUserCurrencyByLocation =
+  async (): Promise<SupportedCurrencies> => {
+    const ip = await getUserIp();
+
+    if (ip) {
+      const countryCode = await getUserCountryCode(ip);
+
+      if (countryCode && countryCode in countryCodeToCurrencyMap) {
+        return countryCodeToCurrencyMap[countryCode];
+      }
+    }
+
+    return SupportedCurrencies.Usd;
+  };
