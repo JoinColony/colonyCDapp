ee196a28c3747eeb89b153986a180bee2da5d0dc
Tomasz Chmiel <tomasz.chmiel@pagepro.co>
2023-12-21 09:56:24 +0100

    Fallback to action metadate description when cutom title is empty

--- src/components/common/ColonyActionsTable/partials/ActionDescription/ActionDescription.tsx
+++ src/components/common/ColonyActionsTable/partials/ActionDescription/ActionDescription.tsx
@@ -36,6 +36,10 @@ const ActionDescription: FC<ActionDescriptionProps> = ({
     motionState || null,
   );
 
+  const actionMetadataDescription = colony
+    ? formatText({ id: 'action.title' }, getActionTitleValues(action, colony))
+    : '';
+
   return (
     <div className="flex gap-4 items-center w-full">
       <Avatar
@@ -58,7 +62,7 @@ const ActionDescription: FC<ActionDescriptionProps> = ({
               },
             )}
           >
-            {metadata?.customTitle || '-'}
+            {metadata?.customTitle || actionMetadataDescription || '-'}
           </p>
           {colony && (
             <p
@@ -69,12 +73,7 @@ const ActionDescription: FC<ActionDescriptionProps> = ({
                 },
               )}
             >
-              {loading
-                ? ''.padEnd(40, '-')
-                : formatText(
-                    { id: 'action.title' },
-                    getActionTitleValues(action, colony),
-                  )}
+              {loading ? ''.padEnd(40, '-') : actionMetadataDescription}
             </p>
           )}
         </div>
