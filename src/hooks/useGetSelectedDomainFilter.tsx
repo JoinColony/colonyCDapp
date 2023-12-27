80ed5aac52af2ca1f24204dd019411e32692a56c
Jakub Zajac <jakub@colony.io>
2023-12-27 18:35:23 +0000

    Cleanup: Simplify useGetSelectedDomainFilter

--- src/hooks/useGetSelectedDomainFilter.tsx
+++ src/hooks/useGetSelectedDomainFilter.tsx
@@ -1,25 +1,19 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { TEAM_SEARCH_PARAM } from '~routes';
-import { notNull } from '~utils/arrays';
+import { findDomainByNativeId } from '~utils/domains';
 
 import useColonyContext from './useColonyContext';
 
 const useGetSelectedDomainFilter = () => {
   const { colony } = useColonyContext();
-  const { domains } = colony || {};
 
   const [searchParams, setSearchParams] = useSearchParams();
   const domainId = searchParams.get(TEAM_SEARCH_PARAM);
-
-  const selectedDomain = useMemo(
-    () =>
-      domains?.items
-        .filter(notNull)
-        .find((domain) => domain?.nativeId === parseInt(domainId ?? '0', 10)),
-    [domains?.items, domainId],
-  );
+  const selectedDomain = domainId
+    ? findDomainByNativeId(parseInt(domainId, 10), colony)
+    : undefined;
 
   useEffect(() => {
     if (selectedDomain && !domainId) {
