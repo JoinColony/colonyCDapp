14d26b4dfc76d05fa29aab2e329b0bc6f99fafec
Armando Graterol <armandogrtrl@gmail.com>
2024-01-04 17:12:12 -0300

    Fixed global styles applying after specific styles

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import ReactModal from 'react-modal';
 
-import '~utils/yup/customMethods'; // ensures custom yup methods are available when components load
-// eslint-disable-next-line import/order
+import '~utils/yup/customMethods'; // @NOTE: This is placed here to ensure custom yup methods are available when components load
+
+import './styles/main.global.css'; // @NOTE: This is placed here to ensure it is loaded before any other styles specific to the components (like CSS modules)
+
+/* eslint-disable import/order */
 import store from '~redux/createReduxStore';
 
 import Entry from './Entry';
-
-import './styles/main.global.css';
+/* eslint-enable import/order */
 
 Decimal.set({ toExpPos: 78 });
 
