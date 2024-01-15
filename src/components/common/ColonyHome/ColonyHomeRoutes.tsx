4284c5e2b90d32fd493a32c2ab8aa6d5cc29b837
Jakub Zajac <jakub@colony.io>
2024-01-15 13:36:27 +0000

    Cleanup: Remove ColonyHomeLayout and related files

--- src/components/common/ColonyHome/ColonyHomeRoutes.tsx
+++ src/components/common/ColonyHome/ColonyHomeRoutes.tsx
@@ -4,16 +4,12 @@ import { Outlet } from 'react-router-dom';
 import { ColonyHomeProvider, useSetPageHeadingTitle } from '~context';
 import { formatText } from '~utils/intl';
 
-import ColonyHomeLayout from './ColonyHomeLayout';
-
 const ColonyHomeRoutes = () => {
   useSetPageHeadingTitle(formatText({ id: 'colonyHome.title' }));
 
   return (
     <ColonyHomeProvider>
-      <ColonyHomeLayout>
-        <Outlet />
-      </ColonyHomeLayout>
+      <Outlet />
     </ColonyHomeProvider>
   );
 };
