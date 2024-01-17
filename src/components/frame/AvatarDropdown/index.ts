99cd6a6c5c331445a0579ed635d3af1cb99083d7
Jakub Zajac <jakub@colony.io>
2024-01-17 23:09:43 +0000

    Cleanup: shared folder

--- src/components/frame/AvatarDropdown/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AvatarDropdown } from './AvatarDropdown';
-export { default as ItemContainer } from './ItemContainer';
--- src/components/shared/Warning/Warning.css.d.ts
+++
@@ -1,2 +0,0 @@
-export const warning: string;
-export const content: string;
