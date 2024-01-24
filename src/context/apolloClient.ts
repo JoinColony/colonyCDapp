5335b59ceceba18ab1ccc962e78fff0db85d597a
Jakub Zajac <jakub@colony.io>
2024-01-24 21:44:38 +0000

    Fix activity feed issues: Downgrade apollo to last working version

--- src/context/apolloClient.ts
+++ src/context/apolloClient.ts
@@ -1,18 +1,14 @@
-import { ApolloClient, from, HttpLink } from '@apollo/client';
-import { removeTypenameFromVariables } from '@apollo/client/link/remove-typename';
+import { ApolloClient, HttpLink } from '@apollo/client';
 
 import cache from '~cache';
 
-const removeTypenameLink = removeTypenameFromVariables();
 const httpLink = new HttpLink({
   uri: `${process.env.AUTH_PROXY_ENDPOINT || 'http://localhost:3005'}/graphql`,
   credentials: 'include',
 });
 
-const link = from([removeTypenameLink, httpLink]);
-
 export default new ApolloClient({
-  link,
+  link: httpLink,
   connectToDevTools: true,
   cache,
   /*
