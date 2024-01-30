760d63666b8d9237a1e6c95791d3dbfdea339787
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-01-30 16:14:49 +0000

    Fix: Teams UI optimisations

--- src/components/v5/common/TeamCardList/TeamCardList.tsx
+++ src/components/v5/common/TeamCardList/TeamCardList.tsx
@@ -7,14 +7,13 @@ import TeamCard from '../TeamCard/index.ts';
 import { type TeamCardListProps } from './types.ts';
 
 const TeamCardList: FC<TeamCardListProps> = ({ items, className }) => (
-  <ul className={clsx(className, 'flex flex-wrap gap-6')}>
+  <ul className={clsx(className, 'grid sm:grid-cols-2 lg:grid-cols-3 gap-6')}>
     {items.map(({ key, ...item }) => (
       <motion.li
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         key={key}
-        className="w-full sm:w-[calc(50%-.75rem)]"
       >
         <TeamCard {...item} />
       </motion.li>
