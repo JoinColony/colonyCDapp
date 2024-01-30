ca88a633108a2e2bb574c079ba46e171934f293f
Chris Maniewski <code@chmanie.com>
2024-01-30 12:15:59 +0100

    delete: remove even more unused components

--- src/components/shared/BreadCrumb/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './BreadCrumb.tsx';
-export { Crumb } from './BreadCrumb.tsx';
--- src/components/shared/ColonyAvatar/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ColonyAvatar.tsx';
-
-export { default } from './ColonyAvatar.tsx';
