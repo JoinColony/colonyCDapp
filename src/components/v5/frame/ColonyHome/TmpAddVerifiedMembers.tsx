1f99eade70d2e68ee99865398e71850dec5cfc2e
Bassgeta <kerry.mahne@gmail.com>
2024-01-30 15:49:38 +0100

    feat: connect dummy add verified members component to saga

--- src/components/v5/frame/ColonyHome/TmpAddVerifiedMembers.tsx
+++ src/components/v5/frame/ColonyHome/TmpAddVerifiedMembers.tsx
@@ -1,29 +1,46 @@
 import { Id } from '@colony/colony-js';
+import { pipe } from 'lodash/fp';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import { useColonyContext } from '~hooks';
+import { useAsyncFunction, useColonyContext } from '~hooks';
 import { ActionTypes } from '~redux/actionTypes';
+import { mapPayload, withMeta } from '~utils/actions';
+import { setQueryParamOnUrl } from '~utils/urls';
 
 export const TmpAddVerifiedMembers = () => {
   const {
     colony: { colonyAddress, name },
   } = useColonyContext();
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [members, setMembers] = useState('');
 
+  const verifyMembers = useAsyncFunction({
+    submit: ActionTypes.ACTION_ADD_VERIFIED_MEMBERS,
+    error: ActionTypes.ACTION_ADD_VERIFIED_MEMBERS_ERROR,
+    success: ActionTypes.ACTION_ADD_VERIFIED_MEMBERS_SUCCESS,
+    transform: pipe(
+      mapPayload((payload) => {
+        return payload;
+      }),
+      withMeta({
+        setTxHash: (txHash: string) => {
+          navigate(setQueryParamOnUrl(window.location.pathname, 'tx', txHash), {
+            replace: true,
+          });
+        },
+      }),
+    ),
+  });
+
   const handleSubmit = () => {
-    // console.log('sabamito!');
-    dispatch({
-      type: ActionTypes.ACTION_ADD_VERIFIED_MEMBERS,
-      payload: {
-        colonyAddress,
-        colonyName: name,
-        members: members.split(','),
-        customActionTitle: 'Verifying members!',
-        domainId: Id.RootDomain,
-        annotationMessage: 'Annotated bruh',
-      },
+    verifyMembers({
+      colonyAddress,
+      colonyName: name,
+      members: members.split(','),
+      customActionTitle: 'Verifying members!',
+      domainId: Id.RootDomain,
+      annotationMessage: 'Annotated bruh',
     });
   };
 
