ad4d8cc4e4854fa6dceb963657049fab2f9dab44
Bassgeta <kerry.mahne@gmail.com>
2024-02-01 12:55:52 +0100

    feat: add dummy verified members motion saga and connect it

--- src/redux/types/actions/colonyActions.ts
+++ src/redux/types/actions/colonyActions.ts
@@ -304,7 +304,6 @@ export type ColonyActionsActionTypes =
         colonyAddress: Address;
         colonyName: string;
         members: string[];
-        domainId: number;
         annotationMessage?: string;
         customActionTitle: string;
       },
@@ -331,6 +330,24 @@ export type ColonyActionsActionTypes =
   | ErrorActionType<ActionTypes.ACTION_REMOVE_VERIFIED_MEMBERS_ERROR, object>
   | UniqueActionType<
       ActionTypes.ACTION_REMOVE_VERIFIED_MEMBERS_SUCCESS,
+      object,
+      object
+    >
+  | UniqueActionType<
+      ActionTypes.MOTION_ADD_VERIFIED_MEMBERS,
+      {
+        colonyAddress: Address;
+        colonyName: string;
+        members: string[];
+        domainId: number;
+        annotationMessage?: string;
+        customActionTitle: string;
+      },
+      MetaWithSetter<object>
+    >
+  | ErrorActionType<ActionTypes.MOTION_ADD_VERIFIED_MEMBERS_ERROR, object>
+  | UniqueActionType<
+      ActionTypes.MOTION_ADD_VERIFIED_MEMBERS_SUCCESS,
       object,
       object
     >
