b9a4d229baed737cc648883d4993fa60fcb64a61
Bassgeta <kerry.mahne@gmail.com>
2024-02-01 12:54:27 +0100

    feat: add ADD_VERIFIED_MEMBERS_MOTION type

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -369,6 +369,7 @@ export type ColonyActionRolesInput = {
 export enum ColonyActionType {
   /** An action related to adding verified members */
   AddVerifiedMembers = 'ADD_VERIFIED_MEMBERS',
+  AddVerifiedMembersMotion = 'ADD_VERIFIED_MEMBERS_MOTION',
   /** An action related to a motion to cancel a staked expenditure */
   CancelStakedExpenditureMotion = 'CANCEL_STAKED_EXPENDITURE_MOTION',
   /** An action related to editing a Colony's details */
