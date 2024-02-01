f9de4a2205828e323f65996a91c3ab6cb845376a
Bassgeta <kerry.mahne@gmail.com>
2024-02-01 16:09:30 +0100

    feat: construct the add verified members motion transaction

--- src/redux/sagas/motions/addVerifiedMembers.ts
+++ src/redux/sagas/motions/addVerifiedMembers.ts
@@ -1,25 +1,43 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import {
+  type AnyColonyClient,
+  ClientType,
+  getChildIndex,
+  Id,
+} from '@colony/colony-js';
+import { AddressZero } from '@ethersproject/constants';
+import { call, fork, put, takeEvery } from 'redux-saga/effects';
 
 import { ActionTypes } from '~redux';
 import type { Action, AllActions } from '~redux';
 
-import { getTxChannel } from '../transactions/index.ts';
-import { putError } from '../utils/index.ts';
+import {
+  createTransaction,
+  createTransactionChannels,
+  getTxChannel,
+} from '../transactions/index.ts';
+import {
+  createActionMetadataInDB,
+  getColonyManager,
+  initiateTransaction,
+  putError,
+  takeFrom,
+  uploadAnnotation,
+} from '../utils/index.ts';
+import { getAddVerifiedMembersOperation } from '../utils/verifiedMembers.ts';
 
 function* addVerifiedMembersMotion({
   payload: {
     colonyAddress,
-    // colonyName,
-    // domainId,
+    colonyName,
+    domainId,
     members,
-    // customActionTitle,
-    //   annotationMessage,
+    customActionTitle,
+    annotationMessage,
   },
-  meta: { id: metaId /* navigate, setTxHash */ },
+  meta: { id: metaId, navigate, setTxHash },
   meta,
 }: Action<ActionTypes.MOTION_ADD_VERIFIED_MEMBERS>) {
-  let txChannel;
-
+  const txChannel = yield call(getTxChannel, metaId);
   try {
     if (!colonyAddress) {
       throw new Error(
@@ -30,7 +48,112 @@ function* addVerifiedMembersMotion({
       throw new Error('No members set for addVerifiedMembers transaction');
     }
 
-    txChannel = yield call(getTxChannel, metaId);
+    const colonyManager = yield call(getColonyManager);
+    const colonyClient: AnyColonyClient = yield call(
+      [colonyManager, colonyManager.getClient],
+      ClientType.ColonyClient,
+      colonyAddress,
+    );
+
+    const childSkillIndex = yield call(
+      getChildIndex,
+      colonyClient.networkClient,
+      colonyClient,
+      domainId,
+      Id.RootDomain,
+    );
+
+    const { skillId } = yield call(
+      [colonyClient, colonyClient.getDomain],
+      domainId,
+    );
+
+    const { key, value, branchMask, siblings } = yield call(
+      colonyClient.getReputation,
+      skillId,
+      AddressZero,
+    );
+
+    const batchKey = 'createMotion';
+
+    const { createMotion, annotateAddVerifiedMembersMotion } =
+      yield createTransactionChannels(metaId, [
+        'createMotion',
+        'annotateAddVerifiedMembersMotion',
+      ]);
+
+    const encodedAction = colonyClient.interface.encodeFunctionData(
+      'editColonyByDelta',
+      [JSON.stringify(getAddVerifiedMembersOperation(colonyAddress, members))],
+    );
+
+    yield fork(createTransaction, createMotion.id, {
+      context: ClientType.VotingReputationClient,
+      methodName: 'createMotion',
+      identifier: colonyAddress,
+      params: [
+        domainId,
+        childSkillIndex,
+        AddressZero,
+        encodedAction,
+        key,
+        value,
+        branchMask,
+        siblings,
+      ],
+      group: {
+        key: batchKey,
+        id: metaId,
+        index: 0,
+      },
+      ready: false,
+    });
+
+    if (annotationMessage) {
+      yield fork(createTransaction, annotateAddVerifiedMembersMotion.id, {
+        context: ClientType.ColonyClient,
+        methodName: 'annotateTransaction',
+        identifier: colonyAddress,
+        params: [],
+        group: {
+          key: batchKey,
+          id: metaId,
+          index: 1,
+        },
+        ready: false,
+      });
+    }
+
+    yield takeFrom(createMotion.channel, ActionTypes.TRANSACTION_CREATED);
+    if (annotationMessage) {
+      yield takeFrom(
+        annotateAddVerifiedMembersMotion.channel,
+        ActionTypes.TRANSACTION_CREATED,
+      );
+    }
+
+    yield initiateTransaction({ id: createMotion.id });
+
+    const {
+      payload: { hash: txHash },
+    } = yield takeFrom(
+      createMotion.channel,
+      ActionTypes.TRANSACTION_HASH_RECEIVED,
+    );
+
+    setTxHash?.(txHash);
+
+    yield takeFrom(createMotion.channel, ActionTypes.TRANSACTION_SUCCEEDED);
+
+    yield createActionMetadataInDB(txHash, customActionTitle);
+
+    if (annotationMessage) {
+      yield uploadAnnotation({
+        txChannel: annotateAddVerifiedMembersMotion,
+        message: annotationMessage,
+        txHash,
+      });
+    }
 
     yield put<AllActions>({
       type: ActionTypes.MOTION_ADD_VERIFIED_MEMBERS_SUCCESS,
@@ -38,13 +161,11 @@ function* addVerifiedMembersMotion({
       meta,
     });
 
-    /*
     if (colonyName && navigate) {
       navigate(`/${colonyName}?tx=${txHash}`, {
         state: { isRedirect: true },
       });
     }
-    */
   } catch (error) {
     return yield putError(
       ActionTypes.MOTION_ADD_VERIFIED_MEMBERS_ERROR,
