3751dd220e853f66bd7b9792738b39670482d4da
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-02-05 10:51:40 +0100

    feat: added agreements page
    fix: simplified logic
    
    fix: icons

--- src/components/v5/common/EmptyContent/types.ts
+++ src/components/v5/common/EmptyContent/types.ts
@@ -2,12 +2,13 @@ import { type Icon } from '@phosphor-icons/react';
 import { type MessageDescriptor } from 'react-intl';
 
 export interface EmptyContentProps {
-  title: MessageDescriptor | string;
+  title?: MessageDescriptor | string;
   description: MessageDescriptor | string;
-  icon: Icon;
+  icon?: Icon;
   withBorder?: boolean;
   onClick?: () => void;
   buttonText?: MessageDescriptor | string;
   withoutButtonIcon?: boolean;
   className?: string;
+  buttonIcon?: Icon;
 }
