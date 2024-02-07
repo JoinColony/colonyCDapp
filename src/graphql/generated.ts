5e2e8a7ea46cccebe3eaafdde3d85e4ee38e8c90
Bassgeta <kerry.mahne@gmail.com>
2024-02-07 15:51:22 +0100

    feat: make user a nullable field

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -8385,7 +8385,7 @@ export type VerifiedMember = {
   colonyAddress: Scalars['ID'];
   createdAt: Scalars['AWSDateTime'];
   updatedAt: Scalars['AWSDateTime'];
-  user: User;
+  user?: Maybe<User>;
   userAddress: Scalars['ID'];
 };
 
