3c96b8a4c8b594cbafa36743eefd19a82a993fc8
Warren Colliver <Warren@colony.io>
2024-02-13 12:33:33 +0000

    Delete PopularAction component

--- src/components/v5/common/ActionSidebar/types.ts
+++ src/components/v5/common/ActionSidebar/types.ts
@@ -1,14 +1,9 @@
 import { type FieldValues, type UseFormReturn } from 'react-hook-form';
 
-import { type Action } from '~constants/actions.ts';
 import { type ActionFormProps } from '~shared/Fields/Form/ActionForm.tsx';
 
 export interface ActionButtonsProps {
   isActionDisabled?: boolean;
-}
-
-export interface PopularActionsProps {
-  setSelectedAction: (action: Action | null) => void;
 }
 
 export interface ActionFormBaseProps {
