f8935ac48905c2d9d8d999ee0f5c8b3ddacaf824
Jakub Zajac <jakub@colony.io>
2024-02-15 16:10:50 +0000

    Feat: locking expenditure testing UI

--- src/components/v5/payments/TmpAdvancedPayments.tsx
+++ src/components/v5/payments/TmpAdvancedPayments.tsx
@@ -1,27 +1,38 @@
 import { Id } from '@colony/colony-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useAppContext } from '~context/AppContext.tsx';
 import { useColonyContext } from '~context/ColonyContext.tsx';
 import { ExpenditureDecisionMethod } from '~gql';
+import useAsyncFunction from '~hooks/useAsyncFunction.ts';
 import useNetworkInverseFee from '~hooks/useNetworkInverseFee.ts';
 import { ActionTypes } from '~redux';
 import { type CreateExpenditurePayload } from '~redux/sagas/expenditures/createExpenditure.ts';
+import { type LockExpenditurePayload } from '~redux/sagas/expenditures/lockExpenditure.ts';
 import { findDomainByNativeId } from '~utils/domains.ts';
+import InputBase from '~v5/common/Fields/InputBase/InputBase.tsx';
+import Button from '~v5/shared/Button/Button.tsx';
 import { ActionButton } from '~v5/shared/Button/index.ts';
 
 const TmpAdvancedPayments = () => {
   const { colony } = useColonyContext();
   const { user } = useAppContext();
-
   const { networkInverseFee } = useNetworkInverseFee();
+
+  const [expenditureId, setExpenditureId] = useState('');
+
+  const lockExpenditure = useAsyncFunction({
+    submit: ActionTypes.EXPENDITURE_LOCK,
+    error: ActionTypes.EXPENDITURE_LOCK_ERROR,
+    success: ActionTypes.EXPENDITURE_LOCK_SUCCESS,
+  });
 
   const rootDomain = findDomainByNativeId(Id.RootDomain, colony);
   if (!rootDomain) {
     return null;
   }
 
-  const payload: CreateExpenditurePayload = {
+  const createExpenditurePayload: CreateExpenditurePayload = {
     payouts: [
       {
         amount: '10',
@@ -37,10 +48,36 @@ const TmpAdvancedPayments = () => {
     decisionMethod: ExpenditureDecisionMethod.Permissions,
   };
 
+  const handleLockExpenditure = async () => {
+    const payload: LockExpenditurePayload = {
+      colonyAddress: colony.colonyAddress,
+      colonyName: colony.name,
+      nativeExpenditureId: Number(expenditureId),
+    };
+
+    lockExpenditure(payload);
+  };
+
   return (
-    <ActionButton actionType={ActionTypes.EXPENDITURE_CREATE} values={payload}>
-      Create expenditure
-    </ActionButton>
+    <div className="flex flex-col gap-8">
+      <div>
+        <ActionButton
+          actionType={ActionTypes.EXPENDITURE_CREATE}
+          values={createExpenditurePayload}
+        >
+          Create expenditure
+        </ActionButton>
+      </div>
+
+      <div className="flex gap-4">
+        <InputBase
+          value={expenditureId}
+          onChange={(e) => setExpenditureId(e.currentTarget.value)}
+          placeholder="Expenditure ID"
+        />
+        <Button onClick={handleLockExpenditure}>Lock expenditure</Button>
+      </div>
+    </div>
   );
 };
 
