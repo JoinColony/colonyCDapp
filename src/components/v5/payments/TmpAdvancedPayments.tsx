529db5fe539abf919a7714306b1fafa303623e5f
Jakub Zajac <jakub@colony.io>
2024-02-15 16:13:16 +0000

    Feat: funding expenditure testing UI

--- src/components/v5/payments/TmpAdvancedPayments.tsx
+++ src/components/v5/payments/TmpAdvancedPayments.tsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 
 import { useAppContext } from '~context/AppContext.tsx';
 import { useColonyContext } from '~context/ColonyContext.tsx';
-import { ExpenditureDecisionMethod } from '~gql';
+import { ExpenditureDecisionMethod, useGetExpenditureLazyQuery } from '~gql';
 import useAsyncFunction from '~hooks/useAsyncFunction.ts';
 import useNetworkInverseFee from '~hooks/useNetworkInverseFee.ts';
 import { ActionTypes } from '~redux';
 import { type CreateExpenditurePayload } from '~redux/sagas/expenditures/createExpenditure.ts';
+import { type FundExpenditurePayload } from '~redux/sagas/expenditures/fundExpenditure.ts';
 import { type LockExpenditurePayload } from '~redux/sagas/expenditures/lockExpenditure.ts';
+import { getExpenditureDatabaseId } from '~utils/databaseId.ts';
 import { findDomainByNativeId } from '~utils/domains.ts';
 import InputBase from '~v5/common/Fields/InputBase/InputBase.tsx';
 import Button from '~v5/shared/Button/Button.tsx';
@@ -19,12 +21,19 @@ const TmpAdvancedPayments = () => {
   const { user } = useAppContext();
   const { networkInverseFee } = useNetworkInverseFee();
 
+  const [getExpenditure] = useGetExpenditureLazyQuery();
+
   const [expenditureId, setExpenditureId] = useState('');
 
   const lockExpenditure = useAsyncFunction({
     submit: ActionTypes.EXPENDITURE_LOCK,
     error: ActionTypes.EXPENDITURE_LOCK_ERROR,
     success: ActionTypes.EXPENDITURE_LOCK_SUCCESS,
+  });
+  const fundExpenditure = useAsyncFunction({
+    submit: ActionTypes.EXPENDITURE_FUND,
+    error: ActionTypes.EXPENDITURE_FUND_ERROR,
+    success: ActionTypes.EXPENDITURE_FUND_SUCCESS,
   });
 
   const rootDomain = findDomainByNativeId(Id.RootDomain, colony);
@@ -58,6 +67,30 @@ const TmpAdvancedPayments = () => {
     lockExpenditure(payload);
   };
 
+  const handleFundExpenditure = async () => {
+    const response = await getExpenditure({
+      variables: {
+        expenditureId: getExpenditureDatabaseId(
+          colony.colonyAddress,
+          Number(expenditureId),
+        ),
+      },
+    });
+    const expenditure = response.data?.getExpenditure;
+
+    if (!expenditure) {
+      return;
+    }
+
+    const payload: FundExpenditurePayload = {
+      colonyAddress: colony.colonyAddress,
+      expenditure,
+      fromDomainFundingPotId: 1,
+    };
+
+    fundExpenditure(payload);
+  };
+
   return (
     <div className="flex flex-col gap-8">
       <div>
@@ -76,6 +109,7 @@ const TmpAdvancedPayments = () => {
           placeholder="Expenditure ID"
         />
         <Button onClick={handleLockExpenditure}>Lock expenditure</Button>
+        <Button onClick={handleFundExpenditure}>Fund expenditure</Button>
       </div>
     </div>
   );
