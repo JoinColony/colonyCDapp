4721354661280c4e246ab35dd379d948c43939fd
Warren Colliver <Warren@colony.io>
2024-02-15 13:21:28 +0000

    STATUS_TYPES const to enum

--- src/components/v5/shared/StatusText/consts.ts
+++ src/components/v5/shared/StatusText/consts.ts
@@ -1,6 +1,6 @@
-export const STATUS_TYPES = {
-  SUCCESS: 'success',
-  WARNING: 'warning',
-  ERROR: 'error',
-  INFO: 'info',
-} as const;
+export enum StatusTypes {
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  ERROR = 'error',
+  INFO = 'info',
+}
