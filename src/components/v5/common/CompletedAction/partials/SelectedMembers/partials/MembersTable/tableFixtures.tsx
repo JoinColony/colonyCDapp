32576b4ae8b4dc647db9b3ec640fd6f711330fed
Bassgeta <kerry.mahne@gmail.com>
2024-02-16 13:47:09 +0100

    feat: support display of just addresses for verified members completed action

--- src/components/v5/common/CompletedAction/partials/SelectedMembers/partials/MembersTable/tableFixtures.tsx
+++ src/components/v5/common/CompletedAction/partials/SelectedMembers/partials/MembersTable/tableFixtures.tsx
@@ -1,32 +1,47 @@
 import { createColumnHelper, type ColumnDef } from '@tanstack/react-table';
 import React from 'react';
 
-import { type User } from '~types/graphql.ts';
 import { formatText } from '~utils/intl.ts';
 import Avatar from '~v5/shared/Avatar/index.ts';
 
-export type MembersTableModel = User;
+import { SelectedMemberType, type SelectedMember } from '../../types.ts';
 
-const columnHelper = createColumnHelper<User>();
+export type MembersTableModel = SelectedMember;
 
-export const membersColumns: ColumnDef<User>[] = [
+const columnHelper = createColumnHelper<SelectedMember>();
+
+export const membersColumns: ColumnDef<SelectedMember>[] = [
   columnHelper.display({
     id: 'members',
     header: () =>
       formatText({
         id: 'actionSidebar.manageVerifiedMembers.table.column.members',
       }),
-    cell: ({ row }) => (
-      <div className="flex items-center gap-2">
-        <Avatar
-          size="xs"
-          avatar={row.original.profile?.avatar ?? undefined}
-          seed={row.original.walletAddress}
-        />
-        <p className="text-md font-medium text-gray-900">
-          {row.original.profile?.displayName ?? row.original.walletAddress}
-        </p>
-      </div>
-    ),
+    cell: ({ row }) => {
+      if (row.original.type === SelectedMemberType.USER) {
+        return (
+          <div className="flex items-center gap-2">
+            <Avatar
+              size="xs"
+              avatar={row.original.data.profile?.avatar ?? undefined}
+              seed={row.original.data.walletAddress}
+            />
+            <p className="text-md font-medium text-gray-900">
+              {row.original.data.profile?.displayName ??
+                row.original.data.walletAddress}
+            </p>
+          </div>
+        );
+      }
+
+      return (
+        <div className="flex items-center gap-2">
+          <Avatar size="xs" seed={row.original.data.walletAddress} />
+          <p className="text-md font-medium text-gray-900">
+            {row.original.data.walletAddress}
+          </p>
+        </div>
+      );
+    },
   }),
 ];
