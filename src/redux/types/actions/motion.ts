63255e06d7ae1598f019c97d9cab42ca4fcdde2f
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-02-16 15:34:03 +0000

    Fix: Ensure getContributorsByColony cache evicts after motion finalizes and action completes

--- src/redux/types/actions/motion.ts
+++ src/redux/types/actions/motion.ts
@@ -2,7 +2,7 @@ import { type ColonyRole } from '@colony/colony-js';
 import { type BigNumber } from 'ethers';
 
 import { type NetworkInfo } from '~constants/index.ts';
-import { type ColonyActionType, type ExternalLink } from '~gql';
+import { type ExternalLink } from '~gql';
 import {
   type Colony,
   type ColonyObjective,
@@ -55,7 +55,6 @@ export type MotionFinalizePayload = {
   colonyAddress: Address;
   motionId: string;
   gasEstimate: string;
-  type: ColonyActionType;
 };
 
 export type MotionActionTypes =
