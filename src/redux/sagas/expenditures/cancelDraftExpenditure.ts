92231db4ef679d33803fbfcb25049a4684640537
Bassgeta <kerry.mahne@gmail.com>
2024-02-20 15:19:23 +0100

    feat: cancel draft expenditure annotations

--- src/redux/sagas/expenditures/cancelDraftExpenditure.ts
+++ src/redux/sagas/expenditures/cancelDraftExpenditure.ts
@@ -1,23 +1,33 @@
 import { ClientType, ColonyRole, getPermissionProofs } from '@colony/colony-js';
-import { call, fork, put, takeEvery } from 'redux-saga/effects';
+import { fork, put, takeEvery } from 'redux-saga/effects';
 
 import { type ColonyManager } from '~context/index.ts';
 import { type Action, ActionTypes, type AllActions } from '~redux/index.ts';
 
 import {
+  type ChannelDefinition,
   createTransaction,
-  getTxChannel,
+  createTransactionChannels,
   waitForTxResult,
 } from '../transactions/index.ts';
 import {
+  createActionMetadataInDB,
   getColonyManager,
   initiateTransaction,
   putError,
+  takeFrom,
+  uploadAnnotation,
 } from '../utils/index.ts';
 
 function* cancelDraftExpenditure({
   meta,
-  payload: { colonyAddress, expenditure, stakedExpenditureAddress },
+  payload: {
+    colonyAddress,
+    expenditure,
+    stakedExpenditureAddress,
+    annotationMessage,
+    customActionTitle,
+  },
 }: Action<ActionTypes.EXPENDITURE_DRAFT_CANCEL>) {
   const colonyManager: ColonyManager = yield getColonyManager();
   const colonyClient = yield colonyManager.getClient(
@@ -25,7 +35,15 @@ function* cancelDraftExpenditure({
     colonyAddress,
   );
 
-  const txChannel = yield call(getTxChannel, meta.id);
+  const batchKey = 'cancelDraftExpenditure';
+  const {
+    cancelExpenditure,
+    cancelAndReclaimStake,
+    annotateCancelExpenditure,
+  }: Record<string, ChannelDefinition> = yield createTransactionChannels(
+    meta.id,
+    ['cancelExpenditure', 'cancelAndReclaimStake', 'annotateCancelExpenditure'],
+  );
 
   try {
     if (expenditure.isStaked && stakedExpenditureAddress) {
@@ -37,31 +55,129 @@ function* cancelDraftExpenditure({
         stakedExpenditureAddress,
       );
 
-      yield fork(createTransaction, meta.id, {
+      yield fork(createTransaction, cancelAndReclaimStake.id, {
         context: ClientType.StakedExpenditureClient,
         methodName: 'cancelAndReclaimStake',
         identifier: colonyAddress,
+        group: {
+          key: batchKey,
+          id: meta.id,
+          index: 0,
+        },
         params: [permissionDomainId, childSkillIndex, expenditure.nativeId],
       });
-      yield initiateTransaction({ id: meta.id });
+
+      if (annotationMessage) {
+        yield fork(createTransaction, annotateCancelExpenditure.id, {
+          context: ClientType.ColonyClient,
+          methodName: 'annotateTransaction',
+          identifier: colonyAddress,
+          group: {
+            key: batchKey,
+            id: meta.id,
+            index: 1,
+          },
+          ready: false,
+        });
+      }
+
+      yield takeFrom(
+        cancelAndReclaimStake.channel,
+        ActionTypes.TRANSACTION_CREATED,
+      );
+      if (annotationMessage) {
+        yield takeFrom(
+          annotateCancelExpenditure.channel,
+          ActionTypes.TRANSACTION_CREATED,
+        );
+      }
+
+      const {
+        payload: { hash: txHash },
+      } = yield takeFrom(
+        cancelAndReclaimStake.channel,
+        ActionTypes.TRANSACTION_HASH_RECEIVED,
+      );
+      yield initiateTransaction({ id: cancelAndReclaimStake.id });
+
+      yield waitForTxResult(cancelAndReclaimStake.channel);
+
+      if (annotationMessage) {
+        yield uploadAnnotation({
+          txChannel: annotateCancelExpenditure,
+          message: annotationMessage,
+          txHash,
+        });
+      }
+
+      if (customActionTitle) {
+        yield createActionMetadataInDB(txHash, customActionTitle);
+      }
     } else {
-      yield fork(createTransaction, meta.id, {
+      yield fork(createTransaction, cancelExpenditure.id, {
         context: ClientType.ColonyClient,
         methodName: 'cancelExpenditure',
         identifier: colonyAddress,
+        group: {
+          key: batchKey,
+          id: meta.id,
+          index: 0,
+        },
         params: [expenditure.nativeId],
       });
-      yield initiateTransaction({ id: meta.id });
-    }
-    const { type } = yield waitForTxResult(txChannel);
+      if (annotationMessage) {
+        yield fork(createTransaction, annotateCancelExpenditure.id, {
+          context: ClientType.ColonyClient,
+          methodName: 'annotateTransaction',
+          identifier: colonyAddress,
+          group: {
+            key: batchKey,
+            id: meta.id,
+            index: 1,
+          },
+          ready: false,
+        });
+      }
 
-    if (type === ActionTypes.TRANSACTION_SUCCEEDED) {
-      yield put<AllActions>({
-        type: ActionTypes.EXPENDITURE_DRAFT_CANCEL_SUCCESS,
-        payload: {},
-        meta,
-      });
+      yield takeFrom(
+        cancelExpenditure.channel,
+        ActionTypes.TRANSACTION_CREATED,
+      );
+      if (annotationMessage) {
+        yield takeFrom(
+          annotateCancelExpenditure.channel,
+          ActionTypes.TRANSACTION_CREATED,
+        );
+      }
+
+      const {
+        payload: { hash: txHash },
+      } = yield takeFrom(
+        cancelExpenditure.channel,
+        ActionTypes.TRANSACTION_HASH_RECEIVED,
+      );
+      yield initiateTransaction({ id: cancelExpenditure.id });
+
+      yield waitForTxResult(cancelExpenditure.channel);
+
+      if (annotationMessage) {
+        yield uploadAnnotation({
+          txChannel: annotateCancelExpenditure,
+          message: annotationMessage,
+          txHash,
+        });
+      }
+
+      if (customActionTitle) {
+        yield createActionMetadataInDB(txHash, customActionTitle);
+      }
     }
+
+    yield put<AllActions>({
+      type: ActionTypes.EXPENDITURE_DRAFT_CANCEL_SUCCESS,
+      payload: {},
+      meta,
+    });
   } catch (error) {
     return yield putError(
       ActionTypes.EXPENDITURE_DRAFT_CANCEL_ERROR,
@@ -69,8 +185,6 @@ function* cancelDraftExpenditure({
       meta,
     );
   }
-
-  txChannel.close();
 
   return null;
 }
