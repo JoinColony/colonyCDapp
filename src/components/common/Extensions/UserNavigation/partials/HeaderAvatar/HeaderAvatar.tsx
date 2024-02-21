4d23458ef698e982c349f37a1cdc48cd9a22e84f
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-02-21 10:48:10 +0100

    fix: user avatar on not colony pages

--- src/components/common/Extensions/UserNavigation/partials/HeaderAvatar/HeaderAvatar.tsx
+++ src/components/common/Extensions/UserNavigation/partials/HeaderAvatar/HeaderAvatar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useAppContext } from '~context/AppContext.tsx';
+import { useMobile } from '~hooks';
 import UserAvatar from '~v5/shared/UserAvatar/index.ts';
 
 export const displayName =
@@ -8,6 +9,7 @@ export const displayName =
 
 const HeaderAvatar = () => {
   const { wallet, user } = useAppContext();
+  const isMobile = useMobile();
 
   // Prevent content flashes
   if (!wallet) {
@@ -16,7 +18,11 @@ const HeaderAvatar = () => {
 
   return (
     <div className="flex items-center justify-center min-w-[2.625rem] min-h-[2.5rem] px-[0.875rem] py-[0.625rem] bg-base-white border rounded-full border-gray-200">
-      <UserAvatar user={user || wallet?.address} showUsername size="xss" />
+      <UserAvatar
+        user={user || wallet?.address}
+        showUsername={!isMobile}
+        size={isMobile ? 'xss' : 'xxs'}
+      />
     </div>
   );
 };
