bc52d8092915fe56d4dd466a1c0358deb68cae0c
Raul Glogovetan <raul@glogovetan.com>
2024-02-21 22:44:38 +0200

    Add: experimental metamask network add / switch

--- src/components/frame/v5/pages/UserProfileExperimentalPage/UserProfileExperimentalPage.tsx
+++ src/components/frame/v5/pages/UserProfileExperimentalPage/UserProfileExperimentalPage.tsx
@@ -6,7 +6,9 @@ import { useAppContext } from '~context/AppContext.tsx';
 import { useSetPageHeadingTitle } from '~context/PageHeadingContext/index.ts';
 import LoadingTemplate from '~frame/LoadingTemplate/index.ts';
 import { LANDING_PAGE_ROUTE } from '~routes/index.ts';
+import { isFullWallet } from '~types/wallet.ts';
 import { formatText } from '~utils/intl.ts';
+import Button from '~v5/shared/Button/index.ts';
 
 const displayName = 'v5.pages.UserProfileExperimentalPage';
 
@@ -18,7 +20,7 @@ const MSG = defineMessages({
 });
 
 const UserAdvancedPage = () => {
-  const { user, userLoading, walletConnecting } = useAppContext();
+  const { user, userLoading, wallet, walletConnecting } = useAppContext();
   const { formatMessage } = useIntl();
 
   useSetPageHeadingTitle(formatText({ id: 'userProfile.title' }));
@@ -31,6 +33,52 @@ const UserAdvancedPage = () => {
     return <Navigate to={LANDING_PAGE_ROUTE} />;
   }
 
+  const handleAddNetwork = () => {
+    if (isFullWallet(wallet)) {
+      wallet.provider
+        .request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: '0x64',
+              chainName: 'Gnosis (via Colony RPC)',
+              rpcUrls: ['https://app.colony.io/rpc/'],
+              nativeCurrency: {
+                decimals: 18,
+                symbol: 'XDAI',
+                name: 'XDai Token',
+              },
+              blockExplorerUrls: ['https://gnosisscan.io'],
+            },
+          ],
+        })
+        .catch((error) => {
+          console.error('User rehjected adding the network', error);
+        });
+    } else {
+      console.error('Wallet not full!');
+    }
+  };
+
+  const handleSwitchNetwork = () => {
+    if (isFullWallet(wallet)) {
+      wallet.provider
+        .request({
+          method: 'wallet_switchEthereumChain',
+          params: [
+            {
+              chainId: '0x64',
+            },
+          ],
+        })
+        .catch((error) => {
+          console.error('User rehjected switching the network', error);
+        });
+    } else {
+      console.error('Wallet not full!');
+    }
+  };
+
   /*
    * Plance all your user related experimental components here
    */
@@ -39,6 +87,19 @@ const UserAdvancedPage = () => {
       <h4 className="heading-4">
         {formatMessage({ id: 'experimentalSettings.title' })}
       </h4>
+      <div className="flex items-center">
+        <Button
+          className="mr-4"
+          mode="primarySolid"
+          text="Add Gnosis Network"
+          onClick={handleAddNetwork}
+        />
+        <Button
+          mode="primarySolid"
+          text="Switch to Gnosis Network"
+          onClick={handleSwitchNetwork}
+        />
+      </div>
     </div>
   );
 };
