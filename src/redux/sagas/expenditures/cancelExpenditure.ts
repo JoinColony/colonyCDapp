c3158cb403940cc987b150d9afb3096108066859
Jakub Zajac <jakub@colony.io>
2024-02-29 12:27:49 +0000

    Feat: `cancelExpenditure` saga allowing cancelling draft and locked expenditures

---
+++ src/redux/sagas/expenditures/cancelExpenditure.ts
@@ -0,0 +1,166 @@
+import {
+  type AnyColonyClient,
+  ClientType,
+  ColonyRole,
+  getPermissionProofs,
+} from '@colony/colony-js';
+import { BigNumber } from 'ethers';
+import { call, fork, put, takeEvery } from 'redux-saga/effects';
+
+import { type ColonyManager } from '~context/index.ts';
+import { ExpenditureStatus } from '~gql';
+import { type Action, ActionTypes, type AllActions } from '~redux/index.ts';
+import { type Expenditure } from '~types/graphql.ts';
+import { toB32 } from '~utils/solidity.ts';
+
+import {
+  createTransaction,
+  getTxChannel,
+  waitForTxResult,
+} from '../transactions/index.ts';
+import {
+  getColonyManager,
+  initiateTransaction,
+  putError,
+} from '../utils/index.ts';
+
+export type CancelExpenditurePayload =
+  Action<ActionTypes.EXPENDITURE_CANCEL>['payload'];
+
+function* cancelExpenditure({
+  meta,
+  payload: {
+    colonyAddress,
+    expenditure,
+    stakedExpenditureAddress,
+    userAddress,
+  },
+}: Action<ActionTypes.EXPENDITURE_CANCEL>) {
+  const colonyManager: ColonyManager = yield getColonyManager();
+  const colonyClient = yield colonyManager.getClient(
+    ClientType.ColonyClient,
+    colonyAddress,
+  );
+
+  const txChannel = yield call(getTxChannel, meta.id);
+
+  try {
+    if (
+      expenditure.status === ExpenditureStatus.Draft &&
+      expenditure.ownerAddress === userAddress
+    ) {
+      yield cancelDraftExpenditure({
+        expenditure,
+        colonyAddress,
+        colonyClient,
+        stakedExpenditureAddress,
+        meta,
+      });
+    } else {
+      yield cancelLockedExpenditure({
+        expenditure,
+        colonyAddress,
+        colonyClient,
+        userAddress,
+        meta,
+      });
+    }
+
+    const { type } = yield waitForTxResult(txChannel);
+
+    if (type === ActionTypes.TRANSACTION_SUCCEEDED) {
+      yield put<AllActions>({
+        type: ActionTypes.EXPENDITURE_CANCEL_SUCCESS,
+        payload: {},
+        meta,
+      });
+    }
+  } catch (error) {
+    return yield putError(ActionTypes.EXPENDITURE_CANCEL_ERROR, error, meta);
+  }
+
+  txChannel.close();
+  return null;
+}
+
+type CancelExpenditureHelperParam = {
+  colonyAddress: string;
+  expenditure: Expenditure;
+  colonyClient: AnyColonyClient;
+  meta: Action<ActionTypes.EXPENDITURE_CANCEL>['meta'];
+  stakedExpenditureAddress?: string;
+  userAddress?: string;
+};
+
+function* cancelDraftExpenditure({
+  expenditure,
+  colonyClient,
+  colonyAddress,
+  stakedExpenditureAddress,
+  meta,
+}: CancelExpenditureHelperParam) {
+  if (expenditure.isStaked && stakedExpenditureAddress) {
+    const [permissionDomainId, childSkillIndex] = yield getPermissionProofs(
+      colonyClient.networkClient,
+      colonyClient,
+      expenditure.nativeDomainId,
+      ColonyRole.Arbitration,
+      stakedExpenditureAddress,
+    );
+
+    yield fork(createTransaction, meta.id, {
+      context: ClientType.StakedExpenditureClient,
+      methodName: 'cancelAndReclaimStake',
+      identifier: colonyAddress,
+      params: [permissionDomainId, childSkillIndex, expenditure.nativeId],
+    });
+  } else {
+    yield fork(createTransaction, meta.id, {
+      context: ClientType.ColonyClient,
+      methodName: 'cancelExpenditure',
+      identifier: colonyAddress,
+      params: [expenditure.nativeId],
+    });
+  }
+
+  yield initiateTransaction({ id: meta.id });
+}
+
+function* cancelLockedExpenditure({
+  meta,
+  colonyAddress,
+  colonyClient,
+  expenditure,
+  userAddress,
+}: CancelExpenditureHelperParam) {
+  const [permissionDomainId, childSkillIndex] = yield getPermissionProofs(
+    colonyClient.networkClient,
+    colonyClient,
+    expenditure.nativeDomainId,
+    ColonyRole.Arbitration,
+    userAddress,
+  );
+
+  const value = toB32(`${expenditure.ownerAddress}01`);
+
+  yield fork(createTransaction, meta.id, {
+    context: ClientType.ColonyClient,
+    methodName: 'setExpenditureState',
+    identifier: colonyAddress,
+    params: [
+      permissionDomainId,
+      childSkillIndex,
+      expenditure.nativeId,
+      BigNumber.from(26),
+      [true],
+      [BigNumber.from(0)],
+      value,
+    ],
+  });
+
+  yield initiateTransaction({ id: meta.id });
+}
+
+export default function* cancelExpenditureSaga() {
+  yield takeEvery(ActionTypes.EXPENDITURE_CANCEL, cancelExpenditure);
+}
