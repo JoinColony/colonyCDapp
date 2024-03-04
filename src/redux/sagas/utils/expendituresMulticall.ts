91d5271aeaf9c29d3eba8afb004642ad700beea1
jakubcolony <112586815+jakubcolony@users.noreply.github.com>
2024-03-04 11:22:46 +0000

    Merge pull request #1992 from JoinColony/feat/1975-locked-expenditure-changes-using-permissions
    Changes to expenditures after locking using permissions

---
+++ src/redux/sagas/utils/expendituresMulticall.ts
@@ -0,0 +1,104 @@
+import {
+  getPermissionProofs,
+  type AnyColonyClient,
+  ColonyRole,
+} from '@colony/colony-js';
+import { BigNumber, type BigNumberish, utils } from 'ethers';
+
+import { type ExpenditurePayoutFieldValue } from '~types/expenditures.ts';
+import { type Expenditure } from '~types/graphql.ts';
+
+import { getPayoutAmount } from './expenditures.ts';
+
+const toB32 = (input: BigNumberish) =>
+  utils.hexZeroPad(utils.hexlify(input), 32);
+
+const EXPENDITURESLOTS_SLOT = BigNumber.from(26);
+
+const EXPENDITURESLOT_RECIPIENT = toB32(BigNumber.from(0));
+const EXPENDITURESLOT_CLAIMDELAY = toB32(BigNumber.from(1));
+
+/**
+ * Helper function returning an array of encoded multicall data containing transactions
+ * needed to update an expenditure payout
+ */
+export const getMulticallDataForPayouts = async (
+  expenditure: Expenditure,
+  payouts: ExpenditurePayoutFieldValue[],
+  colonyClient: AnyColonyClient,
+  networkInverseFee: string,
+) => {
+  const [permissionDomainId, childSkillIndex] = await getPermissionProofs(
+    colonyClient.networkClient,
+    colonyClient,
+    expenditure.nativeDomainId,
+    ColonyRole.Administration,
+  );
+
+  const encodedMulticallData: string[] = [];
+
+  payouts.forEach((payout) => {
+    const existingSlot = expenditure.slots.find(
+      (slot) => slot.id === payout.slotId,
+    );
+
+    // Set recipient
+    if (
+      !existingSlot ||
+      existingSlot.recipientAddress !== payout.recipientAddress
+    ) {
+      encodedMulticallData.push(
+        colonyClient.interface.encodeFunctionData('setExpenditureState', [
+          permissionDomainId,
+          childSkillIndex,
+          expenditure.nativeId,
+          EXPENDITURESLOTS_SLOT,
+          [false, true],
+          [toB32(payout.slotId ?? ''), EXPENDITURESLOT_RECIPIENT],
+          toB32(payout.recipientAddress),
+        ]),
+      );
+    }
+
+    // Set claim delay
+    if (!existingSlot || existingSlot.claimDelay !== payout.claimDelay) {
+      encodedMulticallData.push(
+        colonyClient.interface.encodeFunctionData('setExpenditureState', [
+          permissionDomainId,
+          childSkillIndex,
+          expenditure.nativeId,
+          EXPENDITURESLOTS_SLOT,
+          [false, true],
+          [toB32(payout.slotId ?? ''), EXPENDITURESLOT_CLAIMDELAY],
+          toB32(BigNumber.from(payout.claimDelay)),
+        ]),
+      );
+    }
+
+    // Set token address and amount
+    const amountWithFee = getPayoutAmount(payout, networkInverseFee);
+    const existingPayout = existingSlot?.payouts?.find(
+      (slotPayout) =>
+        BigNumber.from(slotPayout.amount)
+          .add(slotPayout.networkFee ?? '0')
+          .eq(amountWithFee) && slotPayout.tokenAddress === payout.tokenAddress,
+    );
+    if (!existingPayout) {
+      encodedMulticallData.push(
+        colonyClient.interface.encodeFunctionData(
+          'setExpenditurePayout(uint256,uint256,uint256,uint256,address,uint256)',
+          [
+            permissionDomainId,
+            childSkillIndex,
+            expenditure.nativeId,
+            payout.slotId ?? '',
+            payout.tokenAddress,
+            amountWithFee,
+          ],
+        ),
+      );
+    }
+  });
+
+  return encodedMulticallData;
+};
