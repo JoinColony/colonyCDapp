8275e29ebec5123b5ac319891f905df07ba0e805
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-03-06 15:49:07 +0000

    Fix: Created getResolvedPayouts helper function

--- src/redux/sagas/motions/expenditures/editLockedExpenditureMotion.ts
+++ src/redux/sagas/motions/expenditures/editLockedExpenditureMotion.ts
@@ -5,7 +5,6 @@ import {
   getPermissionProofs,
   Id,
 } from '@colony/colony-js';
-import { BigNumber } from 'ethers';
 import { call, fork, put, takeEvery } from 'redux-saga/effects';
 
 import { ADDRESS_ZERO, APP_URL } from '~constants';
@@ -21,10 +20,9 @@ import {
   initiateTransaction,
   getMulticallDataForUpdatedPayouts,
   uploadAnnotation,
-  getPayoutsWithSlotIds,
+  getResolvedPayouts,
 } from '~redux/sagas/utils/index.ts';
 import { type Action } from '~redux/types/index.ts';
-import { type ExpenditurePayoutFieldValue } from '~types/expenditures.ts';
 import { takeFrom } from '~utils/saga/effects.ts';
 
 export type EditExpenditureMotionPayload =
@@ -50,53 +48,7 @@ function* editLockedExpenditureMotion({
 
   const batchId = 'motion-edit-locked-expenditure';
 
-  /**
-   * @NOTE: Resolving payouts means making sure that for every slot, there's only one payout with non-zero amount.
-   * This is to meet the UI requirement that there should be one payout per row.
-   */
-  const resolvedPayouts: ExpenditurePayoutFieldValue[] = [];
-
-  const payoutsWithSlotIds = getPayoutsWithSlotIds(payouts);
-
-  payoutsWithSlotIds.forEach((payout) => {
-    // Add payout as specified in the form
-    resolvedPayouts.push(payout);
-
-    const existingSlot = expenditure.slots.find(
-      (slot) => slot.id === payout.slotId,
-    );
-
-    // Set the amounts for any existing payouts in different tokens to 0
-    resolvedPayouts.push(
-      ...(existingSlot?.payouts
-        ?.filter(
-          (slotPayout) =>
-            slotPayout.tokenAddress !== payout.tokenAddress &&
-            BigNumber.from(slotPayout.amount).gt(0),
-        )
-        .map((slotPayout) => ({
-          slotId: payout.slotId,
-          recipientAddress: payout.recipientAddress,
-          tokenAddress: slotPayout.tokenAddress,
-          amount: '0',
-          claimDelay: payout.claimDelay,
-        })) ?? []),
-    );
-  });
-
-  // If there are now less payouts than expenditure slots, we need to remove them by setting their amounts to 0
-  const remainingSlots = expenditure.slots.slice(payouts.length);
-  remainingSlots.forEach((slot) => {
-    slot.payouts?.forEach((payout) => {
-      resolvedPayouts.push({
-        slotId: slot.id,
-        recipientAddress: slot.recipientAddress ?? '',
-        tokenAddress: payout.tokenAddress,
-        amount: '0',
-        claimDelay: slot.claimDelay ?? 0,
-      });
-    });
-  });
+  const resolvedPayouts = getResolvedPayouts(payouts, expenditure);
 
   const { createMotion, annotateEditLockedExpenditure } = yield call(
     createTransactionChannels,
