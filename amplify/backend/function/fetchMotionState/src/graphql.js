b06364a36c6fb7d1b89a2a68681fc2604c711b30
Armando Graterol <armandogrtrl@gmail.com>
2024-03-18 18:07:18 -0300

    Added finalize tooltips with endedAt timestamp

--- amplify/backend/function/fetchMotionState/src/graphql.js
+++ amplify/backend/function/fetchMotionState/src/graphql.js
@@ -85,6 +85,7 @@ module.exports = {
           naySideFullyStakedAt
           allVotesSubmittedAt
           allVotesRevealedAt
+          endedAt
           finalizedAt
         }
         messages {
