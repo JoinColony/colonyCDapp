04bddde903b232648e0022feb5d1c90cc9634a4f
Bassgeta <kerry.mahne@gmail.com>
2024-03-18 16:25:41 +0100

    refactor: create temporaryAvatar2 component and use it in the colony switcher

--- src/components/v5/shared/Avatar/Avatar.tsx
+++ src/components/v5/shared/Avatar/Avatar.tsx
@@ -13,25 +13,34 @@ import { type AvatarProps } from './types.ts';
 
 const displayName = 'v5.Avatar';
 
-const sizeClasses = {
-  main: tw`relative inline-block h-[2.8125rem] w-[2.8125rem]`,
-  xxs: tw`relative inline-block h-[1rem] w-[1rem]`,
-  xss: tw`relative inline-block h-[1.125rem] w-[1.125rem]`,
-  xs: tw`relative inline-block h-[1.25rem] w-[1.25rem]`,
-  xxsm: tw`relative inline-block h-[1.5rem] w-[1.5rem]`,
-  xsm: tw`relative inline-block h-[1.625rem] w-[1.625rem]`,
-  smx: tw`relative flex h-[1.75rem] w-[1.75rem] items-center`,
-  sm: tw`relative flex h-[1.875rem] w-[1.875rem] items-center`,
-  s: tw`relative flex h-[2.125rem] w-[2.125rem] items-center`,
-  xms: tw`relative inline-block h-[2.375rem] w-[2.375rem]`,
-  ms: tw`relative inline-block h-[2.75rem] w-[2.75rem]`,
-  xm: tw`relative inline-block h-16 w-16`,
-  m: tw`relative inline-block h-[3.75rem] w-[3.75rem]`,
-  md: tw`relative inline-block h-[5.625rem] w-[5.625rem]`,
-  l: tw`relative inline-block h-[7.5rem] w-[7.5rem]`,
-  xl: tw`relative inline-block h-[10rem] w-[10rem]`,
-};
+interface AvatarProps2 {
+  className?: string;
+  src?: string;
+  address: string;
+  size: number;
+}
 
+export const Avatar2: FC<AvatarProps2> = ({
+  address,
+  className,
+  size,
+  src,
+}) => {
+  const source = src ?? getIcon(address);
+
+  return (
+    <img
+      className={clsx('rounded-full bg-cover', className)}
+      src={source}
+      alt="tmp"
+      style={{
+        width: size,
+        height: size,
+        imageRendering: src ? undefined : 'pixelated',
+      }}
+    />
+  );
+};
 const Avatar: FC<PropsWithChildren<AvatarProps>> = ({
   seed,
   avatar,
--- src/components/v5/shared/ColonyAvatar/ColonyAvatar.tsx
+++ src/components/v5/shared/ColonyAvatar/ColonyAvatar.tsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx';
 import React, { type FC } from 'react';
 
-import { formatText } from '~utils/intl.ts';
-
-import Avatar from '../Avatar/index.ts';
+import { Avatar2 } from '../Avatar/Avatar.tsx';
 
 import { type ColonyAvatarProps } from './types.ts';
 
@@ -13,58 +11,26 @@ const ColonyAvatar: FC<ColonyAvatarProps> = ({
   colonyAddress,
   chainIcon: Icon,
   colonyImageProps,
-  size = 'xms',
+  size,
   className,
 }) => {
   return (
     <div
       className={clsx(
         className,
-        'relative flex h-[1em] w-[1em] flex-shrink-0 items-center justify-center',
-        {
-          'text-6xl h-16 w-16': size === 'xm',
-          'text-6xl h-[3.75em] w-[3.75em]': size === 'm',
-          'text-4xl': size === 'xms',
-          'text-3xl': size === 'smx',
-          'text-2xl': size === 'xxsm',
-          // @TODO: This is getting out of hand, maybe let's just use pixel values (as with the icons)
-          'text-lg': size === 'xxs',
-          'text-xl': size === 'xss',
-        },
+        'flex justify-center items-center flex-shrink-0 relative',
       )}
     >
-      <div
-        className={clsx(
-          'flex h-full w-full items-center justify-center overflow-hidden rounded-full',
-        )}
-      >
-        {colonyImageProps?.src ? (
-          <img
-            {...colonyImageProps}
-            alt={
-              colonyImageProps?.alt ||
-              formatText({ id: 'colonyAvatar.colonyImage.alt' })
-            }
-            className="h-full w-full object-cover object-center"
-          />
-        ) : (
-          <Avatar size={size} seed={colonyAddress.toLowerCase()} />
-        )}
-      </div>
+      <Avatar2
+        size={size}
+        src={colonyImageProps?.src}
+        address={colonyAddress.toLowerCase()}
+      />
       {Icon && (
         <figure
           className={`
-          absolute
-          right-0
-          top-0
-          flex
-          h-[0.5em]
-          w-[0.5em]
-          -translate-y-[.375rem]
-          translate-x-[.375rem]
-          items-center
-          justify-center
-          overflow-hidden
+          h-[1.125rem]
+          w-[1.125rem]
           rounded-full
           border
           border-gray-200
