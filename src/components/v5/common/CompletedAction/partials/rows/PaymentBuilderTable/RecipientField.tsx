9d3e259c817e72c4cb281f1bcf8ec2c7744df174
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-03-19 08:51:20 +0100

    fix: table ui styles, added user popover, types

--- src/components/v5/common/CompletedAction/partials/rows/PaymentBuilderTable/RecipientField.tsx
+++ src/components/v5/common/CompletedAction/partials/rows/PaymentBuilderTable/RecipientField.tsx
@@ -1,9 +1,10 @@
-import { SealCheck } from '@phosphor-icons/react';
+import { WarningCircle } from '@phosphor-icons/react';
 import clsx from 'clsx';
 import React, { type FC } from 'react';
 
 import { useMemberContext } from '~context/MemberContext/MemberContext.ts';
-import UserAvatar from '~v5/shared/UserAvatar/UserAvatar.tsx';
+import Avatar from '~shared/Avatar/Avatar.tsx';
+import UserPopover from '~v5/shared/UserPopover/UserPopover.tsx';
 
 import { type RecipientFieldProps } from './types.ts';
 
@@ -17,19 +18,47 @@ const RecipientField: FC<RecipientFieldProps> = ({ address }) => {
     <>
       {!loading && recipientMember?.user && (
         <div className="flex items-center">
-          <UserAvatar
-            user={recipientMember?.user}
-            size="xs"
-            showUsername
-            className={clsx({
+          <UserPopover
+            user={recipientMember.user}
+            walletAddress={recipientMember.contributorAddress}
+            withVerifiedBadge={recipientMember.isVerified}
+            className={clsx('flex items-center sm:hover:text-blue-400', {
+              'pointer-events-none': loading,
               'text-warning-400': !recipientMember?.isVerified,
+              'text-gray-900': recipientMember?.isVerified,
             })}
-          />
-          {recipientMember?.isVerified && (
-            <span className="ml-2 flex text-blue-400">
-              <SealCheck size={20} />
-            </span>
-          )}
+          >
+            <Avatar
+              seed={recipientMember.contributorAddress?.toLowerCase()}
+              title={
+                recipientMember.user?.profile?.displayName ||
+                recipientMember.contributorAddress
+              }
+              avatar={
+                recipientMember.user?.profile?.thumbnail ||
+                recipientMember.user?.profile?.avatar
+              }
+              size="xxs"
+              className={clsx({
+                'skeleton before:rounded-full': loading,
+              })}
+            />
+            <p
+              className={clsx('ml-2 inline-block text-md', {
+                skeleton: loading,
+              })}
+            >
+              {loading
+                ? 'Loading...'
+                : recipientMember.user?.profile?.displayName ||
+                  recipientMember.contributorAddress}
+            </p>
+            {!recipientMember?.isVerified && (
+              <span className="ml-1">
+                <WarningCircle size={14} className="text-warning-400" />
+              </span>
+            )}
+          </UserPopover>
         </div>
       )}
     </>
