76cf64359f220552865cdf977811449fc986dd68
Jakub Zajac <jakub@colony.io>
2024-03-21 22:29:16 +0000

    Schema update: make actions to expenditures relation many-to-many

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -320,6 +320,8 @@ export type ColonyAction = {
   /**
    * Whether to show the motion in the actions list
    * True for (forced) actions. True for motions if staked above 10%
+   * @TODO: Refactor this into more granular fields for better searchability
+   * Currently it is impossible to tell the reason for the action being hidden from the actions list
    */
   showInActionsList: Scalars['Boolean'];
   /** The target Domain of the action, if applicable */
@@ -1876,7 +1878,7 @@ export type DomainMetadataChangelogInput = {
 
 export type Expenditure = {
   __typename?: 'Expenditure';
-  action?: Maybe<ColonyAction>;
+  actions?: Maybe<ModelColonyActionConnection>;
   /** Array containing balances of tokens in the expenditure */
   balances?: Maybe<Array<ExpenditureBalance>>;
   /** The Colony to which the expenditure belongs */
@@ -1922,6 +1924,14 @@ export type Expenditure = {
   userStake?: Maybe<UserStake>;
   /** ID of the user stake associated with the expenditure, if any */
   userStakeId?: Maybe<Scalars['ID']>;
+};
+
+
+export type ExpenditureActionsArgs = {
+  filter?: InputMaybe<ModelColonyActionFilterInput>;
+  limit?: InputMaybe<Scalars['Int']>;
+  nextToken?: InputMaybe<Scalars['String']>;
+  sortDirection?: InputMaybe<ModelSortDirection>;
 };
 
 
@@ -5429,6 +5439,7 @@ export type ProfileMetadataInput = {
 /** Root query type */
 export type Query = {
   __typename?: 'Query';
+  getActionByExpenditureId?: Maybe<ModelColonyActionConnection>;
   getActionsByColony?: Maybe<ModelColonyActionConnection>;
   getAnnotation?: Maybe<Annotation>;
   getColoniesByNativeTokenId?: Maybe<ModelColonyConnection>;
@@ -5550,6 +5561,16 @@ export type Query = {
   listUserTokens?: Maybe<ModelUserTokensConnection>;
   listUsers?: Maybe<ModelUserConnection>;
   searchColonyActions?: Maybe<SearchableColonyActionConnection>;
+};
+
+
+/** Root query type */
+export type QueryGetActionByExpenditureIdArgs = {
+  expenditureId: Scalars['ID'];
+  filter?: InputMaybe<ModelColonyActionFilterInput>;
+  limit?: InputMaybe<Scalars['Int']>;
+  nextToken?: InputMaybe<Scalars['String']>;
+  sortDirection?: InputMaybe<ModelSortDirection>;
 };
 
 
