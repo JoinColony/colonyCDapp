12d6176b45a55774973784cc4ff3f3cf5d091545
Bassgeta <kerry.mahne@gmail.com>
2024-03-25 13:48:22 +0100

    refactor: remove remaining uses of old Avatar

--- src/components/v5/shared/MembersSelect/partials/CustomOption/CustomOption.tsx
+++ src/components/v5/shared/MembersSelect/partials/CustomOption/CustomOption.tsx
@@ -1,17 +1,12 @@
-import { UserCircle } from '@phosphor-icons/react';
 import React from 'react';
 
-import Avatar from '~v5/shared/Avatar/index.ts';
+import { UserAvatar2 } from '~v5/shared/UserAvatar/UserAvatar.tsx';
 
 import { type MembersSelectOption } from '../../types.ts';
 
 const CustomOption: React.FC<MembersSelectOption> = ({ label, avatar }) => (
-  <span className="flex w-full items-center gap-2 text-md">
-    <Avatar
-      avatar={avatar || undefined}
-      seed={label.toLowerCase()}
-      placeholderIcon={UserCircle}
-    />
+  <span className="flex items-center w-full text-md gap-2">
+    <UserAvatar2 userAvatarSrc={avatar} userAddress={label} size={20} />
     {label}
   </span>
 );
