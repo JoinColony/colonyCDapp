1a1dbb53b7602a35fdc1f7fc05690c3559a73e7c
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-03-26 09:53:00 +0100

    feat: added release modal
    feat: added select option disable
    
    feat: connected finalize function
    
    fix: moved logic to paymentbuilder
    
    fix: added icon, logic change
    
    fix: added form to handle errors

---
+++ src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/ReleasePaymentModal/types.ts
@@ -0,0 +1,6 @@
+import { type Expenditure } from '~types/graphql.ts';
+import { type ModalProps } from '~v5/shared/Modal/types.ts';
+
+export interface ReleasePaymentModalProps extends ModalProps {
+  expenditure: Expenditure;
+}
