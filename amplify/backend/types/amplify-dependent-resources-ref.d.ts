13eba9134857718fd8529e1f86793e0be42f38ea
bogdan-1337 <bogdan@colony.io>
2024-03-26 15:18:22 +0000

    feat: update amplify stack with qapolytest env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,126 +1,133 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    colonycdapp: {
-      GraphQLAPIEndpointOutput: 'string';
-      GraphQLAPIIdOutput: 'string';
-      GraphQLAPIKeyOutput: 'string';
-    };
-  };
-  function: {
-    colonycdappSSMAccess: {
-      Arn: 'string';
-    };
-    createColonyEtherealMetadata: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    createPrivateBetaInvite: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    createUniqueUser: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchColonyBalances: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchColonyNativeFundsClaim: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchExpenditureBalances: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchMotionState: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchMotionTimeoutPeriods: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchTokenFromChain: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    fetchVoterRewards: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    getSafeTransactionStatus: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    getUserReputation: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    getUserTokenBalance: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    qaSSMtest: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    updateContributorsWithReputation: {
-      Arn: 'string';
-      LambdaExecutionRole: 'string';
-      LambdaExecutionRoleArn: 'string';
-      Name: 'string';
-      Region: 'string';
-    };
-    validateUserInvite: {
+  "api": {
+    "colonycdapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "function": {
+    "colonycdappSSMAccess": {
+      "Arn": "string"
+    },
+    "createColonyEtherealMetadata": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "createPrivateBetaInvite": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "createUniqueUser": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchColonyBalances": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchColonyNativeFundsClaim": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchExpenditureBalances": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchMotionState": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchMotionTimeoutPeriods": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchTokenFromChain": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "fetchVoterRewards": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "getSafeTransactionStatus": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "getUserReputation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "getUserTokenBalance": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "qaSSMtest": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    removeMemberFromColonyWhitelist: {
       Arn: 'string';
       LambdaExecutionRole: 'string';
       LambdaExecutionRoleArn: 'string';
       Name: 'string';
       Region: 'string';
     };
-  };
-};
+    "updateContributorsWithReputation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "validateUserInvite": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
