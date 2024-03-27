c53a4e56242da852e1752bad6bdb006825a94c93
Bassgeta <kerry.mahne@gmail.com>
2024-03-27 11:32:55 +0100

    refactor: member signature uses enw ContributorTypeBorder, fix wallet address display in payment form

--- src/components/v5/common/ActionSidebar/partials/Motions/steps/ExitRecoveryStep/consts.ts
+++ src/components/v5/common/ActionSidebar/partials/Motions/steps/ExitRecoveryStep/consts.ts
@@ -1,20 +1,25 @@
+import { ContributorType } from '~gql';
+
 export const membersList = [
   {
     userName: 'John Doe',
     address: '0x1234567890123456789012345678901234567890',
     hasSigned: false,
     key: '1',
+    contributorType: ContributorType.Active,
   },
   {
     userName: 'Andrew Doe',
     address: '0x12345678901234567890',
     hasSigned: false,
     key: '2',
+    contributorType: ContributorType.General,
   },
   {
     userName: 'Thomas Doe',
     address: '0x1234567890',
     hasSigned: false,
     key: '3',
+    contributorType: ContributorType.Dedicated,
   },
 ];
