1bd9129ea1908117e80b95ab1b4e64c05e73195f
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-03-28 12:44:48 +0100

    fix: changed paddings to margins

--- src/components/v5/shared/UserPopover/partials/UserInfo.tsx
+++ src/components/v5/shared/UserPopover/partials/UserInfo.tsx
@@ -86,7 +86,7 @@ const UserInfo: FC<UserInfoProps> = ({
       {aboutDescriptionText && (
         <div
           className={clsx({
-            'px-6 pt-6': isTopContributorType,
+            'mt-6 px-6': isTopContributorType,
           })}
         >
           <TitleLabel
@@ -98,7 +98,7 @@ const UserInfo: FC<UserInfoProps> = ({
       )}
       {additionalContent && (
         <div
-          className={clsx('pt-6', {
+          className={clsx('mt-6', {
             'px-6': isTopContributorType,
           })}
         >
@@ -108,7 +108,7 @@ const UserInfo: FC<UserInfoProps> = ({
       {domains?.length ? (
         <div
           className={clsx({
-            'px-6 pb-6': isTopContributorType,
+            'mb-6 px-6': isTopContributorType,
           })}
         >
           <div className="my-6 border-t border-gray-200" />
