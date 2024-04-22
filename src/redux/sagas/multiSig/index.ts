fc616837481cfcedad6f7fb52471de418b0e3a6a
Bassgeta <kerry.mahne@gmail.com>
2024-04-22 16:54:09 +0200

    feat: implement mint tokens creation multi sig support

--- src/redux/sagas/multiSig/index.ts
+++ src/redux/sagas/multiSig/index.ts
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 
+import createRootMultiSig from './createRootMultiSig.ts';
 import setGlobalThresholdSaga from './setGlobalThreshold.ts';
 
 export default function* multiSigSagas() {
-  yield all([call(setGlobalThresholdSaga)]);
+  yield all([call(setGlobalThresholdSaga), call(createRootMultiSig)]);
 }
