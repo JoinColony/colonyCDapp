92f553246ce2f7bff60e5e76bde01af3ce9246d7
Chris Maniewski <code@chmanie.com>
2024-04-23 11:05:42 +0200

    feat: add apollo subscription links

--- src/apollo/apolloClient.ts
+++ src/apollo/apolloClient.ts
@@ -1,19 +1,46 @@
-import { ApolloClient, from, HttpLink } from '@apollo/client';
+import { ApolloClient, ApolloLink, from } from '@apollo/client';
+import { createAuthLink, AUTH_TYPE } from 'aws-appsync-auth-link';
+import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
 
 import cache from './cache/index.ts';
 import removeTypenameLink from './removeTypenameLink.ts';
 
-const httpLink = new HttpLink({
-  uri: `${
+const noAuth = {
+  type: AUTH_TYPE.NONE as const,
+};
+
+const httpLink = createAuthLink({
+  url: `${
     import.meta.env.AUTH_PROXY_ENDPOINT || 'http://localhost:3005'
   }/graphql`,
-  credentials: 'include',
+  region: 'us-east-1',
+  // auth is handled by auth proxy
+  auth: noAuth,
+});
+
+const subscriptionLink = createSubscriptionHandshakeLink({
+  url: `${
+    import.meta.env.AUTH_PROXY_ENDPOINT || 'http://localhost:3005'
+  }/graphql`,
+  region: 'us-east-1',
+  // auth is handled by auth proxy
+  auth: noAuth,
+});
+
+// Middleware link for setting credentials to include (aws links do not do that!)
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    // ensures cookies are sent with every request
+    credentials: 'include',
+  });
+
+  return forward(operation);
 });
 
 const apolloClient = new ApolloClient({
-  link: from([removeTypenameLink, httpLink]),
-  connectToDevTools: true,
   cache,
+  connectToDevTools: true,
+  link: from([removeTypenameLink, authLink, httpLink, subscriptionLink]),
   /*
    * @TODO Most likely we'll need to add resolvers here as well
    */
