3a2d68a46d67bc97ff9e12ac1b4f2e612a4180fc
Bassgeta <kerry.mahne@gmail.com>
2024-04-24 17:15:59 +0200

    feat: implement dummy sidebar for multiSig, navigate after tx success

--- src/components/v5/common/CompletedAction/CompletedAction.tsx
+++ src/components/v5/common/CompletedAction/CompletedAction.tsx
@@ -9,6 +9,7 @@ import { getExtendedActionType } from '~utils/colonyActions.ts';
 
 import PermissionSidebar from '../ActionSidebar/partials/ActionSidebarContent/partials/PermissionSidebar.tsx';
 import Motions from '../ActionSidebar/partials/Motions/index.ts';
+import MultiSigSidebar from '../ActionSidebar/partials/MultiSigSidebar/MultiSigSidebar.tsx';
 
 import AddVerifiedMembers from './partials/AddVerifiedMembers/index.ts';
 import CreateDecision from './partials/CreateDecision/index.ts';
@@ -44,6 +45,7 @@ const CompletedAction = ({ action }: CompletedActionProps) => {
         return <SimplePayment action={action} />;
       case ColonyActionType.MintTokens:
       case ColonyActionType.MintTokensMotion:
+      case ColonyActionType.MintTokensMultisig:
         return <MintTokens action={action} />;
       case ColonyActionType.MoveFunds:
       case ColonyActionType.MoveFundsMotion:
@@ -93,6 +95,10 @@ const CompletedAction = ({ action }: CompletedActionProps) => {
   };
 
   const getSidebarWidgetContent = () => {
+    if (action.isMultiSig) {
+      return <MultiSigSidebar transactionId={action.transactionHash} />;
+    }
+
     switch (actionType) {
       case ColonyActionType.AddVerifiedMembersMotion:
       case ColonyActionType.RemoveVerifiedMembersMotion:
