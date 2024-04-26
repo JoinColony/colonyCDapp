a066f2c54135b25bfe3b20dd47a6517bdebf47e7
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-04-26 10:56:54 +0100

    fix: add translation
    fix: buttton loadings
    
    fix: permission translation

--- src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/FinalizeButton/FinalizeButton.tsx
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/FinalizeButton/FinalizeButton.tsx
@@ -1,36 +1,71 @@
+import { SpinnerGap } from '@phosphor-icons/react';
 import React from 'react';
 import { type FC } from 'react';
+import { defineMessages } from 'react-intl';
 
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import useAsyncFunction from '~hooks/useAsyncFunction.ts';
 import { ActionTypes } from '~redux/actionTypes.ts';
+import { ActionForm } from '~shared/Fields/index.ts';
+import { mapPayload } from '~utils/actions.ts';
+import { formatText } from '~utils/intl.ts';
 import Button from '~v5/shared/Button/Button.tsx';
+import TxButton from '~v5/shared/Button/TxButton.tsx';
 
 const displayName =
   'v5.common.ActionSidebar.partials.MultiSig.partials.FinalizeButton';
 
 interface FinalizeButtonProps {
   multiSigId: string;
+  isPending: boolean;
+  setIsPending: (isPending: boolean) => void;
 }
 
-const FinalizeButton: FC<FinalizeButtonProps> = ({ multiSigId }) => {
+const MSG = defineMessages({
+  finalizeButton: {
+    id: `${displayName}.finalizeButton`,
+    defaultMessage: 'Finalize approve',
+  },
+});
+
+const FinalizeButton: FC<FinalizeButtonProps> = ({
+  multiSigId,
+  isPending,
+  setIsPending,
+}) => {
   const { colony } = useColonyContext();
-  const finalizeMultiSig = useAsyncFunction({
-    submit: ActionTypes.MULTISIG_FINALIZE,
-    error: ActionTypes.MULTISIG_FINALIZE_ERROR,
-    success: ActionTypes.MULTISIG_FINALIZE_SUCCESS,
-  });
+  const transform = mapPayload(() => ({
+    colonyAddress: colony.colonyAddress,
+    multiSigId,
+  }));
 
-  const handleFinalizeClick = async () => {
-    const voteForPayload = {
-      colonyAddress: colony.colonyAddress,
-      multiSigId,
-    };
-
-    await finalizeMultiSig(voteForPayload);
-  };
-
-  return <Button onClick={handleFinalizeClick}>Finalize approval</Button>;
+  return (
+    <ActionForm
+      actionType={ActionTypes.MULTISIG_FINALIZE}
+      transform={transform}
+      onSuccess={() => setIsPending(true)}
+      onError={() => setIsPending(false)}
+    >
+      {({ formState: { isSubmitting } }) =>
+        isPending || isSubmitting ? (
+          <TxButton
+            rounded="s"
+            isFullSize
+            text={{ id: 'button.pending' }}
+            icon={
+              <span className="ml-2 flex shrink-0">
+                <SpinnerGap size={18} className="animate-spin" />
+              </span>
+            }
+            className="!px-4 !text-md"
+          />
+        ) : (
+          <Button type="submit" isFullSize>
+            {formatText(MSG.finalizeButton)}
+          </Button>
+        )
+      }
+    </ActionForm>
+  );
 };
 
 FinalizeButton.displayName = displayName;
