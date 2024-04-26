1a685821d0c2a77991e649309274e6717b6bffba
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-04-26 10:56:54 +0100

    fix: add translation
    fix: buttton loadings
    
    fix: permission translation

--- src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/VoteButton/VoteButton.tsx
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/VoteButton/VoteButton.tsx
@@ -1,15 +1,22 @@
+import { SpinnerGap } from '@phosphor-icons/react';
 import React from 'react';
 import { type FC } from 'react';
+import { defineMessages } from 'react-intl';
 
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import { type ColonyActionType, type MultiSigVote } from '~gql';
-import useAsyncFunction from '~hooks/useAsyncFunction.ts';
+import { type ColonyActionType, MultiSigVote } from '~gql';
 import { ActionTypes } from '~redux/actionTypes.ts';
-import { type VoteOnMultiSigActionPayload } from '~redux/sagas/multiSig/voteOnMultiSig.ts';
+import { ActionForm } from '~shared/Fields/index.ts';
+import { mapPayload } from '~utils/actions.ts';
 import { extractColonyRoles } from '~utils/colonyRoles.ts';
 import { extractColonyDomains } from '~utils/domains.ts';
+import { formatText } from '~utils/intl.ts';
 import { getMultiSigRequiredRole } from '~utils/multiSig.ts';
 import Button from '~v5/shared/Button/Button.tsx';
+import TxButton from '~v5/shared/Button/TxButton.tsx';
+import { type ButtonProps } from '~v5/shared/Button/types.ts';
+
+import { VoteExpectedStep } from '../MultiSigWidget/types.ts';
 
 const displayName =
   'v5.common.ActionSidebar.partials.MultiSig.partials.VoteButton';
@@ -19,36 +26,87 @@ interface VoteButtonProps {
   multiSigId: string;
   multiSigDomainId: number;
   voteType: Exclude<MultiSigVote, MultiSigVote.None>;
+  isPending: boolean;
+  setExpectedStep: (step: VoteExpectedStep | null) => void;
+  setCurrentVote: (vote: MultiSigVote | null) => void;
+  buttonProps?: ButtonProps;
 }
+
+const MSG = defineMessages({
+  approve: {
+    id: `${displayName}.approve`,
+    defaultMessage: 'Approve',
+  },
+  reject: {
+    id: `${displayName}.reject`,
+    defaultMessage: 'Reject',
+  },
+});
 
 const VoteButton: FC<VoteButtonProps> = ({
   actionType,
   multiSigId,
   multiSigDomainId,
   voteType,
+  buttonProps,
+  setExpectedStep,
+  setCurrentVote,
+  isPending,
 }) => {
   const { colony } = useColonyContext();
-  const voteOnMultiSig = useAsyncFunction({
-    submit: ActionTypes.MULTISIG_VOTE,
-    error: ActionTypes.MULTISIG_VOTE_ERROR,
-    success: ActionTypes.MULTISIG_VOTE_SUCCESS,
-  });
 
-  const handleVoteClick = async () => {
-    const votePayload: VoteOnMultiSigActionPayload = {
-      colonyAddress: colony.colonyAddress,
-      colonyDomains: extractColonyDomains(colony.domains),
-      colonyRoles: extractColonyRoles(colony.roles),
-      vote: voteType,
-      domainId: multiSigDomainId,
-      multiSigId,
-      requiredRole: getMultiSigRequiredRole(actionType),
-    };
-
-    await voteOnMultiSig(votePayload);
+  const buttonText = {
+    [MultiSigVote.Approve]: MSG.approve,
+    [MultiSigVote.Reject]: MSG.reject,
   };
 
-  return <Button onClick={handleVoteClick}>{voteType}</Button>;
+  const transform = mapPayload(() => ({
+    colonyAddress: colony.colonyAddress,
+    colonyDomains: extractColonyDomains(colony.domains),
+    colonyRoles: extractColonyRoles(colony.roles),
+    vote: voteType,
+    domainId: multiSigDomainId,
+    multiSigId,
+    requiredRole: getMultiSigRequiredRole(actionType),
+  }));
+
+  return (
+    <ActionForm
+      actionType={ActionTypes.MULTISIG_VOTE}
+      transform={transform}
+      onSuccess={() => {
+        setExpectedStep(VoteExpectedStep.cancel);
+      }}
+      onError={() => {
+        setExpectedStep(null);
+      }}
+    >
+      {({ formState: { isSubmitting } }) =>
+        isPending || isSubmitting ? (
+          <TxButton
+            rounded="s"
+            isFullSize
+            text={{ id: 'button.pending' }}
+            icon={
+              <span className="ml-2 flex shrink-0">
+                <SpinnerGap size={18} className="animate-spin" />
+              </span>
+            }
+            className="!px-4 !text-md"
+          />
+        ) : (
+          <Button
+            type="submit"
+            {...buttonProps}
+            isFullSize
+            onClick={() => setCurrentVote(voteType)}
+          >
+            {formatText(buttonText[voteType])}
+          </Button>
+        )
+      }
+    </ActionForm>
+  );
 };
 
 VoteButton.displayName = displayName;
--- src/components/v5/common/CompletedAction/partials/MultiSigMeatballMenu/MultiSigMeatballMenu.tsx
+++ src/components/v5/common/CompletedAction/partials/MultiSigMeatballMenu/MultiSigMeatballMenu.tsx
@@ -1,24 +1,16 @@
-import { Copy, Prohibit } from '@phosphor-icons/react';
+import { Copy } from '@phosphor-icons/react';
 import React, { type FC } from 'react';
 import { generatePath } from 'react-router-dom';
 
 import MeatballMenuCopyItem from '~common/ColonyActionsTable/partials/MeatballMenuCopyItem/MeatballMenuCopyItem.tsx';
 import { APP_URL } from '~constants';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import { useCompletedActionContext } from '~context/CompletedActionContext/CompletedActionContext.ts';
-import { MultiSigVote } from '~gql';
-import useAsyncFunction from '~hooks/useAsyncFunction.ts';
-import { ActionTypes } from '~redux';
-import { type VoteOnMultiSigActionPayload } from '~redux/sagas/multiSig/voteOnMultiSig.ts';
 import {
   COLONY_ACTIVITY_ROUTE,
   COLONY_HOME_ROUTE,
   TX_SEARCH_PARAM,
 } from '~routes';
-import { extractColonyRoles } from '~utils/colonyRoles.ts';
-import { extractColonyDomains } from '~utils/domains.ts';
 import { formatText } from '~utils/intl.ts';
-import { getMultiSigRequiredRole } from '~utils/multiSig.ts';
 import MeatBallMenu from '~v5/shared/MeatBallMenu/index.ts';
 import { type MeatBallMenuItem } from '~v5/shared/MeatBallMenu/types.ts';
 
@@ -26,63 +18,10 @@ import { type MultiSigMeatballMenuProps } from './types.ts';
 
 const displayName = 'v5.common.CompletedAction.partials.MultiSigMeatballMenu';
 
-const MULTISIG_EXPIRY_PERIOD = 604_800_000; // 1 week
-
 const MultiSigMeatballMenu: FC<MultiSigMeatballMenuProps> = ({
   transactionHash,
-  multiSigData,
-  isOwner,
-  actionType,
 }) => {
   const { colony } = useColonyContext();
-  const { showRejectMultiSigStep, setShowRejectMultiSigStep } =
-    useCompletedActionContext();
-
-  const { isRejected, nativeMultiSigDomainId, nativeMultiSigId } =
-    multiSigData || {};
-
-  const cancelMultiSigMotion = useAsyncFunction({
-    submit: ActionTypes.MULTISIG_CANCEL,
-    error: ActionTypes.MULTISIG_CANCEL_ERROR,
-    success: ActionTypes.MULTISIG_CANCEL_SUCCESS,
-  });
-
-  const voteOnMultiSig = useAsyncFunction({
-    submit: ActionTypes.MULTISIG_VOTE,
-    error: ActionTypes.MULTISIG_VOTE_ERROR,
-    success: ActionTypes.MULTISIG_VOTE_SUCCESS,
-  });
-
-  const handleCancelMultiSigAsOwner = () => {
-    cancelMultiSigMotion({
-      colonyAddress: colony.colonyAddress,
-      motionId: multiSigData?.nativeMultiSigId,
-    });
-  };
-
-  const handleRejectMultiSig = async () => {
-    const rejectVotePayload: VoteOnMultiSigActionPayload = {
-      colonyAddress: colony.colonyAddress,
-      colonyDomains: extractColonyDomains(colony.domains),
-      colonyRoles: extractColonyRoles(colony.roles),
-      vote: MultiSigVote.Reject,
-      domainId: Number(nativeMultiSigDomainId),
-      multiSigId: nativeMultiSigId,
-      requiredRole: getMultiSigRequiredRole(actionType),
-    };
-
-    await voteOnMultiSig(rejectVotePayload);
-    setShowRejectMultiSigStep(true);
-  };
-
-  const canCancelMultiSig =
-    !isRejected &&
-    (isOwner ||
-      Date.now() - new Date(multiSigData.createdAt).getTime() >
-        MULTISIG_EXPIRY_PERIOD);
-
-  const canRejectMultiSig =
-    !canCancelMultiSig && !isRejected && !isOwner && !showRejectMultiSigStep;
 
   const multiSigMeatballOptions: MeatBallMenuItem[] = [
     {
@@ -100,26 +39,6 @@ const MultiSigMeatballMenu: FC<MultiSigMeatballMenuProps> = ({
       ),
       icon: Copy,
     },
-    ...(canCancelMultiSig
-      ? [
-          {
-            key: '2',
-            label: formatText({ id: 'multiSig.rejectAction' }),
-            icon: Prohibit,
-            onClick: handleCancelMultiSigAsOwner,
-          },
-        ]
-      : []),
-    ...(canRejectMultiSig
-      ? [
-          {
-            key: '2',
-            label: formatText({ id: 'multiSig.rejectAction' }),
-            icon: Prohibit,
-            onClick: handleRejectMultiSig,
-          },
-        ]
-      : []),
   ];
 
   return <MeatBallMenu items={multiSigMeatballOptions} />;
