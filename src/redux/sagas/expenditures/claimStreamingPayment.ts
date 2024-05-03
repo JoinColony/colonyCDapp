ebe38caf9630fcfd59170a75a5d1d43fbdb74617
Dave Creaser <david.creaser@outlook.com>
2024-05-03 17:30:01 +0100

    Create claim streaming payment saga

---
+++ src/redux/sagas/expenditures/claimStreamingPayment.ts
@@ -0,0 +1,85 @@
+import { ClientType, ColonyRole, getPermissionProofs } from '@colony/colony-js';
+import { call, fork, put, takeEvery } from 'redux-saga/effects';
+
+import { type Action, ActionTypes, type AllActions } from '~redux/index.ts';
+
+import {
+  createTransaction,
+  getTxChannel,
+  waitForTxResult,
+} from '../transactions/index.ts';
+import {
+  getColonyManager,
+  initiateTransaction,
+  putError,
+} from '../utils/index.ts';
+
+export type ClaimStreamingPaymentPayload =
+  Action<ActionTypes.STREAMING_PAYMENT_CLAIM>['payload'];
+
+function* claimStreamingPayment({
+  payload: {
+    colonyAddress,
+    streamingPaymentsAddress,
+    streamingPayment,
+    tokenAddress,
+  },
+  meta,
+}: Action<ActionTypes.STREAMING_PAYMENT_CLAIM>) {
+  const txChannel = yield call(getTxChannel, meta.id);
+
+  try {
+    const colonyManager = yield getColonyManager();
+    const colonyClient = yield colonyManager.getClient(
+      ClientType.ColonyClient,
+      colonyAddress,
+    );
+
+    const [extensionPermissionDomainId, extensionChildSkillIndex] =
+      yield getPermissionProofs(
+        colonyClient.networkClient,
+        colonyClient,
+        streamingPayment.nativeDomainId,
+        ColonyRole.Arbitration,
+        streamingPaymentsAddress,
+      );
+
+    yield fork(createTransaction, meta.id, {
+      context: ClientType.StreamingPaymentsClient,
+      methodName: 'claim',
+      identifier: colonyAddress,
+      params: [
+        extensionPermissionDomainId,
+        extensionChildSkillIndex,
+        extensionChildSkillIndex,
+        extensionChildSkillIndex,
+        streamingPayment.id,
+        [tokenAddress],
+      ],
+    });
+
+    yield initiateTransaction({ id: meta.id });
+
+    yield waitForTxResult(txChannel);
+
+    yield put<AllActions>({
+      type: ActionTypes.STREAMING_PAYMENT_CLAIM_SUCCESS,
+      payload: {},
+      meta,
+    });
+  } catch (error) {
+    return yield putError(
+      ActionTypes.STREAMING_PAYMENT_CLAIM_ERROR,
+      error,
+      meta,
+    );
+  }
+
+  txChannel.close();
+
+  return null;
+}
+
+export default function* claimStreamingPaymentSaga() {
+  yield takeEvery(ActionTypes.STREAMING_PAYMENT_CLAIM, claimStreamingPayment);
+}
