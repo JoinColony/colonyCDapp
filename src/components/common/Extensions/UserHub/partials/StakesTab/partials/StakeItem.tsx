6eef85b362ccd24a08e86116f9e2cdf22b19624b
Dave Creaser <david.creaser@outlook.com>
2024-05-13 15:19:07 +0100

    Make stake items in userhub clickable

--- src/components/common/Extensions/UserHub/partials/StakesTab/partials/StakeItem.tsx
+++ src/components/common/Extensions/UserHub/partials/StakesTab/partials/StakeItem.tsx
@@ -1,7 +1,9 @@
 import React, { type FC } from 'react';
 import { FormattedDate, useIntl } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
 
 import { getActionTitleValues } from '~common/ColonyActions/index.ts';
+import { TX_SEARCH_PARAM } from '~routes';
 import Numeral from '~shared/Numeral/index.ts';
 import UserStakeStatusBadge from '~v5/common/Pills/UserStakeStatusBadge/index.ts';
 
@@ -12,6 +14,7 @@ const displayName =
 
 const StakeItem: FC<StakeItemProps> = ({ nativeToken, stake, colony }) => {
   const { formatMessage } = useIntl();
+  const navigate = useNavigate();
 
   const stakeItemTitle =
     stake.action?.metadata?.customTitle ||
@@ -20,34 +23,46 @@ const StakeItem: FC<StakeItemProps> = ({ nativeToken, stake, colony }) => {
 
   return (
     <li className="flex flex-col border-b border-gray-100 py-3.5 first:pt-2 last:pb-6 sm:first:pt-0 sm:last:border-none sm:last:pb-1.5">
-      <div className="relative w-full">
-        <div className="flex items-center justify-between">
-          <div className="mr-2 flex min-w-0 items-center">
-            <p className="mr-2 min-w-0 truncate text-1">{stakeItemTitle}</p>
-            <span className="text-xs text-gray-400">
-              <FormattedDate value={stake.createdAt} />
-            </span>
+      <button
+        type="button"
+        onClick={() =>
+          navigate(
+            `${window.location.pathname}?${TX_SEARCH_PARAM}=${stake.action?.transactionHash}`,
+            {
+              replace: true,
+            },
+          )
+        }
+      >
+        <div className="relative w-full sm:hover:bg-gray-50">
+          <div className="flex items-center justify-between">
+            <div className="mr-2 flex min-w-0 items-center">
+              <p className="mr-2 min-w-0 truncate text-1">{stakeItemTitle}</p>
+              <span className="text-xs text-gray-400">
+                <FormattedDate value={stake.createdAt} />
+              </span>
+            </div>
+            <UserStakeStatusBadge status={stake.status} />
           </div>
-          <UserStakeStatusBadge status={stake.status} />
+          <div className="flex text-xs">
+            <div className="mr-2 font-medium">
+              <Numeral
+                value={stake.amount}
+                decimals={nativeToken.decimals}
+                suffix={` ${nativeToken.symbol}`}
+              />
+            </div>
+            <div className="text-gray-600">
+              {stake.action
+                ? formatMessage(
+                    { id: 'action.title' },
+                    getActionTitleValues(stake.action, colony),
+                  )
+                : '-'}
+            </div>
+          </div>
         </div>
-        <div className="flex text-xs">
-          <div className="mr-2 font-medium">
-            <Numeral
-              value={stake.amount}
-              decimals={nativeToken.decimals}
-              suffix={` ${nativeToken.symbol}`}
-            />
-          </div>
-          <div className="text-gray-600">
-            {stake.action
-              ? formatMessage(
-                  { id: 'action.title' },
-                  getActionTitleValues(stake.action, colony),
-                )
-              : '-'}
-          </div>
-        </div>
-      </div>
+      </button>
     </li>
   );
 };
