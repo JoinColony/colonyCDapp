9b9568310fb97ad1a9930bf20fed32236cb4c52c
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-17 09:55:14 +0100

    Fix: Release expenditure stage via motion

--- src/hooks/useActivityFeed/helpers.ts
+++ src/hooks/useActivityFeed/helpers.ts
@@ -103,6 +103,7 @@ const ACTION_TYPES_TO_HIDE = [
   ColonyActionType.SetExpenditureStateMotion,
   ColonyActionType.EditExpenditure,
   ColonyActionType.EditExpenditureMotion,
+  ColonyActionType.ReleaseStagedPaymentMotion,
 ];
 
 export const filterByActionTypes = (
