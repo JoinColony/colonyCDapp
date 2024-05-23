88fcf892a18db4e18873ddbe3daac964d55e29b0
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-23 16:36:00 +0100

    fix: restore createRootMultiSig saga

--- src/redux/sagas/multiSig/index.ts
+++ src/redux/sagas/multiSig/index.ts
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 
+import createRootMultiSig from './createRootMultiSig.ts';
 import setThresholds from './setThresholds.ts';
 
 export default function* multiSigSagas() {
-  yield all([call(setThresholds)]);
+  yield all([call(createRootMultiSig), call(setThresholds)]);
 }
