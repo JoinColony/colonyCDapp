7204468e7c359c0749594a62beccf477c6915875
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-24 16:25:11 +0100

    Feat: Multi sig permissions page

--- src/hooks/members/types.ts
+++ src/hooks/members/types.ts
@@ -7,4 +7,5 @@ export interface DomainWithPermissionsAndReputation {
   reputationPercentage: number;
   reputationRaw: string;
   permissions: AvailablePermission[];
+  multiSigPermissions: AvailablePermission[];
 }
