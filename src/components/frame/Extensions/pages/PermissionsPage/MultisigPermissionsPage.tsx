32aff3d721196383114ee6493f710eb4b3509382
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-05-24 16:25:11 +0100

    Feat: Multi sig permissions page

--- src/components/frame/Extensions/pages/PermissionsPage/MultisigPermissionsPage.tsx
+++ src/components/frame/Extensions/pages/PermissionsPage/MultisigPermissionsPage.tsx
@@ -1,3 +1,5 @@
+import { Binoculars } from '@phosphor-icons/react';
+import { capitalize } from 'lodash';
 import React from 'react';
 
 import PermissionsPageInnerContent from './partials/PermissionsPageRows.tsx';
