a8cfbd06f71ca1598e0c180dc3d9a1b89fcda583
Bassgeta <kerry.mahne@gmail.com>
2024-05-27 16:50:20 +0200

    feat: implement multisig vote removal and basic display of signatures

--- src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/MultiSigSidebar.tsx
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/MultiSigSidebar.tsx
@@ -1,8 +1,13 @@
 import { type FC } from 'react';
 import React from 'react';
 
+import { useAppContext } from '~context/AppContext/AppContext.ts';
+import { type MultiSigUserSignature } from '~gql';
+import { notMaybe } from '~utils/arrays/index.ts';
+
 import useGetColonyAction from '../../hooks/useGetColonyAction.ts';
 
+import RemoveVoteButton from './partials/RemoveVoteButton/RemoveVoteButton.tsx';
 import Signees from './partials/Signees/Signees.tsx';
 import VoteButton from './partials/VoteButton/VoteButton.tsx';
 
@@ -11,6 +16,7 @@ interface MultiSigSidebarProps {
   transactionId: string;
 }
 const MultiSigSidebar: FC<MultiSigSidebarProps> = ({ transactionId }) => {
+  const { user } = useAppContext();
   const { action, loadingAction } = useGetColonyAction(transactionId);
 
   if (loadingAction || !action) {
@@ -22,15 +28,34 @@ const MultiSigSidebar: FC<MultiSigSidebarProps> = ({ transactionId }) => {
     return null;
   }
 
+  // @TODO check why types are nagging me here
+  const signatures = action.multiSigData?.signatures?.items ?? [];
+  const userSignature = signatures.find(
+    (signature) => signature?.userAddress === user?.walletAddress,
+  );
+
   return (
     <div>
-      <Signees signees={[]} />
-      <VoteButton
-        actionType={action.type}
-        multiSigColonyAddress={action.colonyAddress}
-        multiSigId={action.multiSigData.nativeMultiSigId}
-        multiSigDomainId={Number(action.multiSigData.nativeMultiSigDomainId)}
+      <Signees
+        signees={
+          signatures.filter(notMaybe) as unknown as MultiSigUserSignature[]
+        }
       />
+      {userSignature ? (
+        <RemoveVoteButton
+          actionType={action.type}
+          multiSigColonyAddress={action.colonyAddress}
+          multiSigId={action.multiSigData.nativeMultiSigId}
+          multiSigDomainId={Number(action.multiSigData.nativeMultiSigDomainId)}
+        />
+      ) : (
+        <VoteButton
+          actionType={action.type}
+          multiSigColonyAddress={action.colonyAddress}
+          multiSigId={action.multiSigData.nativeMultiSigId}
+          multiSigDomainId={Number(action.multiSigData.nativeMultiSigDomainId)}
+        />
+      )}
     </div>
   );
 };
--- src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/Signees/Signees.tsx
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/Signees/Signees.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { type FC } from 'react';
 
 import { type MultiSigUserSignature } from '~gql';
@@ -9,7 +10,16 @@ interface SigneesProps {
   signees: MultiSigUserSignature[];
 }
 
-const Signees: FC<SigneesProps> = () => {};
+const Signees: FC<SigneesProps> = ({ signees }) => (
+  <div className="flex flex-col">
+    {signees.map((signature) => (
+      <div className="flex w-full flex-row justify-between">
+        <span>{signature.userAddress.slice(0, 10)}...</span>
+        <span>{signature.vote}</span>
+      </div>
+    ))}
+  </div>
+);
 
 Signees.displayName = displayName;
 export default Signees;
