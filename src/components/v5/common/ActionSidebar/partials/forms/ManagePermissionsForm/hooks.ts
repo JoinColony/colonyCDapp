3c5f2b5d5366b61a4816466a438bf01d5b4a4096
Bassgeta <kerry.mahne@gmail.com>
2024-05-28 16:57:08 +0200

    feat: get permissions local refactor

--- src/components/v5/common/ActionSidebar/partials/forms/ManagePermissionsForm/hooks.ts
+++ src/components/v5/common/ActionSidebar/partials/forms/ManagePermissionsForm/hooks.ts
@@ -13,6 +13,7 @@ import { DecisionMethod } from '~types/actions.ts';
 import { Authority } from '~types/authority.ts';
 import { mapPayload, pipe } from '~utils/actions.ts';
 import { notMaybe } from '~utils/arrays/index.ts';
+import { extractColonyRoles } from '~utils/colonyRoles.ts';
 
 import useActionFormBaseHook from '../../../hooks/useActionFormBaseHook.ts';
 import { type ActionFormBaseProps } from '../../../types.ts';
@@ -100,7 +101,7 @@ export const useManagePermissions = (
         const isMultiSig = authority === Authority.ViaMultiSig;
 
         const userPermissions = getUserRolesForDomain({
-          colony,
+          colonyRoles: extractColonyRoles(colony.roles),
           userAddress: member,
           domainId: Number(team),
           excludeInherited: true,
