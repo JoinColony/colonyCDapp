6dd5535abcd3ed9a4afa99f428f00b52b3956f69
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-06-04 16:15:50 +0100

    Feat: Implement manage tokens completed action screen

---
+++ src/components/v5/common/CompletedAction/partials/ManageTokens/ManageTokens.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import { ColonyActionType, type ColonyAction } from '~types/graphql.ts';
+import { formatText } from '~utils/intl.ts';
+import UserInfoPopover from '~v5/shared/UserInfoPopover/UserInfoPopover.tsx';
+
+import {
+  ActionDataGrid,
+  ActionSubtitle,
+  ActionTitle,
+} from '../Blocks/index.ts';
+import {
+  ActionTypeRow,
+  CreatedInRow,
+  DecisionMethodRow,
+  DescriptionRow,
+} from '../rows/index.ts';
+
+import TokensTable from './ApprovedTokens.tsx';
+
+const displayName = 'v5.common.CompletedAction.partials.ManageTokens';
+
+interface ManageTokensProps {
+  action: ColonyAction;
+}
+
+const ManageTokens = ({ action }: ManageTokensProps) => {
+  const {
+    customTitle = formatText(
+      {
+        id: 'action.type',
+      },
+      {
+        actionType: ColonyActionType.ManageTokens,
+      },
+    ),
+  } = action?.metadata || {};
+  const { initiatorUser } = action;
+
+  const { approvedTokenChanges } = action;
+
+  return (
+    <>
+      <ActionTitle>{customTitle}</ActionTitle>
+      <ActionSubtitle>
+        {formatText(
+          {
+            id: 'action.title',
+          },
+          {
+            actionType: ColonyActionType.ManageTokens,
+            initiator: initiatorUser ? (
+              <UserInfoPopover
+                walletAddress={initiatorUser.walletAddress}
+                user={initiatorUser}
+                withVerifiedBadge={false}
+              >
+                {initiatorUser.profile?.displayName}
+              </UserInfoPopover>
+            ) : null,
+          },
+        )}
+      </ActionSubtitle>
+      <ActionDataGrid>
+        <ActionTypeRow actionType={action.type} />
+        <DecisionMethodRow isMotion={action.isMotion || false} />
+
+        {action.motionData?.motionDomain.metadata && (
+          <CreatedInRow
+            motionDomainMetadata={action.motionData.motionDomain.metadata}
+          />
+        )}
+      </ActionDataGrid>
+      {action.annotation?.message && (
+        <DescriptionRow description={action.annotation.message} />
+      )}
+      {approvedTokenChanges !== undefined && approvedTokenChanges !== null && (
+        <TokensTable approvedTokenChanges={approvedTokenChanges} />
+      )}
+    </>
+  );
+};
+
+ManageTokens.displayName = displayName;
+export default ManageTokens;
