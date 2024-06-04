dcc53460ca97b8ed951787420bdc36492d0835d2
Bassgeta <kerry.mahne@gmail.com>
2024-06-04 14:44:23 +0200

    feat: handle finalization of multiSig motion

---
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/FinalizeButton/FinalizeButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { type FC } from 'react';
+
+import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import useAsyncFunction from '~hooks/useAsyncFunction.ts';
+import { ActionTypes } from '~redux/actionTypes.ts';
+import Button from '~v5/shared/Button/Button.tsx';
+
+const displayName =
+  'v5.common.ActionSidebar.partials.MultiSig.partials.FinalizeButton';
+
+interface FinalizeButtonProps {
+  multiSigId: string;
+}
+
+const FinalizeButton: FC<FinalizeButtonProps> = ({ multiSigId }) => {
+  const { colony } = useColonyContext();
+  const finalizeMultiSig = useAsyncFunction({
+    submit: ActionTypes.MULTISIG_FINALIZE,
+    error: ActionTypes.MULTISIG_FINALIZE_ERROR,
+    success: ActionTypes.MULTISIG_FINALIZE_SUCCESS,
+  });
+
+  const handleFinalizeClick = async () => {
+    const voteForPayload = {
+      colonyAddress: colony.colonyAddress,
+      multiSigId,
+    };
+
+    await finalizeMultiSig(voteForPayload);
+  };
+
+  return <Button onClick={handleFinalizeClick}>Finalize</Button>;
+};
+
+FinalizeButton.displayName = displayName;
+export default FinalizeButton;
---
+++ src/redux/sagas/multiSig/finalizeMultiSig.ts
@@ -0,0 +1,52 @@
+import { ClientType } from '@colony/colony-js';
+import { takeEvery, call, fork, put } from 'redux-saga/effects';
+
+import { type Action, ActionTypes, type AllActions } from '~redux';
+
+import {
+  createTransaction,
+  getTxChannel,
+  waitForTxResult,
+} from '../transactions/index.ts';
+import { initiateTransaction, putError, takeFrom } from '../utils/effects.ts';
+
+function* finalizeMultiSigAction({
+  payload: { colonyAddress, multiSigId },
+  meta,
+}: Action<ActionTypes.MULTISIG_FINALIZE>) {
+  const txChannel = yield call(getTxChannel, meta.id);
+
+  try {
+    if (!colonyAddress || !multiSigId) {
+      throw new Error('No colony address or multiSigId');
+    }
+
+    yield fork(createTransaction, meta.id, {
+      context: ClientType.MultisigPermissionsClient,
+      methodName: 'execute',
+      identifier: colonyAddress,
+      params: [multiSigId],
+    });
+
+    yield takeFrom(txChannel, ActionTypes.TRANSACTION_CREATED);
+
+    yield initiateTransaction({ id: meta.id });
+
+    const { type } = yield waitForTxResult(txChannel);
+
+    if (type === ActionTypes.TRANSACTION_SUCCEEDED) {
+      yield put<AllActions>({
+        type: ActionTypes.MULTISIG_FINALIZE_SUCCESS,
+        meta,
+      });
+    }
+  } catch (error) {
+    yield putError(ActionTypes.MULTISIG_FINALIZE_ERROR, error, meta);
+  }
+
+  txChannel.close();
+}
+
+export default function* finalizeMultiSigSaga() {
+  yield takeEvery(ActionTypes.MULTISIG_FINALIZE, finalizeMultiSigAction);
+}
