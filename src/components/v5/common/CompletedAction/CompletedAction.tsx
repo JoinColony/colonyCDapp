203387fe39fafe17774492ea828e893400f753d2
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-04 14:17:27 +0100

    fix: handle the remove verified mmotions action

--- src/components/v5/common/CompletedAction/CompletedAction.tsx
+++ src/components/v5/common/CompletedAction/CompletedAction.tsx
@@ -95,6 +95,7 @@ const CompletedAction = ({ action }: CompletedActionProps) => {
   const getSidebarWidgetContent = () => {
     switch (actionType) {
       case ColonyActionType.AddVerifiedMembersMotion:
+      case ColonyActionType.RemoveVerifiedMembersMotion:
       case ColonyActionType.PaymentMotion:
       case ColonyActionType.MintTokensMotion:
       case ColonyActionType.MoveFundsMotion:
