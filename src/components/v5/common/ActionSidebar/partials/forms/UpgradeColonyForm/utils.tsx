882c896342db0d7344e5847e0e7683190537de13
Bassgeta <kerry.mahne@gmail.com>
2024-06-05 15:35:02 +0200

    feat: refactor multi sig motions to use the same sagas as reputation motions, rework fetching of role holders

--- src/components/v5/common/ActionSidebar/partials/forms/UpgradeColonyForm/utils.tsx
+++ src/components/v5/common/ActionSidebar/partials/forms/UpgradeColonyForm/utils.tsx
@@ -1,5 +1,8 @@
 import { RootMotionMethodNames } from '~redux/index.ts';
+import { DecisionMethod } from '~types/actions.ts';
 import { type Colony } from '~types/graphql.ts';
+import { extractColonyRoles } from '~utils/colonyRoles.ts';
+import { extractColonyDomains } from '~utils/domains.ts';
 import { sanitizeHTML } from '~utils/strings.ts';
 
 import { type UpgradeColonyFormValues } from './consts.ts';
@@ -7,13 +10,32 @@ import { type UpgradeColonyFormValues } from './consts.ts';
 export const getUpgradeColonyPayload = (
   colony: Colony,
   values: UpgradeColonyFormValues,
-) => ({
-  operationName: RootMotionMethodNames.Upgrade,
-  colonyAddress: colony.colonyAddress,
-  colonyName: colony.name,
-  version: colony.version,
-  motionParams: [colony.version + 1],
-  annotationMessage: values.description
-    ? sanitizeHTML(values.description)
-    : undefined,
-});
+) => {
+  const { description: annotationMessage, title, decisionMethod } = values;
+
+  const commonPayload = {
+    colonyAddress: colony.colonyAddress,
+    colonyName: colony.name,
+    version: colony.version,
+    annotationMessage: annotationMessage
+      ? sanitizeHTML(annotationMessage)
+      : undefined,
+    customActionTitle: title,
+  };
+
+  if (
+    decisionMethod === DecisionMethod.Reputation ||
+    decisionMethod === DecisionMethod.MultiSig
+  ) {
+    return {
+      ...commonPayload,
+      operationName: RootMotionMethodNames.Upgrade,
+      colonyRoles: extractColonyRoles(colony.roles),
+      colonyDomains: extractColonyDomains(colony.domains),
+      motionParams: [colony.version + 1],
+      isMultiSig: decisionMethod === DecisionMethod.MultiSig,
+    };
+  }
+
+  return commonPayload;
+};
