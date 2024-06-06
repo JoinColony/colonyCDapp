3b94c31d5f4fa0285dc5d4bb2663d945d2a3149c
Dave Creaser <david.creaser@outlook.com>
2024-06-06 18:29:37 +0100

    Switch to using composition

--- src/components/v5/common/FourOFourMessage/types.ts
+++ src/components/v5/common/FourOFourMessage/types.ts
@@ -1,18 +1,7 @@
-export enum FourOFourMessageLinkType {
-  Internal = 'internal',
-  External = 'external',
-}
+import { type ReactNode } from 'react';
 
 export interface FourOFourMessageProps {
   description: string;
-  links: {
-    type: FourOFourMessageLinkType;
-    text: string;
-    location: string;
-  }[];
-  primaryLinkButton: {
-    onClick?: () => void;
-    text: string;
-    location?: string;
-  };
+  links: ReactNode;
+  primaryLinkButton: ReactNode;
 }
