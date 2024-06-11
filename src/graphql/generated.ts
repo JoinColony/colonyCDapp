bbb2559eb8c7f4f1f39e2a85248a5b22927b317d
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-11 12:42:15 +0100

    feat: add a ColonyActionType enum entry for cancel and waive

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -588,7 +588,12 @@ export enum ColonyActionType {
   /** An action related to adding verified members */
   AddVerifiedMembers = 'ADD_VERIFIED_MEMBERS',
   AddVerifiedMembersMotion = 'ADD_VERIFIED_MEMBERS_MOTION',
+<<<<<<< HEAD
   AddVerifiedMembersMultisig = 'ADD_VERIFIED_MEMBERS_MULTISIG',
+=======
+  /** An action related to cancelling and waiving a streaming payment */
+  CancelAndWaiveStreamingPayment = 'CANCEL_AND_WAIVE_STREAMING_PAYMENT',
+>>>>>>> 34b866c5a (feat: add a ColonyActionType enum entry for cancel and waive)
   /** An action related to canceling an expenditure */
   CancelExpenditure = 'CANCEL_EXPENDITURE',
   /** An action related to a motion to cancel an expenditure */
