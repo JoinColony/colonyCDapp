420c5ab656b7280f353a7cf55533e24c614e8e45
Bassgeta <kerry.mahne@gmail.com>
2024-06-11 14:19:39 +0200

    feat: realign after rebase

--- src/components/v5/common/ActionSidebar/hooks/permissions/useHasActionPermissions.ts
+++ src/components/v5/common/ActionSidebar/hooks/permissions/useHasActionPermissions.ts
@@ -43,13 +43,13 @@ const useHasActionPermissions = () => {
     return hasPermissions;
   }
 
-  const hasMultiSigPermissions = getHasActionPermissions(
+  const hasMultiSigPermissions = getHasActionPermissions({
     colony,
-    user?.walletAddress ?? '',
+    userAddress: user?.walletAddress ?? '',
     actionType,
     formValues,
-    true,
-  );
+    isMultiSig: true,
+  });
 
   if (decisionMethod === DecisionMethod.MultiSig) {
     return hasMultiSigPermissions;
