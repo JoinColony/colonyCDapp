04434721042c1bab21ad59bbe460841ed3c19dc8
Dave Creaser <david.creaser@outlook.com>
2024-06-12 17:15:35 +0100

    Use correct asset platform when calling coingecko

--- src/hooks/useCurrency.ts
+++ src/hooks/useCurrency.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { Network, SupportedCurrencies } from '~gql';
+import { SupportedCurrencies } from '~gql';
+import { Network } from '~types/network.ts';
 import {
   fetchCurrentPrice,
   type FetchCurrentPriceArgs,
@@ -8,7 +9,7 @@ import {
 
 const useCurrency = ({
   contractAddress,
-  chainId = Network.Gnosis,
+  chainId = Network.ArbitrumOne,
   conversionDenomination = SupportedCurrencies.Usd,
 }: FetchCurrentPriceArgs) => {
   const [price, setPrice] = useState<number | null>(0);
