17cf37c4c4a8a0e35d1576cdb08ffc19195a3258
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-12 17:13:25 +0100

    feat: fix permission proof for StreamingPayment cancel

--- src/components/v5/common/CompletedAction/CompletedAction.tsx
+++ src/components/v5/common/CompletedAction/CompletedAction.tsx
@@ -109,7 +109,8 @@ const CompletedAction = ({ action }: CompletedActionProps) => {
       case ColonyActionType.ReleaseStagedPaymentMotion:
       case ColonyActionType.EditExpenditureMotion:
       case ColonyActionType.FundExpenditureMotion:
-        // @NOTE: Enabling those 2 above temporarily
+      case ColonyActionType.CancelStreamingPaymentMotion:
+        // @NOTE: Enabling those 4 above temporarily
         return <Motions transactionId={action.transactionHash} />;
       case ColonyActionType.CreateExpenditure:
         return <PaymentBuilderWidget action={action} />;
