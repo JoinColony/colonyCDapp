616149a3b6dae017de951a4e178737f0134deda7
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-06-13 13:52:20 +0200

    Fix: Style for onboarding flow
    Fix styling issue on smaller devices

--- src/components/frame/LandingPage/LandingPage.tsx
+++ src/components/frame/LandingPage/LandingPage.tsx
@@ -135,7 +135,7 @@ const LandingPage = () => {
           </p>
         </div>
         <div className="flex w-full justify-center gap-4">
-          <div className="flex w-1/2 flex-col justify-between">
+          <div className="flex gap-6 sm:gap-4 w-full sm:w-1/2 flex-col justify-between">
             <LandingPageItem
               headingText={MSG.createColonyTitle}
               headingDescription={MSG.createColonyDescription}
@@ -206,7 +206,7 @@ const LandingPage = () => {
           <img
             src={landingImagesSrc[1]} // @TODO: Change to hoveredItem once we enable the create colony landing page item
             alt=""
-            className="w-1/2 rounded-lg border border-gray-200 object-cover shadow-sm"
+            className="hidden sm:block w-1/2 rounded-lg border border-gray-200 object-cover shadow-sm"
           />
         </div>
         {hasShareableInvitationCode && <InvitationBlock />}
