c99d4d32a3e829e0c502d21a16cef2216a7e8d2e
Dave Creaser <david.creaser@outlook.com>
2024-06-14 15:17:41 +0100

    Set correct end time for streaming payments based on end condition

--- src/components/v5/frame/ColonyHome/ColonyHome.tsx
+++ src/components/v5/frame/ColonyHome/ColonyHome.tsx
@@ -14,7 +14,8 @@ import {
 } from '~routes/index.ts';
 import { formatText } from '~utils/intl.ts';
 import { setQueryParamOnUrl } from '~utils/urls.ts';
-// import TmpAdvancedPayments from '~v5/payments/TmpAdvancedPayments.tsx';
+import TmpAdvancedPayments from '~v5/payments/TmpAdvancedPayments.tsx';
+import TmpStreamingPayments from '~v5/payments/TmpStreamingPayments.tsx';
 import Link from '~v5/shared/Link/index.ts';
 
 import Agreements from './partials/Agreements/index.ts';
@@ -45,9 +46,10 @@ const ColonyHome = () => {
           <TokenBalance />
         </div>
       </div>
-      {/* <div className="mx-auto">
+      <div className="mx-auto flex flex-col gap-8">
         <TmpAdvancedPayments />
-      </div> */}
+        <TmpStreamingPayments />
+      </div>
       <div className="flex w-full flex-col gap-6 lg:grid lg:grid-cols-[39%_1fr]">
         <div className="flex w-full flex-col gap-6 sm:grid sm:grid-cols-2 sm:gap-[1.125rem] lg:flex lg:flex-col lg:gap-[1.125rem]">
           <Objective />
