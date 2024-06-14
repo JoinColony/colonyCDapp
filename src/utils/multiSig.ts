caecb21429796638ffaf8aa607acf921b9e36949
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-06-14 15:55:41 +0100

    Feat: Handle not enough signatories

--- src/utils/multiSig.ts
+++ src/utils/multiSig.ts
@@ -1,10 +1,12 @@
+import { Action } from '~constants/actions.ts';
 import { UserRole } from '~constants/permissions.ts';
 import { ColonyActionType } from '~gql';
 
 export const getMultiSigRequiredRole = (
-  actionType: ColonyActionType,
+  actionType: ColonyActionType | Action,
 ): UserRole => {
   switch (actionType) {
+    case Action.MintTokens:
     case ColonyActionType.MintTokensMultisig:
       return UserRole.Owner;
     default:
