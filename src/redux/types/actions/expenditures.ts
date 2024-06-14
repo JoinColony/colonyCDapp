a8d20620cd8817395ade1d34cdde2cb419194402
Dave Creaser <david.creaser@outlook.com>
2024-06-14 15:23:41 +0100

    Fix typings

--- src/redux/types/actions/expenditures.ts
+++ src/redux/types/actions/expenditures.ts
@@ -1,5 +1,3 @@
-import { type BigNumber } from 'ethers';
-
 import {
   type SplitPaymentDistributionType,
   type StreamingPaymentEndCondition,
@@ -213,8 +211,8 @@ export type ExpendituresActionTypes =
         tokenAddress: Address;
         tokenDecimals: number;
         amount: string;
-        startTimestamp: BigNumber;
-        endTimestamp?: BigNumber;
+        startTimestamp: string;
+        endTimestamp?: string;
         interval: number;
         endCondition: StreamingPaymentEndCondition;
         limitAmount?: string;
