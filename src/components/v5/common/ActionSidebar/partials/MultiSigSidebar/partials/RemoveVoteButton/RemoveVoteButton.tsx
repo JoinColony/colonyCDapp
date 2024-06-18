9484d38e4813752071c490bde23a55a867764036
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-06-18 16:50:03 +0100

    Feat: Refactor useEligbleSignees to accept multiple domains and required roles

--- src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/RemoveVoteButton/RemoveVoteButton.tsx
+++ src/components/v5/common/ActionSidebar/partials/MultiSigSidebar/partials/RemoveVoteButton/RemoveVoteButton.tsx
@@ -11,7 +11,7 @@ import { mapPayload } from '~utils/actions.ts';
 import { extractColonyRoles } from '~utils/colonyRoles.ts';
 import { extractColonyDomains } from '~utils/domains.ts';
 import { formatText } from '~utils/intl.ts';
-import { getMultiSigRequiredRole } from '~utils/multiSig.ts';
+import { getRolesNeededForMultiSigAction } from '~utils/multiSig.ts';
 import Button from '~v5/shared/Button/Button.tsx';
 import TxButton from '~v5/shared/Button/TxButton.tsx';
 
@@ -51,7 +51,7 @@ const RemoveVoteButton: FC<RemoveVoteButtonProps> = ({
     vote: MultiSigVote.None,
     domainId: multiSigDomainId,
     multiSigId,
-    requiredRole: getMultiSigRequiredRole(actionType),
+    requiredRoles: getRolesNeededForMultiSigAction(actionType) || [],
   }));
 
   return (
