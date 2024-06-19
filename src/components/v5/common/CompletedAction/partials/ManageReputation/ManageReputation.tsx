d7ea3c810386539d101fcef29c9fbe29726fd1ba
Dave Creaser <david.creaser@outlook.com>
2024-06-19 14:05:02 +0100

    Implement unlock token multi-sig motion

--- src/components/v5/common/CompletedAction/partials/ManageReputation/ManageReputation.tsx
+++ src/components/v5/common/CompletedAction/partials/ManageReputation/ManageReputation.tsx
@@ -174,7 +174,10 @@ const ManageReputation: FC<ManageReputationProps> = ({ action }) => {
             actionType={action.type}
           />
         )}
-        <DecisionMethodRow isMotion={action.isMotion || false} />
+        <DecisionMethodRow
+          isMotion={action.isMotion || false}
+          isMultisig={action.isMultiSig || false}
+        />
         {action.motionData?.motionDomain.metadata && (
           <CreatedInRow
             motionDomainMetadata={action.motionData.motionDomain.metadata}
