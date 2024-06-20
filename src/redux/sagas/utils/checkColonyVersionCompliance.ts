8222c6b5965ad9438b7b4451db49069d70ac0727
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-20 09:43:56 +0100

    feat: genericise error message used for colony compliance version check

--- src/redux/sagas/utils/checkColonyVersionCompliance.ts
+++ src/redux/sagas/utils/checkColonyVersionCompliance.ts
@@ -1,19 +1,16 @@
 import { ColonyVersion } from '~constants';
 import { type ColonyFragment } from '~gql';
-import { type ActionTypes } from '~redux/actionTypes.ts';
 
 export const checkColonyVersionCompliance = ({
   colony,
-  actionType,
   minimumColonyVersion = ColonyVersion.V15,
 }: {
   colony: ColonyFragment;
-  actionType: ActionTypes;
   minimumColonyVersion?: ColonyVersion;
 }) => {
   if (colony.version < minimumColonyVersion) {
     throw new Error(
-      `Action: ${actionType} is only available in Colony version ${minimumColonyVersion} and above.`,
+      `This is only available in Colony version ${minimumColonyVersion} and above.`,
     );
   }
 };
