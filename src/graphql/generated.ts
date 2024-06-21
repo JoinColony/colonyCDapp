5c16e1152f069eb0e1a0b95b1ed8bb38a081dd35
Jakub Zajac <jakub@colony.io>
2024-06-21 15:54:05 +0100

    Schema update: Add UserStakeType enum and field to UserStake model

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -1584,6 +1584,7 @@ export type CreateUserStakeInput = {
   id?: InputMaybe<Scalars['ID']>;
   isClaimed: Scalars['Boolean'];
   isForfeited?: InputMaybe<Scalars['Boolean']>;
+  type?: InputMaybe<UserStakeType>;
   userAddress: Scalars['ID'];
 };
 
@@ -4014,6 +4015,7 @@ export type ModelSubscriptionUserStakeFilterInput = {
   isClaimed?: InputMaybe<ModelSubscriptionBooleanInput>;
   isForfeited?: InputMaybe<ModelSubscriptionBooleanInput>;
   or?: InputMaybe<Array<InputMaybe<ModelSubscriptionUserStakeFilterInput>>>;
+  type?: InputMaybe<ModelSubscriptionStringInput>;
   userAddress?: InputMaybe<ModelSubscriptionIdInput>;
 };
 
@@ -4173,6 +4175,7 @@ export type ModelUserStakeConditionInput = {
   isForfeited?: InputMaybe<ModelBooleanInput>;
   not?: InputMaybe<ModelUserStakeConditionInput>;
   or?: InputMaybe<Array<InputMaybe<ModelUserStakeConditionInput>>>;
+  type?: InputMaybe<ModelUserStakeTypeInput>;
   userAddress?: InputMaybe<ModelIdInput>;
 };
 
@@ -4193,7 +4196,13 @@ export type ModelUserStakeFilterInput = {
   isForfeited?: InputMaybe<ModelBooleanInput>;
   not?: InputMaybe<ModelUserStakeFilterInput>;
   or?: InputMaybe<Array<InputMaybe<ModelUserStakeFilterInput>>>;
+  type?: InputMaybe<ModelUserStakeTypeInput>;
   userAddress?: InputMaybe<ModelIdInput>;
+};
+
+export type ModelUserStakeTypeInput = {
+  eq?: InputMaybe<UserStakeType>;
+  ne?: InputMaybe<UserStakeType>;
 };
 
 export type ModelUserTokensConditionInput = {
@@ -8413,6 +8422,7 @@ export type UpdateUserStakeInput = {
   id: Scalars['ID'];
   isClaimed?: InputMaybe<Scalars['Boolean']>;
   isForfeited?: InputMaybe<Scalars['Boolean']>;
+  type?: InputMaybe<UserStakeType>;
   userAddress?: InputMaybe<Scalars['ID']>;
 };
 
@@ -8510,10 +8520,17 @@ export type UserStake = {
   isClaimed: Scalars['Boolean'];
   /** Only applicable for expenditure stakes, indicates if the creator's stake was forfeited when expenditure was cancelled */
   isForfeited?: Maybe<Scalars['Boolean']>;
+  type?: Maybe<UserStakeType>;
   updatedAt: Scalars['AWSDateTime'];
   user: User;
   userAddress: Scalars['ID'];
 };
+
+/** Type of stake a user can make */
+export enum UserStakeType {
+  Motion = 'MOTION',
+  StakedExpenditure = 'STAKED_EXPENDITURE'
+}
 
 export type UserTokens = {
   __typename?: 'UserTokens';
