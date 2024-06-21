5e7105ad49e0ff137f93db2dfdbffbe2bed47abf
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-06-21 10:22:05 +0200

    Fix: Update style for user and token popovers
    Add domain color to ContributorReputation and ContributorRoles fragments
    Update styling for user popover
    Update styling for token popover

--- src/hooks/members/types.ts
+++ src/hooks/members/types.ts
@@ -1,9 +1,12 @@
+import { type DomainColor } from '~gql';
+
 export type AvailablePermission = 0 | 1 | 2 | 3 | 5 | 6;
 
 export interface DomainWithPermissionsAndReputation {
   domainId: string;
   nativeId: number; // used for sorting
   domainName: string;
+  domainColor?: DomainColor;
   reputationPercentage: number;
   reputationRaw: string;
   permissions: AvailablePermission[];
