61f1c495adc4389804ac59232766381c55ed054a
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-21 18:56:23 +0100

    feat: maintain edit team behaviour

--- src/components/v5/common/ActionSidebar/partials/forms/EditTeamForm/utils.tsx
+++ src/components/v5/common/ActionSidebar/partials/forms/EditTeamForm/utils.tsx
@@ -1,23 +1,62 @@
-import { type Colony, type Domain } from '~types/graphql.ts';
+import { RootMotionMethodNames } from '~redux';
+import { DecisionMethod } from '~types/actions.ts';
+import { type Domain, type Colony } from '~types/graphql.ts';
+import { extractColonyRoles } from '~utils/colonyRoles.ts';
+import { extractColonyDomains } from '~utils/domains.ts';
 import { sanitizeHTML } from '~utils/strings.ts';
 
 import { type EditTeamFormValues } from './consts.ts';
+
+// export const getEditDomainPayload = (
+//   colony: Colony,
+//   values: EditTeamFormValues,
+//   selectedDomain: Domain,
+// ) => ({
+//   colonyAddress: colony.colonyAddress,
+//   colonyName: colony.name,
+//   domainName: values.teamName,
+//   domainColor: values.domainColor,
+//   domainPurpose: values.domainPurpose,
+//   domain: selectedDomain,
+//   annotationMessage: values.description
+//     ? sanitizeHTML(values.description)
+//     : undefined,
+//   customActionTitle: values.title,
+//   motionDomainId: values.createdIn,
+//   isCreateDomain: false,
+// });
 
 export const getEditDomainPayload = (
   colony: Colony,
   values: EditTeamFormValues,
   selectedDomain: Domain,
-) => ({
-  colonyAddress: colony.colonyAddress,
-  colonyName: colony.name,
-  domainName: values.teamName,
-  domainColor: values.domainColor,
-  domainPurpose: values.domainPurpose,
-  domain: selectedDomain,
-  annotationMessage: values.description
-    ? sanitizeHTML(values.description)
-    : undefined,
-  customActionTitle: values.title,
-  motionDomainId: values.createdIn,
-  isCreateDomain: false,
-});
+) => {
+  const baseDomainPayload = {
+    colonyAddress: colony.colonyAddress,
+    colonyName: colony.name,
+    domainName: values.teamName,
+    domainColor: values.domainColor,
+    domainPurpose: values.domainPurpose,
+    domain: selectedDomain,
+    annotationMessage: values.description
+      ? sanitizeHTML(values.description)
+      : undefined,
+    customActionTitle: values.title,
+    isCreateDomain: false,
+  };
+
+  if (values.decisionMethod === DecisionMethod.Permissions) {
+    return {
+      ...baseDomainPayload,
+    };
+  }
+
+  return {
+    ...baseDomainPayload,
+    operationName: RootMotionMethodNames.EditDomain,
+    colonyRoles: extractColonyRoles(colony.roles),
+    colonyDomains: extractColonyDomains(colony.domains),
+    isMultiSig: values.decisionMethod === DecisionMethod.MultiSig,
+    motionDomainId: values.createdIn,
+  };
+};
