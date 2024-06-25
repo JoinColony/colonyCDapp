601f3f07442d83f36b8a490854ec83ea1f155ef1
Jakub Zajac <jakub@colony.io>
2024-06-25 10:11:56 +0100

    Cleanup: Remove unused files

--- src/utils/dataReader.ts
+++
@@ -1,9 +0,0 @@
-export const getBase64image = (blob): Promise<any> => {
-  return new Promise((resolve) => {
-    const fileReader = new FileReader();
-    fileReader.onload = () => {
-      return resolve(fileReader.result);
-    };
-    fileReader.readAsDataURL(blob);
-  });
-};
