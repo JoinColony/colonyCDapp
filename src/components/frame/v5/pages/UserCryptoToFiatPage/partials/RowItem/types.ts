0c2ecfbc0156f3eb30180d96f2d0f31e8c4b889e
Romeo Ledesma <r_ledesma@outlook.com>
2024-06-26 14:48:05 +0100

    hackathon: crypto to fiat tab ROUGH

---
+++ src/components/frame/v5/pages/UserCryptoToFiatPage/partials/RowItem/types.ts
@@ -0,0 +1,22 @@
+import { type ReactNode, type PropsWithChildren } from 'react';
+import { type MessageDescriptor } from 'react-intl';
+
+import type React from 'react';
+
+export interface RowItemProps extends PropsWithChildren {}
+
+export interface RowItemHeadingProps {
+  title: MessageDescriptor;
+  accessory: MessageDescriptor;
+  statusPill: ReactNode;
+  itemOrder: number;
+}
+
+export interface RowItemBodyProps {
+  title?: MessageDescriptor;
+  description?: MessageDescriptor;
+  descriptionComponent?: React.ReactNode;
+  ctaTitle?: MessageDescriptor;
+  ctaOnClick?: () => void;
+  ctaComponent?: React.ReactNode;
+}
