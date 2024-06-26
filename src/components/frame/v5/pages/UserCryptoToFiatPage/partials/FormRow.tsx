953b46fbd268835aba4418089fb9636847a49ee4
Anastasia <anastasia.nortsova@gmail.com>
2024-06-26 17:18:17 +0100

    add validation to personal details and contact details forms

--- src/components/frame/v5/pages/UserCryptoToFiatPage/partials/FormRow.tsx
+++ src/components/frame/v5/pages/UserCryptoToFiatPage/partials/FormRow.tsx
@@ -1,5 +1,5 @@
 import React, { type FC, type PropsWithChildren } from 'react';
 
 export const FormRow: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="py-1">{children}</div>;
+  return <div className="py-1 pb-3">{children}</div>;
 };
