dcfadbf76be2c6c31ffb6b66a64c18c6a1e4a8c7
Bassgeta <kerry.mahne@gmail.com>
2024-06-26 19:07:30 +0200

    feat: fetch liquidation address from the DB

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,11 +1,36 @@
+import { apolloClient } from '~apollo';
+import {
+  GetUserLiquidationAddressesDocument,
+  type GetUserLiquidationAddressesQuery,
+  type GetUserLiquidationAddressesQueryVariables,
+} from '~gql';
 import { type Address } from '~types';
 import { type User } from '~types/graphql.ts';
 
-export const getUserPaymentAddress = (user: User): Address => {
+export const getUserPaymentAddress = async (user: User): Promise<Address> => {
   if (!user.profile?.isAutoOfframpEnabled) {
     return user.walletAddress;
   }
 
   // @TODO fetch user address via the lambda, for now it's a random address
-  return '0x0000000000000000000000000000000000000F21';
+
+  const { data } = await apolloClient.query<
+    GetUserLiquidationAddressesQuery,
+    GetUserLiquidationAddressesQueryVariables
+  >({
+    query: GetUserLiquidationAddressesDocument,
+    variables: {
+      userAddress: user.walletAddress,
+    },
+  });
+
+  const liquidationAddress =
+    data.getLiquidationAddressesByUserAddress?.items[0]?.liquidationAddress;
+
+  if (!liquidationAddress) {
+    console.error('User has autoramp enabled and no liquidation address');
+    return user.walletAddress;
+  }
+
+  return liquidationAddress;
 };
