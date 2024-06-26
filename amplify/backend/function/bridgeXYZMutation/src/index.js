ca29fc587d98472ee1e9fab3fd4c4539133e46b8
Dave Creaser <david.creaser@outlook.com>
2024-06-26 15:09:35 +0100

    Finish kyc links handler

--- amplify/backend/function/bridgeXYZMutation/src/index.js
+++ amplify/backend/function/bridgeXYZMutation/src/index.js
@@ -1,17 +1,41 @@
 const fetch = require('cross-fetch');
+const { graphqlRequest } = require('./utils');
 
-const AWS_SESSION_TOKEN = process.env.AWS_SESSION_TOKEN;
-const ENV = process.env.ENV;
+let apiKey = 'da2-fakeApiId123456';
+let apiUrl = 'http://mocking:3000/bridgexyz';
+let graphqlURL = 'http://localhost:20002/graphql';
 
-const bridgeBasePath = 'http://mocking:3000/bridgexyz';
+/*
+ * @TODO This needs to be imported properly into the project (maybe?)
+ * So that we can always ensure it follows the latest schema
+ * (currently it's just saved statically)
+ */
+const { updateUser } = require('./graphql');
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+
+  if (ENV === 'qa' || ENV === 'prod') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [apiKey, apiUrl, graphqlURL] = await getParams([
+      'bridgeXYZApiKey',
+      'bridgeXYZApiUrl',
+      'graphqlUrl',
+    ]);
+  }
+};
 
 exports.handler = async (event) => {
+  try {
+    await setEnvVariables();
+  } catch (e) {
+    throw new Error('Unable to set environment variables. Reason:', e);
+  }
+
+  const checksummedWalletAddress = event.request.headers['x-wallet-address'];
   const { body, path, pathParams } = event.arguments?.input || {};
 
-  console.log('Here ');
-
   const kycLinkHandler = async () => {
-    console.log('Running kycLinkHandler');
     try {
       const parsedBody = JSON.parse(body);
 
@@ -20,20 +44,49 @@ exports.handler = async (event) => {
         type: 'individual',
       };
 
-      const res = await fetch(`${bridgeBasePath}/${path}`, {
+      const res = await fetch(`${apiUrl}/${path}`, {
         headers: {
+          'Content-Type': 'application/json',
           'Idempotency-Key': 'thisisadifferentkey',
-          'Api-Key': 'thisisakey',
+          'Api-Key': apiKey,
         },
-        body: bodyToSend,
+        body: JSON.stringify(bodyToSend),
         method: 'POST',
       });
 
-      console.log(await res.text());
+      const data = await res.json();
 
       if (!data.customer_id) {
         throw new Error('No customer_id returned');
       } else {
+        console.log('checksummedWalletAddress is: ', checksummedWalletAddress);
+
+        // Add customer_id to the user object
+        const mutation = await graphqlRequest(
+          updateUser,
+          {
+            input: {
+              id: checksummedWalletAddress,
+              bridgeCustomerId: data.customer_id,
+            },
+          },
+          graphqlURL,
+          apiKey,
+        );
+
+        if (mutation.errors || !mutation.data) {
+          const [error] = mutation.errors;
+          throw new Error(
+            error?.message ||
+              `Could not update user with wallet address "${checksummedWalletAddress}"`,
+          );
+        }
+
+        // Return the two urls
+        return {
+          tos_link: data.tos_link,
+          kyc_link: data.kyc_link,
+        };
       }
     } catch (e) {
       console.error(e);
