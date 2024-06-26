a2a265f98fb92f10c58dc8a6ff802cc72c24fa5b
Bassgeta <kerry.mahne@gmail.com>
2024-06-26 17:13:08 +0200

    feat: add fetching of user by liquidation address, realign completed action screens and activity table description

--- src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/PaymentBuilderTable/partials/RecipientField/RecipientField.tsx
+++ src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/PaymentBuilderTable/partials/RecipientField/RecipientField.tsx
@@ -1,16 +1,12 @@
-import clsx from 'clsx';
 import React, { type FC } from 'react';
 
-import { useMemberContext } from '~context/MemberContext/MemberContext.ts';
+import useUserByAddress from '~hooks/useUserByAddress.ts';
 import UserPopover from '~v5/shared/UserPopover/UserPopover.tsx';
 
 import { type RecipientFieldProps } from './types.ts';
 
 const RecipientField: FC<RecipientFieldProps> = ({ address }) => {
-  const { totalMembers, loading } = useMemberContext();
-  const recipientMember = totalMembers.find(
-    (member) => member.contributorAddress === address,
-  );
+  const { user, loading } = useUserByAddress(address, true);
 
   return (
     <>
@@ -18,15 +14,8 @@ const RecipientField: FC<RecipientFieldProps> = ({ address }) => {
         <div className="flex items-center">
           <UserPopover
             size={18}
-            walletAddress={
-              recipientMember ? recipientMember.contributorAddress : address
-            }
-            withVerifiedBadge={!!recipientMember?.isVerified}
-            className={clsx('flex items-center sm:hover:text-blue-400', {
-              'pointer-events-none': loading,
-              'text-warning-400': !recipientMember?.isVerified,
-              'text-gray-900': recipientMember?.isVerified,
-            })}
+            walletAddress={user?.walletAddress ?? address}
+            className="flex items-center sm:hover:text-blue-400"
           />
         </div>
       )}
