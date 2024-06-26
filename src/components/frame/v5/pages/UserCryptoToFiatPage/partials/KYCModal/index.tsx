7720b13c0d1404ef8d3c180fa1f05d32483a5d90
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-06-26 22:09:36 +0100

    Fix: update user account modals
    Fix: update user account modals and state pills for crypto to fiat feature

--- src/components/frame/v5/pages/UserCryptoToFiatPage/partials/KYCModal/index.tsx
+++ src/components/frame/v5/pages/UserCryptoToFiatPage/partials/KYCModal/index.tsx
@@ -1,122 +1,30 @@
-import React, { useEffect, useState, type FC } from 'react';
-import { defineMessages, useIntl } from 'react-intl';
-import { toast } from 'react-toastify';
+import React, { useEffect, type FC } from 'react';
+import { useIntl } from 'react-intl';
 
-import {
-  useCreateKycLinksMutation,
-  useUpdateBridgeCustomerMutation,
-} from '~gql';
-import Toast from '~shared/Extensions/Toast/Toast.tsx';
-import { type CountryData } from '~utils/countries.ts';
-import { formatText } from '~utils/intl.ts';
 import { CloseButton } from '~v5/shared/Button/index.ts';
 import ModalBase from '~v5/shared/Modal/ModalBase.tsx';
 
-import { BankDetailsForm } from '../BankDetailsForm/index.tsx';
-import { ContactDetailsForm } from '../ContactDetailsForm/index.tsx';
-import ModalHeading from '../ModalHeading/ModalHeading.tsx';
-import { PersonalDetailsForm } from '../PersonalDetailsForm/index.tsx';
-import Stepper from '../Stepper/index.tsx';
-
 interface KYCModalProps {
+  url: string;
   isOpened: boolean;
   onClose: () => void;
 }
 
-enum TabId {
-  PersonalDetails = 0,
-  Terms = 1,
-  ContactDetails = 2,
-  BankDetails = 3,
-}
-
 const displayName = 'v5.pages.UserCryptoToFiatPage.partials.KYCModal';
 
-const MSG = defineMessages({
-  tcTitle: {
-    id: `${displayName}.tcTitle`,
-    defaultMessage: 'Terms & Privacy',
-  },
-  tcSubtitle: {
-    id: `${displayName}.tcSubtitle`,
-    defaultMessage:
-      'Accept the terms and privacy of our partner provider Bridge to enable crypto to fiat functionality.',
-  },
-  bankDetailsConfirmed: {
-    id: `${displayName}.bankDetailsConfirmed`,
-    defaultMessage: 'Bank details confirmed',
-  },
-  bankInfoAddeddSuccessfully: {
-    id: `${displayName}.bankInfoAddeddSuccessfully`,
-    defaultMessage: 'Your information has been added sucessfully',
-  },
-});
-
-export const KYCModal: FC<KYCModalProps> = ({ isOpened, onClose }) => {
+export const KYCModal: FC<KYCModalProps> = ({ url, isOpened, onClose }) => {
   const { formatMessage } = useIntl();
-
-  const [activeTab, setActiveTab] = useState<TabId>(TabId.PersonalDetails);
-
-  const [kycFields, setKycFields] = useState<{
-    signedAgreementId: string;
-    birthDate: string;
-    firstName: string;
-    lastName: string;
-    taxIdNumber: string;
-    tosLink: string;
-    email: string;
-    address: {
-      city: string;
-      country: string;
-      postcode: string;
-      state: string;
-      address1: string;
-      address2: string;
-    };
-  }>({
-    email: '',
-    signedAgreementId: '',
-    birthDate: '',
-    firstName: '',
-    lastName: '',
-    taxIdNumber: '',
-    tosLink: '',
-    address: {
-      city: '',
-      country: '',
-      postcode: '',
-      state: '',
-      address1: '',
-      address2: '',
-    },
-  });
-
-  const [selectedCountry, setSelectedCountry] = useState<CountryData | null>(
-    null,
-  );
 
   useEffect(() => {
     const handler = (ev: MessageEvent<{ type: string; message: string }>) => {
-      if (ev?.data && 'signedAgreementId' in ev.data) {
-        const { signedAgreementId } = ev.data;
-
-        setKycFields((state) => ({
-          ...state,
-          signedAgreementId: signedAgreementId as string,
-        }));
-
-        setActiveTab((prev) => prev + 1);
-      }
+      // eslint-disable-next-line no-console
+      console.log(ev.data);
     };
 
     window.addEventListener('message', handler);
 
     return () => window.removeEventListener('message', handler);
   }, []);
-
-  const [createKycLinks] = useCreateKycLinksMutation();
-
-  const [updateBridgeCustomer] = useUpdateBridgeCustomerMutation();
 
   return (
     <ModalBase
@@ -132,181 +40,10 @@ export const KYCModal: FC<KYCModalProps> = ({ isOpened, onClose }) => {
         className="absolute right-4 top-4 text-gray-400 hover:text-gray-600"
       />
       <div className="px-6 py-12">
-        <Stepper
-          activeStepKey={activeTab}
-          items={[
-            {
-              key: TabId.PersonalDetails,
-              heading: {
-                label: 'Personal details',
-              },
-
-              content: (
-                <div>
-                  <PersonalDetailsForm
-                    setSelectedCountry={setSelectedCountry}
-                    onSubmit={async ({
-                      email,
-                      firstName,
-                      lastName,
-                      country,
-                    }) => {
-                      const response = await createKycLinks({
-                        variables: {
-                          email,
-                          fullName: `${firstName} ${lastName}`,
-                        },
-                      });
-
-                      const tosLink =
-                        response.data?.bridgeXYZMutation?.tos_link;
-
-                      if (tosLink) {
-                        setKycFields((state) => ({
-                          ...state,
-                          firstName,
-                          lastName,
-                          tosLink,
-                          email,
-                          address: {
-                            ...state.address,
-                            country,
-                          },
-                        }));
-
-                        setActiveTab(TabId.Terms);
-                      } else {
-                        // Notify the user via a Toast or something
-                      }
-                    }}
-                    onClose={onClose}
-                  />
-                </div>
-              ),
-            },
-            {
-              key: TabId.Terms,
-              heading: {
-                label: 'Terms',
-              },
-              content: (
-                <div>
-                  <ModalHeading title={MSG.tcTitle} subtitle={MSG.tcSubtitle} />
-                  <div className="flex justify-center">
-                    <iframe
-                      title="Terms iframe"
-                      src={kycFields.tosLink}
-                      className="min-h-[20.2rem] min-w-[25rem]"
-                    />
-                  </div>
-                </div>
-              ),
-            },
-            {
-              key: TabId.ContactDetails,
-              heading: {
-                label: 'Contact details',
-              },
-              content: (
-                <ContactDetailsForm
-                  selectedCountry={selectedCountry}
-                  onSubmit={({
-                    tax,
-                    date: birthDate,
-                    address1,
-                    address2,
-                    city,
-                    postcode,
-                  }) => {
-                    setKycFields((state) => ({
-                      ...state,
-                      taxIdNumber: tax,
-                      birthDate,
-                      address: {
-                        ...state.address,
-                        address1,
-                        address2,
-                        city,
-                        postcode,
-                      },
-                    }));
-
-                    setActiveTab(TabId.BankDetails);
-                  }}
-                  onClose={onClose}
-                />
-              ),
-            },
-            {
-              key: TabId.BankDetails,
-              heading: {
-                label: 'Bank details',
-              },
-
-              content: (
-                <BankDetailsForm
-                  onSubmit={async () => {
-                    const {
-                      signedAgreementId,
-                      birthDate,
-                      firstName,
-                      lastName,
-                      taxIdNumber,
-                      email,
-                      address: {
-                        address1,
-                        address2,
-                        city,
-                        country,
-                        postcode,
-                        state,
-                      },
-                    } = kycFields;
-
-                    const result = await updateBridgeCustomer({
-                      variables: {
-                        signedAgreementId,
-                        birthDate,
-                        firstName,
-                        lastName,
-                        taxIdNumber,
-                        email,
-                        address: {
-                          city,
-                          country,
-                          // eslint-disable-next-line camelcase
-                          street_line_1: address1,
-                          // eslint-disable-next-line camelcase
-                          street_line_2: address2,
-                          postcode,
-                          state: state || 'DAY',
-                        },
-                      },
-                    });
-
-                    if (result.data?.bridgeXYZMutation?.success) {
-                      toast.success(
-                        <Toast
-                          type="success"
-                          title={formatText(MSG.bankDetailsConfirmed)}
-                          description={formatText(
-                            MSG.bankInfoAddeddSuccessfully,
-                          )}
-                        />,
-                      );
-
-                      onClose();
-                    } else {
-                      // Show an error toast or something
-                    }
-                  }}
-                  onClose={onClose}
-                />
-              ),
-            },
-          ]}
-        />
+        <iframe title="Getting started" src={url} />
       </div>
     </ModalBase>
   );
 };
+
+KYCModal.displayName = displayName;
--- src/components/frame/v5/pages/UserCryptoToFiatPage/partials/Verification/consts.ts
+++ src/components/frame/v5/pages/UserCryptoToFiatPage/partials/Verification/consts.ts
@@ -1,24 +1,125 @@
-import { ExclamationMark, type Icon } from '@phosphor-icons/react';
-import { defineMessages } from 'react-intl';
+import { WarningCircle, type Icon } from '@phosphor-icons/react';
+import { type MessageDescriptor, defineMessages } from 'react-intl';
 
 const displayName = 'v5.pages.UserCryptoToFiatPage.partials.Verification';
 
+export enum STATUS {
+  NOT_STARTED = 'not_started',
+  INCOMPLETE = 'incomplete',
+  UNDER_REVIEW = 'under_review',
+  APPROVED = 'approved',
+  REJECTED = 'rejected',
+}
+
+// @TODO add keys translation file
 export const STATUS_MSGS = defineMessages({
   // update these keys to match the actual status
-  notStarted: {
+  [STATUS.NOT_STARTED]: {
     id: `${displayName}.headingTitle`,
-    defaultMessage: 'Bank details',
+    defaultMessage: 'Not started',
+  },
+  [STATUS.INCOMPLETE]: {
+    id: `${displayName}.headingTitle`,
+    defaultMessage: 'Incomplete',
+  },
+  [STATUS.UNDER_REVIEW]: {
+    id: `${displayName}.headingTitle`,
+    defaultMessage: 'Under Review',
+  },
+  [STATUS.APPROVED]: {
+    id: `${displayName}.headingTitle`,
+    defaultMessage: 'Approved',
+  },
+  [STATUS.REJECTED]: {
+    id: `${displayName}.headingTitle`,
+    defaultMessage: 'Rejected',
   },
 });
 
 // Add more status pills here
-export const statusPillScheme: Record<
-  keyof typeof STATUS_MSGS,
-  { icon?: Icon; bgClassName: string; textClassName: string }
-> = {
-  notStarted: {
-    icon: ExclamationMark,
-    bgClassName: 'bg-teams-red-50',
-    textClassName: 'text-xs text-teams-red-400',
-  },
+export const getStatusPillScheme = (
+  status?: string | null,
+): {
+  icon?: Icon;
+  iconClassName?: string;
+  bgClassName: string;
+  textClassName: string;
+} => {
+  switch (status) {
+    case STATUS.APPROVED: {
+      return {
+        bgClassName: 'bg-teams-green-100',
+        textClassName: 'text-xs text-teams-green-400',
+      };
+    }
+    case STATUS.INCOMPLETE: {
+      return {
+        bgClassName: 'bg-warning-100',
+        textClassName: 'text-xs text-warning-400',
+      };
+    }
+    case STATUS.UNDER_REVIEW: {
+      return {
+        bgClassName: 'bg-gray-100',
+        textClassName: 'text-xs text-gray-500',
+      };
+    }
+    default: {
+      return {
+        icon: WarningCircle,
+        iconClassName: 'text-teams-red-400',
+        bgClassName: 'bg-teams-red-50',
+        textClassName: 'text-xs text-teams-red-400',
+      };
+    }
+  }
+};
+
+export const getCTAProps = (
+  status?: string | null,
+): { ctaTitle?: MessageDescriptor; ctaDisabled?: boolean } => {
+  switch (status) {
+    case STATUS.REJECTED: {
+      return {
+        ctaTitle: {
+          id: `${displayName}.headingTitle`,
+          defaultMessage: 'Re-submit application',
+        },
+      };
+    }
+    case STATUS.APPROVED: {
+      return {
+        ctaTitle: {
+          id: `${displayName}.headingTitle`,
+          defaultMessage: 'Completed',
+        },
+        ctaDisabled: true,
+      };
+    }
+    case STATUS.INCOMPLETE: {
+      return {
+        ctaTitle: {
+          id: `${displayName}.headingTitle`,
+          defaultMessage: 'Complete application',
+        },
+      };
+    }
+    case STATUS.UNDER_REVIEW: {
+      return {
+        ctaTitle: {
+          id: `${displayName}.headingTitle`,
+          defaultMessage: 'Pending',
+        },
+        ctaDisabled: true,
+      };
+    }
+    default: {
+      return {
+        ctaTitle: {
+          id: `${displayName}.headingTitle`,
+          defaultMessage: 'Start KYC',
+        },
+      };
+    }
+  }
 };
