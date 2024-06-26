eb3bd545d86626b7ea3b2cfa986013932218a3c5
Dave Creaser <david.creaser@outlook.com>
2024-06-26 17:08:28 +0100

    Make check kyc status lambda

--- amplify/backend/function/bridgeXYZQuery/src/index.js
+++ amplify/backend/function/bridgeXYZQuery/src/index.js
@@ -1,7 +1,39 @@
+let appSyncApiKey = 'da2-fakeApiId123456';
+let apiKey = 'da2-fakeApiId123456';
+let apiUrl = 'http://mocking:3000/bridgexyz';
+let graphqlURL = 'http://localhost:20002/graphql';
+
+const setEnvVariables = async () => {
+  const ENV = process.env.ENV;
+
+  if (ENV === 'qa' || ENV === 'prod') {
+    const { getParams } = require('/opt/nodejs/getParams');
+    [appSyncApiKey, apiKey, apiUrl, graphqlURL] = await getParams([
+      'appsyncApiKey',
+      'bridgeXYZApiKey',
+      'bridgeXYZApiUrl',
+      'graphqlUrl',
+    ]);
+  }
+};
+
 exports.handler = async (event) => {
-  const all = event.arguments?.input || {};
+  try {
+    await setEnvVariables();
+  } catch (e) {
+    throw new Error('Unable to set environment variables. Reason:', e);
+  }
 
-  console.log(all);
+  const { path } = event.arguments?.input || {};
 
-  return 'Yay';
+  const handlers = {
+    default: () => {
+      console.log('Running default handler');
+      return null;
+    },
+  };
+
+  const handler = handlers[path] || handlers.default;
+
+  return handler(event, { appSyncApiKey, apiKey, apiUrl, graphqlURL });
 };
