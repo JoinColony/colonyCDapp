498e0df857779686757ae589dbad65c27fdda321
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-07-01 10:33:51 +0200

    Fix: Style to match designs
    Remove right padding from permission pill
    Update reputation styling
    Update team row height from teams section
    Update team section title
    Adjust card max height based on member verification status

--- src/components/v5/common/Pills/PillsBase.tsx
+++ src/components/v5/common/Pills/PillsBase.tsx
@@ -15,36 +15,42 @@ const PillsBase: FC<PropsWithChildren<PillsProps>> = ({
   isCapitalized = true,
   textClassName,
   ...rest
-}) => (
-  <span
-    className={clsx(
-      'inline-flex shrink-0 items-center rounded-3xl px-3 py-1 text-center',
-      className,
-      {
-        capitalize: isCapitalized,
-        'h-[1.625rem] text-3': pillSize === 'medium',
-        'h-[1.5rem] text-4': pillSize === 'small',
-      },
-    )}
-    {...rest}
-  >
-    {Icon && (
-      <span className="flex shrink-0">
-        <Icon
-          size={pillSize === 'medium' ? 14 : 12}
-          className={iconClassName}
-        />
-      </span>
-    )}
+}) => {
+  const content = text || children;
+
+  return (
     <span
-      className={clsx(textClassName, {
-        'ml-1': !!Icon,
-      })}
+      className={clsx(
+        'inline-flex shrink-0 items-center rounded-3xl px-2.5 py-1.5 text-center',
+        className,
+        {
+          capitalize: isCapitalized,
+          'h-[1.625rem] text-3': pillSize === 'medium',
+          'h-[1.5rem] text-4': pillSize === 'small',
+        },
+      )}
+      {...rest}
     >
-      {text || children}
+      {Icon && (
+        <span className="flex shrink-0">
+          <Icon
+            size={pillSize === 'medium' ? 14 : 12}
+            className={iconClassName}
+          />
+        </span>
+      )}
+      {content && (
+        <span
+          className={clsx(textClassName, {
+            'ml-1': !!Icon,
+          })}
+        >
+          {content}
+        </span>
+      )}
     </span>
-  </span>
-);
+  );
+};
 
 PillsBase.displayName = displayName;
 
