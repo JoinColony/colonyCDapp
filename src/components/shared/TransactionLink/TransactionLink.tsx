bc1fdeb0cfcebf475c9236212904d173146b2290
Anastasia <anastasia.nortsova@gmail.com>
2024-07-02 18:11:24 +0200

    fix: remove unused props from TransactionLink component

--- src/components/shared/TransactionLink/TransactionLink.tsx
+++ src/components/shared/TransactionLink/TransactionLink.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { createElement, type FC } from 'react';
+import React, { type FC } from 'react';
 
 import ExternalLink from '~shared/ExternalLink/index.ts';
 import { getBlockExplorerLink } from '~utils/external/index.ts';
@@ -8,24 +8,17 @@ import { type TransactionLinkProps } from './types.ts';
 
 const displayName = 'TransactionLink';
 
-const TransactionLink: FC<TransactionLinkProps> = ({
-  hash,
-  children,
-  className,
-  ...rest
-}) =>
-  createElement(
-    ExternalLink,
-    {
-      href: getBlockExplorerLink({
-        linkType: 'tx',
-        addressOrHash: hash,
-      }),
-      className: clsx(className, 'hover:text-gray-900'),
-      ...rest,
-    },
-    children,
-  );
+const TransactionLink: FC<TransactionLinkProps> = ({ hash, children }) => (
+  <ExternalLink
+    href={getBlockExplorerLink({
+      linkType: 'tx',
+      addressOrHash: hash,
+    })}
+    className={clsx('hover:text-gray-900')}
+  >
+    {children}
+  </ExternalLink>
+);
 
 TransactionLink.displayName = displayName;
 
