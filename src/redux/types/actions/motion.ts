8c0fe9b99abe8ebaf3f1f13d84af94c09a3320eb
Dave Creaser <david.creaser@outlook.com>
2024-07-03 13:03:47 +0100

    Move funds multisig

--- src/redux/types/actions/motion.ts
+++ src/redux/types/actions/motion.ts
@@ -35,6 +35,7 @@ import {
 
 export enum RootMotionMethodNames {
   MintTokens = 'mintTokens',
+  MoveFunds = 'moveFunds',
   Upgrade = 'upgrade',
   UnlockToken = 'unlockToken',
   EditColonyByDelta = 'editColonyByDelta',
@@ -240,6 +241,10 @@ export type MotionActionTypes =
         toDomain: Domain;
         amount: BigNumber;
         annotationMessage?: string;
+        isMultiSig?: boolean;
+        colonyRoles: ColonyRoleFragment[];
+        colonyDomains: Domain[];
+        createdInDomain: Domain;
       },
       MetaWithSetter<object>
     >
