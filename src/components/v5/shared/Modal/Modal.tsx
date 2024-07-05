f795ae6460939af117f53d4624239b0104ef329d
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-07-05 06:31:01 +0200

    Fix: Style changes
    Style changes

--- src/components/v5/shared/Modal/Modal.tsx
+++ src/components/v5/shared/Modal/Modal.tsx
@@ -8,6 +8,8 @@ import Button, { CloseButton } from '~v5/shared/Button/index.ts';
 import ModalBase from './ModalBase.tsx';
 import { type ModalProps } from './types.ts';
 
+import styles from './Modal.module.css';
+
 const displayName = 'v5.Modal';
 
 const Modal: FC<PropsWithChildren<ModalProps>> = ({
@@ -25,6 +27,7 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({
   buttonMode = 'secondarySolid',
   withPadding = true,
   withBorder,
+  withPaddingBottom,
   shouldShowHeader = false,
   isOpen,
   ...props
@@ -42,63 +45,68 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({
       onRequestClose={onClose}
       isOpen={isOpen}
       {...{ isFullOnMobile, ...props }}
-      isHighlighted={withBorder}
-      hasPadding={withPadding}
+      withBorder={withBorder}
+      withPadding={withPadding}
+      withPaddingBottom={withPaddingBottom}
     >
-      <div className="relative mr-[-6px]">
-        {Icon && (
-          <span
+      <div
+        className={`${styles.modalContentWrapper} overflow-x-hidden overflow-y-scroll`}
+      >
+        <div className="relative mr-[-6px]">
+          {Icon && (
+            <span
+              className={clsx(
+                'mb-4 flex h-[2.5rem] w-[2.5rem] flex-shrink-0 items-center justify-center rounded border border-gray-200 shadow-content',
+                {
+                  'border-negative-200 text-negative-400': isWarning,
+                },
+              )}
+            >
+              <Icon size={24} />
+            </span>
+          )}
+          <CloseButton
+            aria-label={formatMessage({ id: 'ariaLabel.closeModal' })}
+            title={formatMessage({ id: 'button.cancel' })}
+            onClick={onClose}
+            className="absolute right-4 top-4 text-gray-400 hover:text-gray-600"
+          />
+          <div
             className={clsx(
-              'mb-4 flex h-[2.5rem] w-[2.5rem] flex-shrink-0 items-center justify-center rounded border border-gray-200 shadow-content',
+              'flex w-full flex-grow flex-col [-webkit-overflow-scrolling:touch]',
               {
-                'border-negative-200 text-negative-400': isWarning,
+                'pb-6 pr-6': withPadding,
               },
             )}
           >
-            <Icon size={24} />
-          </span>
-        )}
-        <CloseButton
-          aria-label={formatMessage({ id: 'ariaLabel.closeModal' })}
-          title={formatMessage({ id: 'button.cancel' })}
-          onClick={onClose}
-          className="absolute right-4 top-4 text-gray-400 hover:text-gray-600"
-        />
-        <div
-          className={clsx(
-            'flex w-full flex-grow flex-col [-webkit-overflow-scrolling:touch]',
-            {
-              'pb-6 pr-6': withPadding,
-            },
-          )}
-        >
-          <div className="flex flex-grow flex-col">
-            {title && <h4 className="mb-2 heading-5">{title}</h4>}
-            {subTitle && <p className="text-md text-gray-600">{subTitle}</p>}
-            {children}
-          </div>
-          {(closeMessage || confirmMessage) && (
-            <div className="mt-8 flex flex-col-reverse gap-3 sm:flex-row">
-              {closeMessage && (
-                <Button mode="primaryOutline" isFullSize onClick={onClose}>
-                  {closeMessage}
-                </Button>
-              )}
-              {confirmMessage && (
-                <Button
-                  mode={buttonMode}
-                  isFullSize
-                  disabled={disabled}
-                  onClick={() => {
-                    onConfirm?.();
-                    onClose();
-                  }}
-                >
-                  {confirmMessage}
-                </Button>
-              )}
+            <div className="flex flex-grow flex-col">
+              {title && <h4 className="mb-2 heading-5">{title}</h4>}
+              {subTitle && <p className="text-md text-gray-600">{subTitle}</p>}
+              {children}
             </div>
-          )}
+            {(closeMessage || confirmMessage) && (
+              <div className="mt-8 flex flex-col-reverse gap-3 sm:flex-row">
+                {closeMessage && (
+                  <Button mode="primaryOutline" isFullSize onClick={onClose}>
+                    {closeMessage}
+                  </Button>
+                )}
+                {confirmMessage && (
+                  <Button
+                    mode={buttonMode}
+                    isFullSize
+                    disabled={disabled}
+                    onClick={() => {
+                      onConfirm?.();
+                      onClose();
+                    }}
+                  >
+                    {confirmMessage}
+                  </Button>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </ModalBase>
