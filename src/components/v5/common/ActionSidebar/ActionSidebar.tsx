077378e09aa5ec72b5e0a50edd2017179cfa6664
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-07-08 18:01:59 +0100

    Fix: Resolve rebase issues - multi-sig pills, threshold progress, rejectedAt

--- src/components/v5/common/ActionSidebar/ActionSidebar.tsx
+++ src/components/v5/common/ActionSidebar/ActionSidebar.tsx
@@ -287,7 +287,7 @@ const ActionSidebar: FC<PropsWithChildren<ActionSidebarProps>> = ({
                   </button>
                   {getShareButton()}
                 </div>
-                {action && !isMotion && !expenditure && (
+                {action && !isMotion && !expenditure && !isMultiSig && (
                   <PillsBase
                     className="bg-success-100 text-success-400"
                     isCapitalized={false}
