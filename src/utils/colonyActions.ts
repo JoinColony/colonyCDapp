321c4a60512ca5c3d84a8277ecd1828683540562
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-07-08 11:08:11 +0200

    Fix: Add UpdateColonyObjectiveMotion for handling objective update motion
    Add UpdateColonyObjectiveMotion for handling objective update motion

--- src/utils/colonyActions.ts
+++ src/utils/colonyActions.ts
@@ -579,7 +579,9 @@ export const getExtendedActionType = (
   const changelogItem = getChangelogItem(actionData, metadata);
 
   if (changelogItem?.hasObjectiveChanged) {
-    return ExtendedColonyActionType.UpdateColonyObjective;
+    return actionData.isMotion
+      ? ExtendedColonyActionType.UpdateColonyObjectiveMotion
+      : ExtendedColonyActionType.UpdateColonyObjective;
   }
 
   if (!isEqual(changelogItem?.newSafes, changelogItem?.oldSafes)) {
