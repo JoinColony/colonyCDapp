f94d24b3210079fa571709eba335236ff3e528fb
Daniel Kronovet <kronovet@gmail.com>
2024-07-10 09:40:46 -0700

    Add hardhat:trace npm command

---
+++ scripts/hardhat-trace.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const { exec } = require('node:child_process');
+
+const argv = process.argv.slice(2)
+
+const hash = argv[0];
+const rpc = 'http://localhost:8545';
+
+const cmd = `docker exec network bash -c \"cd colonyNetwork && npx hardhat trace --rpc ${rpc} --hash ${hash}\"`;
+
+exec(cmd, (error, stdout, stderr) => {
+  if (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
+  if (stderr) {
+    console.error(stderr);
+  }
+});
