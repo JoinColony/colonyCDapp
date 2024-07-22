f96d454f12360dead224e4e26f39628345c1abcd
Bassgeta <kerry.mahne@gmail.com>
2024-07-22 17:35:19 +0200

    feat: flatten requiredRoles array for multisig helpers to reduce code complexity

--- src/utils/multiSig/getEligibleSignees.ts
+++ src/utils/multiSig/getEligibleSignees.ts
@@ -1,14 +1,15 @@
-/* eslint-disable camelcase */
 import { ColonyRole, Id } from '@colony/colony-js';
 
 import { apolloClient } from '~apollo';
 import {
+  type ColonyUserRoleFragment,
   GetMultiSigRolesForDomainDocument,
   type GetMultiSigRolesForDomainQuery,
   type GetMultiSigRolesForDomainQueryVariables,
   type ModelColonyRoleFilterInput,
 } from '~gql';
 import { type EligibleSignee } from '~types/multiSig.ts';
+import { notMaybe } from '~utils/arrays/index.ts';
 
 const COLONY_ROLE_TO_FILTER_KEY = {
   [ColonyRole.Root]: 'role_1',
@@ -21,7 +22,7 @@ const COLONY_ROLE_TO_FILTER_KEY = {
 interface GetEligibleSigneesParams {
   colonyAddress: string;
   domainId: number;
-  requiredRoles: ColonyRole[][];
+  requiredRoles: ColonyRole[];
 }
 
 interface SigneeEntry {
@@ -50,87 +51,81 @@ export async function getEligibleSignees({
   const signeesPerRole: SigneesPerRole = {};
   const uniqueEligibleSignees: SigneeEntry = {};
 
-  // Loop through each array of required roles and fetch permission holders for each domain
-  // It may seem scary, but this array will be longer than 1 just for managing permissions in a subdomain :)
+  const roleFilter = requiredRoles
+    .filter((role) => !!COLONY_ROLE_TO_FILTER_KEY[role])
+    .map((role) => ({ [COLONY_ROLE_TO_FILTER_KEY[role]]: { eq: true } }));
+
+  const queryFilter: ModelColonyRoleFilterInput = {
+    isMultiSig: { eq: true },
+    or: roleFilter,
+  };
+  // amplify doesn't provide it in the UI, but the syntax of or: [{att: {eq: SOMETHING}}, {att2: {eq: SOMETHING@}}] works!
+  // but sadly having 2 or statements is impossible so we need to loop through domains
+
   await Promise.all(
-    requiredRoles.map(async (roleSet) => {
-      // amplify doesn't provide it in the UI, but the syntax of or: [{att: {eq: SOMETHING}}, {att2: {eq: SOMETHING@}}] works!
-      const roleFilter = roleSet
-        .filter((role) => !!COLONY_ROLE_TO_FILTER_KEY[role])
-        .map((role) => ({ [COLONY_ROLE_TO_FILTER_KEY[role]]: { eq: true } }));
+    domainIds.map(async (permissionDomainId) => {
+      const response = await apolloClient.query<
+        GetMultiSigRolesForDomainQuery,
+        GetMultiSigRolesForDomainQueryVariables
+      >({
+        query: GetMultiSigRolesForDomainDocument,
+        variables: {
+          domainId: `${colonyAddress}_${permissionDomainId}`,
+          filter: queryFilter,
+        },
+      });
 
-      const queryFilter: ModelColonyRoleFilterInput = {
-        isMultiSig: { eq: true },
-        or: roleFilter,
-      };
+      const usersWithRoles: ColonyUserRoleFragment[] = (
+        response.data.getRoleByDomainAndColony?.items || []
+      ).filter(notMaybe);
 
-      const matchingUsers = await Promise.all(
-        domainIds.map(async (permissionDomainId) => {
-          const response = await apolloClient.query<
-            GetMultiSigRolesForDomainQuery,
-            GetMultiSigRolesForDomainQueryVariables
-          >({
-            query: GetMultiSigRolesForDomainDocument,
-            variables: {
-              domainId: `${colonyAddress}_${permissionDomainId}`,
-              filter: queryFilter,
-            },
-            fetchPolicy: 'cache-first',
-          });
+      usersWithRoles.forEach((userRoleEntry) => {
+        if (!notMaybe(userRoleEntry.targetUser)) {
+          return;
+        }
 
-          const usersWithRoles = (
-            response.data.getRoleByDomainAndColony?.items ?? []
-          ).filter((item) => {
-            return item !== undefined && item !== null && !!item.targetUser;
-          });
+        const userAddress = userRoleEntry.targetAddress;
 
-          return usersWithRoles;
-        }),
-      );
+        // first we determine all the roles user is holding
+        const userRoles = requiredRoles.filter(
+          (role) => !!userRoleEntry[COLONY_ROLE_TO_FILTER_KEY[role]],
+        );
 
-      // @TODO for some reason flatMap Typescript still assumes it's a 2d array
-      matchingUsers.forEach((matchingUserSet) => {
-        matchingUserSet.forEach((matchingUser) => {
-          if (!matchingUser || !matchingUser.targetUser) {
+        // This means a user has the same permissions in both the parent and the child domain
+        // The main edge case here is: a user has Administration in root and Funding in the child domain, the action is simple payment
+        // in that case we can just concat the array since they will sign for both roles
+        const existingUserEntry = uniqueEligibleSignees[userAddress];
+
+        if (existingUserEntry) {
+          uniqueEligibleSignees[userAddress] = {
+            ...existingUserEntry,
+            userRoles: [
+              ...new Set([...existingUserEntry.userRoles, ...userRoles]),
+            ],
+          };
+        } else {
+          uniqueEligibleSignees[userAddress] = {
+            userAddress,
+            user: userRoleEntry.targetUser,
+            userRoles,
+          };
+        }
+
+        userRoles.forEach((role) => {
+          if (!signeesPerRole[role]) {
+            signeesPerRole[role] = {};
+          }
+
+          // Because it's a nested loop, the upper if statement doesn't infer the type in here
+          if (!notMaybe(userRoleEntry.targetUser)) {
             return;
           }
 
-          const userAddress = matchingUser.targetAddress;
-
-          // first we determine all the roles user is holding
-          const userRoles = roleSet.filter(
-            (role) =>
-              matchingUser !== null &&
-              !!matchingUser[COLONY_ROLE_TO_FILTER_KEY[role]],
-          );
-
-          // extremely unlikely, as this entire loop is going to be ran more than once just for one action type
-          const existingUserEntry = uniqueEligibleSignees[userAddress];
-          // but if the user exists, we just add the role into the array of held roles
-          if (existingUserEntry) {
-            uniqueEligibleSignees[userAddress] = {
-              ...existingUserEntry,
-              userRoles: [...existingUserEntry.userRoles, ...userRoles],
-            };
-          } else {
-            uniqueEligibleSignees[userAddress] = {
-              userAddress,
-              user: matchingUser.targetUser,
-              userRoles,
-            };
-          }
-
-          userRoles.forEach((role) => {
-            if (!signeesPerRole[role]) {
-              signeesPerRole[role] = {};
-            }
-
-            signeesPerRole[role][userAddress] = {
-              userAddress,
-              user: matchingUser.targetUser,
-              userRoles,
-            };
-          });
+          signeesPerRole[role][userAddress] = {
+            userAddress,
+            user: userRoleEntry.targetUser,
+            userRoles,
+          };
         });
       });
     }),
