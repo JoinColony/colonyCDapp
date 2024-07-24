3764a66dc5a886eff83461a291e9c4073901fa18
Joanna <joanna.dyszkiewicz@pagepro.pl>
2024-07-24 11:05:44 +0200

    feat: datepicker with time

--- src/components/v5/common/Fields/datepickers/DatepickerWithTime/DatepickerWithTime.tsx
+++ src/components/v5/common/Fields/datepickers/DatepickerWithTime/DatepickerWithTime.tsx
@@ -8,36 +8,48 @@ import { formatText } from '~utils/intl.ts';
 import Button from '~v5/shared/Button/index.ts';
 import { type ButtonMode } from '~v5/shared/Button/types.ts';
 
-import { DATEPICKER_PORTAL_ID, DEFAULT_DATE_FORMAT } from '../common/consts.ts';
+import {
+  DATEPICKER_PORTAL_ID,
+  DEFAULT_DATE_FORMAT,
+  DEFAULT_DATE_TIME_FORMAT,
+} from '../common/consts.ts';
 import DatepickerContainer from '../common/DatepickerContainer/index.ts';
 
-import DatepickerCustomHeader from './partials/DatepickerCustomHeader/index.ts';
-import DatepickerCustomInput from './partials/DatepickerCustomInput/index.ts';
-import { type DatepickerProps } from './types.ts';
+import DatepickerCustomHeader from './partials/DatepickerCustomHeader/DatepickerCustomHeader.tsx';
+import DatepickerCustomInput from './partials/DatepickerCustomInput/DatepickerCustomInput.tsx';
+import DatepickerTimePicker from './partials/DatepickerTimePicker/DatepickerTimePicker.tsx';
+import { type DatepickerWithTimeProps } from './types.ts';
 
 import styles from '../common/Datepicker.module.css';
 
-const Datepicker: FC<DatepickerProps> = ({
+const DatepickerWithTime: FC<DatepickerWithTimeProps> = ({
   cancelButtonProps,
   applyButtonProps,
   popperModifiers,
   onChange,
   selected: selectedProp,
-  dateFormat = DEFAULT_DATE_FORMAT,
+  dateFormat = DEFAULT_DATE_TIME_FORMAT,
   popperClassName,
   minYear,
   maxYear,
+  customInput,
+  inline,
+  withCloseButton,
+  onClose,
   ...rest
 }) => {
   const isMobile = useMobile();
   const calendarRef = useRef<DatePicker>(null);
-
   const [selectedDate, setSelectedDate] = useState<Date | null>(
+    selectedProp || null,
+  );
+  const [selectedTime, setSelectedTime] = useState<Date | null>(
     selectedProp || null,
   );
 
   const resetValues = () => {
     setSelectedDate(selectedProp || null);
+    setSelectedTime(selectedProp || null);
   };
 
   const {
@@ -54,17 +66,18 @@ const Datepicker: FC<DatepickerProps> = ({
   return (
     <DatePicker
       portalId={isMobile ? DATEPICKER_PORTAL_ID : undefined}
-      calendarContainer={DatepickerContainer}
+      calendarContainer={inline ? undefined : DatepickerContainer}
       calendarClassName={clsx(styles.wrapper, styles['wrapper--simple'])}
       renderCustomHeader={(props) => (
         <DatepickerCustomHeader
           startDate={selectedDate}
-          dateFormat={dateFormat}
+          dateFormat={DEFAULT_DATE_FORMAT}
           setStartDate={setSelectedDate}
           minYear={minYear}
           maxYear={maxYear}
+          inline={inline}
           onClose={
-            isMobile
+            isMobile && withCloseButton
               ? () => {
                   resetValues();
                   calendarRef.current?.setOpen(false);
@@ -75,7 +88,7 @@ const Datepicker: FC<DatepickerProps> = ({
         />
       )}
       ref={calendarRef}
-      customInput={<DatepickerCustomInput />}
+      customInput={customInput || <DatepickerCustomInput />}
       dateFormat={dateFormat}
       popperClassName={clsx(popperClassName, '!z-top max-w-[20.5rem]')}
       renderDayContents={(day) => (
@@ -92,12 +105,68 @@ const Datepicker: FC<DatepickerProps> = ({
         },
       ]}
       selectsRange={false}
-      onChange={(date) => setSelectedDate(date)}
-      selected={selectedProp}
-      onClickOutside={resetValues}
+      onChange={(date, event) => {
+        date?.setHours(
+          selectedTime?.getHours() || 0,
+          selectedTime?.getMinutes() || 0,
+          0,
+          0,
+        );
+        setSelectedDate(date);
+
+        if (isMobile) {
+          return;
+        }
+
+        onChange(date, event);
+      }}
+      selected={selectedDate}
+      showTimeInput
+      onBlur={(event) => {
+        if (isMobile || !selectedDate) {
+          return;
+        }
+
+        onChange(selectedDate, event);
+        onClose?.();
+      }}
+      customTimeInput={
+        <div className="react-datepicker-ignore-onclickoutside relative">
+          <DatepickerTimePicker
+            onChange={(date, event) => {
+              const hours = date?.getHours() || 0;
+              const minutes = date?.getMinutes() || 0;
+
+              selectedDate?.setHours(hours, minutes, 0, 0);
+
+              setSelectedTime(selectedDate ?? date);
+              setSelectedDate(selectedDate ?? date);
+
+              if (event) {
+                return;
+              }
+
+              if (!isMobile) {
+                onChange(selectedDate ?? date, event);
+                onClose?.();
+              }
+            }}
+            onBlur={(event) => {
+              if (isMobile) {
+                return;
+              }
+
+              onChange(selectedDate, event);
+              onClose?.();
+            }}
+            selected={selectedTime}
+          />
+        </div>
+      }
+      inline={inline}
       {...rest}
     >
-      {selectedDate && (
+      {selectedDate && isMobile && (
         <div className="flex w-full items-center justify-between gap-2 border-t border-t-gray-200 p-4">
           <Button
             {...cancelButtonProps}
@@ -106,6 +175,7 @@ const Datepicker: FC<DatepickerProps> = ({
               resetValues();
               calendarRef.current?.setOpen(false);
               cancelButtonOnClick?.(event);
+              onClose?.();
             }}
             text={cancelButtonText || formatText({ id: 'button.cancel' })}
             className="flex-grow"
@@ -117,6 +187,7 @@ const Datepicker: FC<DatepickerProps> = ({
               onChange(selectedDate, undefined);
               calendarRef.current?.setOpen(false);
               applyButtonOnClick?.(event);
+              onClose?.();
             }}
             text={applyButtonText || formatText({ id: 'button.apply' })}
             className="flex-grow"
@@ -127,4 +198,4 @@ const Datepicker: FC<DatepickerProps> = ({
   );
 };
 
-export default Datepicker;
+export default DatepickerWithTime;
