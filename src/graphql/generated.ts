f70609eb5ae5d9ebe0853b7684e46451ac38df3d
Anastasia <anastasia.nortsova@gmail.com>
2024-07-25 13:18:55 +0200

    fix: missing translation for UserHub

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -10115,6 +10115,14 @@ export const ExpenditureFragmentDoc = gql`
       ...ExpenditureStage
     }
   }
+  actions(filter: {type: {eq: CREATE_EXPENDITURE}}) {
+    items {
+      metadata {
+        customTitle
+      }
+      transactionHash: id
+    }
+  }
   finalizedAt
   createdAt
   motions {
