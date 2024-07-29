1f7793e536ca8400b757d632922cf86a350c9b61
Bassgeta <kerry.mahne@gmail.com>
2024-07-29 14:51:33 +0200

    refactor: render multisig members error banner as a separate component, sync loading state with submit button

---
+++ src/components/v5/common/ActionSidebar/partials/ActionSidebarContent/partials/MultiSigMembersError/useHasEnoughMembersWithPermissions.ts
@@ -0,0 +1,65 @@
+import { useMemo } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { type Action } from '~constants/actions.ts';
+import { useDomainThreshold } from '~hooks/multiSig/useDomainThreshold.ts';
+import { useEligibleSignees } from '~hooks/multiSig/useEligibleSignees.ts';
+
+import { getPermissionsNeededForAction } from '../../../../hooks/permissions/helpers.ts';
+
+interface UseHasEnoughMembersWithPermissionsResult {
+  hasEnoughMembersWithPermissions: boolean;
+  isLoading: boolean;
+}
+
+export const useHasEnoughMembersWithPermissions = ({
+  selectedAction,
+  createdIn,
+}: {
+  selectedAction: Action;
+  createdIn: number;
+}): UseHasEnoughMembersWithPermissionsResult => {
+  const { watch } = useFormContext();
+  const formValues = watch();
+
+  const requiredRoles = useMemo(
+    () => getPermissionsNeededForAction(selectedAction, formValues) || [],
+    [selectedAction, formValues],
+  );
+
+  /*
+   * This may seem like a hack, but for display purposes, we always fetch all possible roles
+   * The only action where this can break is managing permissions in a subdomain via permissions, not via multisig, so we are good
+   */
+  const multiSigRoles = requiredRoles.flat();
+
+  const { thresholdPerRole, isLoading: isDomainThresholdLoading } =
+    useDomainThreshold({
+      domainId: createdIn,
+      requiredRoles: multiSigRoles,
+    });
+
+  const { countPerRole, isLoading: areEligibleSigneesLoading } =
+    useEligibleSignees({
+      domainId: createdIn,
+      requiredRoles: multiSigRoles,
+    });
+
+  const isLoading = isDomainThresholdLoading || areEligibleSigneesLoading;
+
+  if (!thresholdPerRole || !requiredRoles) {
+    return {
+      hasEnoughMembersWithPermissions: true,
+      isLoading,
+    };
+  }
+
+  const hasEnoughMembersWithPermissions = requiredRoles.some((roles) =>
+    roles.every((role) => countPerRole[role] >= thresholdPerRole[role]),
+  );
+
+  return {
+    hasEnoughMembersWithPermissions,
+    isLoading,
+  };
+};
