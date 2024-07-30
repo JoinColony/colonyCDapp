eb99caa8e036f8a71725f0b30ea0cf17649305e8
Anastasia <anastasia.nortsova@gmail.com>
2024-07-30 15:23:25 +0200

    fix: rename actions with creatingActions

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -10115,7 +10115,7 @@ export const ExpenditureFragmentDoc = gql`
       ...ExpenditureStage
     }
   }
-  actions(filter: {type: {eq: CREATE_EXPENDITURE}}) {
+  creatingActions: actions(filter: {type: {eq: CREATE_EXPENDITURE}}) {
     items {
       metadata {
         customTitle
