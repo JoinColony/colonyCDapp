d5fd2de5b838d1109cc8387b27a7e9c51874e78e
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-08-09 16:25:28 +0200

    Fix: disable MultiSigPageSetup form inputs and hide button if user doesn't have ColonyRole.Root role
    Fix: disable MultiSigPageSetup form inputs and hide button if user doesn't have ColonyRole.Root role

--- src/components/common/Extensions/Fields/InputGroup/InputGroup.tsx
+++ src/components/common/Extensions/Fields/InputGroup/InputGroup.tsx
@@ -15,6 +15,7 @@ interface InputGroupProps {
   step?: number;
   type?: string;
   isError?: boolean;
+  isDisabled?: boolean;
   errorMessage?: string;
   appendMessage?: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
@@ -26,6 +27,7 @@ export const InputGroup = forwardRef<HTMLInputElement, InputGroupProps>(
     {
       className,
       isError,
+      isDisabled,
       errorMessage,
       appendMessage,
       name,
@@ -46,7 +48,9 @@ export const InputGroup = forwardRef<HTMLInputElement, InputGroupProps>(
           className={clsx(
             `mt-2 w-fit divide-x rounded border border-gray-300 text-md text-gray-600 focus-within:border-blue-400`,
             {
-              'divide-negative-400 !border-negative-400': isError,
+              'divide-negative-400 !border-negative-400':
+                isError && !isDisabled,
+              'pointer-events-none': isDisabled,
             },
           )}
         >
@@ -59,15 +63,20 @@ export const InputGroup = forwardRef<HTMLInputElement, InputGroupProps>(
             step={step}
             placeholder={placeholder}
             type={type}
+            disabled={isDisabled}
             onChange={onChange}
             onKeyDown={onKeyDown}
-            className="w-16 bg-transparent py-3 pl-[14px] pr-3 outline-0 placeholder:text-gray-400"
+            className="w-16 bg-transparent py-3 pl-[14px] pr-3 outline-0 placeholder:text-gray-400 disabled:text-gray-300"
           />
-          <span className="inline-block h-full py-3 pl-[14px] pr-3">
+          <span
+            className={clsx('inline-block h-full py-3 pl-[14px] pr-3', {
+              'text-gray-300': isDisabled,
+            })}
+          >
             {appendMessage}
           </span>
         </div>
-        {isError && (
+        {isError && !isDisabled && (
           <p className="mt-1 text-xs text-negative-400">{errorMessage}</p>
         )}
       </div>
