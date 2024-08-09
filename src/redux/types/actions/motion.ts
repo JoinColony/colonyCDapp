cdda44ce2a6a5ea3975140f186953032a8782f49
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-08-09 10:17:23 +0100

    Feat: Implement manage tokens via multi-sig

--- src/redux/types/actions/motion.ts
+++ src/redux/types/actions/motion.ts
@@ -466,6 +466,9 @@ export type MotionActionTypes =
         tokenAddresses: Address[];
         customActionTitle: string;
         annotationMessage?: string;
+        isMultiSig: boolean;
+        colonyRoles: ColonyRoleFragment[];
+        colonyDomains: Domain[];
       },
       MetaWithSetter<object>
     >
