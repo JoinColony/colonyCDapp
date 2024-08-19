dd7ab9e760b77f88b4db623a2c05c4de62722de9
Dave Creaser <david.creaser@outlook.com>
2024-08-19 16:28:50 +0100

    Disable multi sig extension install when colony version is too low

--- src/types/extensions.ts
+++ src/types/extensions.ts
@@ -34,6 +34,7 @@ export interface ExtensionConfig {
   descriptionShort: MessageDescriptor;
   descriptionLong: MessageDescriptor;
   neededColonyPermissions: ColonyRole[];
+  neededColonyVersion?: number;
   initializationParams?: ExtensionInitParam[];
   uninstallable: boolean;
   createdAt: number;
