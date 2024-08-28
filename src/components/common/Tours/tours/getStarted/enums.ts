1151503c5a10752701e19277c9a1470e8056bcf2
Arren V <33682027+arrenv@users.noreply.github.com>
2024-08-28 22:49:39 +0200

    Setup Joyride and first tours

---
+++ src/components/common/Tours/tours/getStarted/enums.ts
@@ -0,0 +1,11 @@
+export enum GetStartedTourTargets {
+  WelcomeButton = 'getStartedWelcome',
+  EnterTitleField = 'getStartedEnterTitle',
+  ChooseActionButton = 'getStartedChooseAction',
+  SelectActionButton = 'getStartedSelectAction',
+  CustomizeColonyFields = 'getStartedCustomizeColony',
+  SelectLogoField = 'getStartedSelectLogo',
+  EnterDescriptionField = 'getStartedEnterDescription',
+  SelectDecisionMethod = 'getStartedSelectDecisionMethod',
+  ConfirmActionButton = 'getStartedConfirmAction',
+}
