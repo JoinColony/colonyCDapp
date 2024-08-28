63dc1eb422a1f7ad8b36733811a352f49d6b46c2
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-08-28 08:27:59 +0200

    fix: finalize step copy, motion box loader

--- src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/MotionBox/MotionBox.tsx
+++ src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/MotionBox/MotionBox.tsx
@@ -127,7 +127,21 @@ const MotionBox: FC<MotionBoxProps> = ({ transactionId }) => {
         <React.Fragment key={key}>{isVisible && content}</React.Fragment>
       ))}
     </MotionProvider>
-  ) : null;
+  ) : (
+    <div className="h-[11.25rem] w-full overflow-hidden rounded-lg border border-gray-200">
+      <div className="w-full border-b border-gray-200 bg-gray-50 p-[1.125rem] py-3">
+        <div className="h-3 w-3/4 skeleton" />
+      </div>
+      <div>
+        <div className="gap-4 p-[1.125rem]">
+          <div className="mb-4 h-3 w-4/5 skeleton" />
+          <div className="mb-4 h-3 w-3/4 skeleton" />
+          <div className="mb-4 h-3 w-3/4 skeleton" />
+          <div className="h-3 w-3/4 skeleton" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MotionBox;
