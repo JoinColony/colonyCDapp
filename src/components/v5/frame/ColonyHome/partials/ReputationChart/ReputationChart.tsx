cf0021a78588a9cb9b5031cbef3de8831705cac8
Bassgeta <kerry.mahne@gmail.com>
2024-08-29 13:37:12 +0200

    feat: implement new team reputation chart

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/ReputationChart.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/ReputationChart.tsx
@@ -1,149 +1,87 @@
-import { Id } from '@colony/colony-js';
-import clsx from 'clsx';
-import React, { useState } from 'react';
+import { type PieTooltipProps, ResponsivePie } from '@nivo/pie';
+import React, { type FC } from 'react';
 import { defineMessages } from 'react-intl';
 
-import { Action } from '~constants/actions.ts';
-import { useActionSidebarContext } from '~context/ActionSidebarContext/ActionSidebarContext.ts';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import Numeral from '~shared/Numeral/Numeral.tsx';
 import { notNull } from '~utils/arrays/index.ts';
 import { formatText } from '~utils/intl.ts';
-import { ACTION_TYPE_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
-import TeamReputationSummaryRow from '~v5/common/TeamReputationSummary/partials/TeamReputationSummaryRow.tsx';
-import WidgetBox from '~v5/common/WidgetBox/index.ts';
-import EmptyWidgetState from '~v5/common/WidgetBox/partials/EmptyWidgetState.tsx';
-import DonutChart from '~v5/shared/DonutChart/index.ts';
-import { type ChartData } from '~v5/shared/DonutChart/types.ts';
 
-import { summaryLegendColor } from './consts.ts';
-import { getTeamReputationChartData } from './utils.ts';
+import { pieChartConfig } from './consts.ts';
+import Legend from './partials/Legend.tsx';
+import LegendItem from './partials/LegendItem.tsx';
+import NoTeamsWithReputation from './partials/NoTeamsWithReputation.tsx';
+import { type ReputationChartDataItem } from './types.ts';
+import {
+  getTeamReputationChartData,
+  isThereReputationInDomains,
+} from './utils.ts';
 
 const displayName = 'v5.frame.ColonyHome.ReputationChart';
 
 const MSG = defineMessages({
-  legendNoTeams: {
-    id: `${displayName}.legendNoTeams`,
-    defaultMessage: 'There are no teams with reputation in the Colony',
-  },
-  legendNoTeamsAction: {
-    id: `${displayName}.legendNoTeamsAction`,
-    defaultMessage: 'Create a new action',
+  reputationTitle: {
+    id: `${displayName}.influenceTitle`,
+    defaultMessage: 'Influence by team',
   },
 });
+
+const TemporaryTooltip: FC<PieTooltipProps<ReputationChartDataItem>> = ({
+  datum: { label, value },
+}) => {
+  return (
+    <div className="bg-base-black p-2 text-base-white">
+      {label}
+      (<Numeral value={value.toFixed(1)} />
+      %)
+    </div>
+  );
+};
 
 const ReputationChart = () => {
   const {
     colony: { domains },
   } = useColonyContext();
 
-  const [hoveredSegment, setHoveredSegment] = useState<
-    ChartData | undefined | null
-  >();
-
-  const {
-    actionSidebarToggle: [, { toggleOn: toggleActionSidebarOn }],
-  } = useActionSidebarContext();
-
-  const updateHoveredSegment = (segmentData: ChartData | null | undefined) => {
-    setHoveredSegment(segmentData);
-  };
-
-  const openCreateNewTeam = () => {
-    toggleActionSidebarOn({
-      [ACTION_TYPE_FIELD_NAME]: Action.CreateNewTeam,
-    });
-  };
-
   const allTeams = (domains?.items || [])
     .filter(notNull)
-    .filter(({ nativeId }) => nativeId !== Id.RootDomain)
     .sort(
       (a, b) => Number(b.reputationPercentage) - Number(a.reputationPercentage),
     );
-
   const chartData = getTeamReputationChartData(allTeams);
 
-  const getChartLegend = () => {
-    const doesReputationExist = chartData[0] ? chartData[0].value > 0 : false;
-
-    if (chartData.length === 0) {
-      return (
-        <EmptyWidgetState
-          title={formatText({
-            id: 'dashboard.team.widget.noData',
-          })}
-          actionTitle={formatText({
-            id: 'dashboard.team.widget.createTeam',
-          })}
-          className="h-full p-2"
-          onClick={openCreateNewTeam}
-        />
-      );
-    }
-
-    if (!doesReputationExist) {
-      return (
-        <EmptyWidgetState
-          title={<p className="text-center">{formatText(MSG.legendNoTeams)}</p>}
-          actionTitle={formatText(MSG.legendNoTeamsAction)}
-          className="px-[1.8rem] py-[2.3rem]"
-          onClick={() => {
-            toggleActionSidebarOn();
-          }}
-        />
-      );
+  const getChartAndLegend = () => {
+    if (chartData.length === 0 || !isThereReputationInDomains(allTeams)) {
+      return <NoTeamsWithReputation />;
     }
 
     return (
-      <div className="w-full">
-        <ul className="flex flex-col justify-center gap-2">
-          {chartData.map(({ id, label, color, value }) => (
-            <li
-              key={id}
-              className={clsx('flex items-center text-sm', {
-                'font-semibold transition-all': hoveredSegment?.id === id,
-              })}
-            >
-              <TeamReputationSummaryRow
-                color={summaryLegendColor[color] || summaryLegendColor.default}
-                name={label}
-                totalReputation={value.toString()}
-              />
-            </li>
+      <>
+        <div className="mb-5 mt-3 flex h-[140px] w-full flex-shrink-0 items-center justify-center">
+          <ResponsivePie
+            {...pieChartConfig}
+            data={chartData}
+            tooltip={TemporaryTooltip}
+          />
+        </div>
+        <Legend>
+          {chartData.map((chartItem) => (
+            <LegendItem key={chartItem.id} chartItem={chartItem} />
           ))}
-        </ul>
-      </div>
+        </Legend>
+      </>
     );
   };
 
   return (
-    <WidgetBox
-      value={
-        <div className="grid w-full grid-cols-[36%_1fr] justify-items-center gap-6">
-          <div className="relative flex w-full max-w-[9.375rem] flex-shrink-0 items-center justify-center">
-            <DonutChart
-              data={chartData || []}
-              hoveredSegment={hoveredSegment}
-              updateHoveredSegment={updateHoveredSegment}
-            />
-          </div>
-          <div className="flex w-full flex-col">
-            <div>
-              <p className="uppercase text-gray-400 text-4">
-                {formatText({ id: 'dashboard.team.widget.title' })}
-              </p>
-              <h3 className="mb-3 text-2">
-                {formatText({ id: 'dashboard.team.widget.subtitle' })}
-              </h3>
-            </div>
-            {getChartLegend()}
-          </div>
-        </div>
-      }
-      className="flex-col bg-base-white p-6"
-      // @BETA: Disabled for now
-      // href={allTeams?.length ? COLONY_TEAMS_ROUTE : undefined}
-    />
+    <div className="flex w-full flex-col items-start rounded-lg border px-5 py-6">
+      <div className="flex w-full justify-between">
+        <h5 className="text-gray-900 heading-5">
+          {formatText(MSG.reputationTitle)}
+        </h5>
+      </div>
+      {getChartAndLegend()}
+    </div>
   );
 };
 
