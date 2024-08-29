1bebd9b362c27388150379d69e2f603deb076068
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-08-29 14:25:28 +0100

    Feat: Add members and followers to colony header

--- src/components/v5/frame/ColonyHome/ColonyHome.tsx
+++ src/components/v5/frame/ColonyHome/ColonyHome.tsx
@@ -21,7 +21,6 @@ import Agreements from './partials/Agreements/index.ts';
 import DashboardHeader from './partials/DashboardHeader/index.ts';
 import FundsCards from './partials/FundsCards/index.ts';
 import LeaveColonyModal from './partials/LeaveColonyModal/index.ts';
-import Members from './partials/Members/index.ts';
 import ReputationChart from './partials/ReputationChart/index.ts';
 import TokenBalance from './partials/TokenBalance/index.ts';
 import TotalActions from './partials/TotalActions/index.ts';
@@ -45,7 +44,6 @@ const ColonyHome = () => {
         <DashboardHeader />
         <div className="flex w-full flex-col items-center gap-[1.125rem] sm:flex-row">
           <TotalActions />
-          <Members />
           <TokenBalance />
         </div>
       </div>
