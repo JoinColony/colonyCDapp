bd8eeb020b85095d275f0cd7c71e2bf04c570db5
Joanna <joanna.dyszkiewicz@pagepro.pl>
2024-09-03 14:13:23 +0200

    feat: user hub updated with streams

--- src/components/common/Extensions/UserHub/partials/BalanceTab/BalanceTab.tsx
+++ src/components/common/Extensions/UserHub/partials/BalanceTab/BalanceTab.tsx
@@ -1,52 +1,53 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
+import React, { type FC } from 'react';
+import { defineMessages } from 'react-intl';
 
 import { useAppContext } from '~context/AppContext/AppContext.ts';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import { formatText } from '~utils/intl.ts';
 
-import Balance from './partials/Balance.tsx';
-import PendingReputation from './partials/PendingReputation/index.ts';
-import TotalReputation from './partials/TotalReputation.tsx';
+import BalanceInfoRow from './partials/BalanceInfoRow/BalanceInfoRow.tsx';
+import StreamsInfoRow from './partials/StreamsInfoRow/StreamsInfoRow.tsx';
 import { type BalanceTabProps } from './types.ts';
 
 const displayName = 'common.Extensions.UserHub.partials.BalanceTab';
 
-const BalanceTab = ({ onTabChange }: BalanceTabProps) => {
-  const { formatMessage } = useIntl();
-  const {
-    colony: { colonyAddress, nativeToken },
-  } = useColonyContext();
+const MSG = defineMessages({
+  title: {
+    id: `${displayName}.title`,
+    defaultMessage: 'Your overview in {colonyName}',
+  },
+  titleColonyOverview: {
+    id: `${displayName}.titleColonyOverview`,
+    defaultMessage: 'Your overview',
+  },
+});
+
+const BalanceTab: FC<BalanceTabProps> = ({ onTabChange }) => {
+  const { colony } = useColonyContext();
+  const { metadata, nativeToken } = colony;
+  const { displayName: colonyDisplayName = '' } = metadata || {};
+
   const { wallet } = useAppContext();
 
   if (!wallet) {
     return null;
   }
 
-  // @TODO: handle empty state <EmptyContent />
   return (
     <div className="p-6">
-      <p className="mb-6 heading-5 md:mb-4">
-        {formatMessage({ id: 'userHub.reputation' })}
-      </p>
-      <Balance
+      <h5 className="mb-6 heading-5 sm:mb-4">
+        {formatText(MSG.title, { colonyName: colonyDisplayName })}
+      </h5>
+      <BalanceInfoRow
         nativeToken={nativeToken}
         wallet={wallet}
         onTabChange={onTabChange}
+        className="mb-6 border-b border-gray-200 pb-6"
       />
-      <TotalReputation
-        colonyAddress={colonyAddress}
-        wallet={wallet}
-        nativeToken={nativeToken}
-      />
-      <PendingReputation
-        colonyAddress={colonyAddress}
-        wallet={wallet}
-        nativeToken={nativeToken}
-      />
+      <StreamsInfoRow />
     </div>
   );
 };
 
 BalanceTab.displayName = displayName;
-
 export default BalanceTab;
---
+++ src/components/common/Extensions/UserHub/partials/BalanceTab/partials/StreamsInfoRow/hooks.ts
@@ -0,0 +1,129 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { useAppContext } from '~context/AppContext/AppContext.ts';
+import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import { useCurrencyContext } from '~context/CurrencyContext/CurrencyContext.ts';
+import { useGetStreamingPaymentsByColonyQuery } from '~gql';
+import useCurrentBlockTime from '~hooks/useCurrentBlockTime.ts';
+import { notNull } from '~utils/arrays/index.ts';
+
+import { type StreamingPaymentItems } from './types.ts';
+import { calculateTotalsFromStreams } from './utils.ts';
+
+export const useStreamingPaymentsTotalFunds = () => {
+  const { user } = useAppContext();
+  const { walletAddress } = user ?? {};
+  const { currentBlockTime: blockTime } = useCurrentBlockTime();
+  const { colony } = useColonyContext();
+
+  const { data, loading, fetchMore } = useGetStreamingPaymentsByColonyQuery({
+    variables: {
+      recipientAddress: walletAddress ?? '',
+      colonyId: colony.colonyAddress,
+    },
+    onCompleted: (receivedData) => {
+      if (receivedData?.getStreamingPaymentsByColony?.nextToken) {
+        fetchMore({
+          variables: {
+            nextToken: receivedData.getStreamingPaymentsByColony.nextToken,
+          },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            if (!fetchMoreResult) return prev;
+
+            return {
+              ...prev,
+              getStreamingPaymentsByColony: {
+                ...prev.getStreamingPaymentsByColony,
+                items: [
+                  ...(prev?.getStreamingPaymentsByColony?.items || []),
+                  ...(fetchMoreResult?.getStreamingPaymentsByColony?.items ||
+                    []),
+                ],
+                nextToken:
+                  fetchMoreResult?.getStreamingPaymentsByColony?.nextToken,
+              },
+            };
+          },
+        });
+      }
+    },
+  });
+
+  const streamingPayments = useMemo(
+    () => data?.getStreamingPaymentsByColony?.items?.filter(notNull) || [],
+    [data?.getStreamingPaymentsByColony?.items],
+  );
+
+  const { currency } = useCurrencyContext();
+
+  const [totalFunds, setTotalFunds] = useState<{
+    totalAvailable: number;
+    totalClaimed: number;
+  }>({
+    totalAvailable: 0,
+    totalClaimed: 0,
+  });
+  const [isAnyPaymentActive, setIsAnyPaymentActive] = useState(false);
+  const [ratePerSecond, setRatePerSecond] = useState<number>(0);
+
+  const getTotalFunds = useCallback(
+    async (items: StreamingPaymentItems) => {
+      const {
+        totalAvailable,
+        totalClaimed,
+        isAtLeastOnePaymentActive,
+        ratePerSecond: ratePerSecondValue,
+      } = await calculateTotalsFromStreams({
+        streamingPayments: items,
+        currentTimestamp: Math.floor(blockTime ?? Date.now() / 1000),
+        currency,
+        colony,
+      });
+
+      setIsAnyPaymentActive(isAtLeastOnePaymentActive);
+      setTotalFunds({ totalAvailable, totalClaimed });
+      setRatePerSecond(ratePerSecondValue);
+    },
+    [blockTime, colony, currency],
+  );
+
+  useEffect(() => {
+    fetchMore({
+      variables: {
+        recipientAddress: walletAddress ?? '',
+        colonyId: colony.colonyAddress,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+
+        return {
+          ...prev,
+          getStreamingPaymentsByColony: {
+            ...prev.getStreamingPaymentsByColony,
+            items: [
+              ...(prev?.getStreamingPaymentsByColony?.items || []),
+              ...(fetchMoreResult?.getStreamingPaymentsByColony?.items || []),
+            ],
+            nextToken: fetchMoreResult?.getStreamingPaymentsByColony?.nextToken,
+          },
+        };
+      },
+    });
+  }, [colony.colonyAddress, fetchMore, walletAddress]);
+
+  useEffect(() => {
+    if (streamingPayments.length) {
+      getTotalFunds(streamingPayments);
+    }
+  }, [getTotalFunds, streamingPayments]);
+
+  return {
+    totalFunds,
+    isAnyPaymentActive,
+    ratePerSecond,
+    loading,
+    currency,
+    getTotalFunds,
+    streamingPayments,
+  };
+};
