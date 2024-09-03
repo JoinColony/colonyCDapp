570a9fd5119dc52c69773ea9f4cc6f79d9739899
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-03 18:59:28 +0200

    Feat: add payments and income sections to TotalInOutBalance card
    Feat: add payments and income sections to TotalInOutBalance card

---
+++ src/components/v5/frame/ColonyHome/partials/TotalInOutBalance/partials/IncomeSection.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
+import useToggle from '~hooks/useToggle/index.ts';
+import { formatText } from '~utils/intl.ts';
+import { AddFundsModal } from '~v5/common/Modals/AddFundsModal/AddFundsModal.tsx';
+import Button from '~v5/shared/Button/Button.tsx';
+
+import { MSG } from '../consts.ts';
+import { useLast30DaysData } from '../hooks.ts';
+
+import { ClaimFundsButton } from './ClaimFundsButton.tsx';
+import { TitledSection } from './TitledSection.tsx';
+
+export const IncomeSection = () => {
+  const { totalIn, loading: isLoading } = useLast30DaysData();
+  const [
+    isAddFundsModalOpened,
+    { toggleOn: toggleAddFundsModalOn, toggleOff: toggleAddFundsModalOff },
+  ] = useToggle();
+
+  return (
+    <TitledSection
+      title={formatText(MSG.incomeSectionTitle)}
+      caption={formatText(MSG.last30DaysPeriod)}
+      value={totalIn}
+      isLoading={isLoading}
+    >
+      <div className="flex flex-row gap-2">
+        <LoadingSkeleton isLoading={isLoading} className="h-8 w-14 rounded">
+          <ClaimFundsButton />
+        </LoadingSkeleton>
+        <LoadingSkeleton isLoading={isLoading} className="h-8 w-12 rounded">
+          <Button
+            onClick={toggleAddFundsModalOn}
+            text={formatText(MSG.addFundsCTA)}
+            mode="primaryOutlineFull"
+            size="small"
+            className="h-fit border-gray-900 px-3 py-2 text-gray-900"
+          />
+        </LoadingSkeleton>
+      </div>
+      <AddFundsModal
+        isOpen={isAddFundsModalOpened}
+        onClose={toggleAddFundsModalOff}
+      />
+    </TitledSection>
+  );
+};
