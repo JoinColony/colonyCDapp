b7dc424346d58d0ee48095c264038698aca7e410
Anastasiia <anastasiia@colony.io>
2024-09-04 11:20:15 +0200

    fix: update mobile and tablet styles for total funds cards

---
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsTotalItem.tsx
@@ -0,0 +1,52 @@
+import React, { type FC } from 'react';
+
+import { currencySymbolMap } from '~constants/currency.ts';
+import { useCurrencyContext } from '~context/CurrencyContext/CurrencyContext.ts';
+import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
+import Numeral from '~shared/Numeral/index.ts';
+import { formatText } from '~utils/intl.ts';
+import WidgetCards from '~v5/common/WidgetCards/index.ts';
+
+import { useTotalFunds } from '../hooks.ts';
+
+import { FundsCardsSubTitle } from './FundsCardsSubTitle.tsx';
+import { FundsCardsTotalDescription } from './FundsCardsTotalDescription.tsx';
+
+interface FundsCardsTotalItemProps {
+  className?: string;
+}
+export const FundsCardsTotalItem: FC<FundsCardsTotalItemProps> = ({
+  className,
+}) => {
+  const selectedDomain = useGetSelectedDomainFilter();
+  const selectedTeamName = selectedDomain?.metadata?.name;
+
+  const totalFunds = useTotalFunds();
+
+  const { currency } = useCurrencyContext();
+
+  return (
+    <WidgetCards.Item
+      className={className}
+      title={
+        <span className="font-semibold">
+          {formatText({ id: 'dashboard.team.cards.totalFunds' })}
+          {!!selectedTeamName && ` ${selectedTeamName}`}
+        </span>
+      }
+      subTitle={
+        <FundsCardsSubTitle
+          value={
+            <Numeral
+              value={totalFunds ?? '-'}
+              prefix={currencySymbolMap[currency]}
+            />
+          }
+          currency={currency}
+        />
+      }
+    >
+      <FundsCardsTotalDescription direction="up" />
+    </WidgetCards.Item>
+  );
+};
