6624b0459ca0eaed18f9464b4d7cd986d4c67e3d
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-09-05 15:36:57 +0100

    chore: remove unused cards

--- src/components/v5/frame/ColonyHome/ColonyHome.tsx
+++ src/components/v5/frame/ColonyHome/ColonyHome.tsx
@@ -17,13 +17,11 @@ import { setQueryParamOnUrl } from '~utils/urls.ts';
 import useGetActionData from '~v5/common/ActionSidebar/hooks/useGetActionData.ts';
 import Link from '~v5/shared/Link/index.ts';
 
-import Agreements from './partials/Agreements/index.ts';
 import DashboardHeader from './partials/DashboardHeader/index.ts';
 import FundsCards from './partials/FundsCards/index.ts';
 import LeaveColonyModal from './partials/LeaveColonyModal/index.ts';
 import ReputationChart from './partials/ReputationChart/index.ts';
 import TokenBalance from './partials/TokenBalance/index.ts';
-import TotalActions from './partials/TotalActions/index.ts';
 
 const displayName = 'v5.frame.ColonyHome';
 
@@ -43,7 +41,6 @@ const ColonyHome = () => {
       <div className="flex flex-col gap-9 sm:gap-10">
         <DashboardHeader />
         <div className="flex w-full flex-col items-center gap-[1.125rem] sm:flex-row">
-          <TotalActions />
           <TokenBalance />
         </div>
       </div>
@@ -51,7 +48,6 @@ const ColonyHome = () => {
       <div className="flex h-fit w-full flex-col gap-6 lg:grid lg:grid-cols-[39%_1fr]">
         <div className="flex w-full flex-1 flex-col gap-6 sm:grid sm:grid-cols-2 sm:gap-[1.125rem] lg:flex lg:flex-col lg:gap-[1.125rem]">
           <ReputationChart />
-          <Agreements />
         </div>
         <div className="w-full">
           <FiltersContextProvider>
