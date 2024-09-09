b38e1b3836f2c7ae6f585bd988f38a99c61d5c9a
Anastasiia <anastasiia@colony.io>
2024-09-09 12:27:15 +0200

    fix: update UI for fund cards

--- src/components/v5/common/WidgetCards/partials/CarouselButton.tsx
+++ src/components/v5/common/WidgetCards/partials/CarouselButton.tsx
@@ -4,12 +4,13 @@ import React, { type ButtonHTMLAttributes, type FC } from 'react';
 export const CarouselButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
   onClick,
   disabled,
+  className,
   children,
 }) => (
   <button
     onClick={onClick}
     type="button"
-    className={clsx(disabled ? 'opacity-0' : 'opacity-100')}
+    className={clsx(className, disabled ? 'opacity-20' : 'opacity-100')}
     disabled={disabled}
   >
     {children}
