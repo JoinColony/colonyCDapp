2371329b906caa34d1b196c73102bde98d3b23c8
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-09-10 09:46:13 +0100

    Feat: Update recent activity table

---
+++ src/components/common/ColonyActionsTable/RecentActivityTable.tsx
@@ -0,0 +1,46 @@
+import React, { type FC } from 'react';
+
+import useActionsCount from '~hooks/useActionsCount.ts';
+import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
+import { type ColonyAction } from '~types/graphql.ts';
+import Table from '~v5/common/Table/index.ts';
+
+import { useActionsTableProps } from './hooks/useActionsTableProps.tsx';
+import { type ColonyActionsTableProps } from './types.ts';
+
+const displayName = 'common.RecentActivityTable';
+
+const RecentActivityTable: FC<ColonyActionsTableProps> = ({ ...props }) => {
+  const selectedDomain = useGetSelectedDomainFilter();
+  const nativeDomainId = selectedDomain?.nativeId;
+
+  const { actionsCount: totalActions, loading: loadingActionsCount } =
+    useActionsCount({
+      domainId: nativeDomainId,
+    });
+
+  const pageCount = Math.ceil(totalActions / (props.pageSize ?? 10));
+
+  const { tableProps, renderSubComponent } = useActionsTableProps({
+    ...props,
+    showUserAvatar: false,
+    hasHorizontalPadding: false,
+    showTotalPagesNumber: !loadingActionsCount,
+    isRecentActivityVariant: true,
+  });
+
+  return (
+    <Table<ColonyAction>
+      {...tableProps}
+      showTableHead={false}
+      showBorder={false}
+      hasHorizontalPadding={false}
+      renderSubComponent={renderSubComponent}
+      pageCount={pageCount}
+    />
+  );
+};
+
+RecentActivityTable.displayName = displayName;
+
+export default RecentActivityTable;
