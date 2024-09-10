a53faab7ee3e34fb1dcc8c907a47652031d3d021
Anastasiia <anastasiia@colony.io>
2024-09-10 14:44:54 +0200

    fix: update styles for tablet and desktop sidebar items

--- src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/partials/SidebarRoutesSection.tsx
+++ src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/partials/SidebarRoutesSection.tsx
@@ -5,7 +5,7 @@ import SidebarRouteItem from '~v5/shared/Navigation/Sidebar/partials/SidebarRout
 import { sidebarNavigationScheme } from '../consts.ts';
 
 export const SidebarRoutesSection = () => (
-  <section className="flex w-full flex-col">
+  <section className="flex w-full flex-col gap-0 md:gap-0.5">
     {sidebarNavigationScheme.map((scheme) => (
       <SidebarRouteItem key={scheme.id} {...scheme} />
     ))}
