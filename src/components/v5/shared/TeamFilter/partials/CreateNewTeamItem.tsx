cff3165ce258783567a69e7450ec0ddc642718d6
Bassgeta <kerry.mahne@gmail.com>
2024-09-11 17:05:47 +0200

    feat: add ArrowScroller component and use it for the team filter

--- src/components/v5/shared/TeamFilter/partials/CreateNewTeamItem.tsx
+++ src/components/v5/shared/TeamFilter/partials/CreateNewTeamItem.tsx
@@ -21,7 +21,7 @@ const CreateNewTeamItem = () => {
   return (
     <button
       type="button"
-      className="bg-base-white px-4 py-2 font-medium text-gray-400 text-3 hover:bg-gray-50 hover:text-gray-900"
+      className="border-gray-200 bg-base-white px-4 py-2 font-medium text-gray-400 text-3 hover:bg-gray-50 hover:text-gray-900"
       onClick={handleClick}
     >
       <Plus size={16} />
