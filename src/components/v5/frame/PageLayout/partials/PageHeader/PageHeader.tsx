6f9e0782a0e27a13094fe2b384292e2347c5faa0
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-09-11 09:42:35 +0100

    Fix: UI Page header adjustments

--- src/components/v5/frame/PageLayout/partials/PageHeader/PageHeader.tsx
+++ src/components/v5/frame/PageLayout/partials/PageHeader/PageHeader.tsx
@@ -62,7 +62,7 @@ const PageHeader: FC<PageHeaderProps> = ({ pageHeadingProps }) => {
               </section>
             )}
             {isTablet && (
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-6">
                 <MobileColonySwitcherToggle />
                 {colonyContext && (
                   <MobileColonyPageSidebarToggle
