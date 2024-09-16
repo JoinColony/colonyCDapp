fc926d0b2f01c98fc751b8cce36cc84db861aeda
Anastasiia <anastasiia@colony.io>
2024-09-16 15:54:45 +0200

    fix: refactor useMembersPage and create useContributorsByDomain instead

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/ReputationChart.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/ReputationChart.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { defineMessages } from 'react-intl';
 
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import { useMembersPage } from '~frame/v5/pages/MembersPage/hooks.ts';
 import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
 import { notNull } from '~utils/arrays/index.ts';
 import { formatText } from '~utils/intl.ts';
 
+import { useContributorsByDomain } from './hooks/useContributorsByDomain.tsx';
 import { Chart } from './partials/Chart.tsx';
 import {
   getContributorReputationChartData,
@@ -32,7 +32,7 @@ const ReputationChart = () => {
     colony: { domains },
   } = useColonyContext();
 
-  const { contributorsList } = useMembersPage();
+  const { contributorsList } = useContributorsByDomain();
 
   const selectedDomain = useGetSelectedDomainFilter();
 
@@ -42,18 +42,12 @@ const ReputationChart = () => {
       (a, b) => Number(b.reputationPercentage) - Number(a.reputationPercentage),
     );
 
-  const allContributors = (contributorsList || [])
-    .filter((item: any) => {
-      return item !== null && !!item.reputation;
-    })
-    .sort((a, b) => Number(b.reputation) - Number(a.reputation));
-
   const chartDataTeams = !isThereReputationInDomains(allTeams)
     ? []
     : getTeamReputationChartData(allTeams);
 
   const chartDataContributors =
-    getContributorReputationChartData(allContributors);
+    getContributorReputationChartData(contributorsList);
 
   // if domain/team is selected we should show Top Contributors chart instead
   const chartData = selectedDomain ? chartDataContributors : chartDataTeams;
