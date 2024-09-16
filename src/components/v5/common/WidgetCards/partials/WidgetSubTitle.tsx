bef468991ba6588d31d2aaf84779e61cb948fd7f
Anastasiia <anastasiia@colony.io>
2024-09-16 17:31:28 +0200

    fix: refactor widget content components

--- src/components/v5/common/WidgetCards/partials/WidgetSubTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type FC } from 'react';
-
-interface SubTitleProps {
-  title: React.ReactNode;
-}
-
-export const WidgetSubTitle: FC<SubTitleProps> = ({ title }) => (
-  <h4 className="heading-4">{title}</h4>
-);
