ce403ef9f0b77b279927a44af75a6153c9c27c3b
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-16 10:44:45 +0200

    Refactor: changes after review
    Refactor: changes after review

--- src/components/v5/frame/ColonyHome/partials/TotalInOutBalance/TotalInOutBalance.tsx
+++ src/components/v5/frame/ColonyHome/partials/TotalInOutBalance/TotalInOutBalance.tsx
@@ -23,9 +23,10 @@ const TotalInOutBalance = () => {
         </div>
       }
       titleClassName="text-2 mb-4"
-      className="col-span-2 !px-5 !items-start"
+      className="col-span-2 !items-start !px-5"
+      contentClassName="flex flex-col w-full h-full"
       value={
-        <div className="flex flex-col justify-center gap-8 sm:flex-row">
+        <div className="flex h-full flex-col justify-center gap-8 sm:flex-row">
           <div className="flex flex-1 flex-col gap-4 sm:self-center">
             <PaymentsSection />
             <div className="w-full border-t border-gray-200" />
