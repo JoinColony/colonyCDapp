e24e4d35c6bd57dc9469f3283147b77b124847a6
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-16 18:21:05 +0200

    Refactor: Changes after testing historical data
    Refactor: Changes after testing historical data

--- amplify/backend/function/fetchDomainBalance/src/index.js
+++ amplify/backend/function/fetchDomainBalance/src/index.js
@@ -1,12 +1,17 @@
 require('cross-fetch/polyfill');
 
 const {
-    BigNumber,
-    utils: { Logger },
+  BigNumber,
+  utils: { Logger },
 } = require('ethers');
 const { getPeriodFromNow } = require('./utils');
 const ExchangeRatesFactory = require('./config/exchangeRates');
-const { getInOutActions, getTokensDatesMap, filterActionsWithinTimeframe, groupBalanceByPeriod } = require('./services/actions');
+const {
+  getInOutActions,
+  getTokensDatesMap,
+  filterActionsWithinTimeframe,
+  groupBalanceByPeriod,
+} = require('./services/actions');
 const { getTotalFiatAmountFor } = require('./services/tokens');
 Logger.setLogLevel(Logger.levels.ERROR);
 
@@ -16,51 +21,68 @@ Logger.setLogLevel(Logger.levels.ERROR);
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    try {
-        const {
-            colonyAddress,
-            domainId,
-            chainId,
-            selectedCurrency,
-            timeframePeriod = 4,
-            timeframeType,
-            timeframePeriodEndDate,
-        } = event.arguments?.input || {};
-        const periodFromNow = getPeriodFromNow(timeframePeriod, timeframeType, timeframePeriodEndDate);
-        console.log('fetchDomainBalance', periodFromNow, timeframeType, timeframePeriodEndDate)
+  try {
+    const {
+      colonyAddress,
+      domainId,
+      chainId,
+      selectedCurrency,
+      timeframePeriod = 4,
+      timeframeType,
+      timeframePeriodEndDate,
+    } = event.arguments?.input || {};
+    const periodFromNow = getPeriodFromNow(
+      timeframePeriod,
+      timeframeType,
+      timeframePeriodEndDate,
+    );
 
-        const inOutActions = await getInOutActions(colonyAddress, domainId);
-        const inOutActionsWithinTimeframe = filterActionsWithinTimeframe(inOutActions, periodFromNow, timeframePeriodEndDate);
-        const periodBalance = groupBalanceByPeriod(timeframeType, timeframePeriod, timeframePeriodEndDate, inOutActionsWithinTimeframe, domainId);
-        const exchangeRates = await ExchangeRatesFactory.getExchangeRates(getTokensDatesMap(inOutActionsWithinTimeframe), selectedCurrency, chainId);
+    const inOutActions = await getInOutActions(colonyAddress, domainId);
+    const inOutActionsWithinTimeframe = filterActionsWithinTimeframe(
+      inOutActions,
+      periodFromNow,
+      timeframePeriodEndDate,
+    );
+    const periodBalance = groupBalanceByPeriod(
+      timeframeType,
+      timeframePeriod,
+      timeframePeriodEndDate,
+      inOutActionsWithinTimeframe,
+      domainId,
+    );
+    const exchangeRates = await ExchangeRatesFactory.getExchangeRates(
+      getTokensDatesMap(inOutActionsWithinTimeframe),
+      selectedCurrency,
+      chainId,
+    );
 
-        const inOutPeriodBalance = {};
-        let timeframeTotalIn = BigNumber.from(0);
-        let timeframeTotalOut = BigNumber.from(0);
+    const inOutPeriodBalance = {};
+    let timeframeTotalIn = BigNumber.from(0);
+    let timeframeTotalOut = BigNumber.from(0);
 
-        for (let period in periodBalance) {
-            const balance = periodBalance[period];
-            const totalIn = await getTotalFiatAmountFor(balance.in, exchangeRates);
-            const totalOut = await getTotalFiatAmountFor(balance.out, exchangeRates);
-            inOutPeriodBalance[period] = {
-                totalIn,
-                totalOut,
-            };
+    for (let period in periodBalance) {
+      const balance = periodBalance[period];
+      const totalIn = await getTotalFiatAmountFor(balance.in, exchangeRates);
+      const totalOut = await getTotalFiatAmountFor(balance.out, exchangeRates);
+      inOutPeriodBalance[period] = {
+        totalIn,
+        totalOut,
+      };
 
-            timeframeTotalIn = timeframeTotalIn.add(BigNumber.from(totalIn));
-            timeframeTotalOut = timeframeTotalOut.add(BigNumber.from(totalOut));
-        }
-
-        return {
-            totalIn: timeframeTotalIn.toString(),
-            totalOut: timeframeTotalOut.toString(),
-            timeframe: Object.keys(inOutPeriodBalance).map((period) => ({
-                key: period,
-                value: inOutPeriodBalance[period]
-            }))
-        };
-    } catch (e) {
-        console.log('there was an error', e)
-        console.error(e);
+      timeframeTotalIn = timeframeTotalIn.add(BigNumber.from(totalIn));
+      timeframeTotalOut = timeframeTotalOut.add(BigNumber.from(totalOut));
     }
+
+    return {
+      totalIn: timeframeTotalIn.toString(),
+      totalOut: timeframeTotalOut.toString(),
+      timeframe: Object.keys(inOutPeriodBalance).map((period) => ({
+        key: period,
+        value: inOutPeriodBalance[period],
+      })),
+    };
+  } catch (e) {
+    console.log('there was an error', e);
+    console.error(e);
+  }
 };
