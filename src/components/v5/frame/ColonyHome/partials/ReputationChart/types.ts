1785a8251e1d7d1cb73c4d79962159cb82c7e849
Anastasiia <anastasiia@colony.io>
2024-09-16 15:54:45 +0200

    fix: refactor useMembersPage and create useContributorsByDomain instead

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/types.ts
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/types.ts
@@ -1,6 +1,14 @@
+import { type User } from '~types/graphql.ts';
+
 export interface ReputationChartDataItem {
   label: string;
   value: number;
   id: string;
   color: string;
+}
+
+export interface ContributorItem {
+  user?: User | null;
+  walletAddress: string;
+  reputation?: number;
 }
