98d83d2294176521c9946e9af30237d9e7a88462
Anastasiia <anastasiia@colony.io>
2024-09-16 17:31:28 +0200

    fix: refactor widget content components

--- src/components/v5/common/WidgetCards/partials/WidgetTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type FC } from 'react';
-
-interface WidgetTitleProps {
-  title: React.ReactNode;
-}
-
-export const WidgetTitle: FC<WidgetTitleProps> = ({ title }) => (
-  <h3 className="text-md">{title}</h3>
-);
