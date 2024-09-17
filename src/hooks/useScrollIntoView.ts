a72fa72a1665c52e5dbbf4a9557758647071c811
Bassgeta <kerry.mahne@gmail.com>
2024-09-17 10:38:58 +0200

    feat: realign after code review, auto scroll to selected team on mobile

--- src/hooks/useScrollIntoView.ts
+++ src/hooks/useScrollIntoView.ts
@@ -1,13 +1,13 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 export const useScrollIntoView = <T extends HTMLElement>() => {
   const ref = useRef<T>(null);
 
-  const scroll = (options: ScrollIntoViewOptions) => {
+  const scroll = useCallback((options: ScrollIntoViewOptions) => {
     if (ref.current) {
       ref.current.scrollIntoView({ ...options });
     }
-  };
+  }, []);
 
   return { ref, scroll };
 };
