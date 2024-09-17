8211f77ef581f3033e12f62352804c1e6b66b6c5
Romeo Ledesma <r_ledesma@outlook.com>
2024-09-17 17:17:47 +0100

    ui: implement dark theme for the colony sidebar

--- src/components/v5/shared/SimpleSidebar/SimpleSidebar.tsx
+++ src/components/v5/shared/SimpleSidebar/SimpleSidebar.tsx
@@ -1,13 +1,22 @@
+import clsx from 'clsx';
 import React, { type FC } from 'react';
 
+import { usePageThemeContext } from '~context/PageThemeContext/PageThemeContext.ts';
 import ColonyIcon from '~icons/ColonyIcon.tsx';
 import ColonyLogo from '~images/logo-new.svg?react';
 
 const SimpleSidebar: FC = () => {
+  const { isDarkMode } = usePageThemeContext();
+
   return (
     <nav className="flex h-full flex-col items-center justify-between rounded-lg border border-gray-200 px-4 pb-6 pt-4 text-center ">
       <div className="flex h-11 w-11 items-center justify-center">
-        <ColonyIcon size={34} />
+        <ColonyIcon
+          size={34}
+          className={clsx('text-base-white', {
+            '!text-gray-900': isDarkMode,
+          })}
+        />
       </div>
       <ColonyLogo />
     </nav>
