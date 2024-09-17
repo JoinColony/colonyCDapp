99da8453c50d6784d77d435875c950845993b1cc
Bassgeta <kerry.mahne@gmail.com>
2024-09-17 10:38:58 +0200

    feat: realign after code review, auto scroll to selected team on mobile

--- src/components/v5/shared/TeamFilter/partials/LeftButton.tsx
+++ src/components/v5/shared/TeamFilter/partials/LeftButton.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const LeftButton = () => {
   return (
     <div className="flex h-full items-center rounded-l-lg border border-gray-200 bg-base-bg px-4 py-2 text-gray-400">
-      <CaretLeft size={14} />
+      <CaretLeft size={18} />
     </div>
   );
 };
