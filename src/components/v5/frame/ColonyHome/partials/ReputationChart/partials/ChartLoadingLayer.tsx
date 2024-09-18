468d35ebcade10aa761b8e14b94d2e535e61b6e9
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-18 10:49:15 +0200

    Feat: add styled tooltip and loading skeletons for the reputation chart
    Feat: add styled tooltip and loading skeletons for the reputation chart

---
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/ChartLoadingLayer.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
+import { tw } from '~utils/css/index.ts';
+
+const absoluteCenteredClass = tw`absolute left-1/2 top-1/2 z-base -translate-x-1/2 -translate-y-1/2`;
+
+export const ChartLoadingLayer = () => {
+  return (
+    <div className={absoluteCenteredClass}>
+      <div className="relative">
+        <LoadingSkeleton
+          className="h-[138px] w-[138px] rounded-full"
+          isLoading
+        />
+        <div
+          className={clsx(
+            absoluteCenteredClass,
+            'h-[102px] w-[102px] rounded-full bg-base-white',
+          )}
+        />
+      </div>
+    </div>
+  );
+};
