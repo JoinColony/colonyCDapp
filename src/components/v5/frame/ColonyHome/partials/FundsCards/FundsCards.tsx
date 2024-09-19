84636d1d879b42985f5ff9b061f98b4653a66d94
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-19 18:47:27 +0200

    Feat: Add computed total values to FundsCards
    Add computed total values to FundsCards
    Add loading skeletons to fund card components
    Add useSubDomains hook
    Moved convertFromTokenToCurrency and getValuesTrend to their own files

--- src/components/v5/frame/ColonyHome/partials/FundsCards/FundsCards.tsx
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/FundsCards.tsx
@@ -2,15 +2,13 @@ import { Plus } from '@phosphor-icons/react';
 import React from 'react';
 
 import { Action } from '~constants/actions.ts';
-import { currencySymbolMap } from '~constants/currency.ts';
 import { useActionSidebarContext } from '~context/ActionSidebarContext/ActionSidebarContext.ts';
-import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import Numeral from '~shared/Numeral/index.ts';
+import { useSubDomains } from '~hooks/useSubDomains.ts';
 import { formatText } from '~utils/intl.ts';
 import { ACTION_TYPE_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
 import WidgetCards from '~v5/common/WidgetCards/index.ts';
 
-import { FundsCardsSubTitle } from './partials/FundsCardsSubTitle.tsx';
+import { FundsCardsItem } from './partials/FundsCardsItem.tsx';
 import { FundsCardsTotalItem } from './partials/FundsCardsTotalItem.tsx';
 
 export const FundsCards = () => {
@@ -24,39 +22,22 @@ export const FundsCards = () => {
     });
   };
 
-  const { colony } = useColonyContext();
-
-  const teams = colony.domains?.items || [];
+  const subTeams = useSubDomains();
 
   // Teams always will have at least 1 team by default - it is general
-  const isAddNewTeamVisible = teams.length < 2;
+  const isAddNewTeamVisible = !subTeams || subTeams.length < 1;
   return (
     <div className="flex flex-col gap-4 sm:flex-row sm:gap-2">
       <WidgetCards.List className="w-full">
         <FundsCardsTotalItem />
-        {teams.map((item) => {
-          const currency = 'USD'; // @TODO: use item currency instead
-          return (
-            <WidgetCards.Item
-              key={item?.id}
-              // @TODO: update onClick when it will be ready
-              onClick={() => {}}
-              title={item?.metadata?.name}
-              // @TODO: update with a real teams data
-              subTitle={
-                <FundsCardsSubTitle
-                  currency={currency}
-                  value={
-                    <Numeral
-                      value="15,779.00"
-                      prefix={currencySymbolMap[currency]}
-                    />
-                  }
-                />
-              }
-            />
-          );
-        })}
+        {subTeams?.map((item) => (
+          <FundsCardsItem
+            key={item?.id}
+            domainId={item?.id}
+            domainName={item?.metadata?.name}
+          />
+        ))}
+
         {isAddNewTeamVisible && (
           <WidgetCards.Item
             variant="dashed"
