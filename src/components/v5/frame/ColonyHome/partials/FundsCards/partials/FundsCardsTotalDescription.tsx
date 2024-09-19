fb29ccd0297660a93e23a318882080f809cad94f
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-19 18:47:27 +0200

    Feat: Add computed total values to FundsCards
    Add computed total values to FundsCards
    Add loading skeletons to fund card components
    Add useSubDomains hook
    Moved convertFromTokenToCurrency and getValuesTrend to their own files

--- src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsTotalDescription.tsx
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsTotalDescription.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
 
+import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
+import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
+import Numeral from '~shared/Numeral/Numeral.tsx';
+import { getBalanceForTokenAndDomain } from '~utils/tokens.ts';
 import { FundsTrend } from '~v5/frame/ColonyHome/partials/FundsTrend/FundsTrend.tsx';
 
-interface FundsCardsTotalDescriptionProps {}
+interface FundsCardsTotalDescriptionProps {
+  percent: string;
+  isIncrease?: boolean;
+  isLoading?: boolean;
+}
 
 // @TODO create logic for this component, props etc
 export const FundsCardsTotalDescription: React.FC<
   FundsCardsTotalDescriptionProps
-> = () => {
+> = ({ percent, isIncrease, isLoading }) => {
+  const selectedDomain = useGetSelectedDomainFilter();
+  const {
+    colony: { balances, nativeToken },
+  } = useColonyContext();
+  const tokenBalance = getBalanceForTokenAndDomain(
+    balances,
+    nativeToken.tokenAddress,
+    selectedDomain ? Number(selectedDomain.nativeId) : undefined,
+  );
+
   return (
     <div className="flex w-full justify-between text-xs">
-      <span className="uppercase text-gray-400">Native 1.56M CLNY</span>
-      <FundsTrend isIncrease value="15% Week" />
+      <LoadingSkeleton isLoading={isLoading} className="h-4 w-[114px] rounded">
+        <span className="uppercase text-gray-400">
+          <Numeral
+            prefix="Native"
+            value={tokenBalance.toString()}
+            decimals={nativeToken.decimals}
+            suffix={` ${nativeToken.symbol}`}
+          />
+        </span>
+      </LoadingSkeleton>
+      <LoadingSkeleton isLoading={isLoading} className="h-4 w-[30px] rounded">
+        <FundsTrend isIncrease={isIncrease} value={`${percent} Week`} />
+      </LoadingSkeleton>
     </div>
   );
 };
