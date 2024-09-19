2781f335b54852a5751f49212965bffab094663f
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-19 18:47:27 +0200

    Feat: Add computed total values to FundsCards
    Add computed total values to FundsCards
    Add loading skeletons to fund card components
    Add useSubDomains hook
    Moved convertFromTokenToCurrency and getValuesTrend to their own files

---
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsItem.tsx
@@ -0,0 +1,47 @@
+import React, { type FC } from 'react';
+
+import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
+import { currencySymbolMap } from '~constants/currency.ts';
+import { useCurrencyContext } from '~context/CurrencyContext/CurrencyContext.ts';
+import Numeral from '~shared/Numeral/index.ts';
+import WidgetCards from '~v5/common/WidgetCards/index.ts';
+
+import { useTotalData } from '../hooks.ts';
+
+import { FundsCardsSubTitle } from './FundsCardsSubTitle.tsx';
+
+interface FundsCardsItemProps {
+  domainId?: string;
+  domainName?: string;
+}
+export const FundsCardsItem: FC<FundsCardsItemProps> = ({
+  domainId,
+  domainName,
+}) => {
+  const { total, loading } = useTotalData(domainId);
+  const { currency } = useCurrencyContext();
+
+  return (
+    <WidgetCards.Item
+      // @TODO: update onClick when it will be ready
+      onClick={() => {}}
+      title={
+        <LoadingSkeleton isLoading={loading} className="mb-1 h-5 w-14 rounded">
+          {domainName}
+        </LoadingSkeleton>
+      }
+      subTitle={
+        <FundsCardsSubTitle
+          currency={currency}
+          isLoading={loading}
+          value={
+            <Numeral
+              value={total.toString()}
+              prefix={currencySymbolMap[currency]}
+            />
+          }
+        />
+      }
+    />
+  );
+};
