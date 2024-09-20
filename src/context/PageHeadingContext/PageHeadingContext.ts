97af4ecb3faf16b01eaba47c0ce8f4fb706ba822
Bassgeta <kerry.mahne@gmail.com>
2024-09-20 12:00:48 +0200

    feat: remove unused breadcrumbs code

--- src/context/PageHeadingContext/PageHeadingContext.ts
+++ src/context/PageHeadingContext/PageHeadingContext.ts
@@ -1,20 +1,14 @@
 // @TODO remove this entire thing and render titles on each page instead
 import { createContext, useContext, useEffect } from 'react';
 
-import { type PageHeadingProps } from '~v5/frame/PageLayout/partials/PageHeading/types.ts';
-
 interface PageHeadingContextValue {
   title?: string;
   setTitle: (title: string | undefined) => void;
-  breadcrumbs?: PageHeadingProps['breadcrumbs'];
-  setBreadcrumbs: (breadcrumbs: PageHeadingProps['breadcrumbs']) => void;
 }
 
 export const PageHeadingContext = createContext<PageHeadingContextValue>({
   title: undefined,
   setTitle: () => {},
-  breadcrumbs: undefined,
-  setBreadcrumbs: () => {},
 });
 
 export const usePageHeadingContext = (): PageHeadingContextValue =>
