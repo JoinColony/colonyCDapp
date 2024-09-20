8c789a1ad09a0ae8dce94726051e03af4ff3cb24
Bassgeta <kerry.mahne@gmail.com>
2024-09-20 11:40:31 +0200

    feat: add custom overrides for multi-sig and crypto-to-fiat URL parts

--- src/components/v5/shared/Breadcrumbs/consts.ts
+++ src/components/v5/shared/Breadcrumbs/consts.ts
@@ -1,6 +1,23 @@
-import { type MessageDescriptor } from 'react-intl';
+import { defineMessages, type MessageDescriptor } from 'react-intl';
 
 import { supportedExtensionsConfig } from '~constants';
+import {
+  COLONY_PERMISSIONS_MULTISIG_SECTION,
+  USER_CRYPTO_TO_FIAT_ROUTE,
+} from '~routes/routeConstants.ts';
+
+const displayName = 'v5.Breadcrumbs.RouteNames';
+
+const MSG = defineMessages({
+  cryptoToFiat: {
+    id: `${displayName}.cryptoToFiat`,
+    defaultMessage: 'Crypto to fiat',
+  },
+  multiSig: {
+    id: `${displayName}.multisig`,
+    defaultMessage: 'Multi-sig',
+  },
+});
 
 const extensionsBreadcrumbNames = supportedExtensionsConfig.reduce(
   (names, extensionConfig) => {
@@ -12,6 +29,9 @@ const extensionsBreadcrumbNames = supportedExtensionsConfig.reduce(
   {},
 );
 
+// @TODO we probably want to do it for all possible routes and refrain from automatically just capitalizing the route part
 export const breadcrumbItemNames: Record<string, MessageDescriptor> = {
   ...extensionsBreadcrumbNames,
+  [USER_CRYPTO_TO_FIAT_ROUTE]: MSG.cryptoToFiat,
+  [COLONY_PERMISSIONS_MULTISIG_SECTION]: MSG.multiSig,
 };
