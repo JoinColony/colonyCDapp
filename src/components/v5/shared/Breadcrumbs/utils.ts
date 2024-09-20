9f0e673a3df44e110a0260a6f16acdb8e75d3748
Bassgeta <kerry.mahne@gmail.com>
2024-09-20 11:40:31 +0200

    feat: add custom overrides for multi-sig and crypto-to-fiat URL parts

--- src/components/v5/shared/Breadcrumbs/utils.ts
+++
@@ -1,9 +0,0 @@
-import { formatText } from '~utils/intl.ts';
-
-import { breadcrumbItemNames } from './consts.ts';
-
-export const getBreadcrumbItemName = (parameter: string): string => {
-  return breadcrumbItemNames[parameter]
-    ? formatText(breadcrumbItemNames[parameter])
-    : parameter;
-};
