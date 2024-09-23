d1c0a670621e3df3217f60369dca536204d6ca4b
Chris Maniewski <code@chmanie.com>
2024-09-23 23:13:14 +0200

    feat: add team filtering to dashboard chart

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/Chart.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/Chart.tsx
@@ -60,10 +60,11 @@ export const Chart: FC<ChartProps> = ({ data, isLoading }) => {
               <LegendItem
                 key={EMPTY_CHART_ITEM.id}
                 chartItem={{
+                  id: EMPTY_CHART_ITEM.id,
                   label: EMPTY_CHART_ITEM.label,
                   color: EMPTY_CHART_ITEM.color,
-                  value: undefined,
                   shouldTruncateLegendLabel: false,
+                  value: EMPTY_CHART_ITEM.value,
                 }}
               />
             )}
