2d572dc568d7a81ca355496380bf7c02aa477438
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-23 19:04:01 +0200

    Feat: Add legend item active state on non-empty chart hover
    Feat: Add legend item active state on non-empty chart hover

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/ChartTooltip.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/ChartTooltip.tsx
@@ -1,16 +1,28 @@
 import { type PieTooltipProps } from '@nivo/pie';
 import React, { type FC } from 'react';
 
+import { useReputationChartContext } from '~context/ReputationChartContext/ReputationChartContext.ts';
 import Numeral from '~shared/Numeral/Numeral.tsx';
 import { ChartCustomTooltip } from '~v5/frame/ColonyHome/partials/ChartCustomTooltip/ChartCustomTooltip.tsx';
 
 import { type ReputationChartDataItem } from '../types.ts';
 
 export const ChartTooltip: FC<PieTooltipProps<ReputationChartDataItem>> = ({
-  datum: { label, value },
+  datum: { id, label, value },
 }) => {
+  const { setActiveLegendItem, resetActiveLegendItem } =
+    useReputationChartContext();
+
+  const onVisibleHandler = (isVisible?: boolean) => {
+    if (isVisible && id) {
+      setActiveLegendItem(id.toString());
+    } else {
+      resetActiveLegendItem();
+    }
+  };
+
   return (
-    <ChartCustomTooltip>
+    <ChartCustomTooltip onVisible={onVisibleHandler}>
       {label}
       <Numeral value={value.toFixed(2)} suffix="%" className="ml-1" />
     </ChartCustomTooltip>
