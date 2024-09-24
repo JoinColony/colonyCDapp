5bc98a58968d66ef008bbb25a8290f1a80aa7909
Anastasiia <anastasiia@colony.io>
2024-09-24 13:44:18 +0200

    fix: refactor Numeral component

--- src/components/shared/Numeral/index.ts
+++ src/components/shared/Numeral/index.ts
@@ -1,3 +1,5 @@
 export { default } from './Numeral.tsx';
 
+export * from './NumeralCurrency.tsx';
+
 export * from './helpers.tsx';
--- src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsItem.tsx
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/partials/FundsCardsItem.tsx
@@ -3,7 +3,7 @@ import React, { type FC } from 'react';
 import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
 import { currencySymbolMap } from '~constants/currency.ts';
 import { useCurrencyContext } from '~context/CurrencyContext/CurrencyContext.ts';
-import Numeral from '~shared/Numeral/index.ts';
+import { NumeralCurrency } from '~shared/Numeral/index.ts';
 import WidgetCards from '~v5/common/WidgetCards/index.ts';
 
 import { useTotalData } from '../hooks.ts';
@@ -35,7 +35,7 @@ export const FundsCardsItem: FC<FundsCardsItemProps> = ({
           currency={currency}
           isLoading={loading}
           value={
-            <Numeral
+            <NumeralCurrency
               value={total.toString()}
               prefix={currencySymbolMap[currency]}
             />
