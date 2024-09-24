a682fbd7111515d25fd10e27eada460dc4733e9a
Anastasiia <anastasiia@colony.io>
2024-09-24 13:44:18 +0200

    fix: refactor Numeral component

---
+++ src/components/shared/Numeral/NumeralComponent.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import React, { type FC } from 'react';
+
+import { getMainClasses } from '~utils/css/index.ts';
+
+import { type NumeralProps } from './types.ts';
+
+import styles from './Numeral.module.css';
+
+type Props = Omit<NumeralProps, 'value'>;
+export const NumeralComponent: FC<Props> = ({
+  children,
+  className,
+  prefix,
+  suffix,
+  appearance,
+  ...rest
+}) => {
+  return (
+    <span
+      className={classNames(getMainClasses(appearance, styles), className)}
+      {...rest}
+    >
+      {prefix && `${prefix} `}
+      {children}
+      {suffix}
+    </span>
+  );
+};
---
+++ src/components/shared/Numeral/types.ts
@@ -0,0 +1,22 @@
+import { type BigNumber } from 'ethers';
+import { type HTMLAttributes } from 'react';
+
+import type Decimal from 'decimal.js';
+
+export type NumeralValue = string | number | BigNumber | Decimal;
+
+export interface Appearance {
+  theme?: 'dark';
+  size?: 'small';
+}
+
+export interface NumeralProps extends HTMLAttributes<HTMLSpanElement> {
+  value: NumeralValue;
+  prefix?: string;
+  suffix?: string;
+  className?: string;
+  appearance?: Appearance;
+
+  /** If specified, the value will be shifted by the indicated decimals */
+  decimals?: number;
+}
