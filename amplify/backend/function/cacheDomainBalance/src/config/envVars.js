d2e436a37479f77ddc7817a1369dc2461a9f9cc3
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-24 18:37:50 +0200

    Refactor: Changes after review
    Refactor: Changes after review

--- amplify/backend/function/cacheDomainBalance/src/config/envVars.js
+++ amplify/backend/function/cacheDomainBalance/src/config/envVars.js
@@ -1,61 +1,59 @@
-const {
-  Network,
-} = require('@colony/colony-js');
+const { Network } = require('@colony/colony-js');
 
-const EnvVarsSetupFactory = (() => {
-    let apiKey = 'da2-fakeApiId123456';
-    let graphqlURL = 'http://localhost:20002/graphql';
-    let rpcURL = 'http://network-contracts:8545'; // this needs to be extended to all supported networks
-    let reputationOracleEndpoint =
-        'http://reputation-monitor:3001/reputation/local';
-    let networkAddress;
-    let network = Network.Custom;
-    let isInit = false;
+const EnvVarsConfig = (() => {
+  let apiKey = 'da2-fakeApiId123456';
+  let graphqlURL = 'http://localhost:20002/graphql';
+  let rpcURL = 'http://network-contracts:8545'; // this needs to be extended to all supported networks
+  let reputationOracleEndpoint =
+    'http://reputation-monitor:3001/reputation/local';
+  let networkAddress;
+  let network = Network.Custom;
+  let isInit = false;
 
-    const setEnvVars = async () => {
-        const ENV = process.env.ENV;
-        if (ENV === 'qa' || ENV === 'prod') {
-            const { getParams } = require('/opt/nodejs/getParams');
-            [
-                apiKey,
-                graphqlURL,
-                rpcURL,
-                networkAddress,
-                reputationOracleEndpoint,
-                network,
-            ] = await getParams([
-                'appsyncApiKey',
-                'graphqlUrl',
-                'chainRpcEndpoint',
-                'networkContractAddress',
-                'reputationEndpoint',
-                'chainNetwork',
-            ]);
-        } else {
-            const {
-                etherRouterAddress,
-            } = require('../../../../../mock-data/colonyNetworkArtifacts/etherrouter-address.json');
-            networkAddress = etherRouterAddress;
-        }
-    };
+  const setEnvVars = async () => {
+    const ENV = process.env.ENV;
+    if (ENV === 'qa' || ENV === 'prod') {
+      const { getParams } = require('/opt/nodejs/getParams');
+      [
+        apiKey,
+        graphqlURL,
+        rpcURL,
+        networkAddress,
+        reputationOracleEndpoint,
+        network,
+      ] = await getParams([
+        'appsyncApiKey',
+        'graphqlUrl',
+        'chainRpcEndpoint',
+        'networkContractAddress',
+        'reputationEndpoint',
+        'chainNetwork',
+      ]);
+    } else {
+      const {
+        etherRouterAddress,
+      } = require('../../../../../mock-data/colonyNetworkArtifacts/etherrouter-address.json');
+      networkAddress = etherRouterAddress;
+    }
+  };
 
-    return {
-        getEnvVars: async () => {
-            if (!isInit) {
-                await setEnvVars();
-                isInit = true;
-            }
+  return {
+    getEnvVars: async () => {
+      if (!isInit) {
+        await setEnvVars();
+        isInit = true;
+      }
 
-            return {
-                apiKey,
-                graphqlURL,
-                rpcURL,
-                reputationOracleEndpoint,
-                networkAddress,
-                network,
-            }
-        }
-    };
+      return {
+        apiKey,
+        graphqlURL,
+        rpcURL,
+        reputationOracleEndpoint,
+        networkAddress,
+        network,
+      };
+    },
+  };
 })();
 
-module.exports = EnvVarsSetupFactory;
+module.exports = EnvVarsConfig;
