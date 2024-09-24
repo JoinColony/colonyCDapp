64c6587f9976826f8f9734442e9eb0d1a4d516d6
Bassgeta <kerry.mahne@gmail.com>
2024-09-24 16:04:10 +0200

    feat: undo deletion of RpcForm, left a TODO to refactor it

---
+++ src/components/frame/v5/pages/UserAdvancedPage/partials/RpcForm/RpcForm.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Form } from '~shared/Fields/index.ts';
+import { formatText } from '~utils/intl.ts';
+import noop from '~utils/noop.ts';
+import SettingsInputRow from '~v5/common/SettingsInputRow/index.ts';
+import SettingsRow from '~v5/common/SettingsRow/index.ts';
+
+import { useRpcForm } from './hooks.ts';
+
+// @TODO this needs to be refactored and we need to build a custom toggle button that will show up the input
+const RpcForm = () => {
+  const {
+    customRpc,
+    decentralizedModeEnabled,
+    handleSubmit,
+    isInputVisible,
+    rpcValidationSchema,
+  } = useRpcForm();
+
+  return (
+    <Form
+      validationSchema={rpcValidationSchema}
+      defaultValues={{
+        decentralizedModeEnabled,
+        customRpc,
+      }}
+      onSubmit={noop}
+    >
+      {() => (
+        <SettingsRow.Container>
+          <SettingsRow.Content>
+            <div className="flex items-center gap-1.5">
+              <SettingsRow.Title>
+                {formatText({ id: 'advancedSettings.rpc.title' })}
+              </SettingsRow.Title>
+              <SettingsRow.Tooltip>
+                {formatText({ id: 'advancedSettings.rpc.tooltip' })}
+              </SettingsRow.Tooltip>
+            </div>
+            <SettingsRow.Description>
+              {formatText({ id: 'advancedSettings.rpc.description' })}
+            </SettingsRow.Description>
+          </SettingsRow.Content>
+          <SettingsRow.Content>
+            <SettingsRow.Subtitle>
+              {formatText({ id: 'profilePage.customRpc' })}
+            </SettingsRow.Subtitle>
+          </SettingsRow.Content>
+          <SettingsInputRow
+            isOpen={isInputVisible}
+            handleSubmit={handleSubmit}
+          />
+        </SettingsRow.Container>
+      )}
+    </Form>
+  );
+};
+
+export default RpcForm;
