796852b1261532b10bdbf7c2c065d5fb01a276f4
Anastasiia <anastasiia@colony.io>
2024-09-25 10:13:44 +0200

    fix: rename NumeralCOmponent to NumeralBase

--- src/components/shared/Numeral/NumeralBase.tsx
+++ src/components/shared/Numeral/NumeralBase.tsx
@@ -3,7 +3,7 @@ import React, { type FC } from 'react';
 import { type NumeralProps } from './types.ts';
 
 type Props = Omit<NumeralProps, 'value'>;
-export const NumeralComponent: FC<Props> = ({
+export const NumeralBase: FC<Props> = ({
   children,
   className,
   prefix,
