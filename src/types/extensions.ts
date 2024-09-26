a0138dc73ba1f626909ae7f5e25b576197be728f
Jakub Zajac <jakub@colony.io>
2024-09-26 02:02:13 +0100

    Refactor: Remove unused fields from extension config

--- src/types/extensions.ts
+++ src/types/extensions.ts
@@ -1,6 +1,5 @@
 import { type ColonyRole, type Extension } from '@colony/colony-js';
 import { type Icon } from '@phosphor-icons/react';
-import { type FormatNumeralOptions } from 'cleave-zen';
 import { type BigNumberish } from 'ethers';
 import { type MessageDescriptor } from 'react-intl';
 import { type Schema } from 'yup';
@@ -8,21 +7,12 @@ import { type Schema } from 'yup';
 import { type ExtensionCategory } from '~constants/index.ts';
 import { type ColonyExtension } from '~types/graphql.ts';
 
-export enum ExtensionParamType {
-  Input = 'Input',
-  Radio = 'Radio',
-  Textarea = 'Textarea',
-}
-
 export interface ExtensionInitParam {
   title: MessageDescriptor;
   description?: MessageDescriptor;
   defaultValue?: string | number;
   paramName: string;
   validation: Schema<any>;
-  type: ExtensionParamType;
-  complementaryLabel?: 'hours' | 'periods' | 'percent';
-  formattingOptions?: FormatNumeralOptions;
   // Transform function that will be applied to the param value before passing it to the saga
   transformValue?: (value: string | number) => BigNumberish;
 }
