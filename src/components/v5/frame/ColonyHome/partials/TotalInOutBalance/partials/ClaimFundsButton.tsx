6cadd66cbcab6aeeb95c240a099045bda9cca51c
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-30 17:57:27 +0200

    Fix: update ClaimFundsButton loading state and card buttons skeletons height
    Fix: update ClaimFundsButton loading state and card buttons skeletons height

--- src/components/v5/frame/ColonyHome/partials/TotalInOutBalance/partials/ClaimFundsButton.tsx
+++ src/components/v5/frame/ColonyHome/partials/TotalInOutBalance/partials/ClaimFundsButton.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 import { apolloClient } from '~apollo';
+import LoadingSkeleton from '~common/LoadingSkeleton/LoadingSkeleton.tsx';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
+import useAsyncFunction from '~hooks/useAsyncFunction.ts';
 import useColonyFundsClaims from '~hooks/useColonyFundsClaims.ts';
 import { useTimeout } from '~hooks/useTimeout.ts';
 import { ActionTypes } from '~redux/actionTypes.ts';
 import Tooltip from '~shared/Extensions/Tooltip/Tooltip.tsx';
-import { mergePayload } from '~utils/actions.ts';
 import { formatText } from '~utils/intl.ts';
-import ActionButton from '~v5/shared/Button/ActionButton.tsx';
 import Button from '~v5/shared/Button/Button.tsx';
 
 import { MSG } from '../consts.ts';
@@ -22,13 +22,16 @@ export const ClaimFundsButton = () => {
   } = useColonyContext();
 
   const claims = useColonyFundsClaims();
-  const unclaimedClaims = claims.filter((claim) => !claim.isClaimed);
+  const unclaimedClaims = claims.filter(
+    (claim) => !claim.isClaimed && claim.amount !== '0',
+  );
   const hasUnclaimedClaims = !!unclaimedClaims.length;
   const allClaimableTokenAddresses = Array.from(
     new Set(unclaimedClaims.map((claim) => claim.token?.tokenAddress || '')),
   );
 
   const [isClaimed, setIsClaimed] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
   const [isVisible, setIsVisible] = useState(hasUnclaimedClaims);
   const [shouldRefetchData, setShouldRefetchData] = useState(false);
 
@@ -58,20 +61,32 @@ export const ClaimFundsButton = () => {
     },
   });
 
-  const transform = mergePayload({
-    colonyAddress: colony?.colonyAddress,
-    tokenAddresses: allClaimableTokenAddresses,
+  const claimToken = useAsyncFunction({
+    submit: ActionTypes.CLAIM_TOKEN,
+    error: ActionTypes.CLAIM_TOKEN_ERROR,
+    success: ActionTypes.CLAIM_TOKEN_SUCCESS,
   });
-
-  const handleClaimSuccess = () => {
-    setIsClaimed(true);
-    startPollingColonyData(1_000);
-    setTimeout(stopPollingColonyData, 10_000);
-  };
 
   if (!isVisible) {
     return null;
   }
+
+  const handleClick = async () => {
+    setIsClaiming(true);
+    try {
+      await claimToken({
+        colonyAddress: colony?.colonyAddress,
+        tokenAddresses: allClaimableTokenAddresses,
+      });
+      setIsClaimed(true);
+      startPollingColonyData(1_000);
+      setTimeout(stopPollingColonyData, 10_000);
+    } catch {
+      //
+    } finally {
+      setIsClaiming(false);
+    }
+  };
 
   return isClaimed ? (
     <Button
@@ -80,17 +95,20 @@ export const ClaimFundsButton = () => {
       className="pointer-events-none h-fit border border-success-400 bg-success-400 px-3 py-2 !text-base-white"
     />
   ) : (
-    <Tooltip tooltipContent={formatText(MSG.claimFundsTooltip)} placement="top">
-      <ActionButton
-        actionType={ActionTypes.CLAIM_TOKEN}
-        transform={transform}
-        onSuccess={handleClaimSuccess}
-        disabled={!canInteractWithColony}
-        text={formatText(MSG.claimFundsCTA)}
-        mode="primaryOutlineFull"
-        size="small"
-        className="h-fit border-gray-900 px-3 py-2 text-gray-900"
-      />
-    </Tooltip>
+    <LoadingSkeleton isLoading={isClaiming} className="h-8.5 w-14 rounded-lg">
+      <Tooltip
+        tooltipContent={formatText(MSG.claimFundsTooltip)}
+        placement="top"
+      >
+        <Button
+          onClick={handleClick}
+          disabled={!canInteractWithColony}
+          text={formatText(MSG.claimFundsCTA)}
+          mode="primaryOutlineFull"
+          size="small"
+          className="h-fit border-gray-900 px-3 py-2 text-gray-900"
+        />
+      </Tooltip>
+    </LoadingSkeleton>
   );
 };
