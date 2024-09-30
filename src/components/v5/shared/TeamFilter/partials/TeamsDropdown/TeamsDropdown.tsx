7d1593127748f6408d369a4e655e8fae531b1f4c
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-09-30 17:31:35 +0100

    feat: prevent dropdown menu width changes on bold hover state

--- src/components/v5/shared/TeamFilter/partials/TeamsDropdown/TeamsDropdown.tsx
+++ src/components/v5/shared/TeamFilter/partials/TeamsDropdown/TeamsDropdown.tsx
@@ -56,7 +56,8 @@ const TeamsDropdown: FC<TeamsDropdownProps> = ({ domains }) => {
               type="button"
               key={`TeamsDropdown.${domain.nativeId}`}
               onClick={() => handleClick(domain)}
-              className="w-full text-start text-sm font-medium text-gray-700 hover:font-semibold hover:text-gray-900"
+              aria-label={domain.metadata?.name} // Used to set the after content, which is used to pre-reserve the space required for the bold hover state
+              className="w-full text-start text-sm font-medium text-gray-700 after:invisible after:block after:h-0 after:overflow-hidden after:font-semibold after:content-[attr(aria-label)] hover:font-semibold hover:text-gray-900"
             >
               {domain.metadata?.name}
             </button>
