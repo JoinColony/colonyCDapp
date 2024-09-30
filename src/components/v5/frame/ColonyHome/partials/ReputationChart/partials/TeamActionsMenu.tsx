2c6f7b9909074ab87d11969226d4075a4956b65e
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-09-30 21:33:36 +0200

    Fix: refactor TeamActionsMenu component to use generic components and useDropdown hook
    Fix: refactor TeamActionsMenu component to use generic components and useDropdown hook

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/TeamActionsMenu.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/TeamActionsMenu.tsx
@@ -1,16 +1,19 @@
-import { DotsThreeVertical, Eye, LockKey, Pencil } from '@phosphor-icons/react';
-import clsx from 'clsx';
+import { Eye, LockKey, Pencil } from '@phosphor-icons/react';
 import React, { type FC } from 'react';
 import { defineMessages } from 'react-intl';
-import { usePopperTooltip } from 'react-popper-tooltip';
 import { Link } from 'react-router-dom';
 
 import { Action } from '~constants/actions.ts';
 import { useActionSidebarContext } from '~context/ActionSidebarContext/ActionSidebarContext.ts';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import { tw } from '~utils/css/index.ts';
+import { COLONY_TEAMS_ROUTE } from '~routes/routeConstants.ts';
 import { formatText } from '~utils/intl.ts';
 import { ACTION_TYPE_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
+
+import useDropdown from '../hooks/useDropdown.ts';
+
+import { DropdownItem } from './DropdownItem.tsx';
+import DropdownMenu from './DropdownMenu.tsx';
 
 const displayName =
   'v5.frame.ColonyHome.ReputationChart.partials.TeamActionsMenu';
@@ -30,10 +33,6 @@ const MSG = defineMessages({
   },
 });
 
-// @TODO maybe decouple some of this into a dropdown menu component?
-// eslint-disable-next-line max-len
-const dropdownItemClassName = tw`flex w-full items-center gap-2 rounded-s px-3 py-2 text-md text-gray-900 hover:font-medium md:hover:bg-gray-50`;
-
 interface TeamActionsMenuProps {
   isDisabled?: boolean;
 }
@@ -42,21 +41,18 @@ const TeamActionsMenu: FC<TeamActionsMenuProps> = ({ isDisabled }) => {
   const {
     colony: { name: colonyName },
   } = useColonyContext();
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
-  const { getTooltipProps, setTooltipRef, setTriggerRef, visible } =
-    usePopperTooltip({
-      placement: 'bottom-end',
-      trigger: ['click'],
-      visible: isDropdownOpen,
-      onVisibleChange: setIsDropdownOpen,
-      interactive: true,
-      closeOnOutsideClick: true,
-      offset: [0, 0],
-    });
 
   const {
     actionSidebarToggle: [, { toggleOn: toggleActionSidebarOn }],
   } = useActionSidebarContext();
+
+  const {
+    setIsDropdownOpen,
+    setTriggerRef,
+    setTooltipRef,
+    getTooltipProps,
+    visible,
+  } = useDropdown();
 
   const handleCreateTeamClick = () => {
     toggleActionSidebarOn({
@@ -73,49 +69,29 @@ const TeamActionsMenu: FC<TeamActionsMenuProps> = ({ isDisabled }) => {
   };
 
   return (
-    <>
-      <button
-        type="button"
-        ref={setTriggerRef}
-        className={clsx('hover:text-blue-400', {
-          'text-gray-400': !visible,
-          'text-blue-400': visible,
-          'pointer-events-none text-gray-100': isDisabled,
-        })}
-      >
-        <DotsThreeVertical size={18} />
-      </button>
-      {visible && (
-        <div
-          ref={setTooltipRef}
-          {...getTooltipProps()}
-          className="z-dropdown w-full px-6 sm:w-auto sm:px-0"
-        >
-          <div className="flex w-full flex-col rounded-lg border border-gray-200 bg-base-white px-3 py-4 shadow-default sm:w-[14.25rem]">
-            <Link to={`/${colonyName}/teams`} className={dropdownItemClassName}>
-              <Eye size={18} />
-              <span>{formatText(MSG.viewAllTeams)}</span>
-            </Link>
-            <button
-              type="button"
-              className={dropdownItemClassName}
-              onClick={handleCreateTeamClick}
-            >
-              <LockKey size={18} />
-              <span>{formatText(MSG.createTeam)}</span>
-            </button>
-            <button
-              type="button"
-              className={dropdownItemClassName}
-              onClick={handleEditTeamClick}
-            >
-              <Pencil size={18} />
-              <span>{formatText(MSG.editTeam)}</span>
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+    <DropdownMenu
+      isDisabled={isDisabled}
+      visible={visible}
+      setTriggerRef={setTriggerRef}
+      setTooltipRef={setTooltipRef}
+      getTooltipProps={getTooltipProps}
+    >
+      <>
+        <Link to={`/${colonyName}/${COLONY_TEAMS_ROUTE}`} className="w-full">
+          <DropdownItem icon={Eye} label={formatText(MSG.viewAllTeams)} />
+        </Link>
+        <DropdownItem
+          icon={LockKey}
+          label={formatText(MSG.createTeam)}
+          onClick={handleCreateTeamClick}
+        />
+        <DropdownItem
+          icon={Pencil}
+          label={formatText(MSG.editTeam)}
+          onClick={handleEditTeamClick}
+        />
+      </>
+    </DropdownMenu>
   );
 };
 
