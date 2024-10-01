128e9df04c8ac0ada852b5909a32abb2a1afeb8f
Anastasiia <anastasiia@colony.io>
2024-10-01 14:08:26 +0200

    fix: update UI to match the design

--- src/components/v5/frame/ColonyHome/partials/FundsCards/FundsCards.tsx
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/FundsCards.tsx
@@ -10,6 +10,7 @@ import { formatText } from '~utils/intl.ts';
 import { ACTION_TYPE_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
 import WidgetCards from '~v5/common/WidgetCards/index.ts';
 
+import { useIsAddNewTeamVisible } from './hooks.ts';
 import { FundsCardsItem } from './partials/FundsCardsItem.tsx';
 import { FundsCardsTotalItem } from './partials/FundsCardsTotalItem.tsx';
 
@@ -26,7 +27,7 @@ export const FundsCards = () => {
 
   const subTeams = useSubDomains();
 
-  const isAddNewTeamVisible = !subTeams || subTeams.length < 1;
+  const isAddNewTeamVisible = useIsAddNewTeamVisible();
   return (
     <BalanceCurrencyContextProvider>
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-2 md:pt-2">
@@ -39,6 +40,7 @@ export const FundsCards = () => {
                 key={item?.id}
                 domainId={item?.id}
                 domainName={item?.metadata?.name}
+                nativeId={item.nativeId}
               />
             ))}
 
@@ -46,7 +48,11 @@ export const FundsCards = () => {
             <WidgetCards.Item
               variant="dashed"
               icon={Plus}
-              title={formatText({ id: 'dashboard.team.cards.createTeam' })}
+              title={
+                <div className="text-xs">
+                  {formatText({ id: 'dashboard.team.cards.createTeam' })}
+                </div>
+              }
               onClick={onNewTeamClick}
               className="justify-center uppercase text-gray-200"
             />
