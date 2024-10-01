0d0ca2b1db9f57763ed2773192937ec50ec5929d
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-10-01 16:44:01 +0200

    Fix: support balance exchange to CLNY token and refactoring
    Fix: support balance exchange to CLNY token and refactoring

--- src/components/v5/frame/ColonyHome/partials/FundsCards/hooks.ts
+++ src/components/v5/frame/ColonyHome/partials/FundsCards/hooks.ts
@@ -1,45 +1,70 @@
-import Decimal from 'decimal.js';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 
+import { useBalanceCurrencyContext } from '~context/BalanceCurrencyContext/BalanceCurrencyContext.ts';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
-import { useCurrencyContext } from '~context/CurrencyContext/CurrencyContext.ts';
 import {
   ExtendedSupportedCurrencies,
   TimeframeType,
-  type SupportedCurrencies,
   useGetCachedDomainBalanceQuery,
   useGetDomainBalanceQuery,
 } from '~gql';
 import { useCurrencyHistoricalConversionRate } from '~hooks/useCurrencyHistoricalConversionRate.ts';
 import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
-import { type ColonyBalances } from '~types/graphql.ts';
-import { notNull } from '~utils/arrays/index.ts';
 import { convertFromTokenToCurrency } from '~utils/currency/convertFromTokenToCurrency.ts';
-import { fetchCurrentPrice } from '~utils/currency/currency.ts';
 import { type CoinGeckoSupportedCurrencies } from '~utils/currency/index.ts';
 
 export const useTotalData = (domainId?: string) => {
-  const { colony } = useColonyContext();
+  const {
+    colony: { colonyAddress },
+  } = useColonyContext();
 
-  const { currency } = useCurrencyContext();
+  const {
+    currency,
+    convertAmount,
+    loading: currencyLoading,
+  } = useBalanceCurrencyContext();
 
-  const { data, loading } = useGetDomainBalanceQuery({
-    variables: {
-      input: {
-        colonyAddress: colony.colonyAddress,
-        domainId: domainId ?? '',
-        selectedCurrency: currency as unknown as ExtendedSupportedCurrencies,
-        timeframePeriod: 1,
-        timeframeType: TimeframeType.Total,
+  const memoizedQueryVariables = useMemo(() => {
+    const abortController = new AbortController();
+    return {
+      abortController,
+      queryOptions: {
+        variables: {
+          input: {
+            colonyAddress,
+            domainId: domainId ?? '',
+            selectedCurrency:
+              currency as unknown as ExtendedSupportedCurrencies,
+            timeframePeriod: 1,
+            timeframeType: TimeframeType.Total,
+          },
+        },
+        context: {
+          fetchOptions: {
+            signal: abortController.signal,
+          },
+        },
       },
-    },
-  });
+    };
+  }, [currency, domainId, colonyAddress]);
+
+  const { data, loading } = useGetDomainBalanceQuery(
+    memoizedQueryVariables.queryOptions,
+  );
+
+  useEffect(() => {
+    return () => {
+      if (loading) {
+        memoizedQueryVariables.abortController.abort();
+      }
+    };
+  }, [loading, memoizedQueryVariables.abortController]);
 
   const domainBalanceData = data?.getDomainBalance;
 
   return {
-    total: domainBalanceData?.total ?? '0',
-    loading,
+    total: convertAmount(domainBalanceData?.total ?? '0'),
+    loading: loading || currencyLoading,
   };
 };
 export const usePreviousTotalData = () => {
@@ -47,16 +72,44 @@ export const usePreviousTotalData = () => {
     colony: { colonyAddress },
   } = useColonyContext();
   const selectedDomain = useGetSelectedDomainFilter();
-  const { currency } = useCurrencyContext();
-  const { data, loading } = useGetCachedDomainBalanceQuery({
-    variables: {
-      colonyAddress,
-      filter: {
-        domainId: { eq: selectedDomain?.id ?? '' },
-        timeframeType: { eq: TimeframeType.Total },
+  const {
+    currency,
+    convertAmount,
+    loading: currencyLoading,
+  } = useBalanceCurrencyContext();
+
+  const memoizedQueryVariables = useMemo(() => {
+    const abortController = new AbortController();
+    return {
+      abortController,
+      queryOptions: {
+        variables: {
+          colonyAddress,
+          filter: {
+            domainId: { eq: selectedDomain?.id ?? '' },
+            timeframeType: { eq: TimeframeType.Total },
+          },
+        },
+        context: {
+          fetchOptions: {
+            signal: abortController.signal,
+          },
+        },
       },
-    },
-  });
+    };
+  }, [selectedDomain?.id, colonyAddress]);
+
+  const { data, loading } = useGetCachedDomainBalanceQuery(
+    memoizedQueryVariables.queryOptions,
+  );
+
+  useEffect(() => {
+    return () => {
+      if (loading) {
+        memoizedQueryVariables.abortController.abort();
+      }
+    };
+  }, [loading, memoizedQueryVariables.abortController]);
 
   const previousBalance = data?.cacheTotalBalanceByColonyAddress?.items[0];
 
@@ -66,79 +119,19 @@ export const usePreviousTotalData = () => {
     conversionDenomination: currency as unknown as CoinGeckoSupportedCurrencies,
   });
 
+  /**
+   * The cached data is stored in USDC due to the running the lambda at a scheduled time and not on demand
+   */
+  const previousTotal = convertFromTokenToCurrency(
+    previousBalance?.totalUSDC,
+    conversionRate,
+  );
+
   return {
-    loading,
+    loading: loading || currencyLoading,
     /**
      * The cached data is stored in USDC due to the running the lambda at a scheduled time and not on demand
      */
-    previousTotal: convertFromTokenToCurrency(
-      previousBalance?.totalUSDC,
-      conversionRate,
-    ),
+    previousTotal: convertAmount(previousTotal),
   };
-};
-
-const calculateTotalFunds = async (
-  balances: ColonyBalances,
-  currency: SupportedCurrencies,
-  selectedDomainNativeId: number | undefined,
-): Promise<Decimal | null> => {
-  let isError = false;
-  const funds = balances.items
-    ?.filter(notNull)
-    .filter(({ domain }) =>
-      !selectedDomainNativeId
-        ? domain === null
-        : domain?.nativeId === selectedDomainNativeId,
-    )
-    .reduce(
-      async (total, { balance, token: { tokenAddress, decimals } }) => {
-        const currentPrice = await fetchCurrentPrice({
-          contractAddress: tokenAddress,
-          conversionDenomination: currency,
-        });
-
-        if (currentPrice == null) {
-          isError = true;
-        }
-
-        const balanceInWeiToEth = new Decimal(balance).div(10 ** decimals);
-
-        return (await total).add(
-          new Decimal(balanceInWeiToEth).mul(currentPrice ?? 0),
-        );
-      },
-      Promise.resolve(new Decimal(0)),
-    );
-
-  if (isError) {
-    return null;
-  }
-
-  return (await funds) ?? new Decimal(0);
-};
-
-export const useTotalFunds = () => {
-  const selectedDomain = useGetSelectedDomainFilter();
-  const { colony } = useColonyContext();
-  const { currency } = useCurrencyContext();
-  const { balances: colonyBalances } = colony || {};
-  const [totalFunds, setTotalFunds] = useState<Decimal | null>(new Decimal(0));
-
-  useEffect(() => {
-    const getTotalFunds = async (balances: ColonyBalances) => {
-      const funds = await calculateTotalFunds(
-        balances,
-        currency,
-        selectedDomain?.nativeId,
-      );
-      setTotalFunds(funds);
-    };
-
-    if (colonyBalances) {
-      getTotalFunds(colonyBalances);
-    }
-  }, [colonyBalances, currency, selectedDomain]);
-
-  return totalFunds;
 };
