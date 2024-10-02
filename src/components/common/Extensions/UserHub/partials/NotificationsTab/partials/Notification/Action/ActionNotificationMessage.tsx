99d3dacdc8b4c1db2894ab01c61c06741146bfa8
Dave Creaser <david.creaser@outlook.com>
2024-10-02 10:02:59 +0100

    Handle advanced payment notifications

---
+++ src/components/common/Extensions/UserHub/partials/NotificationsTab/partials/Notification/Action/ActionNotificationMessage.tsx
@@ -0,0 +1,117 @@
+import React, { useMemo, type FC } from 'react';
+import { defineMessages } from 'react-intl';
+
+import { getActionTitleValues } from '~common/ColonyActions/helpers/index.ts';
+import {
+  type ColonyActionFragment,
+  type NotificationColonyFragment,
+} from '~gql';
+import { useAmountLessFee } from '~hooks/useAmountLessFee.ts';
+import { NotificationType, type Notification } from '~types/notifications.ts';
+import { formatText } from '~utils/intl.ts';
+
+import NotificationMessage from '../NotificationMessage.tsx';
+
+const displayName =
+  'common.Extensions.UserHub.partials.ActionNotificationMessage';
+
+interface ActionNotificationMessageProps {
+  action: ColonyActionFragment | null | undefined;
+  colony: NotificationColonyFragment | null | undefined;
+  creator: string;
+  loading: boolean;
+  notification: Notification;
+}
+
+const MSG = defineMessages({
+  mention: {
+    id: `${displayName}.mention`,
+    defaultMessage: '{name} has mentioned you in: ',
+  },
+  someone: {
+    id: `${displayName}.someone`,
+    defaultMessage: 'Someone',
+  },
+  unknownAction: {
+    id: `${displayName}.unknownAction`,
+    defaultMessage: 'Unknown action',
+  },
+});
+
+const ActionNotificationMessage: FC<ActionNotificationMessageProps> = ({
+  action,
+  colony,
+  creator,
+  loading,
+  notification,
+}) => {
+  const amountLessFee = useAmountLessFee(action?.amount, action?.networkFee);
+
+  const actionMetadataDescription = useMemo(() => {
+    if (!action || !colony) {
+      return formatText(MSG.unknownAction);
+    }
+
+    return formatText(
+      { id: 'action.title' },
+      getActionTitleValues({
+        actionData: { ...action, amount: amountLessFee },
+        colony: {
+          nativeToken: {
+            ...colony.nativeToken,
+          },
+          metadata: colony.metadata,
+        },
+      }),
+    );
+  }, [action, amountLessFee, colony]);
+
+  const Message = useMemo(() => {
+    if (
+      !creator ||
+      !action ||
+      !notification.customAttributes?.notificationType
+    ) {
+      return formatText(MSG.unknownAction);
+    }
+
+    if (
+      notification.customAttributes?.notificationType ===
+      NotificationType.PermissionsAction
+    ) {
+      return (
+        <>
+          {action.metadata?.customTitle && `${action.metadata?.customTitle}: `}
+          {actionMetadataDescription}
+        </>
+      );
+    }
+
+    if (
+      notification.customAttributes?.notificationType ===
+      NotificationType.Mention
+    ) {
+      return (
+        <>
+          {formatText(MSG.mention, {
+            name: creator || formatText(MSG.someone),
+          })}
+          {action.metadata?.customTitle || actionMetadataDescription}
+        </>
+      );
+    }
+
+    return null;
+  }, [
+    action,
+    actionMetadataDescription,
+    creator,
+    notification.customAttributes?.notificationType,
+  ]);
+
+  return <NotificationMessage loading={loading}>{Message}</NotificationMessage>;
+};
+
+ActionNotificationMessage.displayName = displayName;
+
+export default ActionNotificationMessage;
---
+++ src/components/common/Extensions/UserHub/partials/NotificationsTab/partials/Notification/Expenditure/ExpenditureNotificationMessage.tsx
@@ -0,0 +1,125 @@
+import React, { useMemo, type FC } from 'react';
+import { defineMessages } from 'react-intl';
+
+import { getActionTitleValues } from '~common/ColonyActions/helpers/index.ts';
+import {
+  type ColonyActionFragment,
+  type NotificationColonyFragment,
+  type ExpenditureFragment,
+} from '~gql';
+import { NotificationType, type Notification } from '~types/notifications.ts';
+import { formatText } from '~utils/intl.ts';
+
+import NotificationMessage from '../NotificationMessage.tsx';
+
+const displayName =
+  'common.Extensions.UserHub.partials.ExpenditureNotificationMessage';
+
+interface ExpenditureNotificationMessageProps {
+  action: ColonyActionFragment | null | undefined;
+  colony: NotificationColonyFragment | null | undefined;
+  creator: string;
+  expenditure: ExpenditureFragment | null | undefined;
+  loading: boolean;
+  notification: Notification;
+}
+
+const MSG = defineMessages({
+  finalized: {
+    id: `${displayName}.finalized`,
+    defaultMessage: 'Payment made:',
+  },
+  cancelled: {
+    id: `${displayName}.cancelled`,
+    defaultMessage: 'Payment cancelled:',
+  },
+  funding: {
+    id: `${displayName}.funding`,
+    defaultMessage: 'Payment ready for funding:',
+  },
+  release: {
+    id: `${displayName}.release`,
+    defaultMessage: 'Payment funded:',
+  },
+  unknownAction: {
+    id: `${displayName}.unknownAction`,
+    defaultMessage: 'A payment was updated',
+  },
+  unknownChange: {
+    id: `${displayName}.unknownChange`,
+    defaultMessage: 'Payment updated: ',
+  },
+});
+
+const ExpenditureNotificationMessage: FC<
+  ExpenditureNotificationMessageProps
+> = ({ action, colony, creator, expenditure, loading, notification }) => {
+  const actionMetadataDescription = useMemo(() => {
+    if (!expenditure || !action || !colony) {
+      return formatText(MSG.unknownAction);
+    }
+
+    return formatText(
+      { id: 'action.title' },
+      getActionTitleValues({
+        actionData: action,
+        colony: {
+          nativeToken: {
+            ...colony.nativeToken,
+          },
+          metadata: colony.metadata,
+        },
+        expenditureData: expenditure,
+      }),
+    );
+  }, [action, colony, expenditure]);
+
+  const Message = useMemo(() => {
+    if (
+      !expenditure ||
+      !creator ||
+      !action ||
+      !notification.customAttributes?.notificationType
+    ) {
+      return formatText(MSG.unknownAction);
+    }
+
+    // If this is an "Expenditure ready for review" action, we simply display it in the same way as an "action created" notification.
+    if (
+      notification.customAttributes.notificationType ===
+      NotificationType.ExpenditureReadyForReview
+    ) {
+      return (
+        <>
+          {action.metadata?.customTitle}: {actionMetadataDescription}
+        </>
+      );
+    }
+
+    const firstPart = {
+      [NotificationType.ExpenditureReadyForFunding]: formatText(MSG.funding),
+      [NotificationType.ExpenditureReadyForRelease]: formatText(MSG.release),
+      [NotificationType.ExpenditureCancelled]: formatText(MSG.cancelled),
+      [NotificationType.ExpenditureFinalized]: formatText(MSG.finalized),
+      default: formatText(MSG.unknownChange),
+    }[notification.customAttributes?.notificationType];
+
+    return (
+      <>
+        {firstPart} {action.metadata?.customTitle || actionMetadataDescription}
+      </>
+    );
+  }, [
+    action,
+    actionMetadataDescription,
+    creator,
+    expenditure,
+    notification.customAttributes?.notificationType,
+  ]);
+
+  return <NotificationMessage loading={loading}>{Message}</NotificationMessage>;
+};
+
+ExpenditureNotificationMessage.displayName = displayName;
+
+export default ExpenditureNotificationMessage;
