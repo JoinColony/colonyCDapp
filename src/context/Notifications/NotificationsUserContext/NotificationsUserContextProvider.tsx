e5a04bc9ffcdd40e69477ed3d64bce0c5af103a4
Bassgeta <kerry.mahne@gmail.com>
2024-10-02 14:47:58 +0200

    feat: move MagicBellProvider to NotificationsDataContextProvider

--- src/context/Notifications/NotificationsUserContext/NotificationsUserContextProvider.tsx
+++ src/context/Notifications/NotificationsUserContext/NotificationsUserContextProvider.tsx
@@ -1,7 +1,5 @@
-import { MagicBellProvider } from '@magicbell/react-headless';
 import React, { type ReactNode, useEffect, useMemo } from 'react';
 
-import { isDev } from '~constants';
 import { useAppContext } from '~context/AppContext/AppContext.ts';
 import { useCreateUserNotificationsDataMutation } from '~gql';
 
@@ -62,36 +60,11 @@ const NotificationsUserContextProvider = ({
   }
 
   return (
-    <MagicBellProvider
-      apiKey={import.meta.env.MAGICBELL_API_KEY}
-      userExternalId={user.notificationsData.magicbellUserId}
-      stores={
-        isDev
-          ? [
-              {
-                id: 'dev-store',
-                defaultQueryParams: {
-                  category: import.meta.env.MAGICBELL_DEV_KEY,
-                  // eslint-disable-next-line camelcase
-                  per_page: 10,
-                },
-              },
-            ]
-          : [
-              {
-                id: 'store',
-                // eslint-disable-next-line camelcase
-                defaultQueryParams: { per_page: 10 },
-              },
-            ]
-      }
-    >
-      <NotificationsUserContext.Provider value={value}>
-        <NotificationsDataContextProvider>
-          {children}
-        </NotificationsDataContextProvider>
-      </NotificationsUserContext.Provider>
-    </MagicBellProvider>
+    <NotificationsUserContext.Provider value={value}>
+      <NotificationsDataContextProvider>
+        {children}
+      </NotificationsDataContextProvider>
+    </NotificationsUserContext.Provider>
   );
 };
 
