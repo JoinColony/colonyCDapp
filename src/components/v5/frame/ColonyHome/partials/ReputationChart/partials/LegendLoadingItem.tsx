8c891311b5e389638ecd8de48b8bf2c129de9451
Anastasiia <anastasiia@colony.io>
2024-10-03 14:54:26 +0200

    fix: update chart loading legend

--- src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/LegendLoadingItem.tsx
+++ src/components/v5/frame/ColonyHome/partials/ReputationChart/partials/LegendLoadingItem.tsx
@@ -9,7 +9,7 @@ const LegendLoadingItem = () => {
   return (
     <div className="flex grow flex-row items-center gap-1 pr-0.5">
       <LoadingSkeleton className="h-2.5 w-2.5 rounded-full" isLoading />
-      <LoadingSkeleton className="h-4 min-w-12 grow rounded" isLoading />
+      <LoadingSkeleton className="h-4 min-w-8 grow rounded" isLoading />
       <LoadingSkeleton className="h-4 w-5 rounded" isLoading />
     </div>
   );
