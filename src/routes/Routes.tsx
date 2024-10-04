cffc74b0434af4ac93579e0a71a91295ff2c6657
strzelec <strzelec@devanddeliver.com>
2024-10-04 08:41:39 +0200

    feat: add streaming payments subpage

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -22,6 +22,7 @@ import MembersPage, {
   ContributorsPage,
 } from '~frame/v5/pages/MembersPage/index.ts';
 import OnboardingPage from '~frame/v5/pages/OnboardingPage/index.ts';
+import StreamingPaymentsPage from '~frame/v5/pages/StreamingPaymentsPage/StreamingPaymentsPage.tsx';
 import TeamsPage from '~frame/v5/pages/TeamsPage/index.ts';
 import UserAdvancedPage from '~frame/v5/pages/UserAdvancedPage/index.ts';
 import UserCryptoToFiatPage from '~frame/v5/pages/UserCryptoToFiatPage/CryptoToFiatPage.tsx';
@@ -65,6 +66,7 @@ import {
   COLONY_AGREEMENTS_ROUTE,
   USER_CRYPTO_TO_FIAT_ROUTE,
   RESERVED_ROUTES,
+  COLONY_STREAMING_PAYMENTS_ROUTE,
   // ACTIONS_PAGE_ROUTE,
   // UNWRAP_TOKEN_ROUTE,
   // CLAIM_TOKEN_ROUTE,
@@ -148,6 +150,10 @@ const Routes = () => {
             <Route path={COLONY_VERIFIED_ROUTE} element={<VerifiedPage />} />
             <Route path={COLONY_BALANCES_ROUTE} element={<BalancePage />} />
             <Route path={COLONY_TEAMS_ROUTE} element={<TeamsPage />} />
+            <Route
+              path={COLONY_STREAMING_PAYMENTS_ROUTE}
+              element={<StreamingPaymentsPage />}
+            />
           </Route>
 
           <Route path={COLONY_REPUTATION_ROUTE} element={<ReputationPage />} />
