290e75330ec26b539166923b624c450b59d5d47e
strzelec <strzelec@devanddeliver.com>
2024-10-11 14:15:45 +0200

    feat: add widgets for streaming payments page

---
+++ src/components/frame/v5/pages/StreamingPaymentsPage/partials/StatsCards/StatsCards.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+import { type NumeralValue } from '~shared/Numeral/Numeral.tsx';
+import Numeral from '~shared/Numeral/Numeral.tsx';
+import { formatText } from '~utils/intl.ts';
+import WidgetBox from '~v5/common/WidgetBox/WidgetBox.tsx';
+
+export interface StatsCardsProps {
+  streamingPerMonth: NumeralValue;
+  totalActiveStreams: number;
+  totalStreamed: NumeralValue;
+  unclaimedFounds: NumeralValue;
+  prefix: string;
+  suffix: string;
+}
+
+const displayName = 'v5.pages.StreamingPaymentsPage.partials.StatsCards';
+
+const StatsCards = ({
+  streamingPerMonth,
+  totalActiveStreams,
+  totalStreamed,
+  unclaimedFounds,
+  prefix,
+  suffix,
+}: StatsCardsProps) => {
+  return (
+    <div className="mt-6 flex flex-col gap-2 md:mt-0  md:flex-row md:gap-6">
+      <WidgetBox
+        title={formatText({
+          id: 'streamingPaymentsPage.widget.totalStreaming',
+        })}
+        value={
+          <div className="align-center flex gap-2">
+            <div className="flex">
+              <h4 className="font-semibold heading-4">{prefix}</h4>
+              <Numeral
+                className="self-center font-semibold heading-4"
+                value={streamingPerMonth}
+              />
+            </div>
+            <p className="self-center text-1">{suffix} / month</p>
+          </div>
+        }
+      />
+
+      <WidgetBox
+        title={formatText({
+          id: 'streamingPaymentsPage.widget.totalStreamed',
+        })}
+        value={
+          <div className="align-center flex gap-2">
+            <div className="flex">
+              <h4 className="font-semibold heading-4">{prefix}</h4>
+              <Numeral
+                className="self-center font-semibold heading-4"
+                value={totalStreamed}
+              />
+            </div>
+            <p className="self-center text-1">{suffix}</p>
+          </div>
+        }
+      />
+      <WidgetBox
+        title={formatText({
+          id: 'streamingPaymentsPage.widget.unclaimedFounds',
+        })}
+        value={
+          <div className="align-center flex gap-2">
+            <div className="flex">
+              <h4 className="font-semibold heading-4">{prefix}</h4>
+              <Numeral
+                className="self-center font-semibold heading-4"
+                value={unclaimedFounds}
+              />
+            </div>
+            <p className="self-center text-1">{suffix}</p>
+          </div>
+        }
+      />
+      <WidgetBox
+        title={formatText({
+          id: 'streamingPaymentsPage.widget.totalActiveStreams',
+        })}
+        value={<h4 className="font-thin heading-4">{totalActiveStreams}</h4>}
+      />
+    </div>
+  );
+};
+
+StatsCards.displayName = displayName;
+
+export default StatsCards;
