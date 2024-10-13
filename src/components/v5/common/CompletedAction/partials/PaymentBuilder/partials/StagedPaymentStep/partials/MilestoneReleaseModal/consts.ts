f9dbe1724cbe459a355eeed6adeb93a8cfd9bd28
Jakub Zajac <jakub@colony.io>
2024-10-13 19:44:54 +0100

    Fix: User role not showing in release modal

--- src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/StagedPaymentStep/partials/MilestoneReleaseModal/consts.ts
+++ src/components/v5/common/CompletedAction/partials/PaymentBuilder/partials/StagedPaymentStep/partials/MilestoneReleaseModal/consts.ts
@@ -3,17 +3,24 @@ import { object, string } from 'yup';
 import { DecisionMethod } from '~types/actions.ts';
 import { formatText } from '~utils/intl.ts';
 
-export const milestoneReleaseDecisionMethodDescriptions = {
-  [DecisionMethod.Permissions]: formatText({
-    id: 'fundingModal.permissionsDescription',
-  }),
+export const getMilestoneReleaseDecisionMethodDescriptions = (
+  userRole: string,
+) => ({
+  [DecisionMethod.Permissions]: formatText(
+    {
+      id: 'fundingModal.permissionsDescription',
+    },
+    {
+      permission: userRole,
+    },
+  ),
   [DecisionMethod.Reputation]: formatText({
     id: 'fundingModal.reputationDescription',
   }),
   [DecisionMethod.PaymentCreator]: formatText({
     id: 'releaseModal.paymentCreatorDescription',
   }),
-};
+});
 
 export const getValidationSchema = () =>
   object()
