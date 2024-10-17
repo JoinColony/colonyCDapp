c0c7a1c12a1365394e29c71bbff6db185ffc15ad
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2024-10-17 07:54:51 +0200

    Fix: Update condition for closing the action sidebar
    Fix: Update condition for closing the action sidebar

--- src/components/frame/Extensions/layouts/ColonyLayout.tsx
+++ src/components/frame/Extensions/layouts/ColonyLayout.tsx
@@ -83,7 +83,7 @@ const ColonyLayout: FC<PropsWithChildren> = ({ children }) => {
   const previousTransactionId = usePrevious(transactionId);
 
   useLocationChange(() => {
-    if ((!!previousTransactionId && !transactionId) || isActionSidebarOpen) {
+    if (!!previousTransactionId && !transactionId && isActionSidebarOpen) {
       toggleActionSidebarOff();
     }
   });
