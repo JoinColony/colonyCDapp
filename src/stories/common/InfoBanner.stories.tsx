8b4817a5170d16367bc21a57f61bd3af504ac529
strzelec <strzelec@devanddeliver.com>
2024-10-18 15:57:44 +0200

    feat: add stories for info banner

---
+++ src/stories/common/InfoBanner.stories.tsx
@@ -0,0 +1,77 @@
+import { type Meta, type StoryObj } from '@storybook/react';
+import React from 'react';
+
+import InfoBanner from '~frame/LandingPage/InfoBanner.tsx';
+
+const meta: Meta<typeof InfoBanner> = {
+  title: 'Common/Info Banner',
+  component: InfoBanner,
+  decorators: [
+    (Story) => (
+      <div className="mx-auto max-w-[41.75rem]">
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    title: {
+      name: 'Title',
+      control: {
+        type: 'text',
+      },
+    },
+    text: {
+      name: 'Text',
+      control: {
+        type: 'text',
+      },
+    },
+    variant: {
+      name: 'Variant',
+      control: {
+        type: 'text',
+      },
+    },
+    loading: {
+      name: 'Loading',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof InfoBanner>;
+
+export const Info: Story = {
+  args: {
+    title: 'Title of info banner',
+    text: 'Text of info banner',
+    variant: 'info',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    title: 'Title of success banner',
+    text: 'Text of success banner',
+    variant: 'success',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    title: 'Title of error banner',
+    text: 'Text of error banner',
+    variant: 'error',
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    title: 'Title of loading banner',
+    text: 'Text of loading banner',
+    loading: true,
+  },
+};
