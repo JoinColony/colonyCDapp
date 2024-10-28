869ccddbc2fd6863e3c3d3f73a6f01ca20470cd2
Olha Pidlisnyuk <olha@colony.io>
2024-10-28 15:50:01 +0100

    test: disable workers for a single file tests and renamed script name

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,8 +12,6 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  */
 export default defineConfig({
   testDir: './playwright/e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
