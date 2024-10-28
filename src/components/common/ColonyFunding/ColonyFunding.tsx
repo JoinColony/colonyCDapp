9ace7de0dabbe96ee99d2010e0712e97fcaa9906
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-10-28 14:03:59 +0000

    Fix: Comment out team filter on incoming funds page

--- src/components/common/ColonyFunding/ColonyFunding.tsx
+++ src/components/common/ColonyFunding/ColonyFunding.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useSetPageHeadingTitle } from '~context/PageHeadingContext/PageHeadingContext.ts';
 import FundsTable from '~frame/v5/pages/FundsPage/partials/FundsTable/index.ts';
 import { formatText } from '~utils/intl.ts';
-import ContentWithTeamFilter from '~v5/frame/ContentWithTeamFilter/ContentWithTeamFilter.tsx';
+// import ContentWithTeamFilter from '~v5/frame/ContentWithTeamFilter/ContentWithTeamFilter.tsx';
 
 const displayName = 'common.ColonyFunding';
 
@@ -11,9 +11,10 @@ const ColonyFunding = () => {
   useSetPageHeadingTitle(formatText({ id: 'incomingFundsPage.title' }));
 
   return (
-    <ContentWithTeamFilter>
-      <FundsTable />
-    </ContentWithTeamFilter>
+    // @TODO: Uncomment when contracts support incoming funds per team
+    // <ContentWithTeamFilter>
+    <FundsTable />
+    // </ContentWithTeamFilter>
   );
 };
 
