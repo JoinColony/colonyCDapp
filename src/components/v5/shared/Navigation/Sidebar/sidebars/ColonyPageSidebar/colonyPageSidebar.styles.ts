3ff39c1c1faa22c06bbff6ce0dd7825e29afe68f
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-10-30 12:01:54 +0000

    Fix: Change navigation z-index so team switcher does not clip

--- src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/colonyPageSidebar.styles.ts
+++ src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/colonyPageSidebar.styles.ts
@@ -6,7 +6,7 @@ export const colonyPageSidebarTabletClass = clsx(
   /** Unfortunately, it's a bit tricky to string interpolate tailwind classes */
   'top-[calc(var(--header-nav-section-height)+var(--top-content-height))]',
   'fixed left-0 !h-[calc(100vh-var(--header-nav-section-height)-var(--top-content-height))] w-full',
-  'z-sidebar flex flex-col justify-between overflow-y-auto bg-base-white p-3 md:bg-gray-900',
+  'z-userNav flex flex-col justify-between overflow-y-auto bg-base-white p-3 md:bg-gray-900',
 );
 
 export const colonyPageSidebarDesktopClass = clsx(
