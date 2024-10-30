d3ed3d0f5afd64e1cad2309b7b2ced0012f408e4
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2024-10-30 12:17:33 +0000

    Fix: Scrollbar shifts main content

--- src/components/v5/frame/PageLayout/PageLayout.tsx
+++ src/components/v5/frame/PageLayout/PageLayout.tsx
@@ -66,7 +66,7 @@ const PageLayout: FC<PropsWithChildren<PageLayoutProps>> = ({
             </section>
             <section
               className={clsx(
-                'modal-blur h-full w-full overflow-auto px-6 md:p-8 md:pb-0 md:pt-2',
+                'modal-blur h-full w-full overflow-auto px-6 scrollbar-gutter-stable md:p-8 md:pb-0 md:pt-2',
                 {
                   'md:!pt-[1.125rem]': !pageTitle,
                 },
