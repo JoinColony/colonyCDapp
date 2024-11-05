58daa142976d25851a385a8f80287ffc40704db9
strzelec <strzelec@devanddeliver.com>
2024-11-05 14:42:47 +0100

    fix: colony card

--- src/components/frame/LandingPage/ColonyCards/ColonyCard.tsx
+++ src/components/frame/LandingPage/ColonyCards/ColonyCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
-import { getFormattedTokenAmount } from '~v5/common/CompletedAction/partials/utils.ts';
+import { getFormattedNumeralValue } from '~shared/Numeral/helpers.tsx';
+import { convertToDecimal } from '~utils/convertToDecimal.ts';
+import ColonyAvatar from '~v5/shared/ColonyAvatar/ColonyAvatar.tsx';
 
 import BaseColonyCard from './BaseColonyCard/BaseColonyCard.tsx';
 
 export interface ColonyCardProps {
+  colonyAddress: string;
   colonyName: string;
-  colonyAvatar: string;
+  colonyAvatar?: string;
   membersCount: number;
 }
 
@@ -21,33 +24,38 @@ const MSG = defineMessages({
 });
 
 export const ColonyCard = ({
+  colonyAddress,
   colonyName,
   colonyAvatar,
   membersCount = 0,
-}: ColonyCardProps) => (
-  <BaseColonyCard
-    isClickable
-    avatarPlaceholder={
-      <img
-        src={colonyAvatar}
-        alt="Colony avatar"
-        className="w-8 rounded-full"
-      />
-    }
-  >
-    <p className="flex-1 truncate text-ellipsis text-md font-medium">
-      {colonyName}
-    </p>
-    <p className="max-w-[150px] justify-self-end text-xs font-normal text-gray-600">
-      <FormattedMessage
-        {...MSG.members}
-        values={{
-          members: getFormattedTokenAmount(`${membersCount}`, 0),
-        }}
-      />
-    </p>
-  </BaseColonyCard>
-);
+}: ColonyCardProps) => {
+  const membersDecimalValue = convertToDecimal(membersCount, 0);
+
+  return (
+    <BaseColonyCard
+      isClickable
+      avatarPlaceholder={
+        <ColonyAvatar
+          colonyAddress={colonyAddress}
+          size={32}
+          colonyImageSrc={colonyAvatar}
+        />
+      }
+    >
+      <p className="flex-1 truncate text-ellipsis text-md font-medium">
+        {colonyName}
+      </p>
+      <p className="max-w-[150px] justify-self-end text-xs font-normal text-gray-600">
+        <FormattedMessage
+          {...MSG.members}
+          values={{
+            members: getFormattedNumeralValue(membersDecimalValue, 0),
+          }}
+        />
+      </p>
+    </BaseColonyCard>
+  );
+};
 
 ColonyCard.displayName = displayName;
 
