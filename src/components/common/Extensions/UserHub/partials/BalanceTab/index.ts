4844ed59508b56ad86b07b7cfb277ec8574e78f2
Cezary Dryl <cezary.dryl@pagepro.pl>
2024-11-07 09:40:27 +0100

    fixes after rebase

---
+++ src/components/common/Extensions/UserHub/partials/BalanceTab/index.ts
@@ -0,0 +1,3 @@
+export { default } from './BalanceTab.tsx';
+export { default as TotalReputation } from './partials/TotalReputation.tsx';
+export { default as PendingReputation } from './partials/PendingReputation/index.ts';
