adc89e979829a99693a43fca9161d8cd383154e2
strzelec <strzelec@devanddeliver.com>
2024-11-08 14:32:04 +0100

    fix: code review improvements and adjust to new designs

--- src/components/frame/LandingPage/LandingPageLoadingSkeleton.tsx
+++ src/components/frame/LandingPage/LandingPageLoadingSkeleton.tsx
@@ -15,20 +15,20 @@ const LandingPageLoadingSkeleton = ({
   loadingCards = false,
 }: LandingPageLoadingSkeletonProps) => {
   return loadingCards ? (
-    <div className="flex h-full w-full items-end px-6 pb-8 md:px-0 md:pb-[3.125rem]">
-      <div className="mt-3 flex h-full w-full flex-col justify-between">
-        <div className="flex h-full w-full flex-col justify-end">
+    <div className="h-full w-full px-6 pb-8 pt-[1.625rem] md:px-0 md:pb-[3.125rem] md:pt-0">
+      <div className="flex flex-col justify-end md:h-full">
+        <div className="flex w-full flex-col justify-end md:h-full md:max-h-[calc(100%-126px)]">
           <LoadingSkeleton
             isLoading
-            className="rounded-4 mb-3 h-[2.375rem] w-full max-w-[25rem]"
+            className="mb-3 h-[2.375rem] w-full max-w-[25rem] rounded"
           />
           <LoadingSkeleton
             isLoading
-            className="rounded-4 mb-[1.625rem] h-5 w-full max-w-[20.0625rem]"
+            className="mb-[1.625rem] h-5 w-full max-w-[20.0625rem] rounded"
           />
           <LoadingSkeleton
             isLoading
-            className="rounded-4 mb-4 h-[.6875rem] w-full max-w-[5.625rem]"
+            className="mb-4 h-[.6875rem] w-full max-w-[5.625rem] rounded"
           />
           <div className="mb-4 flex flex-col gap-4 md:min-h-[28.125rem]">
             <ColonyCardSkeleton />
@@ -37,11 +37,8 @@ const LandingPageLoadingSkeleton = ({
             <ColonyCardSkeleton />
           </div>
         </div>
-        <div className="w-full">
-          <LoadingSkeleton
-            isLoading
-            className="rounded-4 h-[2.125rem] w-full"
-          />
+        <div className="hidden w-full md:block">
+          <LoadingSkeleton isLoading className="h-[2.125rem] w-full rounded" />
         </div>
       </div>
     </div>
@@ -51,19 +48,16 @@ const LandingPageLoadingSkeleton = ({
         <div className="flex h-full w-full flex-col justify-center">
           <LoadingSkeleton
             isLoading
-            className="rounded-4 mb-3 h-[2.375rem] w-full max-w-[25rem]"
+            className="mb-3 h-[2.375rem] w-full max-w-[25rem] rounded"
           />
           <LoadingSkeleton
             isLoading
-            className="rounded-4 mb-8 h-5 w-full max-w-[20.0625rem]"
+            className="mb-8 h-5 w-full max-w-[20.0625rem] rounded"
           />
           <div className="mb-8">
             <InfoBanner text="" title="" loading />
           </div>
-          <LoadingSkeleton
-            isLoading
-            className="rounded-4 h-[2.125rem] w-full"
-          />
+          <LoadingSkeleton isLoading className="h-[2.125rem] w-full rounded" />
         </div>
       </div>
     </div>
