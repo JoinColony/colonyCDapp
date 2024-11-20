e320ca20f56ddd6a934f7349b4617675c529c869
Alex Rea <git@area.io>
2024-11-20 16:02:59 +0000

    Give hardhat:cmd ability to execute on other chains

--- scripts/hardhat-cmd.js
+++ scripts/hardhat-cmd.js
@@ -3,7 +3,7 @@
 const { exec } = require('node:child_process');
 const parseArgs = require('minimist')
 
-strings = ['hash', 'data', 'from', 'to'];
+strings = ['hash', 'data', 'from', 'to', 'network'];
 const argv = parseArgs(process.argv.slice(2), { string: strings });
 
 const action = argv._[0];
@@ -11,11 +11,16 @@ delete argv._;
 
 let cmdFlags = '';
 for (const [k, v] of Object.entries(argv)) {
-  cmdFlags += `--${k} ${v} `;
+  if (k !== 'network') { // network is an argument for docker, not hardhat
+    cmdFlags += `--${k} ${v} `;
+  }
 };
 
+let network = argv.network || 'network';
+
+
 const hardhatCmd = `npx hardhat ${action} ${cmdFlags.trim()}`;
-const fullCmd = `docker exec -t network bash -c \"cd colonyNetwork && ${hardhatCmd}\"`;
+const fullCmd = `docker exec -t ${network} bash -c \"cd colonyNetwork && ${hardhatCmd}\"`;
 
 exec(fullCmd, (error, stdout, stderr) => {
   if (error) {
