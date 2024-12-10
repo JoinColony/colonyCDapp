926716529826087c6af4d73b3a64325a288b6365
Olha Pidlisnyuk <olha@colony.io>
2024-12-10 15:44:50 +0100

    test: advanced payment e2e tests

--- playwright/fixtures/test-data.ts
+++ playwright/fixtures/test-data.ts
@@ -1,20 +1,17 @@
+// Represents content of the expenditures_batch.csv
 export const expendituresBatchRecipients = [
   {
     username: 'fry',
     address: '0x27fF0C145E191C22C75cD123C679C3e1F58a4469',
     amount: '2',
     claimDelay: '24',
+    tokenSymbol: 'ETH',
   },
   {
     username: 'amy',
     address: '0x9dF24e73f40b2a911Eb254A8825103723E13209C',
     amount: '3',
     claimDelay: '12',
-  },
-  {
-    username: 'ulysses-ultimate',
-    address: '0x2da1037D928b395D118f5C72B23C93Ea43892DF0',
-    amount: '5',
-    claimDelay: '6',
+    tokenSymbol: 'ETH',
   },
 ];
