092a1899eff26b33d037d09b01c39e275f8d0c5d
Jakub Zajac <jakub@colony.io>
2024-12-17 23:26:37 +0000

    Temp: Show output of arbitrary transaction decode in action view

--- src/components/v5/common/CompletedAction/partials/ArbitraryTransaction/ArbitraryTransaction.tsx
+++ src/components/v5/common/CompletedAction/partials/ArbitraryTransaction/ArbitraryTransaction.tsx
@@ -1,6 +1,8 @@
 import React, { type FC } from 'react';
 
+import MaskedAddress from '~shared/MaskedAddress/MaskedAddress.tsx';
 import { type ColonyAction } from '~types/graphql.ts';
+import { decodeArbitraryTransaction } from '~utils/arbitraryTxs.ts';
 
 interface ArbitraryTransactionProps {
   action: ColonyAction;
@@ -9,7 +11,46 @@ interface ArbitraryTransactionProps {
 const ArbitraryTransaction: FC<ArbitraryTransactionProps> = ({ action }) => {
   // eslint-disable-next-line no-console
   console.log('action:', action);
-  return <div>in progress</div>;
+
+  return (
+    <div className="flex flex-col gap-4 border-b-2">
+      {action.arbitraryTransactions?.map((transaction) => {
+        const abi = action.metadata?.arbitraryTxAbis?.find(
+          (abiItem) => abiItem.contractAddress === transaction.contractAddress,
+        );
+
+        if (!abi) {
+          return <div>No ABI found</div>;
+        }
+
+        const decodedTx = decodeArbitraryTransaction(
+          abi.jsonAbi,
+          transaction.encodedFunction,
+        );
+
+        if (!decodedTx) {
+          return <div>Failed to decode transaction</div>;
+        }
+
+        return (
+          <div className="flex flex-col gap-2">
+            <div>
+              Contract address:
+              <MaskedAddress address={transaction.contractAddress} />
+            </div>
+            Method: {decodedTx.method}
+            {decodedTx.args?.map((arg) => {
+              return (
+                <div key={arg.name}>
+                  {arg.name} ({arg.type}): {arg.value}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ArbitraryTransaction;
