5361f8695e2b8075c50c480da062bbd7d67f735f
Dave Creaser <david.creaser@outlook.com>
2025-01-09 15:35:23 +0000

    Feat: Allow addition of proxy colonies with multisig

--- src/components/v5/common/ActionSidebar/partials/forms/ManageSupportedChainsForm/ManageSupportedChainsForm.tsx
+++ src/components/v5/common/ActionSidebar/partials/forms/ManageSupportedChainsForm/ManageSupportedChainsForm.tsx
@@ -2,7 +2,6 @@ import { Link, PlusMinus } from '@phosphor-icons/react';
 import React, { type FC } from 'react';
 import { useFormContext } from 'react-hook-form';
 
-import { DecisionMethod } from '~types/actions.ts';
 import { formatText } from '~utils/intl.ts';
 import ActionFormRow from '~v5/common/ActionFormRow/ActionFormRow.tsx';
 import {
@@ -17,7 +16,6 @@ import ChainSelect from '~v5/common/ActionSidebar/partials/ChainSelect/ChainSele
 import DecisionMethodField from '~v5/common/ActionSidebar/partials/DecisionMethodField/DecisionMethodField.tsx';
 import Description from '~v5/common/ActionSidebar/partials/Description/Description.tsx';
 import { type ActionFormBaseProps } from '~v5/common/ActionSidebar/types.ts';
-import { createUnsupportedDecisionMethodFilter } from '~v5/common/ActionSidebar/utils.ts';
 import { FormCardSelect } from '~v5/common/Fields/CardSelect/index.ts';
 
 import { getManageSupportedChainsOptions } from './consts.ts';
@@ -34,9 +32,6 @@ const ManageSupportedChainsForm: FC<ActionFormBaseProps> = ({
   const { resetField } = useFormContext();
   const hasNoDecisionMethods = useHasNoDecisionMethods();
   const chainSelectFilterFn = useFilterChainSelectField();
-  const decisionMethodFilterFn = createUnsupportedDecisionMethodFilter([
-    DecisionMethod.MultiSig,
-  ]);
   const isRemoveOperation = useCheckOperationType(
     MANAGE_SUPPORTED_CHAINS_FIELD_NAME,
     ManageEntityOperation.Remove,
@@ -97,7 +92,7 @@ const ManageSupportedChainsForm: FC<ActionFormBaseProps> = ({
           filterOptionsFn={chainSelectFilterFn}
         />
       </ActionFormRow>
-      <DecisionMethodField filterOptionsFn={decisionMethodFilterFn} />
+      <DecisionMethodField />
       <Description />
     </>
   );
