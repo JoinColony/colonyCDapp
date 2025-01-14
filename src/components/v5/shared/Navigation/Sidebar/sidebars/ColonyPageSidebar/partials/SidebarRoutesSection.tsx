4a9d244fd016c929b03aa715f817242ee74ec610
strzelec <adam.strzelec@pagepro.pl>
2025-01-14 12:12:04 +0100

    fix: display streaming navigation item conditionally

--- src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/partials/SidebarRoutesSection.tsx
+++ src/components/v5/shared/Navigation/Sidebar/sidebars/ColonyPageSidebar/partials/SidebarRoutesSection.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import SidebarRouteItem from '~v5/shared/Navigation/Sidebar/partials/SidebarRouteItem/index.ts';
 
-import { sidebarNavigationScheme } from '../consts.ts';
+import { useSidebarRoutesScheme } from './hooks.ts';
 
 export const SidebarRoutesSection = () => {
+  const { sidebarScheme: sidebarNavigationScheme } = useSidebarRoutesScheme();
+
   return (
     <section className="flex w-full flex-col gap-0 md:gap-0.5">
       {sidebarNavigationScheme.map((scheme) => (
