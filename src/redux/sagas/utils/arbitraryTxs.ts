4f20c86a39570c8de0da4f4b0bbf5e351bd3e13d
Jakub Zajac <jakub@colony.io>
2025-01-15 22:56:18 +0000

    Feat: more sophisticated array parsing, handling nested arrays and strings

--- src/redux/sagas/utils/arbitraryTxs.ts
+++ src/redux/sagas/utils/arbitraryTxs.ts
@@ -2,6 +2,56 @@ import { Interface } from 'ethers/lib/utils';
 
 import { type AddTransactionTableModel } from '~v5/common/ActionSidebar/partials/forms/ArbitraryTxsForm/types.ts';
 
+/**
+ * Parses a string representation of an array into an array of values.
+ * It supports nested arrays and handles strings within arrays.
+ */
+const getArrayFromString = (value: string, type: string) => {
+  if (value === '[]') {
+    return [];
+  }
+
+  if (!value.startsWith('[') || !value.endsWith(']')) {
+    throw new Error('Invalid array format');
+  }
+
+  const contents = value.slice(1, -1);
+  const parsedElements: string[] = [];
+
+  let bracketLevel = 0;
+  let isInString = false;
+  let startIndex = 0;
+
+  for (let i = 0; i < contents.length; i += 1) {
+    const char = contents[i];
+    if (char === '"') {
+      isInString = !isInString;
+    }
+
+    if (char === '[') {
+      bracketLevel += 1;
+    } else if (char === ']') {
+      bracketLevel -= 1;
+    }
+
+    const isLastChar = i === contents.length - 1;
+    if ((char === ',' || isLastChar) && !isInString && bracketLevel === 0) {
+      const endIndex = isLastChar ? contents.length : i;
+      parsedElements.push(contents.slice(startIndex, endIndex).trim());
+      startIndex = i + 1;
+    }
+  }
+
+  return parsedElements.map((element) => {
+    const elementType = type.slice(0, -2);
+
+    if (elementType.endsWith('[]')) {
+      return getArrayFromString(element, elementType);
+    }
+    return element;
+  });
+};
+
 export const encodeArbitraryTransactions = (
   transactions: AddTransactionTableModel[],
 ) => {
@@ -13,7 +63,7 @@ export const encodeArbitraryTransactions = (
       item.method,
       item.args?.map((arg) => {
         if (arg.type.endsWith('[]')) {
-          return JSON.parse(arg.value);
+          return getArrayFromString(arg.value, arg.type);
         }
         return arg.value;
       }),
