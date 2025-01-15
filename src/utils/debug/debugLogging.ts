fa71436eab915f6a8b676a5b67e5fd507ac25ada
Raul Glogovetan <raul@glogovetan.com>
2025-01-15 20:04:04 +0200

    Add: `debugLogging` helper method

---
+++ src/utils/debug/debugLogging.ts
@@ -0,0 +1,10 @@
+const localStorageKey = '__COLONY_DEBUG_ENABLED__';
+
+const debugLogging = <T extends unknown[]>(...messages: T) => {
+  const debugEnabled = localStorage.getItem(localStorageKey) === 'true';
+  if (debugEnabled) {
+    console.info(...messages);
+  }
+};
+
+export default debugLogging;
