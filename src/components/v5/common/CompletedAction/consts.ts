cee40b0866ecf484276f7740e9743375203f0290
Bassgeta <kerry.mahne@gmail.com>
2025-01-15 13:53:59 +0100

    fix: string for create agreement motion action type

--- src/components/v5/common/CompletedAction/consts.ts
+++ src/components/v5/common/CompletedAction/consts.ts
@@ -1,17 +1,2 @@
-import { ColonyActionType } from '~gql';
-
 export const ICON_SIZE = 14;
 export const DEFAULT_TOOLTIP_POSITION = 'top';
-
-export const actionTypeTranslations = {
-  [ColonyActionType.CreateDecisionMotion]: 'actions.createDecision',
-  [ColonyActionType.CreateDomain]: 'actions.createNewTeam',
-  [ColonyActionType.ColonyEdit]: 'actions.editColonyDetails',
-  [ColonyActionType.MintTokens]: 'actions.mintTokens',
-  [ColonyActionType.Payment]: 'actions.simplePayment',
-  [ColonyActionType.MoveFunds]: 'actions.transferFunds',
-  [ColonyActionType.UnlockToken]: 'actions.unlockToken',
-  [ColonyActionType.VersionUpgrade]: 'actions.upgradeColonyVersion',
-  [ColonyActionType.VersionUpgradeMotion]: 'actions.upgradeColonyVersion',
-  default: 'Action',
-};
