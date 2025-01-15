ad9458135c308aeec0c3c785a8073a03e14a038e
strzelec <adam.strzelec@pagepro.pl>
2025-01-15 16:07:00 +0100

    feat: display no extension banner on streaming payments page

--- src/components/frame/v5/pages/StreamingPaymentsPage/StreamingPaymentsPage.tsx
+++ src/components/frame/v5/pages/StreamingPaymentsPage/StreamingPaymentsPage.tsx
@@ -1,3 +1,4 @@
+import { Extension } from '@colony/colony-js';
 import React from 'react';
 import { defineMessages } from 'react-intl';
 
@@ -5,13 +6,16 @@ import { Action } from '~constants/actions.ts';
 import { currencySymbolMap } from '~constants/currency.ts';
 import { useActionSidebarContext } from '~context/ActionSidebarContext/ActionSidebarContext.ts';
 import { useSetPageHeadingTitle } from '~context/PageHeadingContext/PageHeadingContext.ts';
+import useExtensionData from '~hooks/useExtensionData.ts';
 import useGetSelectedDomainFilter from '~hooks/useGetSelectedDomainFilter.tsx';
 import { useStreamingPaymentsTotalFunds } from '~shared/StreamingPayments/hooks.ts';
+import { isInstalledExtensionData } from '~utils/extensions.ts';
 import { formatText } from '~utils/intl.ts';
 import { ACTION_TYPE_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
 import ContentWithTeamFilter from '~v5/frame/ContentWithTeamFilter/ContentWithTeamFilter.tsx';
 import Button from '~v5/shared/Button/Button.tsx';
 
+import NoExtensionBanner from './partials/NoExtensionBanner/NoExtensionBanner.tsx';
 import StatsCards from './partials/StatsCards/StatsCards.tsx';
 import StreamingPaymentPageFilters from './partials/StreamingPaymentsTable/partials/StreamingPaymentFilters/StreamingPaymentFilters.tsx';
 import StreamingPaymentsTable from './partials/StreamingPaymentsTable/StreamingPaymentsTable.tsx';
@@ -48,16 +52,27 @@ const StreamingPaymentsPage = () => {
     nativeDomainId,
   });
 
+  const { extensionData } = useExtensionData(Extension.StreamingPayments);
+  const isStreamingPaymentsInstalled =
+    extensionData && isInstalledExtensionData(extensionData);
+
   return (
     <ContentWithTeamFilter>
-      <StatsCards
-        streamingPerMonth={totalLastMonthStreaming}
-        totalActiveStreams={activeStreamingPayments}
-        totalStreamed={totalStreamed}
-        unclaimedFounds={totalFunds.totalAvailable}
-        prefix={currencySymbolMap[currency]}
-        suffix={currency}
-      />
+      <div className="pb-9">
+        <StatsCards
+          streamingPerMonth={totalLastMonthStreaming}
+          totalActiveStreams={activeStreamingPayments}
+          totalStreamed={totalStreamed}
+          unclaimedFounds={totalFunds.totalAvailable}
+          prefix={currencySymbolMap[currency]}
+          suffix={currency}
+        />
+      </div>
+      {!!extensionData && !isStreamingPaymentsInstalled && (
+        <div className="pb-9">
+          <NoExtensionBanner />
+        </div>
+      )}
       <div className="mb-3.5 mt-9 flex-col justify-between sm:flex sm:flex-row sm:items-center">
         <div className="mb-2.5 flex items-center gap-2 sm:mb-0">
           <h4 className="heading-5">{formatText(MSG.title)}</h4>
