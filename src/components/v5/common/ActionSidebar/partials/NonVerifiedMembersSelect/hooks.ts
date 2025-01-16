5da5f7903dce59c761beb9d7ce5c1951636c4d84
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-01-16 16:04:45 +0100

    Fix: Solve member select dropdown issues
    Fix: Solve member select dropdown issues

--- src/components/v5/common/ActionSidebar/partials/NonVerifiedMembersSelect/hooks.ts
+++ src/components/v5/common/ActionSidebar/partials/NonVerifiedMembersSelect/hooks.ts
@@ -1,47 +1,12 @@
 import { useMemberContext } from '~context/MemberContext/MemberContext.ts';
-import { splitAddress } from '~utils/strings.ts';
-import { type SearchSelectOption } from '~v5/shared/SearchSelect/types.ts';
+import { formatMembersSelectOptions } from '~v5/common/ActionSidebar/utils.ts';
 
 export const useNonVerifiedMembersSelect = () => {
   const { totalMembers } = useMemberContext();
 
-  const options = totalMembers.reduce<SearchSelectOption[]>(
-    (result, member) => {
-      if (member.isVerified) {
-        return result;
-      }
-
-      const { walletAddress, profile } = member.user || {};
-
-      const splittedWalletAddress =
-        walletAddress && splitAddress(walletAddress);
-      const maskedWalletAddress =
-        splittedWalletAddress &&
-        `${splittedWalletAddress.header}${splittedWalletAddress.start}...${splittedWalletAddress.end}`;
-
-      const splittedContributorAddress = splitAddress(
-        member.contributorAddress,
-      );
-      const maskedContributorAddress = `${splittedContributorAddress.header}${splittedContributorAddress.start}...${splittedContributorAddress.end}`;
-
-      return [
-        ...result,
-        {
-          value: walletAddress || '',
-          isVerified: false,
-          label:
-            profile?.displayName ||
-            (walletAddress && maskedWalletAddress) ||
-            maskedContributorAddress,
-          avatar: profile?.thumbnail || profile?.avatar || undefined,
-          id: result.length,
-          showAvatar: true,
-          walletAddress,
-          profile,
-        },
-      ];
-    },
-    [],
+  const options = formatMembersSelectOptions(
+    totalMembers.filter((member) => !member.isVerified),
+    false,
   );
 
   return {
