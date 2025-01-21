53c0834083ce6514de3e6571e26a820ed7864676
Anastasiia <anastasiia@colony.io>
2025-01-21 21:09:22 +0100

    feat: implement arbitrary redo action

--- src/components/v5/common/CompletedAction/partials/ArbitraryTransaction/ArbitraryTransaction.tsx
+++ src/components/v5/common/CompletedAction/partials/ArbitraryTransaction/ArbitraryTransaction.tsx
@@ -1,8 +1,20 @@
 import React, { type FC } from 'react';
 
+import { Action } from '~constants/actions.ts';
 import { type ColonyAction, ColonyActionType } from '~types/graphql.ts';
-import { getFormatValuesArbitraryTransactions } from '~utils/arbitraryTxs.ts';
+import {
+  decodeArbitraryTransaction,
+  getFormatValuesArbitraryTransactions,
+} from '~utils/arbitraryTxs.ts';
 import { formatText } from '~utils/intl.ts';
+import {
+  ACTION_TYPE_FIELD_NAME,
+  ARBITRARY_TRANSACTIONS_FIELD_NAME,
+  DECISION_METHOD_FIELD_NAME,
+  DESCRIPTION_FIELD_NAME,
+  TITLE_FIELD_NAME,
+} from '~v5/common/ActionSidebar/consts.ts';
+import { useDecisionMethod } from '~v5/common/CompletedAction/hooks.ts';
 import UserInfoPopover from '~v5/shared/UserInfoPopover/UserInfoPopover.tsx';
 
 import ArbitraryTransactionsTable from '../ArbitraryTransactionsTable/index.ts';
@@ -24,6 +36,7 @@ interface ArbitraryTransactionProps {
 }
 
 const ArbitraryTransaction: FC<ArbitraryTransactionProps> = ({ action }) => {
+  const decisionMethod = useDecisionMethod(action);
   const {
     customTitle = formatText(
       {
@@ -34,7 +47,31 @@ const ArbitraryTransaction: FC<ArbitraryTransactionProps> = ({ action }) => {
       },
     ),
   } = action?.metadata || {};
-  const { initiatorUser, transactionHash } = action;
+  const { initiatorUser, transactionHash, annotation, arbitraryTransactions } =
+    action;
+
+  const decodedArbitraryTransactions = arbitraryTransactions?.map(
+    ({ contractAddress, encodedFunction }) => {
+      const abi = action.metadata?.arbitraryTxAbis?.find(
+        (abiItem) => abiItem.contractAddress === contractAddress,
+      );
+      if (!abi) {
+        return {
+          contractAddress,
+        };
+      }
+
+      const decodedTx = decodeArbitraryTransaction(
+        abi.jsonAbi,
+        encodedFunction,
+      );
+      return {
+        contractAddress,
+        jsonAbi: abi,
+        ...decodedTx,
+      };
+    },
+  );
 
   const arbitraryMessageValues = getFormatValuesArbitraryTransactions(action);
 
@@ -42,7 +79,16 @@ const ArbitraryTransaction: FC<ArbitraryTransactionProps> = ({ action }) => {
     <>
       <div className="flex items-center justify-between gap-2">
         <ActionTitle>{customTitle}</ActionTitle>
-        <MeatballMenu showRedoItem={false} transactionHash={transactionHash} />
+        <MeatballMenu
+          transactionHash={transactionHash}
+          defaultValues={{
+            [TITLE_FIELD_NAME]: customTitle,
+            [ACTION_TYPE_FIELD_NAME]: Action.ArbitraryTxs,
+            [DECISION_METHOD_FIELD_NAME]: decisionMethod,
+            [DESCRIPTION_FIELD_NAME]: annotation?.message,
+            [ARBITRARY_TRANSACTIONS_FIELD_NAME]: decodedArbitraryTransactions,
+          }}
+        />
       </div>
       <ActionSubtitle>
         {formatText(
