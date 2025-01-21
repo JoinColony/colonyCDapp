83755fbf200de2cd4ced354d15af7451a3484abc
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-01-21 17:30:07 +0100

    Fix: Upgrade colony sdk and update createProxyColony overrides
    Fix: Upgrade colony sdk and update createProxyColony overrides

--- src/redux/sagas/proxyColonies/createProxyColony.ts
+++ src/redux/sagas/proxyColonies/createProxyColony.ts
@@ -56,16 +56,11 @@ function* createProxyColony({
       .createTxCreator('createProxyColony', [
         BigInt(foreignChainId),
         creationSalt,
-        {
-          gasLimit: BigInt(10_000_000),
-        },
-      ] as any)
+      ])
       .tx()
-      // @TODO why can't we use the ovverides exposed by TxCreator
-      // .send({
-      //   gasLimit: BigInt(1000000)
-      // })
-      .send();
+      .send({
+        gasLimit: BigInt(10_000_000),
+      });
 
     put(transactionSent(meta.id));
 
