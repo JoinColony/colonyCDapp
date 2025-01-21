c792b83f0a721aead8088d1633dff6407517a2b2
strzelec <adam.strzelec@pagepro.pl>
2025-01-21 13:34:47 +0100

    fix: available to claim amount and add associated action id

--- src/utils/streamingPayments.ts
+++ src/utils/streamingPayments.ts
@@ -1,7 +1,10 @@
 import { BigNumber } from 'ethers';
 
 import { ONE_DAY_IN_SECONDS, ONE_HOUR_IN_SECONDS } from '~constants/time.ts';
-import { StreamingPaymentEndCondition } from '~gql';
+import {
+  StreamingPaymentEndCondition,
+  type StreamingPaymentFragment,
+} from '~gql';
 import { type StreamingPayment } from '~types/graphql.ts';
 import { StreamingPaymentStatus } from '~types/streamingPayments.ts';
 
@@ -11,6 +14,10 @@ interface GetStreamingPaymentAmountsLeftReturn {
   amountClaimedToDate: string;
   amountAvailableToClaim: string;
 }
+
+export const getStreamingPaymentCreatingActionId = (
+  streamingPayment?: StreamingPaymentFragment | null,
+) => streamingPayment?.metadata?.changelog?.[0].transactionHash;
 
 export const getStreamingPaymentLimit = ({
   streamingPayment,
@@ -50,12 +57,10 @@ export const getStreamingPaymentAmountsLeft = (
 
   const amountClaimedToDate =
     streamingPayment.claims?.reduce((sum, claim) => {
-      const newAmount = BigNumber.from(claim.amount).add(sum);
+      return BigNumber.from(claim.amount).add(sum);
+    }, BigNumber.from(0)) ?? BigNumber.from(0);
 
-      return newAmount.toString();
-    }, '0') ?? '0';
-
-  let amountAvailableToClaim: BigNumber;
+  let amountAvailableToClaim = BigNumber.from(0);
 
   const {
     startTime: startTimeString,
@@ -74,19 +79,29 @@ export const getStreamingPaymentAmountsLeft = (
       ? endTime.sub(startTime) // End time has already passed, the whole duration can be claimed.
       : BigNumber.from(currentTimestamp).sub(startTime); // End time has not passed, calculate duration to be claimed.
 
+    const PRECISION = BigNumber.from(10).pow(18);
+    const intervalBigNumber = BigNumber.from(interval);
+
     const amountAvailableSinceStart = BigNumber.from(amount)
       .mul(durationToClaim)
-      .div(interval);
+      .mul(PRECISION)
+      .div(intervalBigNumber.mul(PRECISION));
 
-    amountAvailableToClaim = amountAvailableSinceStart.sub(amountClaimedToDate);
+    const difference = amountAvailableSinceStart.sub(amountClaimedToDate);
 
-    amountAvailableToClaim = amountAvailableToClaim.lt(0)
-      ? BigNumber.from(0)
-      : amountAvailableToClaim;
+    const tolerance = BigNumber.from(amount).div(BigNumber.from(10).pow(12));
+
+    if (difference.abs().lte(tolerance)) {
+      amountAvailableToClaim = BigNumber.from(0);
+    } else {
+      amountAvailableToClaim = difference.lt(0)
+        ? BigNumber.from(0)
+        : difference;
+    }
   }
 
   return {
-    amountClaimedToDate,
+    amountClaimedToDate: amountClaimedToDate.toString(),
     amountAvailableToClaim: amountAvailableToClaim.toString(),
   };
 };
