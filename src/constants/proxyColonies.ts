11b0f44f3ea4f41f39062a1a4e30ee5a3f27cac1
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-01-22 15:03:23 +0100

    Fix: Add AMOY_NETWORK to supported chains config
    Fix: Add AMOY_NETWORK to supported chains config

--- src/constants/proxyColonies.ts
+++ src/constants/proxyColonies.ts
@@ -1,5 +1,6 @@
 import {
   POLYGON_NETWORK,
+  AMOY_NETWORK,
   GANACHE_NETWORK_1,
   GANACHE_NETWORK_2,
   type NetworkInfo,
@@ -7,6 +8,7 @@ import {
 
 export const SUPPORTED_CHAINS: NetworkInfo[] = [
   POLYGON_NETWORK,
+  AMOY_NETWORK,
   GANACHE_NETWORK_1,
   GANACHE_NETWORK_2,
 ];
