ce7828bfa989d922f830440ebea2d3c095b76c57
Raul Glogovetan <raul@glogovetan.com>
2025-01-29 17:17:29 +0200

    Refactor: dynamic CSS overrides as strings

---
+++ src/styles/dynamicOverrides.ts
@@ -0,0 +1,11 @@
+export const dynamicCssOverridesDark = `
+  .badge__container__primary {
+    background-color: var(--dynamic-badge-dot-background) !important;
+  }
+`;
+
+export const dynamicCssOverridesLight = `
+  .badge__container__primary {
+    background-color: #eff8ff !important;
+  }
+`;
