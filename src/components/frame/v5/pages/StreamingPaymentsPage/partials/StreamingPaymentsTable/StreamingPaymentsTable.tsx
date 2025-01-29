f72094a1dfd903b0b5bdc52e602da0d66047b9b1
strzelec <adam.strzelec@pagepro.pl>
2025-01-29 14:01:05 +0100

    fix: table styles

--- src/components/frame/v5/pages/StreamingPaymentsPage/partials/StreamingPaymentsTable/StreamingPaymentsTable.tsx
+++ src/components/frame/v5/pages/StreamingPaymentsPage/partials/StreamingPaymentsTable/StreamingPaymentsTable.tsx
@@ -1,5 +1,6 @@
 import { Binoculars } from '@phosphor-icons/react';
 import { getPaginationRowModel } from '@tanstack/react-table';
+import clsx from 'clsx';
 import React from 'react';
 
 import EmptyContent from '~v5/common/EmptyContent/EmptyContent.tsx';
@@ -25,7 +26,10 @@ const StreamingPaymentsTable = () => {
 
   return (
     <Table<StreamingTableFieldModel>
-      className="[&_table]:table-auto [&_table]:overflow-hidden lg:[&_table]:table-fixed [&_tbody_td]:h-[57px] [&_td:first-child]:pl-0 [&_td:last-child]:pr-0 [&_td]:border-gray-100 [&_td]:pr-0 [&_th]:border-none [&_tr.expanded-below+tr_td]:pl-0 [&_tr.expanded-below+tr_td]:pr-0 [&_tr.expanded-below>td]:border-gray-200 [&_tr.expanded-below_td]:h-[57px] [&_tr:hover_.toggler]:text-blue-400 [&_tr:not(.expanded-content):hover_td]:bg-gray-25 [&_tr:not(:last-child)_td]:border-b"
+      className={clsx(
+        'overflow-hidden [&_table]:table-auto [&_table]:overflow-hidden lg:[&_table]:table-fixed [&_tbody_td]:h-[57px] [&_td:first-child]:pl-0 [&_td:last-child]:pr-0 [&_td]:border-gray-100 [&_td]:pr-0 [&_th]:border-none [&_tr.expanded-below+tr_td]:pl-0 [&_tr.expanded-below+tr_td]:pr-0 [&_tr.expanded-below>td]:border-gray-200 [&_tr.expanded-below_td]:h-[57px] [&_tr.expanded-item_td]:h-auto [&_tr.table-item:hover_td]:bg-gray-25 [&_tr:hover_.toggler]:text-blue-400 [&_tr:not(:last-child)_td]:border-b',
+        { 'pb-6': items.length > 10 },
+      )}
       data={loading ? Array(4).fill({}) : items}
       columns={columns}
       renderCellWrapper={(_, content) => content}
