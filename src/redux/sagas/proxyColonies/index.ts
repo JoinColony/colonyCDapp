1b620a806a6e541d40b80ca4a8a380e2f4bdadb2
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-01-30 09:21:16 +0100

    Refactor: Remove unused saga for claiming tokens on proxy chain
    Refactor: Remove unused saga for claiming tokens on proxy chain

--- src/redux/sagas/proxyColonies/index.ts
+++ src/redux/sagas/proxyColonies/index.ts
@@ -1,6 +1,5 @@
 import { all, call } from 'redux-saga/effects';
 
-import claimTokenSaga from './claimToken.ts';
 import createProxyColonySaga from './createProxyColony.ts';
 import disableProxyColonySaga from './disableProxyColony.ts';
 import enableProxyColonySaga from './enableProxyColony.ts';
@@ -10,6 +9,5 @@ export default function* proxyColonySagas() {
     call(createProxyColonySaga),
     call(enableProxyColonySaga),
     call(disableProxyColonySaga),
-    call(claimTokenSaga),
   ]);
 }
