2024aaf0264f74cdee5447919a2836e36c13157a
Sam Gibbs <34915414+iamsamgibbs@users.noreply.github.com>
2025-02-06 09:48:31 +0000

    fix: resolve amountField token error

--- src/components/v5/common/ActionSidebar/partials/AmountField/hooks.ts
+++ src/components/v5/common/ActionSidebar/partials/AmountField/hooks.ts
@@ -2,6 +2,7 @@ import { type FormatNumeralOptions } from 'cleave-zen';
 import { useMemo } from 'react';
 import { useFormContext } from 'react-hook-form';
 
+import { DEFAULT_NETWORK_INFO } from '~constants';
 import { useColonyContext } from '~context/ColonyContext/ColonyContext.ts';
 import { notNull } from '~utils/arrays/index.ts';
 import { getTokenDecimalsWithFallback } from '~utils/tokens.ts';
@@ -10,7 +11,7 @@ import { CHAIN_FIELD_NAME } from '~v5/common/ActionSidebar/consts.ts';
 export const useAmountField = (selectedTokenAddress: string | undefined) => {
   const { colony } = useColonyContext();
   const { watch } = useFormContext();
-  const chainId = watch(CHAIN_FIELD_NAME);
+  const chainId = watch(CHAIN_FIELD_NAME) ?? DEFAULT_NETWORK_INFO.chainId;
 
   const colonyTokens =
     colony.tokens?.items
